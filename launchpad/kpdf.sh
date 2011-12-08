944c310016a9e1c0caf30dc2b145b478496f4511
HW <hwhilse@googlemail.com>
2011-12-09 00:11:23 +0100

    reworked launchpad integration a bit

--- launchpad/kpdf.sh
+++ launchpad/kpdf.sh
@@ -1,8 +1,14 @@
+#!/bin/sh
 echo unlock > /proc/keypad
 echo unlock > /proc/fiveway
 cd /mnt/us/test/
-cat /dev/fb0 > screen.fb0 &
-pdf=`lsof | grep /mnt/us/documents | cut -c81- | sort -u`
+cat /dev/fb0 > /tmp/screen.fb0 &
+if [ "x$1" == "x" ] ; then
+	pdf=`lsof | grep /mnt/us/documents | cut -c81- | sort -u`
+else
+	pdf="$1"
+fi
 ./reader.lua "$pdf"
-cat screen.fb0 > /dev/fb0
+cat /tmp/screen.fb0 > /dev/fb0
+rm /tmp/screen.fb0
 echo 1 > /proc/eink_fb/update_display
