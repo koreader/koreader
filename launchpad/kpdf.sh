5a384bc6d9e0ac2800ed2b9cd68e69484570421b
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-12-23 22:47:48 +0100

    open /mnt/us/documents by default
    This option also removed fb save/reload which speeds up
    startup and exit

--- launchpad/kpdf.sh
+++ launchpad/kpdf.sh
@@ -2,13 +2,5 @@
 echo unlock > /proc/keypad
 echo unlock > /proc/fiveway
 cd /mnt/us/test/
-cat /dev/fb0 > /tmp/screen.fb0 &
-if [ "x$1" == "x" ] ; then
-	pdf=`lsof | grep /mnt/us/documents | cut -c81- | sort -u`
-else
-	pdf="$1"
-fi
-./reader.lua "$pdf"
-cat /tmp/screen.fb0 > /dev/fb0
-rm /tmp/screen.fb0
+./reader.lua /mnt/us/documents
 echo 1 > /proc/eink_fb/update_display
