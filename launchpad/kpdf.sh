f74def7a69cd6365a08048b5ea8492167fe347f3
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-01-19 19:33:37 +0100

    create customupdate package for launchpad installation
    This change also modifies installation directory for install
    and customupdate make targets to /mnt/us/kindlepdfviewer

--- launchpad/kpdf.sh
+++ launchpad/kpdf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo unlock > /proc/keypad
 echo unlock > /proc/fiveway
-cd /mnt/us/test/
+cd /mnt/us/kindlepdfviewer/
 ./reader.lua /mnt/us/documents
 echo 1 > /proc/eink_fb/update_display
