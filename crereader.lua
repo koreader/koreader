5d9176907f2c25addb81727f24665df3ae6422cc
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-04-09 15:06:05 +0200

    show_overlap inverts part of page visible in previous view
    This is nice feature of Sony e-book readers which is very useful when
    reading since it shows previous part of visible page and provides
    hint where to continue reading after page refresh

--- crereader.lua
+++ crereader.lua
@@ -102,6 +102,14 @@ function CREReader:goto(pos, pos_type)
 
 	self.doc:drawCurrentPage(self.nulldc, fb.bb)
 
+	print("## self.show_overlap "..self.show_overlap)
+	if self.show_overlap < 0 then
+		fb.bb:invertRect(0,0, width, -self.show_overlap)
+	elseif self.show_overlap > 0 then
+		fb.bb:invertRect(0,height - self.show_overlap, width, self.show_overlap)
+	end
+	self.show_overlap = 0
+
 	if self.rcount == self.rcountmax then
 		print("full refresh")
 		self.rcount = 1
@@ -127,10 +135,12 @@ function CREReader:gotoTocEntry(entry)
 end
 
 function CREReader:nextView()
+	self.show_overlap = -self.pan_overlap_vertical
 	return self.pos + G_height - self.pan_overlap_vertical
 end
 
 function CREReader:prevView()
+	self.show_overlap = self.pan_overlap_vertical
 	return self.pos - G_height + self.pan_overlap_vertical
 end
 
