ed0d1c40fe5d0f79bea7fea95a57042170aab490
HW <hwhilse@googlemail.com>
2012-04-17 00:12:19 +0200

    reworked parameter parsing a bit
    this still allows to use last viewed document even when
    we flag that the framework should be stopped

--- launchpad/kpdf.sh
+++ launchpad/kpdf.sh
@@ -4,15 +4,27 @@ export LC_ALL="en_US.UTF-8"
 echo unlock > /proc/keypad
 echo unlock > /proc/fiveway
 
+# we're always starting from our working directory
 cd /mnt/us/kindlepdfviewer/
 
-grep /mnt/us/kindlepdfviewer/fonts/host /proc/mounts || mount -o bind /usr/java/lib/fonts /mnt/us/kindlepdfviewer/fonts/host
+# bind-mount system fonts
+if ! grep /mnt/us/kindlepdfviewer/fonts/host /proc/mounts; then
+	mount -o bind /usr/java/lib/fonts /mnt/us/kindlepdfviewer/fonts/host
+fi
 
-test "$2" == "framework_stop" && /etc/init.d/framework stop
+# check if we are supposed to shut down the Amazon framework
+if test "$1" == "--framework_stop"; then
+	shift 1
+	/etc/init.d/framework stop
+fi
 
+# finally call reader
 ./reader.lua "$1" 2> /mnt/us/kindlepdfviewer/crash.log || cat /mnt/us/kindlepdfviewer/crash.log
 
-grep /mnt/us/kindlepdfviewer/fonts/host /proc/mounts && umount /mnt/us/kindlepdfviewer/fonts/host
+# unmount system fonts
+if grep /mnt/us/kindlepdfviewer/fonts/host /proc/mounts; then
+	umount /mnt/us/kindlepdfviewer/fonts/host
+fi
 
 # always try to continue cvm
 killall -cont cvm || /etc/init.d/framework start
