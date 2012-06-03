906a5bd48c11c77ab3b10cb8bff40337e34ab4f6
HW <hwhilse@googlemail.com>
2012-06-04 00:53:24 +0200

    removed debugging cruft

--- reader.lua
+++ reader.lua
@@ -16,9 +16,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]--
-local v = require("luajit/lib/v")
-v.on()
-
 require "alt_getopt"
 require "pdfreader"
 require "djvureader"
