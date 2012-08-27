de0f40ce9cf59f06658ea71d8d34248501273d81
Tigran Aivazian <tigran@bibles.org.uk>
2012-08-27 13:14:22 +0100

    1. Disable filtering by file extension so that the program can be used as a proper filemanager. 2. Pass all files to crengine except those handled by PDF and DjVu modules.

--- extentions.lua
+++ extentions.lua
@@ -3,7 +3,7 @@
 ext = {
 	djvuRead = ";djvu;",
 	pdfRead  = ";pdf;xps;cbz;",
-	creRead  = ";epub;txt;rtf;htm;html;mobi;prc;azw;fb2;chm;pdb;doc;tcr;zip;"
+	--creRead  = ";epub;txt;rtf;htm;html;mobi;prc;azw;fb2;chm;pdb;doc;tcr;zip;"
 	-- seems to accept pdb-files for PalmDoc only
 }
 
@@ -15,10 +15,8 @@ function ext:getReader(ftype)
 		return DJVUReader
 	elseif string.find(self.pdfRead,s..ftype..s) then
 		return PDFReader
-	elseif string.find(self.creRead,s..ftype..s) then
-		return CREReader
 	else
-		return nil
+		return CREReader
 	end
 end
 
