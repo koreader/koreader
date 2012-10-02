9acdedcaebed81bf4ac239213ea56ed9e7a5b85d
NiLuJe <ninuje@gmail.com>
2012-10-03 01:41:32 +0200

    Forgot to revert that too, useless now :).

--- kpdfview.c
+++ kpdfview.c
@@ -15,13 +15,9 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <errno.h>
-#include <unistd.h>
-#include <err.h>
-#include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 #include <lua.h>
 #include <lualib.h>
