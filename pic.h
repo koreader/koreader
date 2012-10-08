0e84f275124c8bf1a7ab85c9f0d089a44a8a0779
Tigran Aivazian <tigran@bibles.org.uk>
2012-10-08 23:00:28 +0100

    Fix the leftover from the old name jpg.h

--- pic.h
+++ pic.h
@@ -15,8 +15,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef _JPG_H
-#define _JPG_H
+#ifndef _PIC_H
+#define _PIC_H
 
 #include <lua.h>
 #include <lualib.h>
