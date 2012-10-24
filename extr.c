52e7bb867ef4bbff53e4624be81dee19ecab8102
Tigran Aivazian <tigran@bibles.org.uk>
2012-10-24 16:23:16 +0100

    Use tabs not spaces for indenting.

--- extr.c
+++ extr.c
@@ -1,25 +1,25 @@
 /*
-    extr: Extract attachments from PDF file
+	extr: Extract attachments from PDF file
 
 	Usage: extr /dir/file.pdf pageno
 	Returns 0 if one or more attachments saved, otherwise returns non-zero.
 	Prints the number of saved attachments on stdout.
 	Attachments are saved in /dir directory with the appropriate filenames.
 
-    Copyright (C) 2012 Tigran Aivazian <tigran@bibles.org.uk>
+	Copyright (C) 2012 Tigran Aivazian <tigran@bibles.org.uk>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "mupdf-internal.h"
