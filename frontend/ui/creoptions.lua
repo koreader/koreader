ad1112468f7f727274e1145bae969152b1229418
Qingping Hou <dave2008713@gmail.com>
2013-01-07 12:18:22 -0500

    add view_mode, line space and font weight setting to readerconfig

--- frontend/ui/creoptions.lua
+++ frontend/ui/creoptions.lua
@@ -3,6 +3,19 @@ CreOptions = {
 	default_options = {
 	},
 	{
+		icon = "resources/icons/appbar.column.two.large.png",
+		options = {
+			{
+				name = "line_spacing",
+				name_text = "Line Spacing",
+				item_text = {"decrease", "increase"},
+				args = {"decrease", "increase"},
+				default_arg = nil,
+				event = "ChangeLineSpace",
+			},
+		}
+	},
+	{
 		icon = "resources/icons/appbar.text.size.large.png",
 		options = {
 			{
@@ -14,6 +27,34 @@ CreOptions = {
 				values = {18, 20, 22, 24, 29, 33, 39, 44},
 				default_value = 1,
 				event = "SetFontSize",
+			},
+		}
+	},
+	{
+		icon = "resources/icons/appbar.grade.b.large.png",
+		options = {
+			{
+				name = "font_weight",
+				name_text = "Font weight",
+				item_text = {"toggle bolder"},
+				-- args is indeed not used, we put here just to keep the
+				-- UI happy.
+				args = {1},
+				default_arg = nil,
+				event = "ToggleFontBolder",
+			}
+		}
+	},
+	{
+		icon = "resources/icons/appbar.settings.large.png",
+		options = {
+			{
+				name = "view_mode",
+				name_text = "View mode",
+				item_text = {"scroll", "page"},
+				args = {"scroll", "page"},
+				default_arg = "page",
+				event = "SetViewMode",
 			},
 		}
 	},
