2d0090c6de570bb74f0067df07dd53f3a25fac30
Qingping Hou <dave2008713@gmail.com>
2013-04-08 02:44:41 +0800

    remove debug name

--- frontend/ui/reader/readermenu.lua
+++ frontend/ui/reader/readermenu.lua
@@ -92,7 +92,6 @@ function ReaderMenu:onShowMenu()
 	local main_menu = nil
 	if Device:isTouchDevice() then
 		main_menu = TouchMenu:new{
-			name = _("main_menu"),
 			width = Screen:getWidth(),
 			tab_item_table = {
 				self.tab_item_table.navi,
