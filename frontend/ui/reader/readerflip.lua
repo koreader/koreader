84e67ff1227845e272ce57af9a158505e1d1ea0c
chrox <chrox.huang@gmail.com>
2013-06-26 15:51:12 +0800

    disable scroll mode in flipping for better performance

--- frontend/ui/reader/readerflip.lua
+++ frontend/ui/reader/readerflip.lua
@@ -15,9 +15,12 @@ end
 function ReaderFlipping:onSetFlippingMode(flipping_mode)
 	if flipping_mode then
 		self.orig_reflow_mode = self.view.document.configurable.text_wrap
+		self.orig_scroll_mode = self.view.page_scroll
 		self.view.document.configurable.text_wrap = 0
+		self.view.page_scroll = false
 	else
 		self.view.document.configurable.text_wrap = self.orig_reflow_mode
+		self.view.page_scroll = self.orig_scroll_mode
 	end
 	return true
 end
