8b09ee7cf10e51d903c2c21bd5628c25a3177e35
chrox <chrox.huang@gmail.com>
2014-02-12 15:22:15 +0800

    disable tap zone for config menu by default

--- defaults.lua
+++ defaults.lua
@@ -59,7 +59,7 @@ DLANDSCAPE_CLOCKWISE_ROTATION = true
 -- w: width of tap zone in proportion of screen width
 -- h: height of tap zone in proportion of screen height
 DTAP_ZONE_MENU = {x = 1/8, y = 0, w = 3/4, h = 1/4}
-DTAP_ZONE_CONFIG = {x = 0, y = 11/12, w = 1, h = 1/12}
+DTAP_ZONE_CONFIG = {x = 0, y = 0, w = 0, h = 0}
 DTAP_ZONE_FORWARD = {x = 1/4, y = 0, w = 3/4, h = 1}
 DTAP_ZONE_BACKWARD = {x = 0, y = 0, w = 1/4, h = 1}
 DTAP_ZONE_BOOKMARK = {x = 7/8, y = 0, w = 1/8, h = 1/8}
