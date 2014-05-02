ae3b84138e562194d4f677ad3faf24eb36d227db
chrox <chrox.huang@gmail.com>
2014-05-02 12:42:28 +0800

    fix embeded css setting not persistent between launchings

--- frontend/apps/reader/modules/readercoptlistener.lua
+++ frontend/apps/reader/modules/readercoptlistener.lua
@@ -4,12 +4,6 @@ local Event = require("ui/event")
 local ReaderCoptListener = EventListener:new{}
 
 function ReaderCoptListener:onReadSettings(config)
-    local embedded_css = config:readSetting("copt_embedded_css")
-    local toggle_embedded_css = embedded_css == 0 and false or true
-    table.insert(self.ui.postInitCallback, function()
-        self.ui:handleEvent(Event:new("ToggleEmbeddedStyleSheet", toggle_embedded_css))
-    end)
-    
     local view_mode = config:readSetting("copt_view_mode")
     if view_mode == 0 then
         table.insert(self.ui.postInitCallback, function()
@@ -20,7 +14,7 @@ function ReaderCoptListener:onReadSettings(config)
             self.ui:handleEvent(Event:new("SetViewMode", "scroll"))
         end)
     end
-    
+
     local status_line = config:readSetting("copt_status_line") or DCREREADER_PROGRESS_BAR
     self.document:setStatusLineProp(status_line)
 end
