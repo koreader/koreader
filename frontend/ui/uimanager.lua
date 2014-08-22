45546945e435de19241721d157ea0fde5a2d13ab
WS64 <ws64@hotmail.com>
2014-08-22 09:05:30 +0200

    Removed screensaver functions to new lua file

--- frontend/ui/uimanager.lua
+++ frontend/ui/uimanager.lua
@@ -6,6 +6,7 @@ local DEBUG = require("dbg")
 local _ = require("gettext")
 local util = require("ffi/util")
 local ImageWidget = require("ui/widget/imagewidget")
+local UIToolbox = require("ui/uitoolbox")
 
 -- initialize output module, this must be initialized before Input
 Screen:init()
@@ -59,125 +60,6 @@ local UIManager = {
     suspend_msg = nil
 }
 
-function UIManager:getPicture(file)
-
-    local contentopf
-    local contentpath
-    local epub_folder = "temp/epub"
-
-    local function check_extension(cover)
-        if cover then
-            local itype = string.lower(string.match(cover, ".+%.([^.]+)") or "")
-            if not (itype == "png" or itype == "jpg" or itype == "jpeg" or itype == "tiff") then
-                cover = nil
-            end
-         end
-         return cover
-    end
-
-    local function getValue(zipfile,which_line,to_find,excludecheck)
-        local f = io.open(zipfile,"r")
-        local i
-
-        if f then
-            local line = f:read()
-            while line and not i do
-                i = line:lower():find(which_line:lower()) -- found something
-                if i then
-                    f.close()
-                    line = line:match(to_find .. "\"([^\"]+)\".*")
-                    if not excludecheck then line = check_extension(line) end
-                    if line then
-                        return line
-                    else
-                        i = nil
-                    end
-                end
-                if not i then line = f:read() end
-            end
-            f.close()
-        end
-    end
-
-    local function guess(extension)
-        local cover = contentpath .. "Images/cover." .. extension
-        pcall(os.execute("unzip \"" .. file .. "\" \"" .. cover .. "\" -oq -d " .. epub_folder))
-        cover = epub_folder .. "/" .. cover
-        if not io.open(cover,"r") then
-            cover = nil
-        end
-        return cover
-    end
-
-    local function try_content_opf(which_line,to_find,addimage)
-        local cover = getValue(epub_folder .. "/" .. contentopf,which_line,to_find)
-        local imageadd
-        if cover then
-            if addimage then
-                imageadd = "Images/"
-            else
-                imageadd = ""
-            end
-            cover = contentpath .. imageadd .. cover
-            pcall(os.execute("unzip \"" .. file .. "\" \"" .. cover .. "\" -oq -d " .. epub_folder))
-            cover = epub_folder .. "/" .. cover
-            if not io.open(cover,"r") then cover = nil end
-        end
-        return check_extension(cover)
-    end
-    
-    local cover
-    if file then
-        pcall(lfs.mkdir("temp"))
-        pcall(os.execute("rm -rf " .. epub_folder))
-        pcall(lfs.mkdir(epub_folder))
-        pcall(os.execute("unzip \"" .. file .. "\" cover.jpeg -oq -d " .. epub_folder))
-        if io.open(epub_folder .. "/cover.jpeg","r") then                                          -- picture in main folder
-            cover = epub_folder .. "/cover.jpeg"                                                    -- found one
-        else
-            pcall(os.execute("unzip \"" .. file .. "\" \"META-INF/container.xml\" -oq -d " .. epub_folder)) -- read container.xml
-            contentopf = getValue(epub_folder .. "/META-INF/container.xml","^%s*<rootfile ","full[-]path=",true)
-            if contentopf then
-                contentpath = contentopf:match("(.*)[/][^/]+")
-                if contentpath then
-                    contentpath = contentpath .. "/"
-                else
-                    contentpath = ""
-                end
-
-                pcall(os.execute("unzip \"" .. file .. "\" \"" .. contentopf .. "\" -oq -d " .. epub_folder))  -- read content.opf
-
-                cover = try_content_opf("^%s*<meta name=\"cover\"","content=",true)  -- Make Room
-                if not cover then cover = try_content_opf('id="cover',"item href=",false) end -- Kishon
-                if not cover then cover = try_content_opf("cover","href=",true) end
-                if not cover then cover = try_content_opf("cover","=",true) end
-                if not cover then cover = try_content_opf("cover","=",false) end
-
-                if not cover then guess("jpg") end
-                if not cover then guess("jpeg") end
-                if not cover then guess("png") end
-            end
-        end
-    end
-    return check_extension(cover)
-end
-
-function UIManager:getRandomPicture(dir)
-    local pics = {}
-    local i = 0
-    math.randomseed( os.time() )
-    for entry in lfs.dir(dir) do
-        if lfs.attributes(dir .. entry, "mode") == "file" then
-            local extension = string.lower(string.match(entry, ".+%.([^.]+)") or "")
-            if extension == "jpg" or extension == "jpeg" or extension == "png" then
-                i = i + 1
-                pics[i] = entry
-            end
-        end
-    end
-    return pics[math.random(i)]
-end
-
 function UIManager:init()
     self.event_handlers = {
         __default__ = function(input_event)
@@ -198,7 +80,7 @@ function UIManager:init()
 
                     local file
                     if KOBO_SCREEN_SAVER_LAST_BOOK then
-                        file = self:getPicture(G_reader_settings:readSetting("lastfile"))
+                        file = UIToolbox:getPicture(G_reader_settings:readSetting("lastfile"))
                     end
 
                     if type(KOBO_SCREEN_SAVER) == "string" or file then
@@ -208,7 +90,7 @@ function UIManager:init()
                                 if string.sub(file,string.len(file)) ~= "/" then
                                     file = file .. "/"
                                 end
-                                local dummy = self:getRandomPicture(file)
+                                local dummy = UIToolbox:getRandomPicture(file)
                                 if dummy then file = file .. dummy end
                             end
                         end
@@ -647,4 +529,3 @@ end
 
 UIManager:init()
 return UIManager
-
