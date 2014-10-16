64b102ebc9743d3539b9fd36a2516156e837841f
chrox <chrox.huang@gmail.com>
2014-10-16 09:24:59 +0800

    fix #992 by creating fake command line arguments

--- platform/android/llapp_main.lua
+++ platform/android/llapp_main.lua
@@ -1,5 +1,6 @@
 local A = require("android")
 A.dl.library_path = A.dl.library_path .. ":" .. A.dir .. "/libs"
 
-ARGV = {"-d", "/sdcard"}
+-- create fake command-line arguments
+arg = {"-d", "/sdcard"}
 dofile(A.dir.."/reader.lua")
