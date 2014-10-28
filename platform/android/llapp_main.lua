f2f2833b6aa70b1582623b39e9f4ba3bc59d18cc
chrox <chrox.huang@gmail.com>
2014-10-29 02:27:29 +0800

    fix #1048

--- platform/android/llapp_main.lua
+++ platform/android/llapp_main.lua
@@ -1,6 +1,28 @@
 local A = require("android")
 A.dl.library_path = A.dl.library_path .. ":" .. A.dir .. "/libs"
 
+-- check uri of the intent that starts this application
+local file = A.jni:context(A.app.activity.vm, function(JNI)
+    local uri = JNI:callObjectMethod(
+        JNI:callObjectMethod(
+            A.app.activity.clazz,
+            "getIntent",
+            "()Landroid/content/Intent;"
+        ),
+        "getData",
+        "()Landroid/net/Uri;"
+    )
+    if uri ~= nil then
+        local path = JNI:callObjectMethod(
+            uri,
+            "getPath",
+            "()Ljava/lang/String;"
+        )
+        return JNI:to_string(path)
+    end
+end)
+A.LOGI("intent file path " .. (file or ""))
+
 -- create fake command-line arguments
-arg = {"-d", "/sdcard"}
+arg = {"-d", file or "/sdcard"}
 dofile(A.dir.."/reader.lua")
