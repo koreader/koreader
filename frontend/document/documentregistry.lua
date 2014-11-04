a08ac46e807b3e2e8ad7791e22825b19b39eaf2a
Andrey Golovizin <ag@sologoc.com>
2014-11-04 18:57:09 +0100

    Move credocument before pdfdocument in the registry.

--- frontend/document/documentregistry.lua
+++ frontend/document/documentregistry.lua
@@ -61,9 +61,9 @@ end
 
 -- load implementations:
 
+require("document/credocument"):register(DocumentRegistry)
 require("document/pdfdocument"):register(DocumentRegistry)
 require("document/djvudocument"):register(DocumentRegistry)
-require("document/credocument"):register(DocumentRegistry)
 require("document/picdocument"):register(DocumentRegistry)
 
 return DocumentRegistry
