c3274a9d5ed018b6bb444f35ba69adfd8993a58b
NiLuJe <ninuje@gmail.com>
2014-11-11 03:24:56 +0100

    Unbreak FM by reverting 7a812f
    :D

--- frontend/ui/widget/button.lua
+++ frontend/ui/widget/button.lua
@@ -153,9 +153,12 @@ end
 function Button:onTapSelectButton()
     if self.enabled and self.callback then
         self[1].invert = true
+        -- NOTE: Nope, this breaks the FM, by only refreshing the buttons themselves :D.
+        --[[
         UIManager.update_regions_func = function()
             return {self[1].dimen}
         end
+        --]]
         UIManager:setDirty(self.show_parent, "partial")
         UIManager:scheduleIn(0.1, function()
             self.callback()
