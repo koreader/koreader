fe885be563b2770f23e772187e46b41231180414
chrox <chrox.huang@gmail.com>
2014-11-27 21:59:27 +0800

    refactoring readerbookmark to show both bookmarks and highlights in the bookmark menu and use binary search of the sorted bookmarks table whenever is possible.

--- frontend/apps/reader/modules/readerbookmark.lua
+++ frontend/apps/reader/modules/readerbookmark.lua
@@ -42,12 +42,80 @@ function ReaderBookmark:init()
     self.ui.menu:registerToMainMenu(self)
 end
 
+function ReaderBookmark:addToMainMenu(tab_item_table)
+    -- insert table to main reader menu
+    table.insert(tab_item_table.navi, {
+        text = self.bm_menu_title,
+        callback = function()
+            self:onShowBookmark()
+        end,
+    })
+end
+
+function ReaderBookmark:isBookmarkInTimeOrder(a, b)
+    return a.datetime > b.datetime
+end
+
+function ReaderBookmark:isBookmarkInPageOrder(a, b)
+    if self.ui.document.info.has_pages then
+        return a.page > b.page
+    else
+        return self.ui.document:getPageFromXPointer(a.page) >
+                self.ui.document:getPageFromXPointer(b.page)
+    end
+end
+
+function ReaderBookmark:isBookmarkInReversePageOrder(a, b)
+    if self.ui.document.info.has_pages then
+        return a.page < b.page
+    else
+        return self.ui.document:getPageFromXPointer(a.page) <
+                self.ui.document:getPageFromXPointer(b.page)
+    end
+end
+
+function ReaderBookmark:fixBookmarkSort(config)
+    -- for backward compatibility, since previously bookmarks for credocuments
+    -- are not well sorted. We need to do a whole sorting for at least once.
+    if not config:readSetting("bookmarks_sorted") then
+        table.sort(self.bookmarks, function(a, b)
+            return self:isBookmarkInPageOrder(a, b)
+        end)
+    end
+end
+
+function ReaderBookmark:importSavedHighlight(config)
+    local textmarks = config:readSetting("highlight") or {}
+    -- import saved highlight once, because from now on highlight are added to
+    -- bookmarks when they are created.
+    if not config:readSetting("highlights_imported") then
+        for page, marks in pairs(textmarks) do
+            for _, mark in ipairs(marks) do
+                self:addBookmark({
+                    page = self.ui.document.info.has_pages and page or mark.pos0,
+                    datetime = mark.datetime,
+                    notes = mark.text,
+                    highlighted = true,
+                })
+            end
+        end
+    end
+end
+
 function ReaderBookmark:onReadSettings(config)
     self.bookmarks = config:readSetting("bookmarks") or {}
+    -- need to do this after initialization because checking xpointer
+    -- may cause segfaults before credocuments are inited.
+    self.ui:registerPostInitCallback(function()
+        self:fixBookmarkSort(config)
+        self:importSavedHighlight(config)
+    end)
 end
 
 function ReaderBookmark:onSaveSettings()
     self.ui.doc_settings:saveSetting("bookmarks", self.bookmarks)
+    self.ui.doc_settings:saveSetting("bookmarks_sorted", true)
+    self.ui.doc_settings:saveSetting("highlights_imported", true)
 end
 
 function ReaderBookmark:onToggleBookmark()
@@ -64,7 +132,7 @@ function ReaderBookmark:onToggleBookmark()
 end
 
 function ReaderBookmark:setDogearVisibility(pn_or_xp)
-    if self:isBookmarked(pn_or_xp) then
+    if self:getDogearBookmarkIndex(pn_or_xp) then
         self.ui:handleEvent(Event:new("SetDogearVisibility", true))
     else
         self.ui:handleEvent(Event:new("SetDogearVisibility", false))
@@ -75,15 +143,17 @@ function ReaderBookmark:onPageUpdate(pageno)
     if self.ui.document.info.has_pages then
         self:setDogearVisibility(pageno)
     else
-        -- FIXME: this is a dirty hack to prevent crash in isXPointerInCurrentPage
-        if pageno ~= 1 then
-            self:setDogearVisibility("dummy")
-        end
+        self:setDogearVisibility(self.ui.document:getXPointer())
     end
 end
 
 function ReaderBookmark:onPosUpdate(pos)
-    self:setDogearVisibility("dummy")
+    self:setDogearVisibility(self.ui.document:getXPointer())
+end
+
+function ReaderBookmark:gotoBookmark(pn_or_xp)
+    local event = self.ui.document.info.has_pages and "GotoPage" or "GotoXPointer"
+    self.ui:handleEvent(Event:new(event, pn_or_xp))
 end
 
 function ReaderBookmark:onShowBookmark()
@@ -91,8 +161,8 @@ function ReaderBookmark:onShowBookmark()
     for k, v in ipairs(self.bookmarks) do
         local page = v.page
         -- for CREngine, bookmark page is xpointer
-        if type(page) == "string" then
-            page = self.ui.document:getPageFromXPointer(v.page)
+        if not self.ui.document.info.has_pages then
+            page = self.ui.document:getPageFromXPointer(page)
         end
         v.text = _("Page") .. " " .. page .. " " .. v.notes .. " @ " .. v.datetime
     end
@@ -101,6 +171,7 @@ function ReaderBookmark:onShowBookmark()
         title = _("Bookmarks"),
         item_table = self.bookmarks,
         is_borderless = true,
+        is_popout = false,
         width = Screen:getWidth(),
         height = Screen:getHeight(),
         cface = Font:getFace("cfont", 20),
@@ -117,126 +188,129 @@ function ReaderBookmark:onShowBookmark()
         }
     }
 
-    local menu_container = CenterContainer:new{
+    self.bookmark_menu = CenterContainer:new{
         dimen = Screen:getSize(),
         bm_menu,
     }
 
     -- buid up menu widget method as closure
-    local doc = self.ui.document
-    local view = self.view
-    local sendEv = function(ev)
-        self.ui:handleEvent(ev)
-    end
+    local bookmark = self
     function bm_menu:onMenuChoice(item)
-        if doc.info.has_pages then
-            sendEv(Event:new("PageUpdate", item.page))
-        elseif view.view_mode == "page" then
-            sendEv(Event:new("PageUpdate", doc:getPageFromXPointer(item.page)))
-        else
-            sendEv(Event:new("PosUpdate", doc:getPosFromXPointer(item.page)))
-        end
+        bookmark:gotoBookmark(item.page)
     end
 
     bm_menu.close_callback = function()
-        UIManager:close(menu_container)
+        UIManager:close(self.bookmark_menu)
     end
 
-    bm_menu.show_parent = menu_container
+    bm_menu.show_parent = self.bookmark_menu
 
-    UIManager:show(menu_container)
+    UIManager:show(self.bookmark_menu)
     return true
 end
 
-function ReaderBookmark:addToMainMenu(tab_item_table)
-    -- insert table to main reader menu
-    table.insert(tab_item_table.navi, {
-        text = self.bm_menu_title,
-        callback = function()
-            self:onShowBookmark()
-        end,
-    })
+function ReaderBookmark:isBookmarkMatch(item, pn_or_xp)
+    if self.ui.document.info.has_pages then
+        return item.page == pn_or_xp
+    else
+        return self.ui.document:isXPointerInCurrentPage(item.page)
+    end
 end
 
-function ReaderBookmark:isBookmarked(pn_or_xp)
-    for k,v in ipairs(self.bookmarks) do
-        if (type(pn_or_xp) == "number" and v.page == pn_or_xp) or
-            (type(pn_or_xp) == "string" and self.ui.document:isXPointerInCurrentPage(v.page)) then
-            return true
+function ReaderBookmark:getDogearBookmarkIndex(pn_or_xp)
+    local _start, _middle, _end = 1, 1, #self.bookmarks
+    while _start <= _end do
+        _middle = math.floor((_start + _end)/2)
+        local v = self.bookmarks[_middle]
+        if self:isBookmarkMatch(v, pn_or_xp) then
+            if v.highlighted then
+                return
+            else
+                return _middle
+            end
+        elseif self:isBookmarkInPageOrder({page = pn_or_xp}, v) then
+            _end = _middle - 1
+        else
+            _start = _middle + 1
         end
     end
-    return false
 end
 
-function ReaderBookmark:addBookmark(pn_or_xp)
-    -- build notes from TOC
-    local notes = self.ui.toc:getTocTitleByPage(pn_or_xp)
-    if notes ~= "" then
-        notes = "in "..notes
+-- binary insert of sorted bookmarks
+function ReaderBookmark:addBookmark(item)
+    local _start, _middle, _end, direction = 1, 1, #self.bookmarks, 0
+    while _start <= _end do
+        local v = self.bookmarks[_middle]
+        _middle = math.floor((_start + _end)/2)
+        if self:isBookmarkInPageOrder(item, self.bookmarks[_middle]) then
+            _end, direction = _middle - 1, 0
+        else
+            _start, direction = _middle + 1, 1
+        end
     end
-    mark_item = {
-        page = pn_or_xp,
-        datetime = os.date("%Y-%m-%d %H:%M:%S"),
-        notes = notes,
-    }
-    table.insert(self.bookmarks, mark_item)
-    table.sort(self.bookmarks, function(a,b)
-        return self:isBookmarkInSequence(a, b)
-    end)
-    return true
+    table.insert(self.bookmarks, _middle + direction, item)
 end
 
-function ReaderBookmark:isBookmarkInSequence(a, b)
-    return a.page < b.page
+-- binary search to remove bookmark
+function ReaderBookmark:removeBookmark(item)
+    local _start, _middle, _end = 1, 1, #self.bookmarks
+    while _start <= _end do
+        _middle = math.floor((_start + _end)/2)
+        local v = self.bookmarks[_middle]
+        if item.datetime == v.datetime and item.page == v.page then
+            return table.remove(self.bookmarks, _middle)
+        elseif self:isBookmarkInPageOrder(item, v) then
+            _end = _middle - 1
+        else
+            _start = _middle + 1
+        end
+    end
 end
 
 function ReaderBookmark:toggleBookmark(pn_or_xp)
-    for k,v in ipairs(self.bookmarks) do
-        if (type(pn_or_xp) == "number" and v.page == pn_or_xp) or
-            (type(pn_or_xp) == "string" and self.ui.document:isXPointerInCurrentPage(v.page)) then
-            table.remove(self.bookmarks, k)
-            return
+    local index = self:getDogearBookmarkIndex(pn_or_xp)
+    if index then
+        table.remove(self.bookmarks, index)
+    else
+        -- build notes from TOC
+        local notes = self.ui.toc:getTocTitleByPage(pn_or_xp)
+        if notes ~= "" then
+            notes = "in "..notes
         end
+        self:addBookmark({
+            page = pn_or_xp,
+            datetime = os.date("%Y-%m-%d %H:%M:%S"),
+            notes = notes,
+        })
     end
-    self:addBookmark(pn_or_xp)
 end
 
 function ReaderBookmark:getPreviousBookmarkedPage(pn_or_xp)
-    for i = #self.bookmarks, 1, -1 do
-        if pn_or_xp > self.bookmarks[i].page then
+    DEBUG("go to next bookmark from", pn_or_xp)
+    for i = 1, #self.bookmarks do
+        if self:isBookmarkInPageOrder({page = pn_or_xp}, self.bookmarks[i]) then
             return self.bookmarks[i].page
         end
     end
 end
 
 function ReaderBookmark:getNextBookmarkedPage(pn_or_xp)
-    for i = 1, #self.bookmarks do
-        if pn_or_xp < self.bookmarks[i].page then
+    DEBUG("go to next bookmark from", pn_or_xp)
+    for i = #self.bookmarks, 1, -1 do
+        if self:isBookmarkInReversePageOrder({page = pn_or_xp}, self.bookmarks[i]) then
             return self.bookmarks[i].page
         end
     end
 end
 
 function ReaderBookmark:onGotoPreviousBookmark(pn_or_xp)
-    self:GotoBookmark(self:getPreviousBookmarkedPage(pn_or_xp))
+    self:gotoBookmark(self:getPreviousBookmarkedPage(pn_or_xp))
     return true
 end
 
 function ReaderBookmark:onGotoNextBookmark(pn_or_xp)
-    self:GotoBookmark(self:getNextBookmarkedPage(pn_or_xp))
+    self:gotoBookmark(self:getNextBookmarkedPage(pn_or_xp))
     return true
-end
-
-function ReaderBookmark:GotoBookmark(pn_or_xp)
-    if type(pn_or_xp) == "string" then
-        if self.view.view_mode == "page" then
-            self.ui:handleEvent(Event:new("PageUpdate", self.ui.document:getPageFromXPointer(pn_or_xp)))
-        else
-            self.ui:handleEvent(Event:new("PosUpdate", self.ui.document:getPosFromXPointer(pn_or_xp)))
-        end
-    elseif type(pn_or_xp) == "number" then
-        self.ui:handleEvent(Event:new("PageUpdate", pn_or_xp))
-    end
 end
 
 return ReaderBookmark
