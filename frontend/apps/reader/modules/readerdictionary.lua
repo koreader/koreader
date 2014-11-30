8376e2f8210bdaca97332b63d26430bf13849a09
Hans-Werner Hilse <hwhilse@gmail.com>
2014-11-30 18:11:02 +0000

    dictionary window triggers refresh itself, not via show()

--- frontend/apps/reader/modules/readerdictionary.lua
+++ frontend/apps/reader/modules/readerdictionary.lua
@@ -106,7 +106,7 @@ function ReaderDictionary:showDict(word, results, box)
             -- differentiate between dict and wiki
             wiki = self.wiki,
         }
-        UIManager:show(self.dict_window)
+        UIManager:show(self.dict_window, nil, nil, "none")
     end
 end
 
