9d13d4189b4ddad3093fa08af4e06723b931b0f2
chrox <chrox.huang@gmail.com>
2015-01-25 00:53:04 +0800

    fix no eink optimization on all devices

--- frontend/ui/elements/screen_eink_opt_menu_table.lua
+++ frontend/ui/elements/screen_eink_opt_menu_table.lua
@@ -1,9 +1,6 @@
 local _ = require("gettext")
 local Screen = require("device").screen
 
-local eink = G_reader_settings:readSetting("eink")
-Screen.eink = (eink == nil) and true or eink
-
 return {
     text = _("E-ink optimization"),
     checked_func = function() return Screen.eink end,
