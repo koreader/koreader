932df2a2f915ab08f1a9109072a87bc33201f71d
chrox <chrox.huang@gmail.com>
2015-04-22 14:17:06 +0800

    Refactor out string.gsplit to util.gsplit

--- frontend/util.lua
+++ frontend/util.lua
@@ -8,4 +8,43 @@ function util.stripePunctuations(word)
     return word:gsub("\226[\128-\131][\128-\191]",''):gsub("^%p+",''):gsub("%p+$",'')
 end
 
+--[[
+Lua doesn't have a string.split() function and most of the time
+you don't really need it because string.gmatch() is enough.
+However string.gmatch() has one significant disadvantage for me:
+You can't split a string while matching both the delimited
+strings and the delimiters themselves without tracking positions
+and substrings. The gsplit function below takes care of
+this problem.
+Author: Peter Odding
+License: MIT/X11
+Source: http://snippets.luacode.org/snippets/String_splitting_130
+--]]
+function util.gsplit(str, pattern, capture)
+    pattern = pattern and tostring(pattern) or '%s+'
+    if (''):find(pattern) then
+        error('pattern matches empty string!', 2)
+    end
+    return coroutine.wrap(function()
+        local index = 1
+        repeat
+            local first, last = str:find(pattern, index)
+            if first and last then
+                if index < first then
+                    coroutine.yield(str:sub(index, first - 1))
+                end
+                if capture then
+                    coroutine.yield(str:sub(first, last))
+                end
+                index = last + 1
+            else
+                if index <= #str then
+                    coroutine.yield(str:sub(index))
+                end
+                break
+            end
+        until index > #str
+    end)
+end
+
 return util
---
+++ spec/unit/util_spec.lua
@@ -0,0 +1,36 @@
+require("commonrequire")
+
+local util = require("util")
+
+describe("util module", function()
+    it("should strip punctuations around word", function()
+        assert.is_equal(util.stripePunctuations("\"hello world\""), "hello world")
+        assert.is_equal(util.stripePunctuations("\"hello world?\""), "hello world")
+        assert.is_equal(util.stripePunctuations("\"hello, world?\""), "hello, world")
+        assert.is_equal(util.stripePunctuations("“你好“"), "你好")
+        assert.is_equal(util.stripePunctuations("“你好?“"), "你好")
+    end)
+    it("should split string with patterns", function()
+        local sentence = "Hello world, welcome to KoReader!"
+        local words = {}
+        for word in util.gsplit(sentence, "%s+", false) do
+            table.insert(words, word)
+        end
+        assert.are_same(words, {"Hello", "world,", "welcome", "to", "KoReader!"})
+    end)
+    it("should split command line arguments with quotation", function()
+        local command = "./sdcv -nj \"words\" \"a lot\" 'more or less' --data-dir=dict"
+        local argv = {}
+        for arg1 in util.gsplit(command, "[\"'].-[\"']", true) do
+            for arg2 in util.gsplit(arg1, "^[^\"'].-%s+", true) do
+                for arg3 in util.gsplit(arg2, "[\"']", false) do
+                    local trimed = arg3:gsub("^%s*(.-)%s*$", "%1")
+                    if trimed ~= "" then
+                        table.insert(argv, trimed)
+                    end
+                end
+            end
+        end
+        assert.are_same(argv, {"./sdcv", "-nj", "words", "a lot", "more or less", "--data-dir=dict"})
+    end)
+end)
