4ee8ed39af9a0f223135e261a5325e93559c7445
frankyifei <fredyifei@gmail.com>
2015-08-01 18:02:23 +0930

    coding style

--- frontend/ui/widget/filechooser.lua
+++ frontend/ui/widget/filechooser.lua
@@ -56,8 +56,8 @@ function FileChooser:genItemTableFromPath(path)
         for f in iter, dir_obj do
             if self.show_hidden or not string.match(f, "^%.[^.]") then
                 local filename = self.path.."/"..f
-                if lfs.attributes(filename)~=nil then
-                   local attributes = lfs.attributes(filename)
+                local attributes = lfs.attributes(filename)                
+                if attributes ~= nil then               
                    if attributes.mode == "directory" and f ~= "." and f~=".." then
                       if self.dir_filter(filename) then
                          table.insert(dirs, {name = f, attr = attributes})
