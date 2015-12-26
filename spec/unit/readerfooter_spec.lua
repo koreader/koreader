55234ef899fdefbc82d5db67354e5e9aeace40cb
Qingping Hou <dave2008713@gmail.com>
2015-12-25 20:54:13 -0800

    fix: read stats crashes pdf/djvu documents

---
+++ spec/unit/readerfooter_spec.lua
@@ -0,0 +1,30 @@
+require("commonrequire")
+local DocumentRegistry = require("document/documentregistry")
+local ReaderUI = require("apps/reader/readerui")
+local DEBUG = require("dbg")
+
+describe("Readerfooter module", function()
+    it("should setup footer for epub without error", function()
+        local sample_epub = "spec/front/unit/data/juliet.epub"
+        local readerui = ReaderUI:new{
+            document = DocumentRegistry:openDocument(sample_epub),
+        }
+        readerui.view.footer.settings.all_at_once = true
+        readerui.view.footer:updateFooterPage()
+        timeinfo = readerui.view.footer:getTimeInfo()
+        assert.are.same('B:0% | '..timeinfo..' | => 0 | R:100% | TB: 00:00 | TC: 00:00',
+                        readerui.view.footer.progress_text.text)
+    end)
+
+    it("should setup footer for pdf without error", function()
+        local sample_pdf = "spec/front/unit/data/2col.pdf"
+        local readerui = ReaderUI:new{
+            document = DocumentRegistry:openDocument(sample_pdf),
+        }
+        readerui.view.footer.settings.all_at_once = true
+        readerui.view.footer:updateFooterPage()
+        timeinfo = readerui.view.footer:getTimeInfo()
+        assert.are.same('B:0% | '..timeinfo..' | => 1 | R:50% | TB: na | TC: na',
+                        readerui.view.footer.progress_text.text)
+    end)
+end)
