0a492bee3c50c56b0dcc3917045fa72ced11a74d
Qingping Hou <dave2008713@gmail.com>
2016-01-31 14:28:09 -0800

    add(ui): topcontainer

---
+++ frontend/ui/widget/container/topcontainer.lua
@@ -0,0 +1,8 @@
+local WidgetContainer = require("ui/widget/container/widgetcontainer")
+
+--[[
+TopContainer contains its content (1 widget) at the top of its own dimensions
+--]]
+local TopContainer = WidgetContainer:new()
+
+return TopContainer
