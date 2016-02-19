83cc67b6604779ea24c3d56aa1ce9d58d3bf8243
Qingping Hou <dave2008713@gmail.com>
2016-02-19 00:00:59 -0800

    build: deploy doc update after sucess

---
+++ .ci/before_install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# don't do this for clang
+if [ "$CXX" = "g++" ];
+    then export CXX="g++-4.8" CC="gcc-4.8";
+fi
+# in case anything ignores the environment variables, override through PATH
+mkdir bin
+ln -s $(which gcc-4.8) bin/cc
+ln -s $(which gcc-4.8) bin/gcc
+ln -s $(which c++-4.8) bin/c++
+ln -s $(which g++-4.8) bin/g++
