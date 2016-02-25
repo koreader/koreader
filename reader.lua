109e568162052661c14f8248dca85b66b3cf2a16
zijiehe <zijiehe@google.com>
2016-02-25 20:07:08 +0800

    Update flIntensity to make sure configuration between nickle and koreader are consistent

--- reader.lua
+++ reader.lua
@@ -121,8 +121,7 @@ if Device:isKobo() then
     local powerd = Device:getPowerDevice()
     if powerd and powerd.restore_settings then
         local intensity = G_reader_settings:readSetting("frontlight_intensity")
-        intensity = intensity or powerd.flIntensity
-        powerd:setIntensity(intensity)
+        powerd.flIntensity = intensity or powerd.flIntensity
         local state = G_reader_settings:readSetting("frontlight_state")
         if state then
             -- Default state is off
