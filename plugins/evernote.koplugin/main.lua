bd43e59fe7956cb5f148ea868bc4d6fcf3e93061
Qingping Hou <dave2008713@gmail.com>
2016-11-27 16:40:47 -0800

    fix: disable network info menu when device is not connected

--- plugins/evernote.koplugin/main.lua
+++ plugins/evernote.koplugin/main.lua
@@ -138,7 +138,7 @@ function EvernoteExporter:addToMainMenu(tab_item_table)
 end
 
 function EvernoteExporter:login()
-    if NetworkMgr:getWifiStatus() == false then
+    if not NetworkMgr:isOnline() then
         NetworkMgr:promptWifiOn()
     end
     self.login_dialog = LoginDialog:new{
