187598deb2b93be2e3d8e1b980679af5af8ab8d8
Robert <robert.styla@gmail.com>
2016-12-20 08:34:00 +0100

    Cloud storage - dropbox and ftp integration (#2424)

---
+++ frontend/apps/cloudstorage/dropboxapi.lua
@@ -0,0 +1,135 @@
+local url = require('socket.url')
+local socket = require('socket')
+local http = require('socket.http')
+local https = require('ssl.https')
+local ltn12 = require('ltn12')
+local _ = require("gettext")
+local JSON = require("json")
+local DocumentRegistry = require("document/documentregistry")
+
+local DropBoxApi = {
+}
+
+local API_URL_INFO = "https://api.dropboxapi.com/2/users/get_current_account"
+local API_LIST_FOLDER = "https://api.dropboxapi.com/2/files/list_folder"
+local API_DOWNLOAD_FILE = "https://content.dropboxapi.com/2/files/download"
+
+function DropBoxApi:fetchInfo(token)
+    local request, sink = {}, {}
+    local parsed = url.parse(API_URL_INFO)
+    request['url'] = API_URL_INFO
+    request['method'] = 'POST'
+    local headers = { ["Authorization"] = "Bearer ".. token }
+    request['headers'] = headers
+    request['sink'] = ltn12.sink.table(sink)
+    http.TIMEOUT = 5
+    https.TIMEOUT = 5
+    local httpRequest = parsed.scheme == 'http' and http.request or https.request
+    local headers_request = socket.skip(1, httpRequest(request))
+    local result_response = table.concat(sink)
+    if headers_request == nil then
+        return nil
+    end
+    if result_response ~= "" then
+        local _, result = pcall(JSON.decode, result_response)
+        return result
+    else
+        return nil
+    end
+end
+
+function DropBoxApi:fetchListFolders(path, token)
+    local request, sink = {}, {}
+    if path == nil or path == "/" then path = "" end
+    local parsed = url.parse(API_LIST_FOLDER)
+    request['url'] = API_LIST_FOLDER
+    request['method'] = 'POST'
+    local data = "{\"path\": \"" .. path .. "\",\"recursive\": false,\"include_media_info\": false,"..
+        "\"include_deleted\": false,\"include_has_explicit_shared_members\": false}"
+    local headers = { ["Authorization"] = "Bearer ".. token,
+        ["Content-Type"] = "application/json" ,
+        ["Content-Length"] = #data}
+    request['headers'] = headers
+    request['source'] = ltn12.source.string(data)
+    request['sink'] = ltn12.sink.table(sink)
+    http.TIMEOUT = 5
+    https.TIMEOUT = 5
+    local httpRequest = parsed.scheme == 'http' and http.request or https.request
+    local headers_request = socket.skip(1, httpRequest(request))
+    if headers_request == nil then
+        return nil
+    end
+    local result_response = table.concat(sink)
+    if result_response ~= "" then
+        local ret, result = pcall(JSON.decode, result_response)
+        if ret then
+            return result
+        else
+            return nil
+        end
+    else
+        return nil
+    end
+end
+
+function DropBoxApi:downloadFile(path, token, local_path)
+    local parsed = url.parse(API_DOWNLOAD_FILE)
+    local url_api = API_DOWNLOAD_FILE
+    local data1 = "{\"path\": \"" .. path .. "\"}"
+    local headers = { ["Authorization"] = "Bearer ".. token,
+        ["Dropbox-API-Arg"] = data1}
+    http.TIMEOUT = 5
+    https.TIMEOUT = 5
+    local httpRequest = parsed.scheme == 'http' and http.request or https.request
+    local _, code_return, _ = httpRequest{
+        url = url_api,
+        method = 'GET',
+        headers = headers,
+        sink = ltn12.sink.file(io.open(local_path, "w"))
+    }
+    return code_return
+end
+
+function DropBoxApi:listFolder(path, token)
+    local dropbox_list = {}
+    local dropbox_file = {}
+    local tag, text
+    local ls_dropbox = self:fetchListFolders(path, token)
+    if ls_dropbox == nil then return false end
+    for _, files in ipairs(ls_dropbox.entries) do
+        text = files.name
+        tag = files[".tag"]
+        if tag == "folder" then
+            text = text .. "/"
+            table.insert(dropbox_list, {
+                text = text,
+                url = files.path_display,
+                type = tag
+            })
+        --show only file with supported formats
+        elseif tag == "file" and DocumentRegistry:getProvider(text) then
+            table.insert(dropbox_file, {
+                text = text,
+                url = files.path_display,
+                type = tag
+            })
+        end
+    end
+    --sort
+    table.sort(dropbox_list, function(v1,v2)
+        return v1.text < v2.text
+    end)
+    table.sort(dropbox_file, function(v1,v2)
+        return v1.text < v2.text
+    end)
+    for _, files in ipairs(dropbox_file) do
+        table.insert(dropbox_list, {
+            text = files.text,
+            url = files.url,
+            type = files.type
+        })
+    end
+    return dropbox_list
+end
+
+return DropBoxApi
---
+++ frontend/apps/cloudstorage/ftp.lua
@@ -0,0 +1,152 @@
+local FtpApi = require("frontend/apps/cloudstorage/ftpapi")
+local ConfirmBox = require("ui/widget/confirmbox")
+local InfoMessage = require("ui/widget/infomessage")
+local MultiInputDialog = require("ui/widget/multiinputdialog")
+local UIManager = require("ui/uimanager")
+local _ = require("gettext")
+local T = require("ffi/util").template
+local ReaderUI = require("apps/reader/readerui")
+local Screen = require("device").screen
+
+local Ftp = {
+}
+local function generateUrl(address, user, pass)
+    local colon_sign = ""
+    local at_sign = ""
+    if user ~= "" then
+        at_sign = "@"
+    end
+    if pass ~= "" then
+        colon_sign = ":"
+    end
+    local replace = "://" .. user .. colon_sign .. pass .. at_sign
+    local url = string.gsub(address, "://", replace)
+    return url
+end
+
+function Ftp:run(address, user, pass, path)
+    local url = generateUrl(address, user, pass) .. path
+    return FtpApi:listFolder(url)
+end
+
+function Ftp:downloadFile(item, address, user, pass, path, close)
+    local url = generateUrl(address, user, pass) .. item.url
+    local response = FtpApi:downloadFile(url)
+    if response ~= nil then
+        local file = io.open(path, "w")
+        file:write(response)
+        file:close()
+        UIManager:show(ConfirmBox:new{
+            text = T(_("File saved to:\n %1\nWould you like to read the downloaded book now?"),
+                path),
+            ok_callback = function()
+                close()
+                ReaderUI:showReader(path)
+            end
+        })
+    else
+        UIManager:show(InfoMessage:new{
+            text = _("Could not save file to:\n") .. path,
+            timeout = 3,
+        })
+    end
+end
+
+function Ftp:config(item, callback)
+    local text_info = "FTP address must be in the format ftp://example.domian.com\n"..
+        "Also supported is format with IP e.g: ftp://10.10.10.1\n"..
+        "Username and password are optional."
+    local hint_name = _("Your FTP name")
+    local text_name = ""
+    local hint_address = _("FTP address eg ftp://example.com")
+    local text_address = ""
+    local hint_username = _("FTP username")
+    local text_username = ""
+    local hint_password = _("FTP password")
+    local text_password = ""
+    local title
+    local text_button_right = _("Add")
+    if item then
+        title = _("Edit FTP account")
+        text_button_right = _("Apply")
+        text_name = item.text
+        text_address = item.address
+        text_username = item.username
+        text_password = item.password
+    else
+        title = _("Add FTP account")
+    end
+    self.settings_dialog = MultiInputDialog:new {
+        title = title,
+        fields = {
+            {
+                text = text_name,
+                input_type = "string",
+                hint = hint_name ,
+            },
+            {
+                text = text_address,
+                input_type = "string",
+                hint = hint_address ,
+            },
+            {
+                text = text_username,
+                input_type = "string",
+                hint = hint_username,
+            },
+            {
+                text = text_password,
+                input_type = "string",
+                hint = hint_password,
+            },
+        },
+        buttons = {
+            {
+                {
+                    text = _("Cancel"),
+                    callback = function()
+                        self.settings_dialog:onClose()
+                        UIManager:close(self.settings_dialog)
+                    end
+                },
+                {
+                    text = _("Info"),
+                    callback = function()
+                        UIManager:show(InfoMessage:new{text = text_info })
+                    end
+                },
+                {
+                    text = text_button_right,
+                    callback = function()
+                        local fields = MultiInputDialog:getFields()
+                        if fields[1] ~= "" and fields[2] ~= "" then
+                            if item then
+                                -- edit
+                                callback(item, fields)
+                            else
+                                -- add new
+                                callback(fields)
+                            end
+                            self.settings_dialog:onClose()
+                            UIManager:close(self.settings_dialog)
+                        else
+                            UIManager:show(InfoMessage:new{text = "Please fill in all fields." })
+                        end
+                    end
+                },
+            },
+        },
+        width = Screen:getWidth() * 0.95,
+        height = Screen:getHeight() * 0.2,
+        input_type = "text",
+    }
+    self.settings_dialog:onShowKeyboard()
+    UIManager:show(self.settings_dialog)
+end
+
+function Ftp:info(item)
+    local info_text = T(_"Type: %1\nName: %2\nAddress: %3", "FTP", item.text, item.address)
+    UIManager:show(InfoMessage:new{text = info_text})
+end
+
+return Ftp
