957e8ce768b8c874b75d94b542b1aa28757fce2f
Robert <robert.styla@gmail.com>
2016-12-25 07:36:46 +0100

    Add open last book menu item in file manager (#2450)

--- frontend/apps/filemanager/filemanagermenu.lua
+++ frontend/apps/filemanager/filemanagermenu.lua
@@ -175,6 +175,22 @@ function FileManagerMenu:setUpdateItemTable()
 
     -- info tab
     -- insert common info
+    table.insert(self.tab_item_table.info, {
+        text = _("Open last document"),
+        callback = function()
+            local last_file = G_reader_settings:readSetting("lastfile")
+            if not last_file or lfs.attributes(last_file, "mode") ~= "file" then
+                local InfoMessage = require("ui/widget/infomessage")
+                UIManager:show(InfoMessage:new{
+                    text = _("Cannot open last document"),
+                })
+                return
+            end
+            local ReaderUI = require("apps/reader/readerui")
+            ReaderUI:showReader(last_file)
+            self:onCloseFileManagerMenu()
+        end
+    })
     for i, common_setting in ipairs(require("ui/elements/common_info_menu_table")) do
         table.insert(self.tab_item_table.info, common_setting)
     end
