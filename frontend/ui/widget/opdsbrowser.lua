2e30f7c7d20cfcdad742ceb0894b06f2f7652406
robert00s <robert.styla@gmail.com>
2017-01-04 17:04:30 +0100

    fix

--- frontend/ui/widget/opdsbrowser.lua
+++ frontend/ui/widget/opdsbrowser.lua
@@ -474,8 +474,14 @@ function OPDSBrowser:downloadFile(item, format, remote_url)
     -- download to user selected directory or last opened dir
     local lastdir = G_reader_settings:readSetting("lastdir")
     local download_dir = G_reader_settings:readSetting("download_dir") or lastdir
-    item.author = item.author:gsub('[\\,%/,:,%*,%?,%",%<,%>,%|]','')
-    item.title = item.title:gsub('[\\,%/,:,%*,%?,%",%<,%>,%|]','')
+    local utl = require("frontend/util")
+    if utl.getFilesystemType(download_dir) ~= nil then
+        item.author = utl.replaceInvalidChars(item.author)
+        item.title = utl.replaceInvalidChars(item.title)
+    else
+        item.author = utl.replaceSlashChar(item.author)
+        item.title = utl.replaceSlashChar(item.title)
+    end
     local local_path = download_dir .. "/" .. item.author .. ' - ' .. item.title .. "." .. string.lower(format)
     DEBUG("downloading file", local_path, "from", remote_url)
 
