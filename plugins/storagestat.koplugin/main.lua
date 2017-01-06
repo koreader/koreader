a8ec706e8b2aefd505ab52766ad39225027a7798
Hzj_jie <hzj_jie@hotmail.com>
2017-01-06 01:49:46 -0800

    Update filter for kindle

--- plugins/storagestat.koplugin/main.lua
+++ plugins/storagestat.koplugin/main.lua
@@ -2,6 +2,16 @@
 local Device = require("device")
 
 if not (Device:isKobo() or Device:isKindle() or Device:isPocketBook()) then
+end
+
+local filter
+
+-- TODO(Hzj_jie): Find the right filter for PocketBook
+if Device:isKobo() or Device:isPocketBook() then
+    filter = "mmcblk"
+elseif Device:isKindle() then
+    filter = "' /mnt/us$'"
+else
     return { disabled = true, }
 end
 
@@ -16,7 +26,8 @@ local StorageStat = WidgetContainer:new{
         text = _("Storage Statistics"),
         callback = function()
             local std_out = io.popen(
-                "df -h | sed -r 's/ +/ /g' | grep mmcblk | cut -d ' ' -f 3,4,5,6 | " ..
+                "df -h | sed -r 's/ +/ /g' | grep " .. filter ..
+                " | cut -d ' ' -f 3,4,5,6 | " ..
                 "awk '{print $4 \" U\" $1 \" F\" $2 \" P\" $3}'"
             )
             local msg
