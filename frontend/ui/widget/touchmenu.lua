98a86ba62f5d4563c241096a8fd4c475720e4ba0
Hzj_jie <hzj_jie@hotmail.com>
2017-02-01 07:46:35 -0800

    prepose plus signal

--- frontend/ui/widget/touchmenu.lua
+++ frontend/ui/widget/touchmenu.lua
@@ -78,7 +78,7 @@ function TouchMenuItem:init()
                 item_checked and checked_widget or unchecked_widget
             },
             TextWidget:new{
-                text = (self.item.text or self.item.text_func()) .. ((self.item.sub_item_table == nil) and "" or " +"),
+                text = ((self.item.sub_item_table == nil) and "" or "+ ") .. (self.item.text or self.item.text_func()),
                 fgcolor = item_enabled ~= false and Blitbuffer.COLOR_BLACK or Blitbuffer.COLOR_GREY,
                 face = self.face,
             },
