koreader/koreader

commit 51327911f2fbea00258717d7b284e061ad31832e

Won't track missing files in history, and always keep latest timestamp

frontend/readhistory.lua
@@ -23,11 +23,20 @@ local function buildEntry(input_time, input_file)
 end
 
 function ReadHistory:_sort()
+    for i = #self.hist, 1, -1 do
+        if lfs.attributes(self.hist[i].file, "mode") ~= "file" then
+            table.remove(self.hist, i)
+        end
+    end
     table.sort(self.hist, function(l, r) return l.file < r.file end)
     -- TODO(zijiehe): Use binary insert instead of a loop to deduplicate.
     for i = #self.hist, 2, -1 do
         if self.hist[i].file == self.hist[i - 1].file then
-            table.remove(self.hist, i)
+            if self.hist[i].time < self.hist[i - 1].time then
+                table.remove(self.hist, i)
+            else
+                table.remove(self.hist,i - 1)
+            end
         end
     end
     table.sort(self.hist, function(v1, v2) return v1.time > v2.time end)

commit 51d9612590627ed916d94d55c39e85ed1c0c7a00

FileManagerHistory: remove harmful deletion code and turn it into a feature
Fixes the problem introduced in 51327911f2fbea00258717d7b284e061ad31832e and turns it into a feature that should fix #2477

frontend/apps/filemanager/filemanagerhistory.lua
@@ -128,6 +128,17 @@ function FileManagerHistory:onMenuHold(item)
                     end,
                  },
             },
+            {},
+            {
+                {
+                    text = _("Clear history of deleted files"),
+                    callback = function()
+                        require("readhistory"):clearMissing()
+                        self._manager:updateItemTable()
+                        UIManager:close(self.histfile_dialog)
+                    end,
+                 },
+            },
         },
     }
     UIManager:show(self.histfile_dialog)

frontend/readhistory.lua
@@ -47,11 +47,6 @@ function ReadHistory:_indexing(start)
 end
 
 function ReadHistory:_sort()
-    for i = #self.hist, 1, -1 do
-        if self.hist[i].file == nil or lfs.attributes(self.hist[i].file, "mode") ~= "file" then
-            table.remove(self.hist, i)
-        end
-    end
     table.sort(self.hist, fileFirstOrdering)
     -- TODO(zijiehe): Use binary insert instead of a loop to deduplicate.
     for i = #self.hist, 2, -1 do
@@ -120,6 +115,14 @@ function ReadHistory:_init()
     self:_readLegacyHistory()
     self:_sort()
     self:_reduce()
+end
+
+function ReadHistory:clearMissing()
+    for i = #self.hist, 1, -1 do
+        if self.hist[i].file == nil or lfs.attributes(self.hist[i].file, "mode") ~= "file" then
+            table.remove(self.hist, i)
+        end
+    end
 end
 
 function ReadHistory:removeItem(item)