ddf590eb6d7430904f3de12a308bba37be5461b8
Frans de Jonge <fransdejonge@gmail.com>
2017-03-05 12:46:27 +0100

    MenuSorter: split out menu order configuration

--- frontend/apps/filemanager/filemanagermenu.lua
+++ frontend/apps/filemanager/filemanagermenu.lua
@@ -280,66 +280,7 @@ function FileManagerMenu:setUpdateItemTable()
         end,
     }
 
-    local order = {
-        ["KOMenu:menu_buttons"] = {
-            "setting",
-            "tools",
-            "search",
-            "main",
-        },
-        ["setting"] = {
-            "show_hidden_files",
-            "----------------------------",
-            "sort_by",
-            "reverse_sorting",
-            "----------------------------",
-            "start_with_last_opened_file",
-            "screensaver",
-            "----------------------------",
-            -- common settings
-            -- those that don't exist will simply be skipped during menu gen
-            "frontlight", -- if Device:hasFrontlight()
-            "night_mode",
-            "----------------------------",
-            "network",
-            "screen",
-            "save_document",
-            "----------------------------",
-            "language",
-            "----------------------------",
-            "show_advanced_options",
-            -- end common settings
-        },
-        ["tools"] = {
-            "calibre_wireless_connection",
-            "evernote",
-            "goodreads",
-            "keep_alive",
-            "statistics",
-            "storage_stat",
-            "cloud_storage",
-            "----------------------------",
-            "advanced_settings",
-            "developer_options",
-        },
-        ["search"] = {
-            "dictionary_lookup",
-            "find_book_in_calibre_catalog",
-            "find_file",
-            "----------------------------",
-            "opds_catalog",
-        },
-        ["main"] = {
-            "history",
-            "open_last_document",
-            "----------------------------",
-            "ota_update", -- if Device:isKindle() or Device:isKobo() or Device:isPocketBook() or Device:isAndroid()
-            "version",
-            "help",
-            "----------------------------",
-            "exit",
-        },
-    }
+    local order = require("ui/elements/filemanager_menu_order")
 
     local MenuSorter = require("frontend/ui/menusorter")
     self.tab_item_table = MenuSorter:sort(self.menu_items, order, "filemanager")
@@ -347,7 +288,7 @@ end
 
 function FileManagerMenu:onShowMenu()
     local tab_index = G_reader_settings:readSetting("filemanagermenu_tab_index") or 1
-    if #self.menu_items.setting == 0 then
+    if self.tab_item_table == nil then
         self:setUpdateItemTable()
     end
 
