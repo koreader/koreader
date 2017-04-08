koreader/koreader

commit 3a75b6deab39d82a52db6c49be2ea0783b3ba575

use fullpath for reboot

platform/kobo/koreader.sh
@@ -139,7 +139,7 @@ if [ "${FROM_NICKEL}" = "true" ] ; then
 		else
 			# By default, if we were called from KFMon, just reboot, because there might be a chance Nickel will get its panties in a serious twist on restore for one reason or another...
 			# And at best, we'd still restart with slightly broken suspend behavior anyway...
-			reboot
+			/sbin/reboot
 		fi
 	fi
 else
@@ -147,7 +147,7 @@ else
 	# NOTE: This is actually achieved by checking if KSM or a KSM-related script is running:
 	#       This might lead to false-positives if you use neither KSM nor advboot to launch KOReader *without nickel running*.
 	if ! pgrep -f kbmenu > /dev/null 2>&1 ; then
-		reboot
+		/sbin/reboot
 	fi
 fi
 

commit 5e83bcfdf4c159bf1091985555bea3256bef233b

kobo/koreader.sh: fix rotation issue caused by #2731

platform/kobo/koreader.sh
@@ -117,19 +117,30 @@ if [ -e crash.log ]; then
 	mv -f crash.log.new crash.log
 fi
 
+# workaround 32-bit without KSM (KSM is indicated by ${ksmroot} being set)
+# shellcheck disable=2154
+if [ -z "${ksmroot+x:?}" ]; then
 # TODO: add support for 32bit framebuffer in koreader. This is a workaround
 # to run koreader in 16bpp. Useful since FW 4.0
+CUR_ROTATE="$(cat "/sys/class/graphics/fb0/rotate")"
 FB_BPP=$(cat /sys/class/graphics/fb0/bits_per_pixel)
-[ "$FB_BPP" -gt 16 ] && /usr/sbin/fbset -depth 16
-# fix rotation issues on Kobo Aura HD (dragon) and Kobo Aura H2O (dahlia)
+[ "${FB_BPP}" -gt 16 ] && /usr/sbin/fbset -depth 16
+# this suffices on normal devices, but H2O is stupid and sets the opposite
+echo "${CUR_ROTATE}" > "/sys/class/graphics/fb0/rotate"
+# this therefore turns it around on H2O (and other potential idiots) and merely
+# innocently repeats on sane devices
 # shellcheck disable=SC2094
-case "$PRODUCT" in "dragon" | "dahlia" ) cat /sys/class/graphics/fb0/rotate > /sys/class/graphics/fb0/rotate ;; esac
+cat "/sys/class/graphics/fb0/rotate" > "/sys/class/graphics/fb0/rotate"
+fi
 
 ./reader.lua "${args}" >> crash.log 2>&1
 RESULT=$?
 
-# back to default depth in framebuffer.
-[ "$FB_BPP" -gt 16 ] && /usr/sbin/fbset -depth "$FB_BPP"
+# workaround 32-bit without KSM (KSM is indicated by s${ksmroot} being set)
+if [ -z "${ksmroot+x:?}" ]; then
+	# back to default depth in framebuffer.
+	[ "${FB_BPP}" -gt 16 ] && /usr/sbin/fbset -depth "${FB_BPP}"
+fi
 
 if [ "${FROM_NICKEL}" = "true" ] ; then
 	if [ "${FROM_KFMON}" != "true" ] ; then