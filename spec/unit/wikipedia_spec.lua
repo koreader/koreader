21622b5da8da6630291ec5a28eda579e5160dea1
Frans de Jonge <fransdejonge@gmail.com>
2017-04-15 16:09:28 +0200

    Spec: Wikipedia stub

---
+++ spec/unit/wikipedia_spec.lua
@@ -0,0 +1,14 @@
+describe("Wikipedia module", function()
+    local util
+    setup(function()
+        require("commonrequire")
+        Wikipedia = require("ui/wikipedia")
+    end)
+
+    it("should return Wikipedia server", function()
+        local expected_server_default = "https://en.wikipedia.org"
+        local expected_server_nl = "https://nl.wikipedia.org"
+        assert.is.same(expected_server_default, Wikipedia:getWikiServer())
+        assert.is.same(expected_server_nl, Wikipedia:getWikiServer("nl"))
+    end)
+end)
