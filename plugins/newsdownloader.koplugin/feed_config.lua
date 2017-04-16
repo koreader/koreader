f9716728b6771eb592bd18fc7ee16ed0ddc6be09
Qingping Hou <dave2008713@gmail.com>
2017-04-16 15:46:15 -0700

    chore: migrate config to lua & avoid xml parsing crash

---
+++ plugins/newsdownloader.koplugin/feed_config.lua
@@ -0,0 +1,6 @@
+return {
+    -- list your feeds here:
+    -- only supports http URL for now
+    -- set limit to "0" means no download, "-1" no limit.
+    { "http://www.huffingtonpost.com/feeds/index.xml", limit = 2 },
+}
