f608e7579316883f1dd637d05388d32206755ecc
x <mwoz123@gmail.com>
2017-04-17 21:24:56 +0200

    NewsDownloader - fix travis whitespace error

--- plugins/newsdownloader.koplugin/feed_config.lua
+++ plugins/newsdownloader.koplugin/feed_config.lua
@@ -1,7 +1,7 @@
 return {
     -- list your feeds here:
     -- only supports http URL for now
-    -- Atom is currently not supported, only RSS 
+    -- Atom is currently not supported, only RSS
     -- set limit to "0" means no download, "-1" no limit.
     { "http://www.huffingtonpost.com/feeds/index.xml", limit = 2 },
     { "http://www.pcworld.com/index.rss", limit = 3 },
