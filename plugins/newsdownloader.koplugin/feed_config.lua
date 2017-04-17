41047eff7b95f86ac2132c7864649dda960206d0
x <mwoz123@gmail.com>
2017-04-17 21:12:02 +0200

    NewsDownloader - 2 feed sources in example config

--- plugins/newsdownloader.koplugin/feed_config.lua
+++ plugins/newsdownloader.koplugin/feed_config.lua
@@ -1,6 +1,8 @@
 return {
     -- list your feeds here:
     -- only supports http URL for now
+    -- Atom is currently not supported, only RSS 
     -- set limit to "0" means no download, "-1" no limit.
     { "http://www.huffingtonpost.com/feeds/index.xml", limit = 2 },
+    { "http://www.pcworld.com/index.rss", limit = 3 },
 }
