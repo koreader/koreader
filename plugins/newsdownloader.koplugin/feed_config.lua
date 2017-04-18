e14e4d0ed30c03f8f96a9f0621265b80c0f9a988
x <mwoz123@gmail.com>
2017-04-18 22:48:24 +0200

    NewsDownloader: update example news sources

--- plugins/newsdownloader.koplugin/feed_config.lua
+++ plugins/newsdownloader.koplugin/feed_config.lua
@@ -1,8 +1,9 @@
 return {
-    -- list your feeds here:
-    -- only supports http URL for now
-    -- Atom is currently not supported, only RSS
-    -- set limit to "0" means no download, "-1" no limit.
-    { "http://www.huffingtonpost.com/feeds/index.xml", limit = 2 },
-    { "http://www.pcworld.com/index.rss", limit = 3 },
+	-- list your feeds here:
+	-- only supports http URL for now
+	-- Atom is currently not supported, only RSS
+    { "http://www.pcworld.com/index.rss", limit = 1 },
+    { "http://www.economist.com/sections/science-technology/rss.xml", limit = 2},
+	-- set limit to "0" means no download, "-1" no limit.
+    { "http://www.economist.com/sections/culture/rss.xml", limit = 0 },
 }
