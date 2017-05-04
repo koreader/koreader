19db707e3d350450e70665bd01ce6a8f21c29e9d
mwoz123 <mwoz123@ubuntu.home>
2017-05-04 13:01:45 +0200

    NewsDownloader: update example feed config (use Reuters atom source)

--- plugins/newsdownloader.koplugin/feed_config.lua
+++ plugins/newsdownloader.koplugin/feed_config.lua
@@ -2,7 +2,7 @@ return {
 	-- list your feeds here:
 	-- only supports http URL for now
     { "http://www.pcworld.com/index.rss", limit = 1 },
-    { "http://www.economist.com/sections/science-technology/rss.xml", limit = 2},
+    { "http://feeds.reuters.com/Reuters/worldNews?format=xml", limit = 2},
 	-- set limit to "0" means no download, "-1" no limit.
-    { "http://www.economist.com/sections/culture/rss.xml", limit = 0 },
+    { "http://www.economist.com/sections/science-technology/rss.xml", limit = 0 },
 }
