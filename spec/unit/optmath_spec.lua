5fa70a909145f99b532d3496eaaaed38d741cee3
Frans de Jonge <fransdejonge@gmail.com>
2017-06-13 15:14:44 +0200

    resolve review comment

--- spec/unit/optmath_spec.lua
+++ spec/unit/optmath_spec.lua
@@ -6,44 +6,27 @@ describe("Math module", function()
     end)
 
     it("should round away from zero", function()
-        local num = 1.5
-        assert.are.same(2, Math.roundAwayFromZero(num))
-        num = 1.4
-        assert.are.same(2, Math.roundAwayFromZero(num))
-        num = -1.4
-        assert.are.same(-2, Math.roundAwayFromZero(num))
-        num = 0.2
-        assert.are.same(1, Math.roundAwayFromZero(num))
-        num = -0.2
-        assert.are.same(-1, Math.roundAwayFromZero(num))
+        assert.are.same(2, Math.roundAwayFromZero(1.5))
+        assert.are.same(2, Math.roundAwayFromZero(1.4))
+        assert.are.same(-2, Math.roundAwayFromZero(-1.4))
+        assert.are.same(1, Math.roundAwayFromZero(0.2))
+        assert.are.same(-1, Math.roundAwayFromZero(-0.2))
     end)
     it("should round", function()
-        local num = 1.5
-        assert.are.same(2, Math.round(num))
-        num = 1.4
-        assert.are.same(1, Math.round(num))
-        num = -1.4
-        assert.are.same(-1, Math.round(num))
-        num = 0.2
-        assert.are.same(0, Math.round(num))
-        num = -0.2
-        assert.are.same(0, Math.round(num))
+        assert.are.same(2, Math.round(1.5))
+        assert.are.same(1, Math.round(1.4))
+        assert.are.same(-1, Math.round(-1.4))
+        assert.are.same(0, Math.round(0.2))
+        assert.are.same(0, Math.round(-0.2))
     end)
     it("should determine odd or even", function()
-        local num = 1
-        assert.are.same("odd", Math.oddEven(num))
-        num = 2
-        assert.are.same("even", Math.oddEven(num))
-        num = 3
-        assert.are.same("odd", Math.oddEven(num))
-        num = 4
-        assert.are.same("even", Math.oddEven(num))
-        num = -4
-        assert.are.same("even", Math.oddEven(num))
-        num = -3
-        assert.are.same("odd", Math.oddEven(num))
-        num = 0
-        assert.are.same("even", Math.oddEven(num))
+        assert.are.same("odd", Math.oddEven(1))
+        assert.are.same("even", Math.oddEven(2))
+        assert.are.same("odd", Math.oddEven(3))
+        assert.are.same("even", Math.oddEven(4))
+        assert.are.same("even", Math.oddEven(-4))
+        assert.are.same("odd", Math.oddEven(-3))
+        assert.are.same("even", Math.oddEven(0))
     end)
 
 end)
