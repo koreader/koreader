05769872c717d1e6dc41f5d83d4c59ac92bfd766
Hzj_jie <Hzj_jie@hotmail.com>
2017-06-20 23:57:18 -0700

    Allow Kobo to entirely turn off the front light.

--- frontend/device/kobo/powerd.lua
+++ frontend/device/kobo/powerd.lua
@@ -43,6 +43,8 @@ function KoboPowerD:frontlightIntensityHW()
     return 20
 end
 
+function BasePowerD:turnOffFrontlightHW() self:_setIntensity(0) end
+
 function KoboPowerD:setIntensityHW(intensity)
     if self.fl == nil then return end
     self.fl:setBrightness(intensity)
