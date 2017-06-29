ae62cc8320201e58e73caee4a4a6f03c861fb4b2
mwoz123 <mwoz123@gmail.com>
2017-06-29 19:16:02 +0200

    NewsDownloader: type-o

--- plugins/newsdownloader.koplugin/main.lua
+++ plugins/newsdownloader.koplugin/main.lua
@@ -69,7 +69,7 @@ function NewsDownloader:addToMainMenu(menu_items)
                 text = _("Help"),
                 callback = function()
                     UIManager:show(InfoMessage:new{
-                        text = T(_("News Downloader retrieves RSS and Atom news entries and stores them to:\n%1\n\nEach entry is a separate html file, that can be browsed by KOReader file manager.\nItems download limit can be configured in Settings."),
+                        text = T(_("News downloader retrieves RSS and Atom news entries and stores them to:\n%1\n\nEach entry is a separate html file, that can be browsed by KOReader file manager.\nItems download limit can be configured in Settings."),
                                  news_download_dir_path)
                     })
                 end,
