fbe116845863e7eb2df81c514ca7141e8762065b
mwoz123 <mwoz123@github.com>
2017-09-17 10:57:12 +0200

    Android Fullscreen switcher: code review changes

--- frontend/ui/elements/screen_fullscreen_menu_table.lua
+++ frontend/ui/elements/screen_fullscreen_menu_table.lua
@@ -1,20 +1,19 @@
-local _ = require("gettext")
-local android = require("device/android/device")
+local _, android = pcall(require, "android")
 local logger = require("logger")
+local _ = require("gettext")
 
 return {
     text = _("Fullscreen"),
     checked_func = function()
-        local disabled_fullscreen = G_reader_settings:readSetting("disabled_fullscreen") ~= false
+        local disabled_fullscreen = G_reader_settings:isTrue("disabled_fullscreen")
         logger.dbg("screen_fullscreen_menu_table.lua: Is fullscreen disabled", disabled_fullscreen)
         return disabled_fullscreen
     end,
     callback = function()
-        local disabled_fullscreen = G_reader_settings:readSetting("disabled_fullscreen") ~= false
-        local enabled_fullscreen = not disabled_fullscreen
+        local enabled_fullscreen = G_reader_settings:isFalse("disabled_fullscreen")
 
         logger.dbg("screen_fullscreen_menu_table.lua:  Fullscreen swiching to: ", enabled_fullscreen)
-        android.setFullscreen(android, enabled_fullscreen)
+        android.setFullscreen(enabled_fullscreen)
 
         local status_bar_height = android.getStatusBarHeight()
         logger.dbg("screen_fullscreen_menu_table.lua: Status bar height: ", status_bar_height)
@@ -23,14 +22,6 @@ return {
         local screen_height = android.getScreenHeight()
         logger.dbg("screen_fullscreen_menu_table.lua: Screen height: ", screen_height)
 
--- lines below should be uncommented once #3148 is resolved
---        local Geom = require("ui/geometry")
---        logger.dbg("screen_fullscreen_menu_table.lua: Setting viewport to {x=".. status_bar_height .. ", y=0, w="
---                .. screen_width .. ", h=" .. screen_height - status_bar_height) .. "}")
---       local viewport = Geom:new{x=status_bar_height, y=0, w=screen_width, h=screen_height - status_bar_height}
---        android.screen:setViewport(viewport)
---       android.input:registerEventAdjustHook(
---                android.input.adjustTouchTranslate, {x = 0 + status_bar_height, y = 0})
         G_reader_settings:saveSetting("disabled_fullscreen", enabled_fullscreen)
     end,
 }
