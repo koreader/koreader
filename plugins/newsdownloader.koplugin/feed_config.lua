f39697525f469c51f820fb7813e8bc28e1a012f9
mwoz123 <mwoz123@gmail.com>
2017-09-24 12:49:58 +0200

    Adapt ND to changes in code (bug/feature?) where 0 means no limit
    Adapt ND to changes in code (bug/feature?) where 0 means no limit (instead of previous "no download") + reorganize config, improved comments

--- plugins/newsdownloader.koplugin/feed_config.lua
+++ plugins/newsdownloader.koplugin/feed_config.lua
@@ -1,8 +1,13 @@
 return {
 	-- list your feeds here:
+
 	-- only supports http URL for now
-    { "http://www.pcworld.com/index.rss", limit = 1 },
+
     { "http://feeds.reuters.com/Reuters/worldNews?format=xml", limit = 2},
-	-- set limit to "0" means no download, "-1" no limit.
-    { "http://www.football.co.uk/international/rss.xml", limit = 0 },
+    -- set 'limit' to change number of 'news' to be downloaded from source
+    -- 'limit' equal "0" means no limit.
+    { "http://www.pcworld.com/index.rss", limit = 1 },
+
+    -- comment out line to stop downloading source
+    --{ "http://www.football.co.uk/international/rss.xml", limit = 0 },
 }
