868860a62e27bd78d82ad1dc63f198644ceff976
Frans de Jonge <fransdejonge@gmail.com>
2017-10-10 10:00:26 +0200

    CircleCI parallelization
    * Enable parallelism in .circleci/config.yml
    * Add BUSTED_SPEC_FILE to Makefile testfront
    * Use it in .ci/test.sh with xargs for test parallelization
    
    NB This is the dumb method of improving test time.
    Ideally we want a workflow fan-in/fan-out approach.

--- .ci/test.sh
+++ .ci/test.sh
@@ -4,4 +4,7 @@ CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
 source "${CI_DIR}/common.sh"
 
-make testfront
+pushd koreader-emulator-x86_64-linux-gnu/koreader && {
+    circleci tests glob "spec/front/unit/*_spec.lua" | circleci tests split --split-by=timings --timings-type=filename | xargs -I{} sh -c 'make -C ../.. testfront BUSTED_SPEC_FILE="{}" || exit 255'
+} && popd
+#make testfront
