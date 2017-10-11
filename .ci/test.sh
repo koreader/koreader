53c0756defd9ef6b7a02406c0c69a39b5d1f187e
Frans de Jonge <fransdejonge@gmail.com>
2017-10-11 08:38:30 +0200

    CircleCI: finishing touches (#3340)
    * junit test results; unfortunately this seems to conflict with the verbose out
    
    * fix deps cache: two files can change independently
    
    * verbose print obsoleted by gtest in upsream busted

--- .ci/test.sh
+++ .ci/test.sh
@@ -5,6 +5,8 @@ CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${CI_DIR}/common.sh"
 
 pushd koreader-emulator-x86_64-linux-gnu/koreader && {
-    circleci tests glob "spec/front/unit/*_spec.lua" | circleci tests split --split-by=timings --timings-type=filename | xargs -I{} sh -c 'make -C ../.. testfront BUSTED_SPEC_FILE="{}" || exit 255'
-} && popd
-#make testfront
+    # the circleci command spits out newlines; we want spaces instead
+    BUSTED_SPEC_FILE="$(circleci tests glob "spec/front/unit/*_spec.lua" | circleci tests split --split-by=timings --timings-type=filename | tr '\n' ' ')"
+} && popd || exit
+
+make testfront BUSTED_SPEC_FILE="${BUSTED_SPEC_FILE}"
