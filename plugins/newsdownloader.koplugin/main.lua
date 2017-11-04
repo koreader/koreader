69eb105822d6293d22e35d5717a5319b028e3f44
mwoz123 <mwoz123@gmail.com>
2017-11-04 20:20:59 +0100

    News downloader: Number items in processing

--- plugins/newsdownloader.koplugin/main.lua
+++ plugins/newsdownloader.koplugin/main.lua
@@ -152,11 +152,12 @@ function NewsDownloader:loadConfigAndProcessFeeds()
 
     local unsupported_feeds_urls = {}
 
+    local total_feed_entries = table.getn(feed_config)
     for idx, feed in ipairs(feed_config) do
         local url = feed[1]
         local limit = feed.limit
         if url and limit then
-            info = InfoMessage:new{ text = T(_("Processing: %1"), url) }
+            info = InfoMessage:new{ text = T(_("Processing %1/%2: %3"), idx, total_feed_entries, url) }
             UIManager:show(info)
             -- processFeedSource is a blocking call, so manually force a UI refresh beforehand
             UIManager:forceRePaint()
