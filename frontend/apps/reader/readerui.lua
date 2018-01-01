145c482ff8d7cae9d324be9b9f8f025ce4105f1e
poire-z <poire-z@users.noreply.github.com>
2018-01-01 11:54:45 +0100

    Only if NOT has_pages.............

--- frontend/apps/reader/readerui.lua
+++ frontend/apps/reader/readerui.lua
@@ -89,7 +89,7 @@ end
 function ReaderUI:init()
     -- cap screen refresh on pan to 2 refreshes per second
     local pan_rate = Screen.eink and 2.0 or 30.0
-if self.document.info.has_pages then
+if not self.document.info.has_pages then
 self.document:loadDocument()
 end
 
