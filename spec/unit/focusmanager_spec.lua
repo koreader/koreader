b69c3647bc31a119eabd236358a2e5d9e9f0f150
onde2rock <moncomptedepub@yahoo.fr>
2018-03-21 11:55:57 +0100

    fix focusmanager tests

--- spec/unit/focusmanager_spec.lua
+++ spec/unit/focusmanager_spec.lua
@@ -23,69 +23,69 @@ describe("FocusManager module", function()
         focusmanager.layout = layout
         focusmanager.selected = {y = 1,x = 1}
         Right(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 1,x = 2})
+        assert.are.same({y = 1,x = 2}, focusmanager.selected)
     end)
     it("should go left", function()
         local focusmanager = FocusManager:new{}
         focusmanager.layout = layout
         focusmanager.selected = {y = 1,x = 2}
         Left(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 1,x = 1})
+        assert.are.same({y = 1,x = 1}, focusmanager.selected)
     end)
     it("should go up", function()
         local focusmanager = FocusManager:new{}
         focusmanager.layout = layout
         focusmanager.selected = {y = 2,x = 2}
         Up(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 1,x = 2})
+        assert.are.same({y = 1,x = 2}, focusmanager.selected)
     end)
     it("should go down", function()
         local focusmanager = FocusManager:new{}
         focusmanager.layout = layout
         focusmanager.selected = {y = 2,x = 2}
         Down(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 3,x = 2})
+        assert.are.same({y = 3,x = 2}, focusmanager.selected)
     end)
     it("should vertical wrapAround up", function()
         local focusmanager = FocusManager:new{}
         focusmanager.layout = layout
         focusmanager.selected = {y = 1,x = 1}
         Up(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 3,x = 2})
+        assert.are.same({y = 3,x = 2}, focusmanager.selected)
     end)
     it("should vertical wrapAround down", function()
         local focusmanager = FocusManager:new{}
         focusmanager.layout = layout
         focusmanager.selected = {y = 3,x = 2}
         Down(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 1,x = 2})
+        assert.are.same({y = 1,x = 2}, focusmanager.selected)
     end)
     it("should do vertical step to the right", function()
         local focusmanager = FocusManager:new{}
         focusmanager.layout = layout
         focusmanager.selected = {y = 1,x = 1}
         Down(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 2,x = 2})
+        assert.are.same({y = 2,x = 2}, focusmanager.selected)
     end)
     it("should do vertical step to the left", function()
         local focusmanager = FocusManager:new{}
         focusmanager.layout = layout
         focusmanager.selected = {y = 1,x = 3}
         Down(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 2,x = 2})
+        assert.are.same({y = 2,x = 2}, focusmanager.selected)
     end)
     it("should respect left limit", function()
         local focusmanager = FocusManager:new{}
         focusmanager.layout = layout
         focusmanager.selected = {y = 2,x = 2}
         Left(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 2,x = 2})
+        assert.are.same({y = 2,x = 2}, focusmanager.selected)
     end)
     it("should respect right limit", function()
         local focusmanager = FocusManager:new{}
         focusmanager.layout = layout
         focusmanager.selected = {y = 2,x = 2}
         Right(focusmanager)
-        assert.are.same(focusmanager.selected, {y = 2,x = 2})
+        assert.are.same({y = 2,x = 2}, focusmanager.selected)
     end)
 end)
