9779f06c6455587c794de9a00e66643f2242ad11
onde2rock <moncomptedepub@yahoo.fr>
2018-03-30 21:20:39 +0200

    Change some SDL keybindings

--- frontend/device/input.lua
+++ frontend/device/input.lua
@@ -93,7 +93,7 @@ local Input = {
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-            "Up", "Down", "Left", "Right", "Press",
+            "Up", "Down", "Left", "Right", "Press", "Backspace", "End",
             "Back", "Sym", "AA", "Menu", "Home", "Del",
             "LPgBack", "RPgBack", "LPgFwd", "RPgFwd"
         },
