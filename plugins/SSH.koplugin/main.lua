1951205b6f1302448bc72f7866d2bfb63c7ae4d8
onde2rock <moncomptedepub@yahoo.fr>
2018-04-09 15:06:38 +0200

    Code cleanup

--- plugins/SSH.koplugin/main.lua
+++ plugins/SSH.koplugin/main.lua
@@ -13,103 +13,105 @@ local SSH = WidgetContainer:new{
 }
 
 function SSH:init()
+    self.SSH_port = G_reader_settings:readSetting("SSH_port") or "2222"
+    self.allow_blank_password = false
     self.ui.menu:registerToMainMenu(self)
 end
 
+function SSH:start()
+    local cmd = string.format("%s %s %s %s %s %s",
+        "./dropbear",
+        "-E", "-r SSH/dropbear_rsa_host_key",
+        "-p", self.SSH_port,
+        "-P /tmp/dropbear.pid")
+     if self.allow_blank_password then
+        cmd = string.format("%s %s", cmd, "-B")
+    end
+    os.execute("mkdir SSH")
+    os.execute("./dropbearkey -t rsa -f SSH/dropbear_rsa_host_key")
+    logger.dbg("[Network] Launching SSH server : ", cmd)
+    if os.execute(cmd) == 0 then
+        local info = InfoMessage:new{
+                timeout = 5,
+                text = string.format("%s %s \n %s",
+                    "SSH port: ", self.SSH_port,
+                    Device.retrieveNetworkInfo and Device:retrieveNetworkInfo() or _("Could not retrieve network info.")),
+        }
+        UIManager:show(info)
+    else
+        local info = InfoMessage:new{
+                timeout = 5,
+                text = _("Error"),
+        }
+        UIManager:show(info)
+    end
+end
+
+function SSH:isRunning()
+    return util.pathExists("/tmp/dropbear.pid")
+end
+
+function SSH:stop()
+    os.execute("cat /tmp/dropbear.pid | xargs kill")
+end
+
+function SSH:show_port_dialog()
+    self.port_dialog = InputDialog:new{
+        title = _("Choose SSH port"),
+        input = self.SSH_port,
+        input_type = "number",
+        input_hint = self.SSH_port,
+        buttons = {
+            {
+                {
+                    text = _("Cancel"),
+                    callback = function()
+                        UIManager:close(self.port_dialog)
+                    end,
+                },
+                {
+                    text = _("Save"),
+                    is_enter_default = true,
+                    callback = function()
+                        local value  = tonumber(self.port_dialog:getInputText())
+                        if value then
+                            self.SSH_port = value
+                            G_reader_settings:saveSetting("SSH_port", self.SSH_port)
+                            UIManager:close(self.port_dialog)
+                        end
+                    end
+                },
+            },
+        },
+    }
+    UIManager:show(self.port_dialog)
+    self.port_dialog:onShowKeyboard()
+end
+
 function SSH:addToMainMenu(menu_items)
-    local SSH_port = G_reader_settings:readSetting("SSH_port") or "2222"
-    local allow_blank_password = false
-    local start = function(menu)
-        local cmd = string.format("%s %s %s %s %s %s",
-            "./dropbear",
-            "-E", "-r SSH/dropbear_rsa_host_key",
-            "-p", SSH_port,
-            "-P /tmp/dropbear.pid")
-         if allow_blank_password then
-            cmd = string.format("%s %s", cmd, "-B")
-        end
-        os.execute("mkdir SSH")
-        os.execute("./dropbearkey -t rsa -f SSH/dropbear_rsa_host_key")
-        logger.dbg("[Network] Launching SSH server : ", cmd)
-        if os.execute(cmd) == 0 then
-            local info = InfoMessage:new{
-                    timeout = 5,
-                    text = string.format("%s %s \n %s",
-                        "SSH port: ", SSH_port,
-                        Device.retrieveNetworkInfo and Device:retrieveNetworkInfo() or _("Could not retrieve network info.")),
-            }
-            UIManager:show(info)
-        else
-            local info = InfoMessage:new{
-                    timeout = 5,
-                    text = _("Error"),
-            }
-            UIManager:show(info)
-        end
-    end
-
-    local isRunning = function()
-        return util.pathExists("/tmp/dropbear.pid")
-    end
-
-    local stop = function()
-        os.execute("cat /tmp/dropbear.pid | xargs kill")
-    end
-
     menu_items.SSH = {
-        text = _(_("SSH server")),
+        text = _("SSH server"),
         sub_item_table = {
             {
                 text = _("Start SSH server"),
-                callback = start,
-                enabled_func = function() return not isRunning() end,
+                callback = function() return self:start() end,
+                enabled_func = function() return not self:isRunning() end,
             },
             {
                 text = _("Stop SSH server"),
-                callback = stop,
-                enabled_func = isRunning,
+                callback = self.stop,
+                enabled_func = self.isRunning,
             },
             {
                 text = _("Change SSH port"),
-                enabled_func = function() return not isRunning() end,
-                callback = function()
-                    self.dialog = InputDialog:new{
-                        title = _("Choose SSH port"),
-                        input_type = "number",
-                        input_hint = SSH_port,
-                        buttons = {
-                            {
-                                {
-                                    text = _("Cancel"),
-                                    callback = function()
-                                        UIManager:close(self.dialog)
-                                    end,
-                                },
-                                {
-                                    is_enter_default = true,
-                                    text = _("Save"),
-                                    callback = function()
-                                        local value
-                                        value  = tonumber(self.dialog:getInputText())
-                                        if value then
-                                            SSH_port = value
-                                            G_reader_settings:saveSetting("SSH_port",SSH_port)
-                                            UIManager:close(self.dialog)
-                                        end
-                                    end
-                                },
-                            },
-                        },
-                    }
-                    UIManager:show(self.dialog)
-                    self.dialog:onShowKeyboard()
-                end
+                enabled_func = function() return not self:isRunning() end,
+                callback = function() return self:show_port_dialog() end,
             },
             {
                 text = _("Allow blank password"),
-                checked_func = function() return allow_blank_password end,
-                enabled_func = function() return not isRunning() end,
-                callback = function() allow_blank_password = not allow_blank_password end,
+                checked_func = function() return self.allow_blank_password end,
+                enabled_func = function() return not self:isRunning() end,
+                callback = function() self.allow_blank_password = not self.allow_blank_password end,
             },
        }
     }
