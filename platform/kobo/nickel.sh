a65d4b0a7887db4e4166e4a5980734f4707ad4c3
NiLuJe <ninuje@gmail.com>
2018-04-14 15:38:32 +0200

    Slimmed down the nickel restart script.
    Based on current rcS
    Scrapped a bucketload of irrelevant & legacy crap, since we're never
    bootstrapping nickel, only restarting it.

--- platform/kobo/nickel.sh
+++ platform/kobo/nickel.sh
@@ -1,97 +1,38 @@
 #!/bin/sh
-PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/lib"
+PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/lib:"
 
-# Handle the rotation weirdness on some devices
-cur_rotate="$(cat "/sys/class/graphics/fb0/rotate")"
-
-# start fmon again. Note that we don't have to worry about reaping this, nickel kills on-animator.sh on start.
+# Ensures fmon will restart. Note that we don't have to worry about reaping this, nickel kills on-animator.sh on start.
 (
+    # NOTE: Recent FW versions appear to do away with the sleep on some platforms (I'm assuming the newer, faster, better ones!)
     usleep 400000
     /etc/init.d/on-animator.sh
 ) &
 
-# environment needed by nickel, from /etc/init.d/rcS:
-
-if [ ! -n "${WIFI_MODULE_PATH}" ]; then
-    INTERFACE="wlan0"
-    WIFI_MODULE="ar6000"
-    if [ "${PLATFORM}" != "freescale" ]; then
-        INTERFACE="eth0"
-        WIFI_MODULE="dhd"
-    fi
-    export INTERFACE
-    export WIFI_MODULE
-    export WIFI_MODULE_PATH="/drivers/${PLATFORM}/wifi/${WIFI_MODULE}.ko"
-fi
-
-export NICKEL_HOME="/mnt/onboard/.kobo"
+# We don't need to duplicate any of the env setup from rcS, since we will only ever run this to *restart* nickel, and not bootstrap it.
+# Meaning we've already got most of the necessary env from nickel itself via both our launcher (fmon/KFMon) and our own startup script.
 export LD_LIBRARY_PATH="/usr/local/Kobo"
-
-export LANG="en_US.UTF-8"
 
 # Make sure we kill the WiFi first, because nickel apparently doesn't like it if it's up... (cf. #1520)
 # NOTE: That check is possibly wrong on PLATFORM == freescale (because I don't know if the sdio_wifi_pwr module exists there), but we don't terribly care about that.
 if lsmod | grep -q sdio_wifi_pwr; then
     killall udhcpc default.script wpa_supplicant 2>/dev/null
-    wlarm_le -i ${INTERFACE} down
+    [ "${WIFI_MODULE}" != 8189fs ] && wlarm_le -i ${INTERFACE} down
     ifconfig ${INTERFACE} down
     # NOTE: Kobo's busybox build is weird. rmmod appears to be modprobe in disguise, defaulting to the -r flag. If re-specifying -r starts to fail one day, switch to rmmod without args, or modprobe -r.
-    rmmod -r ${WIFI_MODULE}
+    rmmod -r "${WIFI_MODULE}"
+    # c.f., #2394?
+    usleep 200000
     rmmod -r sdio_wifi_pwr
 fi
 
 # Flush buffers to disk, who knows.
 sync
 
-# start nickel again (inspired from KSM, vlasovsoft & the base rcS), this should
-# cover at least firmware versions from 2.6.1 to 3.12.1 (tested on a kobo
-# mini with 3.4.1 firmware & a H2O on 3.12.1)
-
-# NOTE: Since we're not cold booting, this is technically redundant... On the other hand, it doesn't really hurt either ;).
-(
-    /usr/local/Kobo/pickel disable.rtc.alarm
-
-    if [ ! -e "/etc/wpa_supplicant/wpa_supplicant.conf" ]; then
-        cp "/etc/wpa_supplicant/wpa_supplicant.conf.template" "/etc/wpa_supplicant/wpa_supplicant.conf"
-    fi
-
-    # FWIW, that appears to be gone from recent rcS scripts. AFAICT, still harmless, though.
-    echo 1 >"/sys/devices/platform/mxc_dvfs_core.0/enable"
-
-    /sbin/hwclock -s -u
-) &
-
-# Hey there, nickel!
-if [ ! -e "/usr/local/Kobo/platforms/libkobo.so" ]; then
-    export QWS_KEYBOARD="imx508kbd:/dev/input/event0"
-    export QT_PLUGIN_PATH="/usr/local/Kobo/plugins"
-    if [ -e "/usr/local/Kobo/plugins/gfxdrivers/libimxepd.so" ]; then
-        export QWS_DISPLAY="imxepd"
-    else
-        export QWS_DISPLAY="Transformed:imx508:Rot90"
-        export QWS_MOUSE_PROTO="tslib_nocal:/dev/input/event1"
-    fi
-    # NOTE: Send the output to the void, to avoid spamming the shell with the output of the string of killall commands they periodically send
-    /usr/local/Kobo/hindenburg >/dev/null 2>&1 &
-    /usr/local/Kobo/nickel -qws -skipFontLoad >/dev/null 2>&1 &
-else
-    /usr/local/Kobo/hindenburg >/dev/null 2>&1 &
-    lsmod | grep -q lowmem || insmod "/drivers/${PLATFORM}/misc/lowmem.ko" &
-    if grep -q "dhcpcd=true" "/mnt/onboard/.kobo/Kobo/Kobo eReader.conf"; then
-        dhcpcd -d -t 10 &
-    fi
-    /usr/local/Kobo/nickel -platform kobo -skipFontLoad >/dev/null 2>&1 &
-fi
-
-# Ahoy, annoying sickel!
-if [ -x /usr/local/Kobo/sickel ]; then
-    /usr/local/Kobo/sickel -platform kobo:noscreen >/dev/null 2>&1 &
-fi
-
-# Rotation weirdness, part II
-echo "${cur_rotate}" >"/sys/class/graphics/fb0/rotate"
-# shellcheck disable=SC2094
-cat "/sys/class/graphics/fb0/rotate" >"/sys/class/graphics/fb0/rotate"
+# And finally, simply restart nickel.
+# We don't care about horribly legacy stuff, because if people switch between nickel and KOReader in the first place, I assume they're using a decently recent enough FW version.
+# Last tested on an H2O running FW 4.7.x
+/usr/local/Kobo/hindenburg &
+LIBC_FATAL_STDERR_=1 /usr/local/Kobo/nickel -platform kobo -skipFontLoad &
 
 # Handle sdcard
 if [ -e "/dev/mmcblk1p1" ]; then
