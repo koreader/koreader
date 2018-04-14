72ae67ecc7c268ad96e6fc6c6e0fe53b86b5d6d8
NiLuJe <ninuje@gmail.com>
2018-04-14 15:00:45 +0200

    Start a pass of Kobo script cleanups
    By starting somewhat randomly with this ;p.

--- platform/kobo/release-ip.sh
+++ platform/kobo/release-ip.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 # PATH export is only needed if you run this script manually from a shell
-export PATH=$PATH:/sbin
+export PATH="${PATH}:/sbin"
 
 # Release IP and shutdown udhcpc.
-
 pkill -9 -f '/bin/sh /etc/udhcpc.d/default.script'
 ifconfig eth0 0.0.0.0
