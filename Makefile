koreader/koreader

commit f402ee5f6ff2769fee1f0bca747f5b0b2eafce1b

chore: remove all echo shell out in kobo resume routine

frontend/device/kobo/device.lua
@@ -355,21 +355,20 @@ function Kobo:suspend()
 
     -]]
 
+    -- NOTE: Sets gSleep_Mode_Suspend to 1. Used as a flag throughout the
+    -- kernel to suspend/resume various subsystems
+    -- cf. kernel/power/main.c @ L#207
     f = io.open("/sys/power/state-extended", "w")
     if not f then
         logger.err("Cannot open /sys/power/state-extended for writing!")
         return false
     end
-    -- NOTE: Sets gSleep_Mode_Suspend to 1. Used as a flag throughout the
-    -- kernel to suspend/resume various subsystems
-    -- cf. kernel/power/main.c @ L#207
     re, err_msg, err_code = f:write("1\n")
-    logger.info("Write syscall returned: ", re)
+    io.close(f)
+    logger.info("Kobo Suspend: Asked the kernel to put subsystems to sleep, ret:", re)
     if not re then
         logger.err('write error: ', err_msg, err_code)
     end
-    io.close(f)
-    logger.info("Kobo Suspend: Asked the kernel to put subsystems to sleep")
 
     util.sleep(2)
     logger.info("Kobo Suspend: Waited for 2s because of reasons...")
@@ -400,7 +399,14 @@ function Kobo:suspend()
     logger.info("Kobo Suspend: Asking for a suspend to RAM . . .")
     f = io.open("/sys/power/state", "w")
     if not f then
-        -- TODO: update state-extended?
+        -- reset state-extend back to 0 since we are giving up
+        local ext_fd = io.open("/sys/power/state-extended", "w")
+        if not ext_fd then
+            logger.err("Cannot open /sys/power/state-extended for writing!")
+        else
+            ext_fd:write("0\n")
+            io.close(ext_fd)
+        end
         return false
     end
     re, err_msg, err_code = f:write("mem\n")
@@ -441,17 +447,29 @@ end
 
 function Kobo:resume()
     -- Now that we're up, unflag subsystems for suspend...
-    os.execute("echo 0 > /sys/power/state-extended")
-    logger.info("Kobo Suspend: Unflagged kernel subsystems for suspend")
+    -- NOTE: Sets gSleep_Mode_Suspend to 0. Used as a flag throughout the
+    -- kernel to suspend/resume various subsystems
+    -- cf. kernel/power/main.c @ L#207
+    local f = io.open("/sys/power/state-extended", "w")
+    if not f then
+        logger.err("Cannot open /sys/power/state-extended for writing!")
+        return false
+    end
+    local re, err_msg, err_code = f:write("0\n")
+    io.close(f)
+    logger.info("Kobo Suspend: Unflagged kernel subsystems for suspend, ret:", re)
+    if not re then
+        logger.err('write error: ', err_msg, err_code)
+    end
 
     -- HACK: wait a bit (0.1 sec) for the kernel to catch up
     util.usleep(100000)
     -- cf. #1862, I can reliably break IR touch input on resume...
     -- cf. also #1943 for the rationale behind applying this workaorund in every case...
-    local f = io.open("/sys/devices/virtual/input/input1/neocmd", "r")
+    f = io.open("/sys/devices/virtual/input/input1/neocmd", "r")
     if f ~= nil then
+        f:write("a\n")
         io.close(f)
-        os.execute("echo 'a' > /sys/devices/virtual/input/input1/neocmd")
     end
 end
 

commit 95bc5d70813ba1468b239ba543fac06096642f35

[fix] tar args order & Kobo IR grid regression (#3895)
* Fix order of tar args

Otherwise, current tar versions abort

* Unbreak Kobo IR grid resume workaround

Regression after f402ee5f6ff2769fee1f0bca747f5b0b2eafce1b

I'm going to assume my H2O is really, really weird, because that
commit's more than a year old o_O.

Makefile
@@ -186,8 +186,8 @@ kindleupdate: all
 	# note that the targz file extension is intended to keep ISP from caching
 	# the file, see koreader#1644.
 	cd $(INSTALL_DIR) && \
-		tar czafh ../koreader-$(DIST)-$(MACHINE)-$(VERSION).targz \
-		-T koreader/ota/package.index --no-recursion
+		tar -czah --no-recursion -f ../koreader-$(DIST)-$(MACHINE)-$(VERSION).targz \
+		-T koreader/ota/package.index
 
 koboupdate: all
 	# ensure that the binaries were built for ARM
@@ -214,8 +214,8 @@ koboupdate: all
 		koreader/ota/package.index koreader.png README_kobo.txt
 	# make gzip koboupdate for zsync OTA update
 	cd $(INSTALL_DIR) && \
-		tar czafh ../koreader-kobo-$(MACHINE)-$(VERSION).targz \
-		-T koreader/ota/package.index --no-recursion
+		tar -czah --no-recursion -f ../koreader-kobo-$(MACHINE)-$(VERSION).targz \
+		-T koreader/ota/package.index
 
 pbupdate: all
 	# ensure that the binaries were built for ARM
@@ -251,8 +251,8 @@ pbupdate: all
 		applications/koreader/ota/package.index system
 	# make gzip pbupdate for zsync OTA update
 	cd $(INSTALL_DIR)/applications && \
-		tar czafh ../../koreader-pocketbook-$(MACHINE)-$(VERSION).targz \
-		-T koreader/ota/package.index --no-recursion
+		tar -czah --no-recursion -f ../../koreader-pocketbook-$(MACHINE)-$(VERSION).targz \
+		-T koreader/ota/package.index
 
 utupdate: all
 	# ensure that the binaries were built for ARM
@@ -285,8 +285,8 @@ utupdate: all
 		koreader/ota/package.index
 	# make gzip update for zsync OTA update
 	cd $(INSTALL_DIR) && \
-		tar czafh ../koreader-$(DIST)-$(MACHINE)-$(VERSION).targz \
-		-T koreader/ota/package.index --no-recursion
+		tar -czah --no-recursion -f ../koreader-$(DIST)-$(MACHINE)-$(VERSION).targz \
+		-T koreader/ota/package.index
 
 	# generate ubuntu touch click package
 	rm -rf $(INSTALL_DIR)/tmp && mkdir -p $(INSTALL_DIR)/tmp
@@ -349,8 +349,8 @@ androidupdate: all
 	$(ISED) '/git-rev/d' $(INSTALL_DIR)/koreader/ota/package.index
 	# make gzip android update for zsync OTA update
 	-cd $(INSTALL_DIR)/koreader && \
-		tar czafh ../../koreader-android-$(MACHINE)-$(VERSION).targz \
-		-T ota/package.index --no-recursion
+		tar -czah --no-recursion -f ../../koreader-android-$(MACHINE)-$(VERSION).targz \
+		-T ota/package.index
 	# make android update apk
 	cd $(INSTALL_DIR)/koreader && 7z a -l -mx=1 \
 		../../$(ANDROID_LAUNCHER_DIR)/assets/module/koreader-$(VERSION).7z * \

frontend/device/kobo/device.lua
@@ -517,7 +517,7 @@ function Kobo:resume()
     util.usleep(100000)
     -- cf. #1862, I can reliably break IR touch input on resume...
     -- cf. also #1943 for the rationale behind applying this workaorund in every case...
-    f = io.open("/sys/devices/virtual/input/input1/neocmd", "r")
+    f = io.open("/sys/devices/virtual/input/input1/neocmd", "w")
     if f ~= nil then
         f:write("a\n")
         io.close(f)