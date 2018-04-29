80efe4a74049a513a47dfaafefd98e44e7abb806
onde2rock <moncomptedepub@yahoo.fr>
2018-04-29 14:54:35 +0200

    Exclude newsdownloader.koplugin/lib from tab check

--- plugins/newsdownloader.koplugin/feed_config.lua
+++ plugins/newsdownloader.koplugin/feed_config.lua
@@ -5,7 +5,7 @@ return {
 
     { "https://www.pcworld.com/index.rss", limit = 7 , download_full_article=false},
 
-        -- comment out line ("--" at line start) to stop downloading source
+    -- comment out line ("--" at line start) to stop downloading source
     --{ "http://www.football.co.uk/international/rss.xml", limit = 0 , download_full_article=false},
 
 
@@ -14,14 +14,14 @@ return {
 
 
 
-        --HELP:
-        -- use syntax: {"your_url", limit= max_number_of_items_to_be_created, download_full_article=true/false}
+    --HELP:
+    -- use syntax: {"your_url", limit= max_number_of_items_to_be_created, download_full_article=true/false}
 
-        -- set 'limit' to change number of 'news' to be created
+    -- set 'limit' to change number of 'news' to be created
     -- 'limit' equal "0" means no limit.
 
-        -- 'download_full_article=false' - means download full article using feed link (may not always work correctly)
-        -- 'download_full_article=true' - means use only feed description to create feeds (usually only part of the article)
+    -- 'download_full_article=false' - means download full article using feed link (may not always work correctly)
+    -- 'download_full_article=true' - means use only feed description to create feeds (usually only part of the article)
 
 
 }
