9520d7ac72e7b77ac0116337a3cfc3003d720e22
poire-z <poire-z@users.noreply.github.com>
2018-05-04 23:43:11 +0200

    fixing unit test by chance

--- spec/unit/readerlink_spec.lua
+++ spec/unit/readerlink_spec.lua
@@ -17,7 +17,7 @@ describe("ReaderLink module", function()
             document = DocumentRegistry:openDocument(sample_epub),
         }
         readerui.rolling:onGotoPage(4)
-        readerui.link:onTap(nil, {pos = {x = 336, y = 668}})
+        readerui.link:onTap(nil, {pos = {x = 336, y = 688}})
         assert.is.same(36, readerui.rolling.current_page)
     end)
 
@@ -54,7 +54,7 @@ describe("ReaderLink module", function()
             document = DocumentRegistry:openDocument(sample_epub),
         }
         readerui.rolling:onGotoPage(4)
-        readerui.link:onTap(nil, {pos = {x = 336, y = 668}})
+        readerui.link:onTap(nil, {pos = {x = 336, y = 698}})
         assert.is.same(36, readerui.rolling.current_page)
         readerui.link:onGoBackLink()
         assert.is.same(4, readerui.rolling.current_page)
