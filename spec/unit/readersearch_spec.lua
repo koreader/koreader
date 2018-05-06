c39656c6c84ff61832414e9fd789e258e2bd9264
poire-z <poire-z@users.noreply.github.com>
2018-05-07 00:27:45 +0200

    same old zzzzzzzzzzzz

--- spec/unit/readersearch_spec.lua
+++ spec/unit/readersearch_spec.lua
@@ -65,16 +65,19 @@ describe("Readersearch module", function()
             end
         end)
         it("should find the last occurrence", function()
+            require("logger").warn("ZZZZZ nb pages", doc:getPageCount())
             for i = 100, 180, 10 do
                 rolling:onGotoPage(i)
                 local words = search:searchFromEnd("Verona")
                 assert.truthy(words)
                 local pageno = doc:getPageFromXPointer(words[1].start)
+                require("logger").warn("ZZZZZ last match on page", pageno)
                 assert.truthy(pageno > 200)
             end
             for i = 230, 235, 1 do
                 rolling:onGotoPage(i)
                 local words = search:searchFromEnd("Verona")
+                require("logger").warn("ZZZZZ words:", words)
                 assert(words == nil)
             end
         end)
