061c25a4a669a40e87519013411a0d8c669bca4b
NiLuJe <ninuje@gmail.com>
2018-07-03 02:13:20 +0200

    Fix Version test
    Also fix some dubious logic in there while I'm at it.

--- spec/unit/version_spec.lua
+++ spec/unit/version_spec.lua
@@ -7,24 +7,32 @@ describe("Version module", function()
     it("should get current revision", function()
         local rev = Version:getCurrentRevision()
         local year, month, revision = rev:match("v(%d%d%d%d)%.(%d%d)-?(%d*)")
-        local commit = rev:match("-%d*-g(.*)")
+        local commit = rev:match("-%d*-g(%x*)[%d_%-]*")
         assert.is_truthy(year)
         assert.is_truthy(month)
         assert.is_truthy(revision)
         assert.is_truthy(commit)
-        assert.is_true(4 <= year:len())
+        assert.is_true(4 == year:len())
         assert.is_true(2 == month:len())
         assert.is_true(1 <= revision:len())
-        assert.is_true(5 <= commit:len())
+        assert.is_true(7 <= commit:len())
     end)
     it("should get normalized current version", function()
-        assert.is_true(7 <= tostring(Version:getNormalizedCurrentVersion()):len())
+        assert.is_true(10 == tostring(Version:getNormalizedCurrentVersion()):len())
     end)
     it("should get normalized version", function()
         local rev = "v2015.11-982-g704d4238"
         local version, commit = Version:getNormalizedVersion(rev)
-        local expected_version = 201511982
+        local expected_version = 2015110982
         local expected_commit = "704d4238"
+        assert.are.same(expected_version, version)
+        assert.are.same(expected_commit, commit)
+    end)
+    it("should also get normalized version", function()
+        local rev = "v2015.11-1755-gecd7b5b_2018-07-02"
+        local version, commit = Version:getNormalizedVersion(rev)
+        local expected_version = 2015111755
+        local expected_commit = "ecd7b5b"
         assert.are.same(expected_version, version)
         assert.are.same(expected_commit, commit)
     end)
