2484f0f3bcf22b7df635912b9ba482522d1bc345
Robert <robert.styla@gmail.com>
2018-07-04 12:10:38 +0200

    Style tweaks: adds Allow hyphenation on all text (#4047)
    Also fix vertical-align for sub in 'Smaller sub- and superscript'

--- frontend/ui/data/css_tweaks.lua
+++ frontend/ui/data/css_tweaks.lua
@@ -89,6 +89,15 @@ local CssTweaks = {
             },
         },
         {
+            id = "hyphenate_all_auto";
+            title = _("Allow hyphenation on all text"),
+            description = _("Allow hyphenation to happen on all text (except headings), in case the publisher has disabled it."),
+            css = [[
+* { hyphenate: auto !important; }
+h1, h2, h3, h4, h5, h6 { hyphenate: none !important; }
+            ]],
+        },
+        {
             id = "sub_sup_smaller";
             title = _("Smaller sub- and superscript"),
             description = _("Prevent sub- and superscript from affecting line-height."),
@@ -97,7 +106,7 @@ local CssTweaks = {
             -- https://github.com/koreader/koreader/issues/3923#issuecomment-386510294
             css = [[
 sup { font-size: 50% !important; vertical-align: super !important; }
-sub { font-size: 50% !important; vertical-align: middle !important; }
+sub { font-size: 50% !important; vertical-align: sub !important; }
             ]],
             separator = true,
         },
