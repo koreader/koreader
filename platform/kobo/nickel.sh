6eb01c61d4b592ad641f3b6d5c747506995fdf54
NiLuJe <ninuje@gmail.com>
2018-07-04 18:23:26 +0200

    Restore LD_LIBRARY_PATH earlier, so that pickel doesn't end up (failing) to use our own zlib

--- platform/kobo/nickel.sh
+++ platform/kobo/nickel.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/lib:"
 
+# We don't need to duplicate any of the env setup from rcS, since we will only ever run this to *restart* nickel, and not bootstrap it.
+# Meaning we've already got most of the necessary env from nickel itself via both our launcher (fmon/KFMon) and our own startup script.
+# NOTE: LD_LIBRARY_PATH is the only late export from rcS we don't siphon in koreader.sh, for obvious reasons ;).
+export LD_LIBRARY_PATH="/usr/local/Kobo"
+
 # Ensures fmon will restart. Note that we don't have to worry about reaping this, nickel kills on-animator.sh on start.
 (
     if [ "${PLATFORM}" = "freescale" ] || [ "${PLATFORM}" = "mx50-ntx" ] || [ "${PLATFORM}" = "mx6sl-ntx" ]; then
@@ -8,11 +13,6 @@ PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/lib:"
     fi
     /etc/init.d/on-animator.sh
 ) &
-
-# We don't need to duplicate any of the env setup from rcS, since we will only ever run this to *restart* nickel, and not bootstrap it.
-# Meaning we've already got most of the necessary env from nickel itself via both our launcher (fmon/KFMon) and our own startup script.
-# NOTE: LD_LIBRARY_PATH is the only late export from rcS we don't siphon in koreader.sh, for obvious reasons ;).
-export LD_LIBRARY_PATH="/usr/local/Kobo"
 
 # Make sure we kill the WiFi first, because nickel apparently doesn't like it if it's up... (cf. #1520)
 # NOTE: That check is possibly wrong on PLATFORM == freescale (because I don't know if the sdio_wifi_pwr module exists there), but we don't terribly care about that.
