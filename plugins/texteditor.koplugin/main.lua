9215a112688c4a35797a8364382fda0135b312f7
mwoz123 <mwoz123@github.com>
2018-07-10 20:23:15 +0200

    TextEditor working version

--- plugins/texteditor.koplugin/main.lua
+++ plugins/texteditor.koplugin/main.lua
@@ -1,7 +1,7 @@
 local DataStorage = require("datastorage")
-local Font = require("ui/font")
 local InfoMessage = require("ui/widget/infomessage")
 local InputDialog = require("ui/widget/inputdialog")
+local PathChooser = require("ui/widget/pathchooser")
 local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local logger = require("logger")
@@ -26,46 +26,52 @@ function TextEditor:init(file_path)
 end
 
 function TextEditor:start()
+    local FileManager = require("apps/filemanager/filemanager")
+    FileManager:onClose()
     self.input = InputDialog:new{
         title =  _("Basic text editor"),
         input = self.context,
         text_height = Screen:getHeight() * 0.5,
         width = self.width or Screen:getWidth(),
         input_type = "string",
-        buttons = {{{
-            text = _("Save"),
-            is_enter_default = true,
+        buttons = {{
+        {
+            text = _("Open"),
             callback = function()
-                UIManager:close(self.input)
+                self:chooseFile()
+            end,
+        },
+        {
+            text = _("Save"),
+            callback = function()
                 self:saveFile(self.file_path)
             end,
         },{
             text = _("Cancel"),
             callback = function()
+                self.context = ""
+                self.file_path = ""
                 UIManager:close(self.input)
             end,
         }, 
         {
-            text = _("Open"),
-            is_enter_default = true,
-            callback = function()
-                --TODO use filechoooser or filebrowser
-                logger.dbg(self.file_path)
-                self:readFile(self.file_path)
-            end,
-        },
-        {
             text = _("Pg_up"),
             is_enter_default = true,
             callback = function()
-                logger.dbg("not implemented. Help needed")
+                UIManager:show(InfoMessage:new{
+                    text = _("Not implemented. Please help if you know how.")
+                })
+                UIManager:forceRePaint()
             end,
         },
         {
             text = _("Pg_dwn"),
             is_enter_default = true,
             callback = function()
-                logger.dbg("not implemented. Help needed")
+                UIManager:show(InfoMessage:new{
+                    text = _("Not implemented. Please help if you know how.")
+                })
+                UIManager:forceRePaint()
             end,
         }
 
@@ -76,34 +82,63 @@ function TextEditor:start()
 end
 
 
-function TextEditor:saveFile(file_path)
-    local file = io.open(file_path, "w")
-    if file then
-        file:write(self.context)
-        file:close()
-    else
-        logger.warn("Failed to save to " .. file_path)
-    end
+function TextEditor:chooseFile()
+    self.input:onClose()
+    local path_chooser = PathChooser:new{
+        title = _("Choose file. Hold to confirm"),
+        height = Screen:getHeight(),
+        path = util.realpath(DataStorage:getDataDir()),
+        show_hidden = G_reader_settings:readSetting("show_hidden"),
+        file_filter = function() return true end,
+        onConfirm = function(file_path)
+            logger.dbg("TextEditor: selected file_file_path " .. file_path )
+            self.file_path = file_path
+            self:readFile(self.file_path)
+            self.input:onShowKeyboard()
+        end
+    }
+    UIManager:show(path_chooser)     
 end
 
-function TextEditor:readFile(filepath)
-    logger.dbg(filepath)
-    local file = io.open(filepath, "r")
-    local file = assert(io.open(filepath, "rb"))
+function TextEditor:readFile(file_path)
+    logger.dbg("TextEditor: reading file: " .. file_path)
+    local file = assert(io.open(file_path, "rb"))
     if file then
-        self.file_path = filepath
+        self.file_path = file_path
         self.context = file:read("*all")
         self.input:setInputText(self.context)
         file:close()
     else
-        logger.warn("Failed to read from" .. filepath)
+        UIManager:show(InfoMessage:new{
+            text = _("Failed to read file: " .. file_path)
+        })
     end
+    UIManager:forceRePaint()
+end
 
+function TextEditor:saveFile(file_path)
+    logger.dbg("TextEditor: saving file: " .. file_path)
+    local file = io.open(file_path, "w")
+    if file then
+        file:write(self.input:getInputText())
+        file:close()
+        UIManager:show(InfoMessage:new{
+            text = _("Saved file: " .. file_path)
+        })
+    else
+        UIManager:show(InfoMessage:new{
+            text = _("Failed to save file: " .. file_path)
+        })
+    end
+    UIManager:forceRePaint()
+    UIManager:close(self.input)
+    self.context = ""
+    self.file_path = ""
 end
 
 function TextEditor:addToMainMenu(menu_items)
     menu_items.TextEditor = {
-        text = _("Basic Text editor"),
+        text = _("Basic text editor"),
         callback = function()
             self:start()
         end,
