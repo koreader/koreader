39de1ebeaa17a801dd05a83a8c0c348c1221e9aa
NiLuJe <ninuje@gmail.com>
2018-07-15 20:57:30 +0200

    Revert "Use parent's rectangle when clearing an inputtext"
    This reverts commit def125f840553f2d556123cb4f9679689f5e194f.
    
    Will be solved properly by @poire-z later ;).

--- frontend/ui/widget/inputtext.lua
+++ frontend/ui/widget/inputtext.lua
@@ -315,7 +315,7 @@ function InputText:clear()
     self.charpos = nil
     self:initTextBox("")
     UIManager:setDirty(self.parent, function()
-        return "ui", self.parent.dimen
+        return "ui", self[1][1].dimen
     end)
 end
 
