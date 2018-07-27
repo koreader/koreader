da4af13ce89e7b5cf975179661877bc39ca057d5
NiLuJe <ninuje@gmail.com>
2018-07-27 17:29:25 +0200

    On the off-chance this actually takes, ondemand is probably a safer default

--- platform/kobo/koreader.sh
+++ platform/kobo/koreader.sh
@@ -9,7 +9,7 @@ cd "${KOREADER_DIR}" || exit
 
 # Switch to a sensible CPUFreq governor, even if the HW appears not to give an actual fuck about this...
 ORIG_CPUFREQ_GOV="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)"
-echo "conservative" >"/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+echo "ondemand" >"/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 
 # update to new version from OTA directory
 ko_update_check() {
