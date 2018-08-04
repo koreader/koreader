0da9e752b675478104088b755c6860e0d08ed775
poire-z <poire-z@users.noreply.github.com>
2018-08-04 14:24:06 +0200

    small optimisation

--- frontend/ui/widget/inputdialog.lua
+++ frontend/ui/widget/inputdialog.lua
@@ -185,6 +185,7 @@ function InputDialog:init()
         self.movable = false
         self.border_size = 0
         self.width = Screen:getWidth() - 2*self.border_size
+        self.covers_fullscreen = true -- hint for UIManager:_repaint()
     else
         self.width = self.width or Screen:getWidth() * 0.8
     end
