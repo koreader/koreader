5eff127cf6d23520ee13e79600a67036baa7eb5c
Frans de Jonge <fransdejonge@gmail.com>
2018-08-19 20:20:00 +0200

    [fix, lang] Typo in Evernotes description (#4173)

--- plugins/evernote.koplugin/_meta.lua
+++ plugins/evernote.koplugin/_meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     name = "evernote",
     fullname = _("Evernote"),
-    description = _([[Exports hightlights and notes to the Evernote cloud.]]),
+    description = _([[Exports highlights and notes to the Evernote cloud.]]),
 }
