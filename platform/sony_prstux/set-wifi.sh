41eae99750bed1fb3cf093022a38f1d75c909803
Matias Nitsche <mnitsche@dc.uba.ar>
2018-09-05 22:17:51 -0300

    fix coding style

--- platform/sony_prstux/set-wifi.sh
+++ platform/sony_prstux/set-wifi.sh
@@ -2,13 +2,13 @@
 
 set -x
 if [ "$1" = "on" ]; then
-  wmiconfig -i wlan0 --wlan enable
-  wmiconfig -i wlan0 --setreassocmode 0
-  wmiconfig -i wlan0 --power maxperf
-  /sbin/wpa_supplicant -B -i wlan0 -D wext -C /var/run/wpa_supplicant -f /var/log/wpa_supplicant.log
+    wmiconfig -i wlan0 --wlan enable
+    wmiconfig -i wlan0 --setreassocmode 0
+    wmiconfig -i wlan0 --power maxperf
+    /sbin/wpa_supplicant -B -i wlan0 -D wext -C /var/run/wpa_supplicant -f /var/log/wpa_supplicant.log
 else
-  if [ "$(pidof wpa_supplicant)" != "" ]; then
-    kill $(pidof wpa_supplicant)
-  fi
-  wmiconfig -i wlan0 --wlan disable 
+    if [ "$(pidof wpa_supplicant)" != "" ]; then
+        kill "$(pidof wpa_supplicant)"
+    fi
+    wmiconfig -i wlan0 --wlan disable 
 fi
