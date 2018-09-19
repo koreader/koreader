koreader/koreader

commit 850be52177a08114013fa6af11a36f1e4081918a

Keep some menus open when Tap or Hold (#4189)
TouchMenu: added options to menu items with the following defaults:
    keep_menu_open = false
    hold_keep_menu_open = true
So, default for Tap callback is to close menu, and for Hold callback
to keep menu open.
In both cases, provide the TouchMenu instance as the 1st argument to
the callback functions (instead of a refresh_menu_func I added in #3941)
so the callback can do more things, like closing, refreshing,
changing menu items text and re-ordering...

ReaderZooming: show symbol for default (like it was done for
ReaderFont, ReaderHyphenation...)
TextEditor plugin: update the previously opened files list in real
time, so the menu can be kept open and used as the TextEditor main
interface.
SSH plugin: keep menu open and update the Start/Stop state in real time
ReadTimer plugin: tried to do what feels right (but I don't use it)

Also remove forgotten cp in the move/paste file code

frontend/ui/widget/touchmenu.lua
@@ -693,7 +693,9 @@ function TouchMenu:onMenuSelect(item)
         self.touch_menu_callback()
     end
     if item.tap_input or type(item.tap_input_func) == "function" then
-        self:closeMenu()
+        if not item.keep_menu_open then
+            self:closeMenu()
+        end
         if item.tap_input then
             self:onInput(item.tap_input)
         else
@@ -714,10 +716,14 @@ function TouchMenu:onMenuSelect(item)
                 -- put stuff in scheduler so we can see
                 -- the effect of inverted menu item
                 UIManager:tickAfterNext(function()
+                    -- Provide callback with us, so it can call our
+                    -- closemenu() or updateItems() when it sees fit
+                    -- (if not providing checked or checked_fund, caller
+                    -- must set keep_menu_open=true if that is wished)
                     callback(self)
                     if refresh then
                         self:updateItems()
-                    else
+                    elseif not item.keep_menu_open then
                         self:closeMenu()
                     end
                 end)
@@ -737,7 +743,9 @@ function TouchMenu:onMenuHold(item)
         self.touch_menu_callback()
     end
     if item.hold_input or type(item.hold_input_func) == "function" then
-        self:closeMenu()
+        if item.hold_keep_menu_open == false then
+            self:closeMenu()
+        end
         if item.hold_input then
             self:onInput(item.hold_input)
         else
@@ -750,12 +758,15 @@ function TouchMenu:onMenuHold(item)
         end
         if callback then
             UIManager:tickAfterNext(function()
-                if item.hold_may_update_menu then
-                    callback(function() self:updateItems() end)
-                else
+                -- With hold, the default is to keep menu open, as we're
+                -- most often showing a ConfirmBox that can be cancelled
+                -- (provide hold_keep_menu_open=false to override)
+                if item.hold_keep_menu_open == false then
                     self:closeMenu()
-                    callback()
                 end
+                -- Provide callback with us, so it can call our
+                -- closemenu() or updateItems() when it sees fit
+                callback(self)
             end)
         end
     elseif item.help_text or type(item.help_text_func) == "function" then

frontend/apps/filemanager/filemanager.lua
@@ -637,7 +637,6 @@ function FileManager:pasteHere(file)
                     timeout = 2,
                 })
             end
-            util.execute(self.cp_bin, "-r", orig, dest)
         end
 
         local info_file

frontend/apps/filemanager/filemanagermenu.lua
@@ -115,6 +115,12 @@ function FileManagerMenu:setUpdateItemTable()
     }
     self.menu_items.items_per_page = {
         text = _("Items per page"),
+        help_text = _([[This sets the number of items per page in:
+- File browser and history in 'classic' display mode
+- File and directory selection
+- Table of content
+- Bookmarks list]]),
+        keep_menu_open = true,
         callback = function()
             local SpinWidget = require("ui/widget/spinwidget")
             local curr_items = G_reader_settings:readSetting("items_per_page") or 14

frontend/apps/reader/modules/readerdictionary.lua
@@ -228,6 +228,7 @@ function ReaderDictionary:addToMainMenu(menu_items)
             },
             {
                 text = _("Info on dictionary order"),
+                keep_menu_open = true,
                 callback = function()
                     UIManager:show(InfoMessage:new{
                         text = T(_(
@@ -262,6 +263,7 @@ function ReaderDictionary:addToMainMenu(menu_items)
             },
             {
                 text = _("Clean dictionary lookup history"),
+                keep_menu_open = true,
                 callback = function()
                     UIManager:show(ConfirmBox:new{
                         text = _("Clean dictionary lookup history?"),

frontend/apps/reader/modules/readerfont.lua
@@ -68,9 +68,8 @@ function ReaderFont:init()
             callback = function()
                 self:setFont(v)
             end,
-            hold_may_update_menu = true,
-            hold_callback = function(refresh_menu_func)
-                self:makeDefault(v, refresh_menu_func)
+            hold_callback = function(touchmenu_instance)
+                self:makeDefault(v, touchmenu_instance)
             end,
             checked_func = function()
                 return v == self.font_face
@@ -310,14 +309,14 @@ function ReaderFont:setFont(face)
     end
 end
 
-function ReaderFont:makeDefault(face, refresh_menu_func)
+function ReaderFont:makeDefault(face, touchmenu_instance)
     if face then
         UIManager:show(MultiConfirmBox:new{
             text = T( _("Would you like %1 to be used as the default font (★), or the fallback font (�)?\n\nCharacters not found in the active font are shown in the fallback font instead."), face),
             choice1_text = _("Default"),
             choice1_callback = function()
                 G_reader_settings:saveSetting("cre_font", face)
-                if refresh_menu_func then refresh_menu_func() end
+                if touchmenu_instance then touchmenu_instance:updateItems() end
             end,
             choice2_text = _("Fallback"),
             choice2_callback = function()
@@ -325,7 +324,7 @@ function ReaderFont:makeDefault(face, refresh_menu_func)
                     G_reader_settings:saveSetting("fallback_font", face)
                     self.ui:handleEvent(Event:new("UpdatePos"))
                 end
-                if refresh_menu_func then refresh_menu_func() end
+                if touchmenu_instance then touchmenu_instance:updateItems() end
             end,
         })
     end

frontend/apps/reader/modules/readerhighlight.lua
@@ -103,7 +103,9 @@ function ReaderHighlight:genHighlightDrawerMenu()
             callback = function()
                 self.view.highlight.disabled = not self.view.highlight.disabled
             end,
-            hold_callback = function() self:makeDefault(not self.view.highlight.disabled) end,
+            hold_callback = function(touchmenu_instance)
+                self:makeDefault(not self.view.highlight.disabled)
+            end,
         },
         get_highlight_style("lighten"),
         get_highlight_style("underscore"),
@@ -736,7 +738,6 @@ function ReaderHighlight:makeDefault(highlight_disabled)
             G_reader_settings:saveSetting("highlight_disabled", highlight_disabled)
         end,
     })
-    self.view.highlight.disabled = highlight_disabled
 end
 
 return ReaderHighlight

frontend/apps/reader/modules/readerhyphenation.lua
@@ -112,8 +112,7 @@ function ReaderHyphenation:init()
                     -- signal readerrolling to update pos in new height, and redraw page
                     self.ui:handleEvent(Event:new("UpdatePos"))
                 end,
-                hold_may_update_menu = true,
-                hold_callback = function(refresh_menu_func)
+                hold_callback = function(touchmenu_instance)
                     UIManager:show(MultiConfirmBox:new{
                         -- No real need for a way to remove default one, we can just
                         -- toggle between setting a default OR a fallback (if a default
@@ -125,13 +124,13 @@ function ReaderHyphenation:init()
                         choice1_callback = function()
                             G_reader_settings:saveSetting("hyph_alg_default", v.filename)
                             G_reader_settings:delSetting("hyph_alg_fallback")
-                            if refresh_menu_func then refresh_menu_func() end
+                            if touchmenu_instance then touchmenu_instance:updateItems() end
                         end,
                         choice2_text = _("Fallback"),
                         choice2_callback = function()
                             G_reader_settings:saveSetting("hyph_alg_fallback", v.filename)
                             G_reader_settings:delSetting("hyph_alg_default")
-                            if refresh_menu_func then refresh_menu_func() end
+                            if touchmenu_instance then touchmenu_instance:updateItems() end
                         end,
                     })
                 end,

frontend/apps/reader/modules/readerlink.lua
@@ -154,13 +154,15 @@ If any of the other Swipe to follow link options is enabled, this will work only
         text = _("Go back to previous location"),
         enabled_func = function() return #self.location_stack > 0 end,
         callback = function() self:onGoBackLink() end,
-        hold_callback = function() UIManager:show(ConfirmBox:new{
-            text = _("Clear location history?"),
-            ok_text = _("Clear"),
-            ok_callback = function()
-                self.location_stack = {}
-            end,
-        })
+        hold_callback = function(touchmenu_instance)
+            UIManager:show(ConfirmBox:new{
+                text = _("Clear location history?"),
+                ok_text = _("Clear"),
+                ok_callback = function()
+                    self.location_stack = {}
+                    touchmenu_instance:closeMenu()
+                end,
+            })
         end,
     }
 end

commit a0f69e6e75683b62768d119c12864afa118d0ba5

Don't optimize away a touchmenu unhighlight when we need it
Fix #4234
(Regression after #4189, which, incidentally, gave us a neat way to
handle the issue, which was theoretically present well before that ;)).

frontend/ui/widget/touchmenu.lua
@@ -152,14 +152,15 @@ function TouchMenuItem:onTapSelect(arg, ges)
         UIManager:tickAfterNext(function()
             self.menu:onMenuSelect(self.item)
             self.item_frame.invert = false
-            --[[
-            -- NOTE: We can optimize that repaint away, every entry in our menu will make at least the menu repaint just after anyways ;).
-            --       Plus, leaving that unhighlight as "fast" can lead to weird side-effects, depending on devices.
-            --       If it turns out this need to go back in, consider switching it to "ui".
-            UIManager:setDirty(self.show_parent, function()
-                return "fast", self.dimen
-            end)
-            --]]
+            -- NOTE: We can *usually* optimize that repaint away, as most entries in the menu will at least trigger a menu repaint ;).
+            --       But when stuff doesn't repaint the menu and keeps it open, we need to do it.
+            --       Since it's an *un*highlight containing text, we make it "ui" and not "fast", both so it won't mangle text,
+            --       and because "fast" can have some weird side-effects on some devices in this specific instance...
+            if self.item.hold_keep_menu_open or self.item.keep_menu_open then
+                UIManager:setDirty(self.show_parent, function()
+                    return "ui", self.dimen
+                end)
+            end
         end)
     end
     return true