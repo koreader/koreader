f71627cf1de0395da03efa581d70761b6b123d67
Frans de Jonge <fransdejonge@gmail.com>
2018-11-07 20:41:22 +0100

    [spec] Version: slight refactor

--- spec/unit/version_spec.lua
+++ spec/unit/version_spec.lua
@@ -13,48 +13,50 @@ describe("Version module", function()
         assert.is_true(4 == year:len())
         assert.is_true(2 == month:len())
     end)
-    it("should get normalized current version", function()
-        assert.is_true(12 == tostring(Version:getNormalizedCurrentVersion()):len())
-    end)
-    it("should get normalized version", function()
-        local rev = "v2015.11-982-g704d4238"
-        local version, commit = Version:getNormalizedVersion(rev)
-        local expected_version = 201511000982
-        local expected_commit = "704d4238"
-        assert.are.same(expected_version, version)
-        assert.are.same(expected_commit, commit)
-    end)
-    it("should get normalized version with four number revision", function()
-        local rev = "v2015.11-1755-gecd7b5b_2018-07-02"
-        local version, commit = Version:getNormalizedVersion(rev)
-        local expected_version = 201511001755
-        local expected_commit = "ecd7b5b"
-        assert.are.same(expected_version, version)
-        assert.are.same(expected_commit, commit)
-    end)
-    it("should get normalized stable version", function()
-        local rev = "v2018.11"
-        local version, commit = Version:getNormalizedVersion(rev)
-        local expected_version = 201811000000
-        local expected_commit = nil
-        assert.are.same(expected_version, version)
-        assert.are.same(expected_commit, commit)
-    end)
-    it("should get normalized stable point release version", function()
-        local rev = "v2018.11.1"
-        local version, commit = Version:getNormalizedVersion(rev)
-        local expected_version = 201811010000
-        local expected_commit = nil
-        assert.are.same(expected_version, version)
-        assert.are.same(expected_commit, commit)
-    end)
-    it("should get normalized point release nightly version", function()
-        local rev = "v2018.11.1-1755-gecd7b5b_2018-07-02"
-        local version, commit = Version:getNormalizedVersion(rev)
-        local expected_version = 201811011755
-        local expected_commit = "ecd7b5b"
-        assert.are.same(expected_version, version)
-        assert.are.same(expected_commit, commit)
+    describe("normalized", function()
+        it("should get current version", function()
+            assert.is_true(12 == tostring(Version:getNormalizedCurrentVersion()):len())
+        end)
+        it("should get version", function()
+            local rev = "v2015.11-982-g704d4238"
+            local version, commit = Version:getNormalizedVersion(rev)
+            local expected_version = 201511000982
+            local expected_commit = "704d4238"
+            assert.are.same(expected_version, version)
+            assert.are.same(expected_commit, commit)
+        end)
+        it("should get version with four number revision", function()
+            local rev = "v2015.11-1755-gecd7b5b_2018-07-02"
+            local version, commit = Version:getNormalizedVersion(rev)
+            local expected_version = 201511001755
+            local expected_commit = "ecd7b5b"
+            assert.are.same(expected_version, version)
+            assert.are.same(expected_commit, commit)
+        end)
+        it("should get stable version", function()
+            local rev = "v2018.11"
+            local version, commit = Version:getNormalizedVersion(rev)
+            local expected_version = 201811000000
+            local expected_commit = nil
+            assert.are.same(expected_version, version)
+            assert.are.same(expected_commit, commit)
+        end)
+        it("should get stable point release version", function()
+            local rev = "v2018.11.1"
+            local version, commit = Version:getNormalizedVersion(rev)
+            local expected_version = 201811010000
+            local expected_commit = nil
+            assert.are.same(expected_version, version)
+            assert.are.same(expected_commit, commit)
+        end)
+        it("should get point release nightly version", function()
+            local rev = "v2018.11.1-1755-gecd7b5b_2018-07-02"
+            local version, commit = Version:getNormalizedVersion(rev)
+            local expected_version = 201811011755
+            local expected_commit = "ecd7b5b"
+            assert.are.same(expected_version, version)
+            assert.are.same(expected_commit, commit)
+        end)
     end)
     it("should fail gracefully", function()
         local version, commit = Version:getNormalizedVersion()
