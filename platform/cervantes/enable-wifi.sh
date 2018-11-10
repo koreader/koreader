f3d5b52944466e03b10e5eda600c4a07df29b530
Martín Fdez <paziusss@gmail.com>
2018-11-10 13:17:41 +0100

    wireless: sleep unconditionally, do not launch wpa_supplicant if is already running and a bit of formatting

--- platform/cervantes/enable-wifi.sh
+++ platform/cervantes/enable-wifi.sh
@@ -4,7 +4,7 @@ CTRL_INTERFACE="/var/run/wpa_supplicant"
 
 # create a new configuration if neccesary.
 if [ ! -f "$WPA_SUPPLICANT_CONF" ]; then
-    echo "ctrl_interface=DIR=${CTRL_INTERFACE}" >"$WPA_SUPPLICANT_CONF"
+    echo "ctrl_interface=${CTRL_INTERFACE}" >"$WPA_SUPPLICANT_CONF"
     echo "update_config=1" >>"$WPA_SUPPLICANT_CONF"
     sync
 fi
@@ -14,5 +14,8 @@ if ! lsmod | grep -q 8189fs; then
     sleep 1
 fi
 
-ifconfig eth0 up && sleep 1
-wpa_supplicant -B -D wext -i eth0 -s -O "$CTRL_INTERFACE" -c "$WPA_SUPPLICANT_CONF" 2>/dev/null
+ifconfig eth0 up
+sleep 1
+
+pidof wpa_supplicant >/dev/null \
+    || wpa_supplicant -i eth0 -s -O "$CTRL_INTERFACE" -c "$WPA_SUPPLICANT_CONF" -B -D wext 2>/dev/null
