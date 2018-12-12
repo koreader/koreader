e83f3bfbb92fbe3d1e6738a00ba55938b4979141
Frans de Jonge <fransdejonge@gmail.com>
2018-12-12 21:52:29 +0100

    remove shallow table copy; not needed

--- frontend/util.lua
+++ frontend/util.lua
@@ -139,21 +139,6 @@ function util.tableEquals(o1, o2, ignore_mt)
     return true
 end
 
---- Makes a shallow copy of a table.
-function util.tableShallowCopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in pairs(orig) do
-            copy[orig_key] = orig_value
-        end
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
-end
-
 --- Returns number of keys in a table.
 ---- @param T Lua table
 ---- @treturn int number of keys in table T
