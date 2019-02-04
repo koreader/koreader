d9b973effad2792279d9d83a9f345d0b5b0d75ba
NiLuJe <ninuje@gmail.com>
2019-02-04 03:15:23 +0100

    Don't try to flag a non-toplevel widget as dirty

--- frontend/ui/widget/button.lua
+++ frontend/ui/widget/button.lua
@@ -196,7 +196,7 @@ function Button:onTapSelectButton()
             self[1].invert = true
             -- For most of our buttons, we can't avoid that initial repaint...
             UIManager:widgetRepaint(self[1], self[1].dimen.x, self[1].dimen.y)
-            UIManager:setDirty(self[1], function()
+            UIManager:setDirty(nil, function()
                 return "fast", self[1].dimen
             end)
             -- And we also often have to delay the callback to both see the flash and/or avoid tearing artefacts w/ fast refreshes...
@@ -204,7 +204,7 @@ function Button:onTapSelectButton()
                 self.callback()
                 self[1].invert = false
                 UIManager:widgetRepaint(self[1], self[1].dimen.x, self[1].dimen.y)
-                UIManager:setDirty(self[1], function()
+                UIManager:setDirty(nil, function()
                     return "fast", self[1].dimen
                 end)
             end)
