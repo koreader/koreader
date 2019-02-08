94e0d6983f86beae6874d7c3e60e275773171f2d
NiLuJe <ninuje@gmail.com>
2019-02-08 16:58:22 +0100

    w/ a comment

--- frontend/apps/reader/modules/readerview.lua
+++ frontend/apps/reader/modules/readerview.lua
@@ -218,6 +218,7 @@ function ReaderView:paintTo(bb, x, y)
     self.dialog.dithered = nil
     -- For KOpt, let the user choose.
     if self.ui.document.info.has_pages then
+        -- Also enforce dithering in PicDocument
         if self.ui.document.is_pic or self.document.configurable.hw_dithering == 1 then
             self.dialog.dithered = true
         end
