1417ea94c29adb538d3d74b95023246049707a7d
Matias Nitsche <mnitsche@dc.uba.ar>
2019-02-08 19:13:39 -0300

    PRSTUX: wifi/suspend fixes

--- platform/sony-prstux/set-wifi.sh
+++ platform/sony-prstux/set-wifi.sh
@@ -5,10 +5,9 @@ if [ "$1" = "on" ]; then
     wmiconfig -i wlan0 --wlan enable
     wmiconfig -i wlan0 --setreassocmode 0
     wmiconfig -i wlan0 --power maxperf
-    /sbin/wpa_supplicant -B -i wlan0 -D wext -C /var/run/wpa_supplicant -f /var/log/wpa_supplicant.log
+    echo "WiFi Enabled"
 else
-    if [ "$(pidof wpa_supplicant)" != "" ]; then
-        kill "$(pidof wpa_supplicant)"
-    fi
+    wmiconfig -i wlan0 --abortscan
     wmiconfig -i wlan0 --wlan disable
+    echo "Wifi Disabled"
 fi
