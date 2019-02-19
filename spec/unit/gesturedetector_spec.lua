41825a8f167f5e2607f080234a0db54ce853a2b1
Frans de Jonge <fransdejonge@gmail.com>
2019-02-19 07:21:26 +0100

    expand slightly

--- spec/unit/gesturedetector_spec.lua
+++ spec/unit/gesturedetector_spec.lua
@@ -1,22 +1,48 @@
 describe("gesturedetector module", function()
-    local GestureDetector, util
+    local GestureDetector
     setup(function()
         require("commonrequire")
         GestureDetector = require("device/gesturedetector")
-        util = require("util")
     end)
 
-    it("should translate on rotation", function()
-        local ges = {
-            ges = "swipe",
-            direction = "north",
-        }
-        GestureDetector.screen = {}
-        GestureDetector.screen.cur_rotation_mode = 1
-        assert.is_true(util.tableEquals({
-            ges = "swipe",
-            direction = "east",
-        }, GestureDetector:adjustGesCoordinate(ges)))
-    end)
+    describe("adjustGesCoordinate", function()
+        it("should not translate rotation 0", function()
+            local ges = {
+                ges = "swipe",
+                direction = "north",
+            }
+            GestureDetector.screen = {}
+            GestureDetector.screen.cur_rotation_mode = 0
+            assert.is_equal("north", GestureDetector:adjustGesCoordinate(ges).direction)
 
+            ges.ges = ""
+        end)
+        it("should translate rotation 90", function()
+            local ges = {
+                ges = "swipe",
+                direction = "north",
+            }
+            GestureDetector.screen = {}
+            GestureDetector.screen.cur_rotation_mode = 3
+            assert.is_equal("west", GestureDetector:adjustGesCoordinate(ges).direction)
+        end)
+        it("should translate rotation 180", function()
+            local ges = {
+                ges = "swipe",
+                direction = "north",
+            }
+            GestureDetector.screen = {}
+            GestureDetector.screen.cur_rotation_mode = 2
+            assert.is_equal("south", GestureDetector:adjustGesCoordinate(ges).direction)
+        end)
+        it("should translate rotation 270", function()
+            local ges = {
+                ges = "swipe",
+                direction = "north",
+            }
+            GestureDetector.screen = {}
+            GestureDetector.screen.cur_rotation_mode = 1
+            assert.is_equal("east", GestureDetector:adjustGesCoordinate(ges).direction)
+        end)
+    end)
 end)
