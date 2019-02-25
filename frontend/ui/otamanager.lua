6ddba113927e91add92445695e22903f154bf395
Frans de Jonge <fransdejonge@gmail.com>
2019-02-25 22:50:01 +0100

    [UX] Alternative update message when ota_version < local_version (#4661)
    See https://github.com/koreader/koreader/issues/4650#issuecomment-466965983 and https://github.com/koreader/koreader/pull/4661#issuecomment-467088984

--- frontend/ui/otamanager.lua
+++ frontend/ui/otamanager.lua
@@ -211,14 +211,21 @@ function OTAManager:fetchAndProcessUpdate()
             text = T(_("Update information not available on %1 channel."), channel),
         })
     elseif ota_version then
+        local update_message = T(_("Do you want to update?\nInstalled version: %1\nAvailable version: %2"),
+                                 local_version,
+                                 ota_version)
+        local update_ok_text = _("Update")
+        if ota_version < local_version then
+            update_message =  T(_("The currently installed version is newer than the available version.\nWould you still like to continue and downgrade?\nInstalled version: %1\nAvailable version: %2"),
+                                local_version,
+                                ota_version)
+            update_ok_text = _("Downgrade")
+        end
+
         if OTAManager:getOTAType() == "link" then
             UIManager:show(ConfirmBox:new{
-                text = T(
-                    _("Do you want to update?\nInstalled version: %1\nAvailable version: %2"),
-                    local_version,
-                    ota_version
-                ),
-                ok_text = _("Update"),
+                text = update_message,
+                ok_text = update_ok_text,
                 ok_callback = function()
                     local isAndroid, android = pcall(require, "android")
                     if isAndroid then
@@ -235,12 +242,8 @@ function OTAManager:fetchAndProcessUpdate()
             })
         else
             UIManager:show(ConfirmBox:new{
-                text = T(
-                    _("Do you want to update?\nInstalled version: %1\nAvailable version: %2"),
-                    local_version,
-                    ota_version
-                ),
-                ok_text = _("Update"),
+                text = update_message,
+                ok_text = update_ok_text,
                 ok_callback = function()
                     UIManager:show(InfoMessage:new{
                         text = _("Downloading may take several minutes…"),
