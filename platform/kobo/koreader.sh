e278195c8e7ae3e82b45c9d33c9aefc0c47c01b8
NiLuJe <ninuje@gmail.com>
2019-03-12 20:08:48 +0100

    Explain the rota trickery

--- platform/kobo/koreader.sh
+++ platform/kobo/koreader.sh
@@ -221,6 +221,7 @@ fi
 if [ "${PRODUCT}" = "frost" ]; then
     # Only needed for KSM, pickel -> Nickel will restore its own rota properly
     if [ "${FROM_NICKEL}" != "true" ]; then
+        # The Forma kernels inverts odd orientations, counteract that, àla Plato because one-liner ;p.
         echo "$(((4 - ORIG_FB_ROTA) % 4))" >/sys/class/graphics/fb0/rotate
         # Sleep a bit, for good measure
         usleep 150000
