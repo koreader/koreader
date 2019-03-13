12182931ac204894cfabc608b4720e821a4404f2
Frans de Jonge <fransdejonge@gmail.com>
2019-03-13 14:28:17 +0100

    remove that symlink business

--- .ci/test.sh
+++ .ci/test.sh
@@ -9,7 +9,4 @@ pushd koreader-emulator-x86_64-linux-gnu/koreader && {
     BUSTED_SPEC_FILE="$(circleci tests glob "spec/front/unit/*_spec.lua" | circleci tests split --split-by=timings --timings-type=filename | tr '\n' ' ')"
 } && popd || exit
 
-# symlink to prevent trouble finding the lib on Ubuntu 16.04 in the Docker image
-ln -sf /usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0 koreader-emulator-x86_64-linux-gnu/koreader/libs/libSDL2.so
-
 make testfront BUSTED_SPEC_FILE="${BUSTED_SPEC_FILE}"
