942f3ed5e4b06e70606e29216c4d72a287e03717
Frans de Jonge <fransdejonge@gmail.com>
2019-03-13 14:46:20 +0100

    [chore, CI] Update to Docker koappimage 0.1.3 for Ubuntu 16.04 (#4296)

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -2,14 +2,14 @@
 
 # don't do this for clang
 if [ "$CXX" = "g++" ]; then
-    export CXX="g++-4.8" CC="gcc-4.8"
+    export CXX="g++-5" CC="gcc-5"
 fi
 # in case anything ignores the environment variables, override through PATH
 mkdir bin
-ln -s "$(which gcc-4.8)" bin/cc
-ln -s "$(which gcc-4.8)" bin/gcc
+ln -s "$(which gcc-5)" bin/cc
+ln -s "$(which gcc-5)" bin/gcc
 ln -s "$(which c++)" bin/c++
-ln -s "$(which g++-4.8)" bin/g++
+ln -s "$(which g++-5)" bin/g++
 
 # Travis only makes a shallow clone of --depth=50. KOReader is small enough that
 # we can just grab it all. This is necessary to generate the version number,
