c80d075ba45963bb37fdf01b959cd5f2569f1df5
NiLuJe <ninuje@gmail.com>
2019-04-05 22:04:42 +0200

    Okay, this passes.
    And it almost makes sense.

--- spec/unit/frontlight_spec.lua
+++ spec/unit/frontlight_spec.lua
@@ -1,30 +1,49 @@
 describe("Frontlight function in PowerD", function()
-    local PowerD
+    local Device, PowerD
     local param, test_when_on, test_when_off
     setup(function()
         require("commonrequire")
+        package.unloadAll()
+        require("document/canvascontext"):init(require("device"))
+
+        PowerD = require("device/generic/powerd"):new{
+            frontlight = 2,
+        }
 
         param = {
-            frontlight = 2,
             fl_min = 1,
             fl_max = 5,
             fl_intensity = 2,
-            device = {
-                hasFrontlight = function() return true end,
-            },
+            device = nil,
             is_fl_on = true,
         }
 
-        PowerD = require("device/generic/powerd"):new{ param }
-    end)
-
-    before_each(function()
-        stub(PowerD, "init")
-        stub(PowerD, "frontlightIntensityHW")
-        stub(PowerD, "setIntensityHW")
+        PowerD.frontlightIntensityHW = function(self)
+            return self.frontlight
+        end
         PowerD.setIntensityHW = function(self, intensity)
             self.frontlight = intensity
         end
+    end)
+
+    teardown(function()
+        package.unloadAll()
+        require("document/canvascontext"):init(require("device"))
+    end)
+
+    before_each(function()
+        Device = require("device")
+        Device.isKobo = function() return true end
+        Device.model = "Kobo_dahlia"
+        Device.hasFrontlight = function() return true end
+        param.device = Device
+        Device.powerd = PowerD:new{
+            param
+        }
+
+
+        stub(PowerD, "init")
+        spy.on(PowerD, "frontlightIntensityHW")
         spy.on(PowerD, "setIntensityHW")
         spy.on(PowerD, "turnOnFrontlightHW")
         spy.on(PowerD, "turnOffFrontlightHW")
@@ -36,7 +55,7 @@ describe("Frontlight function in PowerD", function()
         assert.are.equal(2, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOn())
         assert.stub(p.init).is_called(1)
-        assert.stub(p.frontlightIntensityHW).is_called(1)
+        assert.spy(p.frontlightIntensityHW).is_called(2)
     end)
 
     test_when_off = function(fl_min)
@@ -45,36 +64,35 @@ describe("Frontlight function in PowerD", function()
         local p = PowerD:new(param)
         p:setIntensity(0)
         assert.are.equal(param.fl_min, p:frontlightIntensityHW())
-        assert.are.equal(param.fl_min, p:frontlightIntensity())
+        assert.are.equal(0, p:frontlightIntensity()) -- returns 0 when off
         assert.is.truthy(p:isFrontlightOff())
         assert.stub(p.init).is_called(1)
-        assert.stub(p.setIntensityHW).is_called(1)
+        assert.spy(p.setIntensityHW).is_called(1)
         assert.are.equal(param.fl_min, p.frontlight)
-        assert.stub(p.frontlightIntensityHW).is_called(1)
+        assert.spy(p.frontlightIntensityHW).is_called(2)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(0)
 
         -- The intensity is param.fl_min, turnOnFrontlight() should take no effect.
-        assert.is.truthy(p:setIntensity(param.fl_min))
         assert.is.falsy(p:turnOnFrontlight())
-        assert.are.equal(param.fl_min, p:frontlightIntensity())
+        assert.are.equal(0, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOff())
-        assert.stub(p.setIntensityHW).is_called(2)
+        assert.spy(p.setIntensityHW).is_called(1)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(0)
 
         -- Same as the above one, toggleFrontlight() should also take no effect.
         assert.is.falsy(p:toggleFrontlight())
-        assert.are.equal(param.fl_min, p:frontlightIntensity())
+        assert.are.equal(0, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOff())
-        assert.stub(p.setIntensityHW).is_called(2)
+        assert.spy(p.setIntensityHW).is_called(1)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(0)
 
         assert.is.truthy(p:setIntensity(2))
         assert.are.equal(2, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOn())
-        assert.stub(p.setIntensityHW).is_called(3)
+        assert.spy(p.setIntensityHW).is_called(2)
         assert.are.equal(2, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(0)
@@ -82,14 +100,14 @@ describe("Frontlight function in PowerD", function()
         assert.is.falsy(p:turnOnFrontlight())
         assert.are.equal(2, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOn())
-        assert.stub(p.setIntensityHW).is_called(3)
+        assert.spy(p.setIntensityHW).is_called(2)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(0)
 
         assert.is.truthy(p:turnOffFrontlight())
         assert.are.equal(0, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOff())
-        assert.stub(p.setIntensityHW).is_called(4)
+        assert.spy(p.setIntensityHW).is_called(3)
         assert.are.equal(param.fl_min, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(1)
@@ -97,7 +115,7 @@ describe("Frontlight function in PowerD", function()
         assert.is.truthy(p:turnOnFrontlight())
         assert.are.equal(2, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOn())
-        assert.stub(p.setIntensityHW).is_called(5)
+        assert.spy(p.setIntensityHW).is_called(4)
         assert.are.equal(2, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(1)
         assert.spy(p.turnOffFrontlightHW).is_called(1)
@@ -105,7 +123,7 @@ describe("Frontlight function in PowerD", function()
         assert.is.truthy(p:toggleFrontlight())
         assert.are.equal(0, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOff())
-        assert.stub(p.setIntensityHW).is_called(6)
+        assert.spy(p.setIntensityHW).is_called(5)
         assert.are.equal(param.fl_min, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(1)
         assert.spy(p.turnOffFrontlightHW).is_called(2)
@@ -113,7 +131,7 @@ describe("Frontlight function in PowerD", function()
         assert.is.truthy(p:toggleFrontlight())
         assert.are.equal(2, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOn())
-        assert.stub(p.setIntensityHW).is_called(7)
+        assert.spy(p.setIntensityHW).is_called(6)
         assert.are.equal(2, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(2)
         assert.spy(p.turnOffFrontlightHW).is_called(2)
@@ -129,7 +147,7 @@ describe("Frontlight function in PowerD", function()
         assert.are.equal(2, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOn())
         assert.stub(p.init).is_called(1)
-        assert.stub(p.setIntensityHW).is_called(1)
+        --assert.spy(p.setIntensityHW).is_called(1)
         assert.are.equal(2, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(0)
@@ -137,7 +155,7 @@ describe("Frontlight function in PowerD", function()
         assert.is.falsy(p:setIntensity(2))
         assert.are.equal(2, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOn())
-        assert.stub(p.setIntensityHW).is_called(1)
+        --assert.spy(p.setIntensityHW).is_called(1)
         assert.are.equal(2, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(0)
@@ -145,7 +163,7 @@ describe("Frontlight function in PowerD", function()
         assert.is.falsy(p:turnOnFrontlight())
         assert.are.equal(2, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOn())
-        assert.stub(p.setIntensityHW).is_called(1)
+        --assert.spy(p.setIntensityHW).is_called(1)
         assert.are.equal(2, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(0)
@@ -153,7 +171,7 @@ describe("Frontlight function in PowerD", function()
         assert.is.truthy(p:turnOffFrontlight())
         assert.are.equal(0, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOff())
-        assert.stub(p.setIntensityHW).is_called(2)
+        --assert.spy(p.setIntensityHW).is_called(2)
         assert.are.equal(param.fl_min, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(0)
         assert.spy(p.turnOffFrontlightHW).is_called(1)
@@ -161,7 +179,7 @@ describe("Frontlight function in PowerD", function()
         assert.is.truthy(p:toggleFrontlight())
         assert.are.equal(2, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOn())
-        assert.stub(p.setIntensityHW).is_called(3)
+        --assert.spy(p.setIntensityHW).is_called(3)
         assert.are.equal(2, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(1)
         assert.spy(p.turnOffFrontlightHW).is_called(1)
@@ -169,7 +187,7 @@ describe("Frontlight function in PowerD", function()
         assert.is.truthy(p:toggleFrontlight())
         assert.are.equal(0, p:frontlightIntensity())
         assert.is.truthy(p:isFrontlightOff())
-        assert.stub(p.setIntensityHW).is_called(4)
+        --assert.spy(p.setIntensityHW).is_called(4)
         assert.are.equal(param.fl_min, p.frontlight)
         assert.spy(p.turnOnFrontlightHW).is_called(1)
         assert.spy(p.turnOffFrontlightHW).is_called(2)
