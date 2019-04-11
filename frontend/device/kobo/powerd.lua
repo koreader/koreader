koreader/koreader

commit 4005bf69aa5667f520a1e837f9c493874cf192fa

Slightly less crappy Nightmode (#4871)
Companion PR to https://github.com/koreader/koreader-base/pull/884
* Basically flags devices known to be stable when using PxP inversion.
* Plus, random fix for #4870 ;).
* A few FrontLight tweaks & cleanups on Kobo:
  * Moved the Kobo-specific startup status insanity to Kobo-specific init
  * Made turnOff/turnOn frontlight do a smooth ramp down/up
  * On Kobo, use turnOff/turnOn for suspend/resume, to get that smooth toggle
  * On Kobo, for NaturalLight w/ a mixer, only set warmth for setWarmth, and only set Brightness for setBrightness, otherwise, it tried to set both with not in-sync values, which made the FL widget jittery.

frontend/device/kobo/powerd.lua
@@ -112,12 +112,12 @@ function KoboPowerD:init()
     self.initial_is_fl_on = true
     self.autowarmth_job_running = false
 
-    if self.device.hasFrontlight() then
+    if self.device:hasFrontlight() then
         -- If this device has natural light (currently only KA1 & Forma)
         -- Use the SysFS interface, and ioctl otherwise.
         -- NOTE: On the Forma, nickel still appears to prefer using ntx_io to handle the FL,
         --       but it does use sysfs for the NL...
-        if self.device.hasNaturalLight() then
+        if self.device:hasNaturalLight() then
             local nl_config = G_reader_settings:readSetting("natural_light_config")
             if nl_config then
                 for key,val in pairs(nl_config) do
@@ -138,11 +138,20 @@ function KoboPowerD:init()
                 self:_syncKoboLightOnStart()
             end
         end
+        -- See discussion in https://github.com/koreader/koreader/issues/3118#issuecomment-334995879
+        -- for the reasoning behind this bit of insanity.
+        if self:isFrontlightOnHW() then
+            -- Use setIntensity to ensure it sets fl_intensity, and because we don't want the ramping behavior of turnOn
+            self:setIntensity(self:frontlightIntensityHW())
+        else
+            -- Use setIntensityHW so as *NOT* to set fl_intensity, so toggle will still work.
+            self:setIntensityHW(0)
+        end
     end
 end
 
 function KoboPowerD:saveSettings()
-    if self.device.hasFrontlight() then
+    if self.device:hasFrontlight() then
         -- Store BasePowerD values into settings (and not our hw_intensity, so
         -- that if frontlight was toggled off, we save and restore the previous
         -- untoggled intensity and toggle state at next startup)
@@ -199,10 +208,6 @@ function KoboPowerD:isFrontlightOnHW()
         return ret
     end
     return self.hw_intensity > 0
-end
-
-function KoboPowerD:turnOffFrontlightHW()
-    self:_setIntensity(0) -- will call setIntensityHW(0)
 end
 
 function KoboPowerD:setIntensityHW(intensity)
@@ -271,25 +276,52 @@ function KoboPowerD:isChargingHW()
     return self:read_str_file(self.is_charging_file) == "Charging\n"
 end
 
+function KoboPowerD:turnOffFrontlightHW()
+    if self:isFrontlightOff() then
+        return
+    end
+    local util = require("ffi/util")
+    util.runInSubProcess(function()
+        for i = 1,5 do
+            self:_setIntensity(math.floor(self.fl_intensity - ((self.fl_intensity / 5) * i)))
+            -- NOTE: We generally don't need to sleep when using sysfs as a backend...
+            if not self.device:hasNaturalLight() then
+                if (i < 5) then
+                    util.usleep(35 * 1000)
+                end
+            end
+        end
+    end, false, true)
+end
+function KoboPowerD:turnOnFrontlightHW()
+    if self:isFrontlightOn() then
+        return
+    end
+    local util = require("ffi/util")
+    util.runInSubProcess(function()
+        for i = 1,5 do
+            self:_setIntensity(math.ceil(self.fl_min + ((self.fl_intensity / 5) * i)))
+            if not self.device:hasNaturalLight() then
+                if (i < 5) then
+                    util.usleep(35 * 1000)
+                end
+            end
+        end
+    end, false, true)
+end
+
 -- Turn off front light before suspend.
 function KoboPowerD:beforeSuspend()
     if self.fl == nil then return end
-    -- just turn off frontlight without remembering its state
-    self.fl:setBrightness(0)
+    -- Turn off the frontlight
+    self:turnOffFrontlight()
 end
 
 -- Restore front light state after resume.
 function KoboPowerD:afterResume()
     if self.fl == nil then return end
-    -- just re-set it to self.hw_intensity that we haven't change on Suspend
-    if self.fl_warmth == nil then
-        self.fl:setBrightness(self.hw_intensity)
-    else
-        if self.auto_warmth then
-            self:calculateAutoWarmth()
-        end
-        self.fl:setNaturalBrightness(self.hw_intensity, self.fl_warmth)
-    end
+    -- Turn the frontlight back on
+    self:turnOnFrontlight()
 end
 
 return KoboPowerD

frontend/device/cervantes/powerd.lua
@@ -63,8 +63,8 @@ function CervantesPowerD:init()
     self.initial_is_fl_on = true
     self.autowarmth_job_running = false
 
-    if self.device.hasFrontlight() then
-        if self.device.hasNaturalLight() then
+    if self.device:hasFrontlight() then
+        if self.device:hasNaturalLight() then
             local nl_config = G_reader_settings:readSetting("natural_light_config")
             if nl_config then
                 for key,val in pairs(nl_config) do
@@ -86,7 +86,7 @@ function CervantesPowerD:init()
 end
 
 function CervantesPowerD:saveSettings()
-    if self.device.hasFrontlight() then
+    if self.device:hasFrontlight() then
         -- Store BasePowerD values into settings (and not our hw_intensity, so
         -- that if frontlight was toggled off, we save and restore the previous
         -- untoggled intensity and toggle state at next startup)
@@ -119,10 +119,6 @@ function CervantesPowerD:isFrontlightOnHW()
         return ret
     end
     return self.hw_intensity > 0
-end
-
-function CervantesPowerD:turnOffFrontlightHW()
-    self:_setIntensity(0) -- will call setIntensityHW(0)
 end
 
 function CervantesPowerD:setIntensityHW(intensity)

frontend/device/generic/powerd.lua
@@ -17,24 +17,9 @@ function BasePowerD:new(o)
     self.__index = self
     assert(o.fl_min < o.fl_max)
     if o.init then o:init() end
-    if o.device and o.device.hasFrontlight() then
+    if o.device and o.device:hasFrontlight() then
         o.fl_intensity = o:frontlightIntensityHW()
         o:_decideFrontlightState()
-        -- Note added by @Frenzie 2017-10-08
-        -- I believe this should be `if isKobo()`, or better yet that the entire
-        -- block should be moved to `KoboPowerD:init()` because afaik that is the
-        -- only platform where the system doesn't provide trustworthy frontlight
-        -- information. But to be absolutely sure that I don't break anything (and I
-        -- don't want to spend any time on this atm) I'm temporarily excluding only
-        -- Android where this behavior is known to be problematic.
-        -- See discussion in https://github.com/koreader/koreader/issues/3118#issuecomment-334995879
-        if not o.device:isAndroid() then
-            if o:isFrontlightOn() then
-                o:turnOnFrontlightHW()
-            else
-                o:turnOffFrontlightHW()
-            end
-        end
     end
     return o
 end
@@ -63,7 +48,7 @@ end
 
 function BasePowerD:_decideFrontlightState()
     assert(self ~= nil)
-    assert(self.device.hasFrontlight())
+    assert(self.device:hasFrontlight())
     self.is_fl_on = self:isFrontlightOnHW()
 end
 
@@ -73,14 +58,14 @@ end
 
 function BasePowerD:frontlightIntensity()
     assert(self ~= nil)
-    if not self.device.hasFrontlight() then return 0 end
+    if not self.device:hasFrontlight() then return 0 end
     if self:isFrontlightOff() then return 0 end
     return self.fl_intensity
 end
 
 function BasePowerD:toggleFrontlight()
     assert(self ~= nil)
-    if not self.device.hasFrontlight() then return false end
+    if not self.device:hasFrontlight() then return false end
     if self:isFrontlightOn() then
         return self:turnOffFrontlight()
     else
@@ -90,20 +75,20 @@ end
 
 function BasePowerD:turnOffFrontlight()
     assert(self ~= nil)
-    if not self.device.hasFrontlight() then return end
+    if not self.device:hasFrontlight() then return end
     if self:isFrontlightOff() then return false end
-    self.is_fl_on = false
     self:turnOffFrontlightHW()
+    self.is_fl_on = false
     return true
 end
 
 function BasePowerD:turnOnFrontlight()
     assert(self ~= nil)
-    if not self.device.hasFrontlight() then return end
+    if not self.device:hasFrontlight() then return end
     if self:isFrontlightOn() then return false end
     if self.fl_intensity == self.fl_min then return false end
-    self.is_fl_on = true
     self:turnOnFrontlightHW()
+    self.is_fl_on = true
     return true
 end
 
@@ -135,7 +120,7 @@ function BasePowerD:normalizeIntensity(intensity)
 end
 
 function BasePowerD:setIntensity(intensity)
-    if not self.device.hasFrontlight() then return false end
+    if not self.device:hasFrontlight() then return false end
     if intensity == self:frontlightIntensity() then return false end
     self.fl_intensity = self:normalizeIntensity(intensity)
     self:_decideFrontlightState()

frontend/device/kindle/powerd.lua
@@ -15,7 +15,7 @@ function KindlePowerD:init()
 end
 
 function KindlePowerD:frontlightIntensityHW()
-    if not self.device.hasFrontlight() then return 0 end
+    if not self.device:hasFrontlight() then return 0 end
     -- Kindle stock software does not use intensity file directly, so we need to read from its
     -- lipc property first.
     if self.lipc_handle ~= nil then
@@ -73,7 +73,7 @@ function KindlePowerD:_readFLIntensity()
 end
 
 function KindlePowerD:afterResume()
-    if not self.device.hasFrontlight() then
+    if not self.device:hasFrontlight() then
         return
     end
     local UIManager = require("ui/uimanager")

frontend/device/pocketbook/powerd.lua
@@ -22,7 +22,7 @@ function PocketBookPowerD:init()
 end
 
 function PocketBookPowerD:frontlightIntensityHW()
-    if not self.device.hasFrontlight() then return 0 end
+    if not self.device:hasFrontlight() then return 0 end
     return inkview.GetFrontlightState()
 end
 

frontend/device/sony-prstux/powerd.lua
@@ -14,7 +14,7 @@ function SonyPRSTUX_PowerD:init()
 end
 
 function SonyPRSTUX_PowerD:frontlightIntensityHW()
-    if not self.device.hasFrontlight() then return 0 end
+    if not self.device:hasFrontlight() then return 0 end
 end
 
 function SonyPRSTUX_PowerD:setIntensityHW(intensity)

frontend/apps/filemanager/filemanagermenu.lua
@@ -257,7 +257,7 @@ function FileManagerMenu:setUpdateItemTable()
     }
     if Device:isKobo() then
         table.insert(self.menu_items.developer_options.sub_item_table, {
-            text = _("Disable forced 8-bit color space"),
+            text = _("Disable forced 8-bit pixel depth"),
             checked_func = function()
                 return G_reader_settings:isTrue("dev_startup_no_fbdepth")
             end,

frontend/apps/reader/modules/readerfrontlight.lua
@@ -38,7 +38,7 @@ function ReaderFrontLight:init()
 end
 
 function ReaderFrontLight:onAdjust(arg, ges)
-    if not Device.hasFrontlight() then return true end
+    if not Device:hasFrontlight() then return true end
     local powerd = Device:getPowerDevice()
     logger.dbg("frontlight intensity", powerd:frontlightIntensity())
     local step = math.ceil(#self.steps * ges.distance / self.gestureScale)
@@ -65,7 +65,7 @@ function ReaderFrontLight:onAdjust(arg, ges)
 end
 
 function ReaderFrontLight:onShowIntensity()
-    if not Device.hasFrontlight() then return true end
+    if not Device:hasFrontlight() then return true end
     local powerd = Device:getPowerDevice()
     local new_text
     if powerd:isFrontlightOff() then

commit 9b005086ff5da272aa5c70b7061a287e13cbd1f9

Update AutoWarmth state on resume
Regression since #4871
Fix #4895

frontend/device/kobo/powerd.lua
@@ -320,6 +320,10 @@ end
 -- Restore front light state after resume.
 function KoboPowerD:afterResume()
     if self.fl == nil then return end
+    -- Update AutoWarmth state
+    if self.fl_warmth ~= nil and self.auto_warmth then
+        self:calculateAutoWarmth()
+    end
     -- Turn the frontlight back on
     self:turnOnFrontlight()
 end