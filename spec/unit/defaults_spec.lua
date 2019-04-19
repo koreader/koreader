9ccfd6f1877013abdb7833d4ef3db1cefce960fe
NiLuJe <ninuje@gmail.com>
2019-04-19 23:22:17 +0200

    Update defaults tests (+19 keys)

--- spec/unit/defaults_spec.lua
+++ spec/unit/defaults_spec.lua
@@ -8,8 +8,8 @@ describe("defaults module", function()
 
     it("should load all defaults from defaults.lua", function()
         Defaults:init()
-        assert.is_same(94, #Defaults.defaults_name)
-        assert.is_same("DFULL_SCREEN", Defaults.defaults_name[38])
+        assert.is_same(113, #Defaults.defaults_name)
+        assert.is_same("DFULL_SCREEN", Defaults.defaults_name[57])
     end)
 
     it("should save changes to defaults.persistent.lua", function()
@@ -18,17 +18,17 @@ describe("defaults module", function()
 
         -- not in persistent but checked in defaults
         Defaults.changed[21] = true
-        Defaults.changed[29] = true
-        Defaults.changed[38] = true
-        Defaults.changed[73] = true
-        Defaults.changed[89] = true
+        Defaults.changed[48] = true
+        Defaults.changed[57] = true
+        Defaults.changed[92] = true
+        Defaults.changed[108] = true
         Defaults:saveSettings()
-        assert.is_same(94, #Defaults.defaults_name)
-        assert.is_same("DFULL_SCREEN", Defaults.defaults_name[38])
-        assert.is_same("SEARCH_LIBRARY_PATH", Defaults.defaults_name[89])
-        assert.is_same("DTAP_ZONE_BACKWARD", Defaults.defaults_name[73])
-        assert.is_same("DCREREADER_CONFIG_WORD_GAP_LARGE", Defaults.defaults_name[29])
-        assert.is_same("DCREREADER_CONFIG_MARGIN_SIZES_HUGE", Defaults.defaults_name[21])
+        assert.is_same(113, #Defaults.defaults_name)
+        assert.is_same("DFULL_SCREEN", Defaults.defaults_name[57])
+        assert.is_same("SEARCH_LIBRARY_PATH", Defaults.defaults_name[108])
+        assert.is_same("DTAP_ZONE_BACKWARD", Defaults.defaults_name[92])
+        assert.is_same("DCREREADER_CONFIG_WORD_GAP_LARGE", Defaults.defaults_name[48])
+        assert.is_same("DCREREADER_CONFIG_H_MARGIN_SIZES_HUGE", Defaults.defaults_name[21])
         local fd = io.open(persistent_filename, "r")
         assert.Equals(
 [[-- For configuration changes that persists between updates
@@ -41,11 +41,9 @@ DTAP_ZONE_BACKWARD = {
 }
 DCREREADER_CONFIG_WORD_GAP_LARGE = 100
 DFULL_SCREEN = 1
-DCREREADER_CONFIG_MARGIN_SIZES_HUGE = {
-    [1] = 100,
-    [2] = 100,
-    [3] = 100,
-    [4] = 100
+DCREREADER_CONFIG_H_MARGIN_SIZES_HUGE = {
+    [1] = 70,
+    [2] = 70
 }
 ]],
                        fd:read("*a"))
@@ -53,10 +51,10 @@ DCREREADER_CONFIG_MARGIN_SIZES_HUGE = {
 
         -- in persistent
         Defaults:init()
-        Defaults.changed[38] = true
-        Defaults.defaults_value[38] = 2
-        Defaults.changed[73] = true
-        Defaults.defaults_value[73] = {
+        Defaults.changed[57] = true
+        Defaults.defaults_value[57] = 2
+        Defaults.changed[92] = true
+        Defaults.defaults_value[92] = {
             y = 10,
             x = 10.125,
             h = 20.25,
@@ -75,11 +73,9 @@ DTAP_ZONE_BACKWARD = {
 }
 DCREREADER_CONFIG_WORD_GAP_LARGE = 100
 DFULL_SCREEN = 2
-DCREREADER_CONFIG_MARGIN_SIZES_HUGE = {
-    [1] = 100,
-    [2] = 100,
-    [3] = 100,
-    [4] = 100
+DCREREADER_CONFIG_H_MARGIN_SIZES_HUGE = {
+    [1] = 70,
+    [2] = 70
 }
 ]],
                        fd:read("*a"))
@@ -93,11 +89,9 @@ DCREREADER_CONFIG_MARGIN_SIZES_HUGE = {
         fd:write(
 [[-- For configuration changes that persists between updates
 SEARCH_TITLE = true
-DCREREADER_CONFIG_MARGIN_SIZES_LARGE = {
-    [1] = 20,
-    [2] = 20,
-    [3] = 20,
-    [4] = 20
+DCREREADER_CONFIG_H_MARGIN_SIZES_LARGE = {
+    [1] = 15,
+    [2] = 15
 }
 DCREREADER_VIEW_MODE = "page"
 DHINTCOUNT = 2
@@ -106,19 +100,17 @@ DHINTCOUNT = 2
 
         -- in persistent
         Defaults:init()
-        Defaults.changed[38] = true
-        Defaults.defaults_value[38] = 1
+        Defaults.changed[57] = true
+        Defaults.defaults_value[57] = 1
         Defaults:saveSettings()
         fd = io.open(persistent_filename)
         assert.Equals(
 [[-- For configuration changes that persists between updates
 SEARCH_TITLE = true
 DHINTCOUNT = 2
-DCREREADER_CONFIG_MARGIN_SIZES_LARGE = {
-    [1] = 20,
-    [2] = 20,
-    [3] = 20,
-    [4] = 20
+DCREREADER_CONFIG_H_MARGIN_SIZES_LARGE = {
+    [1] = 15,
+    [2] = 15
 }
 DFULL_SCREEN = 1
 DCREREADER_VIEW_MODE = "page"
