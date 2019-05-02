koreader/koreader

commit 888d3591b29259884a4d87f0386e0e31154967d3

Give more control over CRe margins (#4945)
Without having to resort to weird custom defaults.

* Split the current margins setting in three:
  * Horizontal margins (because you generally want those two to be balanced).
  * Top margin & Bottom margin (because you may want to tweak those separately to deal with quirky status bar/final line shenanigans).

* Also, add a "Reclaim bar height from bottom margin" toggle to the status bar menu, to optionally make sure the status bar won't eat into the bottom margin.

* Includes a free fix to diacritics popup refresh handling in the keyboard ;).

frontend/ui/data/strings.lua
@@ -8,7 +8,9 @@ S.PAGE_CROP = _("Page Crop")
 S.FULL_SCREEN = _("Full Screen")
 S.SCROLL_MODE = _("Scroll Mode")
 S.ZOOM_DPI = _("Zoom (dpi)")
-S.PAGE_MARGIN = _("Margin")
+S.H_PAGE_MARGINS = _("L/R Margins")
+S.T_PAGE_MARGIN = _("Top Margin")
+S.B_PAGE_MARGIN = _("Bottom Margin")
 S.LINE_SPACING = _("Line Spacing")
 S.COLUMNS = _("Columns")
 S.TEXT_ALIGN = _("Text Align")

.luacheckrc
@@ -64,14 +64,33 @@ read_globals = {
     "DKOPTREADER_CONFIG_DOC_DEFAULT_LANG_CODE",
     "DCREREADER_CONFIG_FONT_SIZES",
     "DCREREADER_CONFIG_DEFAULT_FONT_SIZE",
-    "DCREREADER_CONFIG_MARGIN_SIZES_SMALL",
-    "DCREREADER_CONFIG_MARGIN_SIZES_MEDIUM",
-    "DCREREADER_CONFIG_MARGIN_SIZES_LARGE",
-    "DCREREADER_CONFIG_MARGIN_SIZES_X_LARGE",
-    "DCREREADER_CONFIG_MARGIN_SIZES_XX_LARGE",
-    "DCREREADER_CONFIG_MARGIN_SIZES_XXX_LARGE",
-    "DCREREADER_CONFIG_MARGIN_SIZES_HUGE",
-    "DCREREADER_CONFIG_MARGIN_SIZES_X_HUGE",
+    "DCREREADER_CONFIG_H_MARGIN_SIZES_SMALL",
+    "DCREREADER_CONFIG_H_MARGIN_SIZES_MEDIUM",
+    "DCREREADER_CONFIG_H_MARGIN_SIZES_LARGE",
+    "DCREREADER_CONFIG_H_MARGIN_SIZES_X_LARGE",
+    "DCREREADER_CONFIG_H_MARGIN_SIZES_XX_LARGE",
+    "DCREREADER_CONFIG_H_MARGIN_SIZES_XXX_LARGE",
+    "DCREREADER_CONFIG_H_MARGIN_SIZES_HUGE",
+    "DCREREADER_CONFIG_H_MARGIN_SIZES_X_HUGE",
+    "DCREREADER_CONFIG_H_MARGIN_SIZES_XX_HUGE",
+    "DCREREADER_CONFIG_T_MARGIN_SIZES_SMALL",
+    "DCREREADER_CONFIG_T_MARGIN_SIZES_MEDIUM",
+    "DCREREADER_CONFIG_T_MARGIN_SIZES_LARGE",
+    "DCREREADER_CONFIG_T_MARGIN_SIZES_X_LARGE",
+    "DCREREADER_CONFIG_T_MARGIN_SIZES_XX_LARGE",
+    "DCREREADER_CONFIG_T_MARGIN_SIZES_XXX_LARGE",
+    "DCREREADER_CONFIG_T_MARGIN_SIZES_HUGE",
+    "DCREREADER_CONFIG_T_MARGIN_SIZES_X_HUGE",
+    "DCREREADER_CONFIG_T_MARGIN_SIZES_XX_HUGE",
+    "DCREREADER_CONFIG_B_MARGIN_SIZES_SMALL",
+    "DCREREADER_CONFIG_B_MARGIN_SIZES_MEDIUM",
+    "DCREREADER_CONFIG_B_MARGIN_SIZES_LARGE",
+    "DCREREADER_CONFIG_B_MARGIN_SIZES_X_LARGE",
+    "DCREREADER_CONFIG_B_MARGIN_SIZES_XX_LARGE",
+    "DCREREADER_CONFIG_B_MARGIN_SIZES_XXX_LARGE",
+    "DCREREADER_CONFIG_B_MARGIN_SIZES_HUGE",
+    "DCREREADER_CONFIG_B_MARGIN_SIZES_X_HUGE",
+    "DCREREADER_CONFIG_B_MARGIN_SIZES_XX_HUGE",
     "DCREREADER_CONFIG_LIGHTER_FONT_GAMMA",
     "DCREREADER_CONFIG_DEFAULT_FONT_GAMMA",
     "DCREREADER_CONFIG_DARKER_FONT_GAMMA",

defaults.lua
@@ -114,15 +114,38 @@ DCREREADER_CONFIG_FONT_SIZES = {12, 16, 20, 22, 24, 26, 28, 30, 34, 38, 44}  -- 
 DCREREADER_CONFIG_DEFAULT_FONT_SIZE = 22    -- default font size
 
 -- crereader margin sizes
--- margin {left, top, right, bottom} in pixels
-DCREREADER_CONFIG_MARGIN_SIZES_SMALL = {5, 10, 5, 10}
-DCREREADER_CONFIG_MARGIN_SIZES_MEDIUM = {10, 15, 10, 15}
-DCREREADER_CONFIG_MARGIN_SIZES_LARGE = {20, 20, 20, 20}
-DCREREADER_CONFIG_MARGIN_SIZES_X_LARGE = {30, 30, 30, 30}
-DCREREADER_CONFIG_MARGIN_SIZES_XX_LARGE = {50, 50, 50, 50}
-DCREREADER_CONFIG_MARGIN_SIZES_XXX_LARGE = {70, 70, 70, 70}
-DCREREADER_CONFIG_MARGIN_SIZES_HUGE = {100, 100, 100, 100}
-DCREREADER_CONFIG_MARGIN_SIZES_X_HUGE = {140, 140, 140, 140}
+-- horizontal margins {left, right} in (relative) pixels
+DCREREADER_CONFIG_H_MARGIN_SIZES_SMALL = {5, 5}
+DCREREADER_CONFIG_H_MARGIN_SIZES_MEDIUM = {10, 10}
+DCREREADER_CONFIG_H_MARGIN_SIZES_LARGE = {15, 15}
+DCREREADER_CONFIG_H_MARGIN_SIZES_X_LARGE = {20, 20}
+DCREREADER_CONFIG_H_MARGIN_SIZES_XX_LARGE = {30, 30}
+DCREREADER_CONFIG_H_MARGIN_SIZES_XXX_LARGE = {50, 50}
+DCREREADER_CONFIG_H_MARGIN_SIZES_HUGE = {70, 70}
+DCREREADER_CONFIG_H_MARGIN_SIZES_X_HUGE = {100, 100}
+DCREREADER_CONFIG_H_MARGIN_SIZES_XX_HUGE = {140, 140}
+
+-- top margin in (relative) pixels
+DCREREADER_CONFIG_T_MARGIN_SIZES_SMALL = 5
+DCREREADER_CONFIG_T_MARGIN_SIZES_MEDIUM = 10
+DCREREADER_CONFIG_T_MARGIN_SIZES_LARGE = 15
+DCREREADER_CONFIG_T_MARGIN_SIZES_X_LARGE = 20
+DCREREADER_CONFIG_T_MARGIN_SIZES_XX_LARGE = 30
+DCREREADER_CONFIG_T_MARGIN_SIZES_XXX_LARGE = 50
+DCREREADER_CONFIG_T_MARGIN_SIZES_HUGE = 70
+DCREREADER_CONFIG_T_MARGIN_SIZES_X_HUGE = 100
+DCREREADER_CONFIG_T_MARGIN_SIZES_XX_HUGE = 140
+
+-- bottom margin in (relative) pixels
+DCREREADER_CONFIG_B_MARGIN_SIZES_SMALL = 5
+DCREREADER_CONFIG_B_MARGIN_SIZES_MEDIUM = 10
+DCREREADER_CONFIG_B_MARGIN_SIZES_LARGE = 15
+DCREREADER_CONFIG_B_MARGIN_SIZES_X_LARGE = 20
+DCREREADER_CONFIG_B_MARGIN_SIZES_XX_LARGE = 30
+DCREREADER_CONFIG_B_MARGIN_SIZES_XXX_LARGE = 50
+DCREREADER_CONFIG_B_MARGIN_SIZES_HUGE = 70
+DCREREADER_CONFIG_B_MARGIN_SIZES_X_HUGE = 100
+DCREREADER_CONFIG_B_MARGIN_SIZES_XX_HUGE = 140
 
 -- crereader font gamma (no longer used)
 -- DCREREADER_CONFIG_LIGHTER_FONT_GAMMA = 10

frontend/apps/reader/modules/readerdogear.lua
@@ -43,10 +43,17 @@ end
 function ReaderDogear:onReadSettings(config)
     if not self.ui.document.info.has_pages then
         -- Adjust to CreDocument margins (as done in ReaderTypeset)
-        self:onSetPageMargins(
-            config:readSetting("copt_page_margins") or
-            G_reader_settings:readSetting("copt_page_margins") or
-            DCREREADER_CONFIG_MARGIN_SIZES_MEDIUM)
+        local h_margins = config:readSetting("copt_h_page_margins") or
+            G_reader_settings:readSetting("copt_h_page_margins") or
+            DCREREADER_CONFIG_H_MARGIN_SIZES_MEDIUM
+        local t_margin = config:readSetting("copt_t_page_margin") or
+            G_reader_settings:readSetting("copt_t_page_margin") or
+            DCREREADER_CONFIG_T_MARGIN_SIZES_LARGE
+        local b_margin = config:readSetting("copt_b_page_margin") or
+            G_reader_settings:readSetting("copt_b_page_margin") or
+            DCREREADER_CONFIG_B_MARGIN_SIZES_LARGE
+        local margins = { h_margins[1], t_margin, h_margins[2], b_margin }
+        self:onSetPageMargins(margins)
     end
 end
 

frontend/apps/reader/modules/readerfooter.lua
@@ -137,6 +137,7 @@ function ReaderFooter:init()
         -- disable_progress_bar = true,
         disabled = false,
         all_at_once = false,
+        reclaim_height = false,
         toc_markers = true,
         battery = true,
         time = true,
@@ -158,6 +159,7 @@ function ReaderFooter:init()
 
     self.pageno = self.view.state.page
     self.has_no_mode = true
+    self.reclaim_height = self.settings.reclaim_height or false
     for _, m in ipairs(MODE_INDEX) do
         if self.settings[m] then
             self.has_no_mode = false
@@ -337,6 +339,7 @@ end
 
 local option_titles = {
     all_at_once = _("Show all at once"),
+    reclaim_height = _("Reclaim bar height from bottom margin"),
     toc_markers = _("Show chapter markers"),
     page_progress = _("Current page"),
     time = _("Current time"),
@@ -387,6 +390,7 @@ function ReaderFooter:addToMainMenu(menu_items)
                 local should_update = false
                 local first_enabled_mode_num
                 local prev_has_no_mode = self.has_no_mode
+                local prev_reclaim_height = self.reclaim_height
                 self.has_no_mode = true
                 for mode_num, m in pairs(MODE_INDEX) do
                     if self.settings[m] then
@@ -395,14 +399,18 @@ function ReaderFooter:addToMainMenu(menu_items)
                         break
                     end
                 end
+                self.reclaim_height = self.settings.reclaim_height or false
                 -- refresh margins position
                 if self.has_no_mode then
-                    self.ui:handleEvent(Event:new("SetPageMargins", self.view.document.configurable.page_margins))
+                    self.ui:handleEvent(Event:new("SetPageBottomMargin", self.view.document.configurable.b_page_margin))
                     self.genFooterText = footerTextGeneratorMap.empty
                     self.mode = MODE.off
                 elseif prev_has_no_mode then
-                    self.ui:handleEvent(Event:new("SetPageMargins", self.view.document.configurable.page_margins))
+                    self.ui:handleEvent(Event:new("SetPageBottomMargin", self.view.document.configurable.b_page_margin))
                     G_reader_settings:saveSetting("reader_footer_mode", first_enabled_mode_num)
+                elseif self.reclaim_height ~= prev_reclaim_height then
+                    self.ui:handleEvent(Event:new("SetPageBottomMargin", self.view.document.configurable.b_page_margin))
+                    should_update = true
                 end
                 if callback then
                     should_update = callback(self)
@@ -429,6 +437,8 @@ function ReaderFooter:addToMainMenu(menu_items)
 
     table.insert(sub_items,
                  getMinibarOption("all_at_once", self.updateFooterTextGenerator))
+    table.insert(sub_items,
+                 getMinibarOption("reclaim_height"))
     table.insert(sub_items, {
         text = _("Progress bar"),
         sub_item_table = {

frontend/apps/reader/modules/readertypeset.lua
@@ -1,5 +1,6 @@
 local ConfirmBox = require("ui/widget/confirmbox")
 local Event = require("ui/event")
+local InfoMessage = require("ui/widget/infomessage")
 local InputContainer = require("ui/widget/container/inputcontainer")
 local UIManager = require("ui/uimanager")
 local lfs = require("libs/libkoreader-lfs")
@@ -11,6 +12,7 @@ local ReaderTypeset = InputContainer:new{
     css_menu_title = _("Style"),
     css = nil,
     internal_css = true,
+    unscaled_margins = nil,
 }
 
 function ReaderTypeset:init()
@@ -58,10 +60,17 @@ function ReaderTypeset:onReadSettings(config)
     -- self.ui.document:setRenderScaleFontWithDPI(1)
 
     -- set page margins
-    self:onSetPageMargins(
-        config:readSetting("copt_page_margins") or
-        G_reader_settings:readSetting("copt_page_margins") or
-        DCREREADER_CONFIG_MARGIN_SIZES_MEDIUM)
+    local h_margins = config:readSetting("copt_h_page_margins") or
+        G_reader_settings:readSetting("copt_h_page_margins") or
+        DCREREADER_CONFIG_H_MARGIN_SIZES_MEDIUM
+    local t_margin = config:readSetting("copt_t_page_margin") or
+        G_reader_settings:readSetting("copt_t_page_margin") or
+        DCREREADER_CONFIG_T_MARGIN_SIZES_LARGE
+    local b_margin = config:readSetting("copt_b_page_margin") or
+        G_reader_settings:readSetting("copt_b_page_margin") or
+        DCREREADER_CONFIG_B_MARGIN_SIZES_LARGE
+    self.unscaled_margins = { h_margins[1], t_margin, h_margins[2], b_margin }
+    self:onSetPageMargins(self.unscaled_margins, true)
 
     -- default to disable floating punctuation
     -- the floating punctuation should not be boolean value for the following
@@ -336,18 +345,45 @@ function ReaderTypeset:makeDefaultStyleSheet(css, text, touchmenu_instance)
     })
 end
 
-function ReaderTypeset:onSetPageMargins(margins)
+function ReaderTypeset:onSetPageHorizMargins(h_margins)
+    self.unscaled_margins = { h_margins[1], self.unscaled_margins[2], h_margins[2], self.unscaled_margins[4] }
+    self:onSetPageMargins(self.unscaled_margins)
+end
+
+function ReaderTypeset:onSetPageTopMargin(t_margin)
+    self.unscaled_margins = { self.unscaled_margins[1], t_margin, self.unscaled_margins[3], self.unscaled_margins[4] }
+    self:onSetPageMargins(self.unscaled_margins)
+end
+
+function ReaderTypeset:onSetPageBottomMargin(b_margin)
+    self.unscaled_margins = { self.unscaled_margins[1], self.unscaled_margins[2], self.unscaled_margins[3], b_margin }
+    self:onSetPageMargins(self.unscaled_margins)
+end
+
+function ReaderTypeset:onSetPageMargins(margins, silent)
     local left = Screen:scaleBySize(margins[1])
     local top = Screen:scaleBySize(margins[2])
     local right = Screen:scaleBySize(margins[3])
     local bottom
-    if self.view.footer.has_no_mode then
+    if self.view.footer.has_no_mode or self.view.footer.reclaim_height then
         bottom = Screen:scaleBySize(margins[4])
     else
         bottom = Screen:scaleBySize(margins[4] + DMINIBAR_HEIGHT)
     end
     self.ui.document:setPageMargins(left, top, right, bottom)
     self.ui:handleEvent(Event:new("UpdatePos"))
+    if not silent then
+        -- Show a toast on set, with the unscaled & scaled values
+        UIManager:show(InfoMessage:new{
+            text = T(_([[
+    Margins set to:
+    horizontal: %1 (%2px)
+    top: %3 (%4px)
+    bottom: %5 (%6px)]]),
+            margins[1], left, margins[2], top, margins[4], bottom),
+            timeout = 4,
+        })
+    end
     return true
 end
 

frontend/apps/reader/readerui.lua
@@ -49,6 +49,7 @@ local ReaderView = require("apps/reader/modules/readerview")
 local ReaderWikipedia = require("apps/reader/modules/readerwikipedia")
 local ReaderZooming = require("apps/reader/modules/readerzooming")
 local Screenshoter = require("ui/widget/screenshoter")
+local SettingsMigration = require("ui/data/settings_migration")
 local UIManager = require("ui/uimanager")
 local lfs = require("libs/libkoreader-lfs")
 local logger = require("logger")
@@ -100,6 +101,8 @@ function ReaderUI:init()
     end
 
     self.doc_settings = DocSettings:open(self.document.file)
+    -- Handle local settings migration
+    SettingsMigration:migrateSettings(self.doc_settings)
 
     if Device:hasKeys() then
         self.key_events.Home = { {"Home"}, doc = "open file browser" }

frontend/ui/data/creoptions.lua
@@ -131,34 +131,105 @@ Note that your selected font size is not affected by this setting.]]),
                 name_text_suffix = "%",
                 name_text_true_values = true,
             },
+        }
+    },
+    {
+        icon = "resources/icons/appbar.crop.large.png",
+        options = {
             {
-                name = "page_margins",
-                name_text = S.PAGE_MARGIN,
+                name = "h_page_margins",
+                name_text = S.H_PAGE_MARGINS,
                 buttonprogress = true,
                 values = {
-                    DCREREADER_CONFIG_MARGIN_SIZES_SMALL,
-                    DCREREADER_CONFIG_MARGIN_SIZES_MEDIUM,
-                    DCREREADER_CONFIG_MARGIN_SIZES_LARGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_X_LARGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_XX_LARGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_XXX_LARGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_HUGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_X_HUGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_SMALL,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_MEDIUM,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_LARGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_X_LARGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_XX_LARGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_XXX_LARGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_HUGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_X_HUGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_XX_HUGE,
                 },
                 default_pos = 2,
-                default_value = DCREREADER_CONFIG_MARGIN_SIZES_MEDIUM,
-                event = "SetPageMargins",
+                default_value = DCREREADER_CONFIG_H_MARGIN_SIZES_MEDIUM,
+                event = "SetPageHorizMargins",
                 args = {
-                    DCREREADER_CONFIG_MARGIN_SIZES_SMALL,
-                    DCREREADER_CONFIG_MARGIN_SIZES_MEDIUM,
-                    DCREREADER_CONFIG_MARGIN_SIZES_LARGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_X_LARGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_XX_LARGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_XXX_LARGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_HUGE,
-                    DCREREADER_CONFIG_MARGIN_SIZES_X_HUGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_SMALL,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_MEDIUM,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_LARGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_X_LARGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_XX_LARGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_XXX_LARGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_HUGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_X_HUGE,
+                    DCREREADER_CONFIG_H_MARGIN_SIZES_XX_HUGE,
                 },
-                name_text_hold_callback = optionsutil.showValuesMargins,
+                name_text_hold_callback = optionsutil.showValuesHMargins,
+            },
+            {
+                name = "t_page_margin",
+                name_text = S.T_PAGE_MARGIN,
+                buttonprogress = true,
+                fine_tune = true,
+                values = {
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_SMALL,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_MEDIUM,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_LARGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_X_LARGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_XX_LARGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_XXX_LARGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_HUGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_X_HUGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_XX_HUGE,
+                },
+                default_pos = 3,
+                default_value = DCREREADER_CONFIG_T_MARGIN_SIZES_LARGE,
+                event = "SetPageTopMargin",
+                args = {
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_SMALL,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_MEDIUM,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_LARGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_X_LARGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_XX_LARGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_XXX_LARGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_HUGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_X_HUGE,
+                    DCREREADER_CONFIG_T_MARGIN_SIZES_XX_HUGE,
+                },
+                name_text_hold_callback = optionsutil.showValues,
+            },
+            {
+                name = "b_page_margin",
+                name_text = S.B_PAGE_MARGIN,
+                buttonprogress = true,
+                fine_tune = true,
+                values = {
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_SMALL,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_MEDIUM,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_LARGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_X_LARGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_XX_LARGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_XXX_LARGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_HUGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_X_HUGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_XX_HUGE,
+                },
+                default_pos = 3,
+                default_value = DCREREADER_CONFIG_B_MARGIN_SIZES_LARGE,
+                event = "SetPageBottomMargin",
+                args = {
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_SMALL,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_MEDIUM,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_LARGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_X_LARGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_XX_LARGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_XXX_LARGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_HUGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_X_HUGE,
+                    DCREREADER_CONFIG_B_MARGIN_SIZES_XX_HUGE,
+                },
+                name_text_hold_callback = optionsutil.showValues,
             },
         }
     },

commit f0c5d2e657642f9554d5494e2629eda996a567a0

Restore PAGE_MARGIN string
It's used by KOptConfig

Fix #4992
Regression since #4945

frontend/ui/data/strings.lua
@@ -8,6 +8,7 @@ S.PAGE_CROP = _("Page Crop")
 S.FULL_SCREEN = _("Full Screen")
 S.SCROLL_MODE = _("Scroll Mode")
 S.ZOOM_DPI = _("Zoom (dpi)")
+S.PAGE_MARGIN = _("Margin")
 S.H_PAGE_MARGINS = _("L/R Margins")
 S.T_PAGE_MARGIN = _("Top Margin")
 S.B_PAGE_MARGIN = _("Bottom Margin")