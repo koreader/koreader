4f8b5d7ed2b28e02c97caa99f527ae8afa717dc2
Frans de Jonge <fransdejonge@gmail.com>
2019-06-09 15:34:06 +0200

    @poire-z comment

--- plugins/newsdownloader.koplugin/main.lua
+++ plugins/newsdownloader.koplugin/main.lua
@@ -89,7 +89,9 @@ function NewsDownloader:addToMainMenu(menu_items)
                 text = _("Go to news folder"),
                 callback = function()
                     local FileManager = require("apps/filemanager/filemanager")
-                    self.ui:onClose()
+                    if self.ui.document then
+                        self.ui:onClose()
+                    end
                     if FileManager.instance then
                         FileManager.instance:reinit(news_download_dir_path)
                     else
