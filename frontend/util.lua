b4d7ac90db1000d2dfe140613a643e05b3470060
Frans de Jonge <fransdejonge@gmail.com>
2019-06-10 16:43:15 +0200

    switch to htmlToPlainTextIfHtml

--- frontend/util.lua
+++ frontend/util.lua
@@ -503,7 +503,7 @@ function util.getSafeFilename(str, path, limit, limit_ext)
         suffix = nil
     end
 
-    filename = util.htmlToPlainText(filename)
+    filename = util.htmlToPlainTextIfHtml(filename)
     filename = filename:sub(1, limit)
     -- the limit might result in broken UTF-8, which we don't want in the result
     filename = util.fixUtf8(filename, "")
