1f1a7f61ef3488f6d6c2aada83966a44a52fc635
Frans de Jonge <fransdejonge@gmail.com>
2019-06-25 16:17:35 +0200

    [fix] OPDS crash with fewer than 4 servers
    Regression introduced in <https://github.com/koreader/koreader/pull/4249>.
    
    Fixes <https://github.com/koreader/koreader/issues/5091>.

--- frontend/ui/widget/opdsbrowser.lua
+++ frontend/ui/widget/opdsbrowser.lua
@@ -89,7 +89,7 @@ function OPDSBrowser:init()
           },
         }
         G_reader_settings:saveSetting("opds_servers", servers)
-    elseif servers[4].title == "Internet Archive" and servers[4].url == "http://bookserver.archive.org/catalog/"  then
+    elseif servers[4] and servers[4].title == "Internet Archive" and servers[4].url == "http://bookserver.archive.org/catalog/"  then
         servers[4].url = "https://bookserver.archive.org"
     end
     self.item_table = self:genItemTableFromRoot()
