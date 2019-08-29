d2536d8b7e9474bd7748b199d1f01a53f332942a
Martín Fernández <paziusss@gmail.com>
2019-08-29 21:59:39 +0200

    android: fix some warnings on launcher script, no need to chmod binaries as they are uncompressed on each update, A becomes android

--- platform/android/llapp_main.lua
+++ platform/android/llapp_main.lua
@@ -1,33 +1,29 @@
-local A = require("android")
-A.dl.library_path = A.dl.library_path .. ":" .. A.dir .. "/libs"
+local android = require("android")
+android.dl.library_path = android.dl.library_path .. ":" .. android.dir .. "/libs"
 
 local ffi = require("ffi")
 local dummy = require("ffi/posix_h")
 local C = ffi.C
 
 -- check uri of the intent that starts this application
-local file = A.getIntent()
+local file = android.getIntent()
 
 if file ~= nil then
-    A.LOGI("intent file path " .. file)
+    android.LOGI("intent file path " .. file)
 end
 
 -- run koreader patch before koreader startup
 pcall(dofile, "/sdcard/koreader/patch.lua")
 
--- set proper permission for binaries
-A.execute("chmod", "755", "./sdcv")
-A.execute("chmod", "755", "./tar")
-A.execute("chmod", "755", "./zsync")
-
 -- set TESSDATA_PREFIX env var
 C.setenv("TESSDATA_PREFIX", "/sdcard/koreader/data", 1)
 
 -- create fake command-line arguments
-if A.isDebuggable() then
+-- luacheck: ignore 121
+if android.isDebuggable() then
     arg = {"-d", file or "/sdcard"}
 else
     arg = {file or "/sdcard"}
 end
 
-dofile(A.dir.."/reader.lua")
+dofile(android.dir.."/reader.lua")
