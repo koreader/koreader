d97618d0b992e010ab21d775cf73abd52bd2e602
Frans de Jonge <fransdejonge@gmail.com>
2019-08-31 21:00:21 +0200

    [fix] Prevent crash when no page boxes
    Can occur with invalid page numbers, for example by changing the font size in a reflowable MuPDF document.
    
    Discussion in <https://github.com/koreader/koreader/pull/5282#issuecomment-526842921>.

--- frontend/document/koptinterface.lua
+++ frontend/document/koptinterface.lua
@@ -1048,10 +1048,12 @@ function KoptInterface:getPageBoxesFromPositions(doc, pageno, ppos0, ppos1)
         local spos0 = self:nativeToReflowPosTransform(doc, pageno, ppos0)
         local spos1 = self:nativeToReflowPosTransform(doc, pageno, ppos1)
         local page_boxes = self:getReflowedTextBoxes(doc, pageno)
+        if not page_boxes then return end
         local text_boxes = self:getTextFromBoxes(page_boxes, spos0, spos1)
         return text_boxes.boxes
     else
         local page_boxes = self:getTextBoxes(doc, pageno)
+        if not page_boxes then return end
         local text_boxes = self:getTextFromBoxes(page_boxes, ppos0, ppos1)
         return text_boxes.boxes
     end
