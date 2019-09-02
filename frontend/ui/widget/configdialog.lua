koreader/koreader

commit b038fa30461b3b2952af25c461832de901a4ee1a

[UX] Add fine tune buttons to horizontal margins (#5303)
Similar to top and bottom margins I add fine tune buttons to L/R margins.

frontend/ui/widget/configdialog.lua
@@ -965,13 +965,28 @@ function ConfigDialog:onConfigFineTuneChoose(values, name, event, args, events, 
             local value
             if direction == "-" then
                 value = self.configurable[name] or values[1]
-                value = value - 1
-                if value < 0 then
-                    value = 0
+                if type(value) == "table" then
+                    for i=1, #value do
+                        value[i] = value[i] - 1
+                        if value[i] < 0 then
+                            value[i] = 0
+                        end
+                    end
+                else
+                    value = value - 1
+                    if value < 0 then
+                        value = 0
+                    end
                 end
             else
                 value = self.configurable[name] or values[#values]
-                value = value + 1
+                if type(value) == "table" then
+                    for i=1, #value do
+                        value[i] = value[i] + 1
+                    end
+                else
+                    value = value + 1
+                end
             end
             self:onConfigChoice(name, value)
         end

frontend/ui/data/creoptions.lua
@@ -64,6 +64,7 @@ Note that this may not be ensured under some conditions: in scroll mode, when a 
                 name = "h_page_margins",
                 name_text = S.H_PAGE_MARGINS,
                 buttonprogress = true,
+                fine_tune = true,
                 values = {
                     DCREREADER_CONFIG_H_MARGIN_SIZES_SMALL,
                     DCREREADER_CONFIG_H_MARGIN_SIZES_MEDIUM,

commit d21c69fc13c0ca56c8aac3b18385ea0ca0612804

Don't crash when setting defaults via the -/+ buttons for horizontal margins.
Regression since #5303

frontend/ui/widget/configdialog.lua
@@ -1064,11 +1064,26 @@ end
 function ConfigDialog:onMakeFineTuneDefault(name, name_text, values, labels, direction)
     local display_value = self.configurable[name] or direction == "-" and labels[1] or labels[#labels]
 
+    local printable_value
+    -- known table value, make it pretty
+    if name == "h_page_margins" then
+            printable_value = T(_([[
+
+  left:  %1
+  right: %2
+]]),
+        display_value[1], display_value[2])
+    elseif type(display_value) == "table" then
+        printable_value = dump(display_value)
+    else
+        printable_value = display_value
+    end
+
     UIManager:show(ConfirmBox:new{
         text = T(
             _("Set default %1 to %2?"),
             (name_text or ""),
-            display_value
+            printable_value
         ),
         ok_text = T(_("Set default")),
         ok_callback = function()