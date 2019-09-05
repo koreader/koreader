748a95adb0c6002f3c1abc919f31f67760e1d1b9
Frans de Jonge <fransdejonge@gmail.com>
2019-09-05 14:16:54 +0200

    ordered menu

--- frontend/ui/elements/menu_keyboard_layout.lua
+++ frontend/ui/elements/menu_keyboard_layout.lua
@@ -1,8 +1,9 @@
 local VirtualKeyboard = require("ui/widget/virtualkeyboard")
+local orderedPairs = require("ffi/util").orderedPairs
 
 local sub_item_table = {}
 
-for k, _ in pairs(VirtualKeyboard.lang_to_keyboard_layout) do
+for k, _ in orderedPairs(VirtualKeyboard.lang_to_keyboard_layout) do
     table.insert(sub_item_table, {
         text = k,
         checked_func = function()
