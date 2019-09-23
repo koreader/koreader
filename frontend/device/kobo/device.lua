0680decd01ea54247e079d715f2e70cec4a1fe0a
NiLuJe <ninuje@gmail.com>
2019-09-23 14:56:21 +0200

    xref

--- frontend/device/kobo/device.lua
+++ frontend/device/kobo/device.lua
@@ -248,7 +248,7 @@ local KoboStorm = Kobo:new{
     misc_ntx_gsensor_protocol = true,
     display_dpi = 300,
     hasNaturalLight = yes,
-    --- @fixme: Someone needs to look into NL handling on this device, as apparently this doesn't work :?
+    --- @fixme: Someone needs to look into NL handling on this device, as apparently this doesn't work (#5416) :?
     frontlight_settings = {
         frontlight_white = "/sys/class/backlight/mxc_msp430.0/brightness",
         frontlight_mixer = "/sys/class/backlight/lm3630a_led/color",
