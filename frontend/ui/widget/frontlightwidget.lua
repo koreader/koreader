9ef5991a0b9cdb63c1a33dc6b367894dd43776b2
Frans de Jonge <fransdejonge@gmail.com>
2019-09-26 23:51:25 +0200

    and make that a little higher
    Maybe so should the skim widget?

--- frontend/ui/widget/frontlightwidget.lua
+++ frontend/ui/widget/frontlightwidget.lua
@@ -156,7 +156,7 @@ function FrontLightWidget:setProgress(num, step, num_warmth)
 
     self.fl_group = ProgressWidget:new{
         width = self.screen_width * 0.9,
-        height = Screen:scaleBySize(30),
+        height = Screen:scaleBySize(40),
         percentage = self.fl_cur / self.fl_max,
         ticks = ticks,
         tick_width = Screen:scaleBySize(0.5),
