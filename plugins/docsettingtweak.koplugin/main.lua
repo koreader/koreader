a4446ba60cb6b0d7baa6186dbab04d6c88f4a85f
yparitcher <y@paritcher.com>
2019-09-26 20:41:53 -0400

    fixes

--- plugins/docsettingtweak.koplugin/main.lua
+++ plugins/docsettingtweak.koplugin/main.lua
@@ -1,8 +1,6 @@
-local Event = require("ui/event")
-local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local _ = require("gettext")
-local logger = require("logger")
+--local logger = require("logger")
 local dirname = require("ffi/util").dirname
 local util = require("util")
 
@@ -15,7 +13,6 @@ local directory_defaults = {
 
 function DocSettingTweak:onDocSettingsLoad(doc_settings, docfile)
     if next(doc_settings.data) == nil then
---        local defaults = G_reader_settings:readSetting("directory_defaults")
         local base = G_reader_settings:readSetting("home_dir") or "/"
         local directory = docfile and docfile ~= "" and dirname(docfile)
         while directory:sub(1, #base) == base do
