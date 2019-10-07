f9667eb9c08c1adc3b9b486bb92d8524c638a4c7
yparitcher <y@paritcher.com>
2019-10-07 00:04:06 -0400

    whitespace

--- plugins/docsettingtweak.koplugin/directory_defaults.lua
+++ plugins/docsettingtweak.koplugin/directory_defaults.lua
@@ -4,15 +4,15 @@ return {--do NOT change this line
  -- you can override default settings for documents per directory in this file
  -- the directories must be under the home folder.
  -- you can find settings to change in the metadata.lua in the .sdr folder.
- 
+
  -- syntax:
 
  --   ["path/to/folder"] = {
  --         ["setting_to_override"] = value,
  --    },
- 
+
  -- examples:
- 
+
  --[[
     ["/mnt/us/documents/hebrew"] = {
         ["inverse_reading_order"] = true
