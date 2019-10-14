0fcdf3815ef062621687331928fdc0f88ec13e33
NiLuJe <ninuje@gmail.com>
2019-10-14 22:52:31 +0200

    Double-check

--- frontend/apps/reader/modules/readerfooter.lua
+++ frontend/apps/reader/modules/readerfooter.lua
@@ -1274,6 +1274,7 @@ end
 -- only call this function after document is fully loaded
 function ReaderFooter:_updateFooterText(force_repaint)
     print("ReaderFooter:_updateFooterText", force_repaint)
+    print("self.view.footer_visible", self.view.footer_visible)
     -- footer is invisible, and we don't need a repaint, go away.
     if not self.view.footer_visible and not force_repaint then
         print("Invisible, early abort")
