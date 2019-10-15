5965e388039bd66924ea0b564802c0069c17aaab
NiLuJe <ninuje@gmail.com>
2019-10-15 17:17:04 +0200

    Squish debug

--- frontend/ui/widget/configdialog.lua
+++ frontend/ui/widget/configdialog.lua
@@ -829,7 +829,6 @@ function ConfigDialog:updateConfigPanel(index)
 end
 
 function ConfigDialog:update()
-    print("ConfigDialog:update")
     self.layout = {}
     self.config_menubar = MenuBar:new{
         config_dialog = self,
@@ -854,12 +853,6 @@ function ConfigDialog:update()
         dimen = Screen:getSize(),
         self.dialog_frame,
     }
-
-    print("self.selected.y:", self.selected.y)
-    print("self.selected.x:", self.selected.x)
-    print("config_panel.dimen.h", self.config_panel.dimen.h)
-    print("total h", self.config_menubar.dimen.h + self.config_panel.dimen.h)
-    print("dimen", self.dialog_frame.dimen)
 end
 
 function ConfigDialog:onCloseWidget()
@@ -872,19 +865,14 @@ end
 function ConfigDialog:onShowConfigPanel(index)
     self.panel_index = index
     local old_dimen = self.dialog_frame.dimen and self.dialog_frame.dimen:copy()
-    print("old_dimen", old_dimen)
     local old_layout_h = self.layout and #self.layout
-    print("old_layout_h", old_panel_h)
     self:update()
-    print("new dimen", self.dialog_frame.dimen)
     -- NOTE: Keep that one as UI to avoid delay when both this and the topmenu are shown.
     --       Plus, this is also called for each tab anyway, so that wouldn't have been great.
     -- NOTE: And we also only need to repaint what's behind us when switch to a smaller height...
     --       This is trickier than in touchmenu, because dimen appear to fluctuate before/after painting...
     --       So we've settled instead for the amount of lines in the panel, as line-height is constant.
     local keep_bg = old_layout_h and #self.layout >= old_layout_h
-    print("is_fresh", self.is_fresh)
-    print("keep_bg", keep_bg)
     UIManager:setDirty((self.is_fresh or keep_bg) and self or "all", function()
         local refresh_dimen =
             old_dimen and old_dimen:combine(self.dialog_frame.dimen)
