56dc0412b33fb2029fed9e735e994a393efcdfac
Robert <robert.styla@gmail.com>
2020-01-23 11:00:29 +0100

    Show or hide hidden folders in folder favorites chooser (#5785)

--- frontend/apps/filemanager/filemanagershortcuts.lua
+++ frontend/apps/filemanager/filemanagershortcuts.lua
@@ -67,6 +67,7 @@ function FileManagerShortcuts:addNewFolder()
         select_directory = true,
         select_file = false,
         path = self.fm_bookmark.curr_path,
+        show_hidden = self.ui.file_chooser.show_hidden,
         onConfirm = function(path)
             local add_folder_input
             local friendly_name = util.basename(path) or _("my folder")
