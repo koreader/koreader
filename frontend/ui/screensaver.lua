5032369c27bca2a6a4d7c6d6a81099cdf0559a58
Rena Kunisaki <hyperhacker@gmail.com>
2020-02-19 17:59:15 -0500

    handle multi-line screen saver message

--- frontend/ui/screensaver.lua
+++ frontend/ui/screensaver.lua
@@ -404,7 +404,6 @@ function Screensaver:show(event, fallback_message)
                     text = screensaver_message,
                     face = face,
                     width = screen_w,
-                    height = face.size,
                     alignment = "center",
                 }
             }
