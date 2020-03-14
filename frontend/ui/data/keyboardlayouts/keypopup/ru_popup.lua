ef44f64617f9056ce3718b93b1d67d871028c2ce
mergen3107 <mergen.alimaganbetov@gmail.com>
2020-03-14 04:59:35 -0400

    New Russian Keyboard Layout

--- frontend/ui/data/keyboardlayouts/keypopup/ru_popup.lua
+++ frontend/ui/data/keyboardlayouts/keypopup/ru_popup.lua
@@ -15,4 +15,85 @@ return {
         "е",
         north = "ё",
     },
+    _SH_ = {
+        "Ь",
+        north = "Ъ",
+    },
+    _sh_ = {
+        "ь",
+        north = "ъ",
+    },
+    --
+    _KA_ = {
+        "А",
+        north = "Ә",
+    },
+    _ka_ = {
+        "а",
+        north = "ә",
+    },
+    _KI_ = {
+        "Ы",
+        north = "І",
+    },
+    _ki_ = {
+        "ы",
+        north = "і",
+    },
+    _KN_ = {
+        "Н",
+        north = "Ң",
+    },
+    _kn_ = {
+        "н",
+        north = "ң",
+    },
+    _KG_ = {
+        "Г",
+        north = "Ғ",
+    },
+    _kg_ = {
+        "г",
+        north = "ғ",
+    },
+    _KU_ = {
+        "У",
+        north = "Ү",
+        northeast = "Ұ",
+    },
+    _ku_ = {
+        "у",
+        north = "ү",
+        northeast = "ұ",
+    },
+    _KK_ = {
+        "К",
+        north = "Қ",
+    },
+    _kk_ = {
+        "к",
+        north = "қ",
+    },
+    _KO_ = {
+        "О",
+        north = "Ө",
+    },
+    _ko_ = {
+        "о",
+        north = "ө",
+    },
+    _KH_ = {
+        "Х",
+        north = "Һ",
+    },
+    _kh_ = {
+        "х",
+        north = "һ",
+    },
+    _qe_ = {
+        "?",
+        north = "!",
+        northwest = '"',
+        northeast = "'",
+    },
 }
