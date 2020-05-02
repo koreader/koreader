e4cd107d8cadfa11ab522ebb66fff8079446cd16
Frans de Jonge <fransdejonge@gmail.com>
2020-05-02 22:53:54 +0200

    Update frontend/apps/filemanager/filemanager.lua

--- frontend/apps/filemanager/filemanager.lua
+++ frontend/apps/filemanager/filemanager.lua
@@ -1056,7 +1056,7 @@ function FileManager:copyFileFromTo(from, to)
     return BaseUtil.execute(self.cp_bin, from, to) == 0
 end
 
---- A shortcut to execute cp recursive command (self.cp_bin) with from and to as parameters.
+--- A shortcut to execute cp recursively.
 -- @treturn boolean result of cp command
 function FileManager:copyRecursive(from, to)
     return BaseUtil.execute(self.cp_bin, "-r", from, to ) == 0
