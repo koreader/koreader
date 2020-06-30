95d1d8795dc7fa6ad7ff08fae07ce6a263b642fc
yparitcher <y@paritcher.com>
2020-06-29 23:02:53 -0400

    labeling

--- frontend/ui/data/creoptions.lua
+++ frontend/ui/data/creoptions.lua
@@ -21,10 +21,10 @@ local CreOptions = {
             {
                 name = "rotation_mode",
                 name_text = S.SCREEN_MODE,
-                toggle = {S.PORTRAIT, S.LANDSCAPE, S.PORTRAIT_ROTATED, S.LANDSCAPE_ROTATED},
+                toggle = {S.LANDSCAPE, S.PORTRAIT, S.PORTRAIT_ROTATED, S.LANDSCAPE_ROTATED},
                 alternate = false,
-                values = {0, 1, 2, 3},
-                args = {0, 1, 2, 3},
+                values = {1, 0, 2, 3},
+                args = {1, 0, 2, 3},
                 default_arg = 0,
                 current_func = function() return Device.screen:getRotationMode() end,
                 event = "SetRotationMode",
