koreader/koreader

commit 70f89c4df113bbfe4b8ea89730e71a36dfaddedc

ReaderGesture: cleanup (#6292)
convert all gesture actions to use events for better modularity
add network event handlers and device event handlers

frontend/apps/reader/modules/readerfooter.lua
@@ -557,7 +557,7 @@ end
 function ReaderFooter:setupAutoRefreshTime()
     if not self.autoRefreshTime then
         self.autoRefreshTime = function()
-            self:updateFooter(true)
+            self:onUpdateFooter(true)
             UIManager:scheduleIn(61 - tonumber(os.date("%S")), self.autoRefreshTime)
         end
     end
@@ -839,7 +839,7 @@ function ReaderFooter:addToMainMenu(menu_items)
                             self.settings.order = self.mode_index
                             G_reader_settings:saveSetting("footer", self.settings)
                             self:updateFooterTextGenerator()
-                            self:updateFooter()
+                            self:onUpdateFooter()
                             UIManager:setDirty(nil, "ui")
                         end
                     }
@@ -1686,7 +1686,7 @@ function ReaderFooter:getDataFromStatistics(title, pages)
     return title .. sec
 end
 
-function ReaderFooter:updateFooter(force_repaint, force_recompute)
+function ReaderFooter:onUpdateFooter(force_repaint, force_recompute)
     if self.pageno then
         self:updateFooterPage(force_repaint, force_recompute)
     else
@@ -1811,7 +1811,7 @@ function ReaderFooter:onReaderReady()
     self:resetLayout(self.settings.progress_margin_width)  -- set widget dimen
     self:setTocMarkers()
     self.updateFooterText = self._updateFooterText
-    self:updateFooter()
+    self:onUpdateFooter()
 end
 
 function ReaderFooter:onReadSettings(config)
@@ -1894,7 +1894,7 @@ function ReaderFooter:onTapFooter(ges)
             local percentage = (pos.x - dimen.x)/dimen.w
             self.ui:handleEvent(Event:new("GotoPercentage", percentage))
         end
-        self:updateFooter(true)
+        self:onUpdateFooter(true)
         return true
     end
     if self.has_no_mode or (self.settings.lock_tap and not ignore_lock) then
@@ -1921,7 +1921,7 @@ function ReaderFooter:onTapFooter(ges)
     end
     self:applyFooterMode()
     G_reader_settings:saveSetting("reader_footer_mode", self.mode)
-    self:updateFooter(true)
+    self:onUpdateFooter(true)
     return true
 end
 
@@ -1949,14 +1949,14 @@ function ReaderFooter:refreshFooter(refresh, signal)
     self:resetLayout(true)
     -- If we signal, the event we send will trigger a full repaint anyway, so we should be able to skip this one.
     -- We *do* need to ensure we at least re-compute the footer layout, though, especially when going from visible to invisible...
-    self:updateFooter(refresh and not signal, refresh and signal)
+    self:onUpdateFooter(refresh and not signal, refresh and signal)
     if signal then
         self.ui:handleEvent(Event:new("SetPageBottomMargin", self.view.document.configurable.b_page_margin))
     end
 end
 
 function ReaderFooter:onResume()
-    self:updateFooter()
+    self:onUpdateFooter()
     if self.settings.auto_refresh_time then
         self:setupAutoRefreshTime()
     end
@@ -1971,7 +1971,7 @@ end
 
 function ReaderFooter:onFrontlightStateChanged()
     if self.settings.frontlight then
-        self:updateFooter(true)
+        self:onUpdateFooter(true)
     end
 end
 

frontend/apps/filemanager/filemanager.lua
@@ -5,6 +5,7 @@ local ButtonDialogTitle = require("ui/widget/buttondialogtitle")
 local CenterContainer = require("ui/widget/container/centercontainer")
 local ConfirmBox = require("ui/widget/confirmbox")
 local Device = require("device")
+local DeviceListener = require("device/devicelistener")
 local DocSettings = require("docsettings")
 local DocumentRegistry = require("document/documentregistry")
 local Event = require("ui/event")
@@ -463,6 +464,7 @@ function FileManager:init()
     table.insert(self, ReaderDictionary:new{ ui = self })
     table.insert(self, ReaderWikipedia:new{ ui = self })
     table.insert(self, ReaderDeviceStatus:new{ ui = self })
+    table.insert(self, DeviceListener:new{ ui = self })
 
     -- koreader plugins
     for _,plugin_module in ipairs(PluginLoader:loadPlugins()) do
@@ -482,6 +484,11 @@ function FileManager:init()
 
     if Device:isTouchDevice() then
         table.insert(self, ReaderGesture:new{ ui = self })
+    end
+
+    if Device:hasWifiToggle() then
+        local NetworkListener = require("ui/network/networklistener")
+        table.insert(self, NetworkListener:new{ ui = self })
     end
 
     if Device:hasKeys() then

frontend/apps/filemanager/filemanagerfilesearcher.lua
@@ -105,7 +105,7 @@ function FileSearcher:close()
     end
 end
 
-function FileSearcher:onShowFileSearch(search_path)
+function FileSearcher:onShowFileSearch()
     local dummy = self.search_value
     local enabled_search_home_dir = true
     if not G_reader_settings:readSetting("home_dir") then
@@ -129,7 +129,7 @@ function FileSearcher:onShowFileSearch(search_path)
                     text = _("Current folder"),
                     enabled = true,
                     callback = function()
-                        self.path = search_path or lfs.currentdir()
+                        self.path = self.ui.file_chooser and self.ui.file_chooser.path or self.ui:getLastDirFile()
                         self.search_value = self.search_dialog:getInputText()
                         if self.search_value == dummy then -- probably DELETE this if/else block
                             self.use_previous_search_results = true

frontend/apps/filemanager/filemanagermenu.lua
@@ -87,7 +87,7 @@ function FileManagerMenu:initGesListener()
     })
 end
 
-function FileManagerMenu:openLastDoc()
+function FileManagerMenu:onOpenLastDoc()
     local last_file = G_reader_settings:readSetting("lastfile")
     if not last_file or lfs.attributes(last_file, "mode") ~= "file" then
         local InfoMessage = require("ui/widget/infomessage")
@@ -478,7 +478,7 @@ function FileManagerMenu:setUpdateItemTable()
         -- @translators Search for files by name.
         text = _("Find a file"),
         callback = function()
-            self.ui:handleEvent(Event:new("ShowFileSearch", self.ui.file_chooser.path))
+            self.ui:handleEvent(Event:new("ShowFileSearch"))
         end
     }
 
@@ -496,7 +496,7 @@ function FileManagerMenu:setUpdateItemTable()
             return G_reader_settings:readSetting("lastfile") ~= nil
         end,
         callback = function()
-            self:openLastDoc()
+            self:onOpenLastDoc()
         end,
         hold_callback = function()
             local last_file = G_reader_settings:readSetting("lastfile")
@@ -504,7 +504,7 @@ function FileManagerMenu:setUpdateItemTable()
                 text = T(_("Would you like to open the last document: %1?"), BD.filepath(last_file)),
                 ok_text = _("OK"),
                 ok_callback = function()
-                    self:openLastDoc()
+                    self:onOpenLastDoc()
                 end,
             })
         end

frontend/apps/reader/modules/readerdictionary.lua
@@ -819,7 +819,7 @@ function ReaderDictionary:showDict(word, results, box, link)
             refresh_callback = function()
                 if self.view then
                     -- update info in footer (time, battery, etc)
-                    self.view.footer:updateFooter()
+                    self.view.footer:onUpdateFooter()
                 end
             end,
             html_dictionary_link_tapped_callback = function(dictionary, html_link)

frontend/apps/reader/modules/readerfont.lua
@@ -320,31 +320,32 @@ function ReaderFont:addToMainMenu(menu_items)
     }
 end
 
--- direction +1 - increase font size
--- direction -1 - decrease font size
-function ReaderFont:onAdjustFontSize(ges, direction)
+function ReaderFont:gesToFontSize(ges)
     if ges.distance == nil then
         ges.distance = 1
     end
-    if direction ~= -1 and direction ~= 1 then
-        -- set default value (increase font size)
-        direction = 1
-    end
     local step = math.ceil(2 * #self.steps * ges.distance / self.gestureScale)
     local delta_int = self.steps[step] or self.steps[#self.steps]
-    if direction == 1 then
-        local info = Notification:new{text = _("Increasing font size…")}
-        UIManager:show(info)
-        UIManager:forceRePaint()
-        self:onChangeSize("increase", delta_int)
-        UIManager:close(info)
-    else
-        local info = Notification:new{text = _("Decreasing font size…")}
-        UIManager:show(info)
-        UIManager:forceRePaint()
-        self:onChangeSize("decrease", delta_int)
-        UIManager:close(info)
-    end
+    return delta_int
+end
+
+function ReaderFont:onIncreaseFontSize(ges)
+    local delta_int = self:gesToFontSize(ges)
+    local info = Notification:new{text = _("Increasing font size…")}
+    UIManager:show(info)
+    UIManager:forceRePaint()
+    self:onChangeSize("increase", delta_int)
+    UIManager:close(info)
+    return true
+end
+
+function ReaderFont:onDecreaseFontSize(ges)
+    local delta_int = self:gesToFontSize(ges)
+    local info = Notification:new{text = _("Decreasing font size…")}
+    UIManager:show(info)
+    UIManager:forceRePaint()
+    self:onChangeSize("decrease", delta_int)
+    UIManager:close(info)
     return true
 end
 

frontend/apps/reader/modules/readerfrontlight.lua
@@ -1,192 +1,0 @@
-local InputContainer = require("ui/widget/container/inputcontainer")
-local Notification = require("ui/widget/notification")
-local UIManager = require("ui/uimanager")
-local Screen = require("device").screen
-local Device = require("device")
-local T = require("ffi/util").template
-local _ = require("gettext")
-
-local ReaderFrontLight = InputContainer:new{
-    steps_fl = { 0.1, 0.1, 0.2, 0.4, 0.7, 1.1, 1.6, 2.2, 2.9, 3.7, 4.6, 5.6, 6.7, 7.9, 9.2, 10.6, },
-    gestureScale = Screen:getWidth() * FRONTLIGHT_SENSITIVITY_DECREASE,
-}
-
--- direction +1 - increase frontlight
--- direction -1 - decrease frontlight
-function ReaderFrontLight:onChangeFlIntensity(ges, direction)
-    local powerd = Device:getPowerDevice()
-    local gestureScale
-    local scale_multiplier
-    if ges.ges == "two_finger_swipe" then
-        -- for backward compatibility
-        scale_multiplier = FRONTLIGHT_SENSITIVITY_DECREASE * 0.8
-    elseif ges.ges == "swipe" then
-        scale_multiplier = 0.8
-    else
-        scale_multiplier = 1
-    end
-    if ges.direction == "south" or ges.direction == "north" then
-        gestureScale = Screen:getHeight() * scale_multiplier
-    elseif ges.direction == "west" or ges.direction == "east" then
-        gestureScale = Screen:getWidth() * scale_multiplier
-    else
-        local width = Screen:getWidth()
-        local height = Screen:getHeight()
-        -- diagonal
-        gestureScale = math.sqrt(width * width + height * height) * scale_multiplier
-    end
-    if powerd.fl_intensity == nil then return false end
-
-    local steps_tbl = {}
-    local scale = (powerd.fl_max - powerd.fl_min) / 2 / 10.6
-    for i = 1, #self.steps_fl, 1
-    do
-        steps_tbl[i] = math.ceil(self.steps_fl[i] * scale)
-    end
-
-    if ges.distance == nil then
-        ges.distance = 1
-    end
-    local step = math.ceil(#steps_tbl * ges.distance / gestureScale)
-    local delta_int = steps_tbl[step] or steps_tbl[#steps_tbl]
-    if direction ~= -1 and direction ~= 1 then
-        -- set default value (increase frontlight)
-        direction = 1
-    end
-    local new_intensity = powerd.fl_intensity + direction * delta_int
-
-    if new_intensity == nil then return true end
-    -- when new_intensity <=0, toggle light off
-    if new_intensity <= 0 then
-        powerd:turnOffFrontlight()
-    else
-        powerd:setIntensity(new_intensity)
-    end
-    self:onShowIntensity()
-    if self.view and self.view.footer_visible and self.view.footer.settings.frontlight then
-        self.view.footer:updateFooter()
-    end
-    return true
-end
-
--- direction +1 - increase frontlight warmth
--- direction -1 - decrease frontlight warmth
-function ReaderFrontLight:onChangeFlWarmth(ges, direction)
-    local powerd = Device:getPowerDevice()
-    if powerd.fl_warmth == nil then return false end
-
-    if powerd.auto_warmth then
-        UIManager:show(Notification:new{
-            text = _("Warmth is handled automatically."),
-            timeout = 1.0,
-        })
-        return true
-    end
-
-    local gestureScale
-    local scale_multiplier
-    if ges.ges == "two_finger_swipe" then
-        -- for backward compatibility
-        scale_multiplier = FRONTLIGHT_SENSITIVITY_DECREASE * 0.8
-    elseif ges.ges == "swipe" then
-        scale_multiplier = 0.8
-    else
-        scale_multiplier = 1
-    end
-
-    if ges.direction == "south" or ges.direction == "north" then
-        gestureScale = Screen:getHeight() * scale_multiplier
-    elseif ges.direction == "west" or ges.direction == "east" then
-        gestureScale = Screen:getWidth() * scale_multiplier
-    else
-        local width = Screen:getWidth()
-        local height = Screen:getHeight()
-        -- diagonal
-        gestureScale = math.sqrt(width * width + height * height) * scale_multiplier
-    end
-
-    local steps_tbl = {}
-    local scale = (powerd.fl_max - powerd.fl_min) / 2 / 10.6
-    for i = 1, #self.steps_fl, 1
-    do
-        steps_tbl[i] = math.ceil(self.steps_fl[i] * scale)
-    end
-
-    if ges.distance == nil then
-        ges.distance = 1
-    end
-
-    local step = math.ceil(#steps_tbl * ges.distance / gestureScale)
-    local delta_int = steps_tbl[step] or steps_tbl[#steps_tbl]
-    local warmth
-    if direction ~= -1 and direction ~= 1 then
-        -- set default value (increase frontlight)
-        direction = 1
-    end
-    warmth = powerd.fl_warmth + direction * delta_int
-    if warmth > 100 then
-        warmth = 100
-    elseif warmth < 0 then
-        warmth = 0
-    end
-    powerd:setWarmth(warmth)
-    self:onShowWarmth()
-    return true
-end
-
-
-function ReaderFrontLight:onShowOnOff()
-    local powerd = Device:getPowerDevice()
-    local new_text
-    if powerd.is_fl_on then
-        new_text = _("Frontlight enabled.")
-    else
-        new_text = _("Frontlight disabled.")
-    end
-    UIManager:show(Notification:new{
-        text = new_text,
-        timeout = 1.0,
-    })
-    return true
-end
-
-function ReaderFrontLight:onShowIntensity()
-    if not Device:hasFrontlight() then return true end
-    local powerd = Device:getPowerDevice()
-    local new_text
-    if powerd:isFrontlightOff() then
-        new_text = _("Frontlight disabled.")
-    else
-        new_text = T(_("Frontlight intensity set to %1."), powerd:frontlightIntensity())
-    end
-    UIManager:show(Notification:new{
-        text = new_text,
-        timeout = 1,
-    })
-    return true
-end
-
-function ReaderFrontLight:onShowWarmth(value)
-    local powerd = Device:getPowerDevice()
-    if powerd.fl_warmth ~= nil then
-        UIManager:show(Notification:new{
-            text = T(_("Warmth set to %1."), powerd.fl_warmth),
-            timeout = 1.0,
-        })
-    end
-    return true
-end
-
-function ReaderFrontLight:onShowFlDialog()
-    local FrontLightWidget = require("ui/widget/frontlightwidget")
-    UIManager:show(FrontLightWidget:new{
-        use_system_fl = Device:hasLightLevelFallback()
-    })
-end
-
-function ReaderFrontLight:close()
-    self.fl_dialog:onClose()
-    UIManager:close(self.fl_dialog)
-end
-
-return ReaderFrontLight

commit 7849d58a767e60af7fda9e801e8425258c2223a4

Fix footer update on `UpdatePos` event (#6392)
Regression from 70f89c4d (#6292).

frontend/apps/reader/modules/readerfooter.lua
@@ -1793,7 +1793,7 @@ end
 
 -- recalculate footer sizes when document page count is updated
 -- see documentation for more info about this event.
-ReaderFooter.onUpdatePos = ReaderFooter.updateFooter
+ReaderFooter.onUpdatePos = ReaderFooter.onUpdateFooter
 
 function ReaderFooter:onReaderReady()
     self.ui.menu:registerToMainMenu(self)