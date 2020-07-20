ea32715c6cf9bc72bd8e523d3df1f8e1b26b5f17
poire-z <poire-z@users.noreply.github.com>
2020-07-20 13:12:07 +0200

    Style tweaks: limit EPUB/FB2 footnotes to these formats
    Avoid conflicts having "In-page EPUB footnotes (smaller)"
    affects FB2 documents that use "<a type=note>12</a>" which
    would then get smaller if both tweaks are enabled (which
    they are, by default).
    Footnote popups: add styles for some non-HTML FB2 tags,
    so MuPDF can render them as expected.

--- frontend/ui/widget/footnotewidget.lua
+++ frontend/ui/widget/footnotewidget.lua
@@ -90,6 +90,11 @@ body > li { list-style-type: none; }
 /* Remove any (possibly multiple) backlinks in Wikipedia EPUBs footnotes */
 .noprint { display: none; }
 
+/* Style some FB2 tags not known to MuPDF */
+emphasis { font-style: italic; }
+strikethrough { text-decoration: line-through; }
+underline { text-decoration: underline; }
+
 /* Attempt to display FB2 footnotes as expected (as crengine does, putting
  * the footnote number on the same line as the first paragraph via its
  * support of "display: run-in" and a possibly added autoBoxing element) */
