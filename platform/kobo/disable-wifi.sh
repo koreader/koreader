04e75d14bc66f733cd7cecf885cc605417c77921
NiLuJe <ninuje@gmail.com>
2020-07-26 01:42:32 +0200

    Everything is terrible.
    busybo's kill doesn't have a wait flag, so we have to wait for udhcpc to
    die ourselves, and it's.... not pretty.

--- platform/kobo/disable-wifi.sh
+++ platform/kobo/disable-wifi.sh
@@ -3,13 +3,35 @@
 # Disable wifi, and remove all modules.
 # NOTE: Save our resolv.conf to avoid ending up with an empty one, in case the DHCP client wipes it on release (#6424).
 cp -a "/etc/resolv.conf" "/tmp/resolv.ko"
+old_hash="$(md5sum "/etc/resolv.conf" | cut -f1 -d' ')"
+
 if [ -x "/sbin/dhcpcd" ]; then
     env -u LD_LIBRARY_PATH dhcpcd -d -k "${INTERFACE}"
     killall -q -TERM udhcpc default.script
 else
     killall -q -TERM udhcpc default.script dhcpcd
 fi
-mv -f "/tmp/resolv.ko" "/etc/resolv.conf"
+
+# NOTE: dhcpcd -k waits for the signaled process to die, but busybox's killall doesn't have a -w, --wait flag,
+#       so we have to wait for udhcpc to die ourselves...
+kill_timeout=0
+while pkill -0 udhcpc; do
+    # Stop waiting after 5s
+    if [ ${kill_timeout} -ge 20 ]; then
+        break
+    fi
+    usleep 250000
+    kill_timeout=$((kill_timeout + 1))
+done
+
+new_hash="$(md5sum "/etc/resolv.conf" | cut -f1 -d' ')"
+# Restore our network-specific resolv.conf if the DHCP client wiped it when releasing the lease...
+if [ "${new_hash}" != "${old_hash}" ]; then
+    mv -f "/tmp/resolv.ko" "/etc/resolv.conf"
+else
+    rm -f "/tmp/resolv.ko"
+fi
+
 wpa_cli terminate
 
 [ "${WIFI_MODULE}" != "8189fs" ] && [ "${WIFI_MODULE}" != "8192es" ] && wlarm_le -i "${INTERFACE}" down
--- platform/kobo/nickel.sh
+++ platform/kobo/nickel.sh
@@ -26,13 +26,32 @@ unset KO_NO_CBB
 if lsmod | grep -q sdio_wifi_pwr; then
     killall -q -TERM restore-wifi-async.sh enable-wifi.sh obtain-ip.sh
     cp -a "/etc/resolv.conf" "/tmp/resolv.ko"
+    old_hash="$(md5sum "/etc/resolv.conf" | cut -f1 -d' ')"
     if [ -x "/sbin/dhcpcd" ]; then
         env -u LD_LIBRARY_PATH dhcpcd -d -k "${INTERFACE}"
         killall -q -TERM udhcpc default.script
     else
         killall -q -TERM udhcpc default.script dhcpcd
     fi
-    mv -f "/tmp/resolv.ko" "/etc/resolv.conf"
+    # NOTE: dhcpcd -k waits for the signaled process to die, but busybox's killall doesn't have a -w, --wait flag,
+    #       so we have to wait for udhcpc to die ourselves...
+    kill_timeout=0
+    while pkill -0 udhcpc; do
+        # Stop waiting after 5s
+        if [ ${kill_timeout} -ge 20 ]; then
+            break
+        fi
+        usleep 250000
+        kill_timeout=$((kill_timeout + 1))
+    done
+
+    new_hash="$(md5sum "/etc/resolv.conf" | cut -f1 -d' ')"
+    # Restore our network-specific resolv.conf if the DHCP client wiped it when releasing the lease...
+    if [ "${new_hash}" != "${old_hash}" ]; then
+        mv -f "/tmp/resolv.ko" "/etc/resolv.conf"
+    else
+        rm -f "/tmp/resolv.ko"
+    fi
     wpa_cli terminate
     [ "${WIFI_MODULE}" != "8189fs" ] && [ "${WIFI_MODULE}" != "8192es" ] && wlarm_le -i "${INTERFACE}" down
     ifconfig "${INTERFACE}" down
