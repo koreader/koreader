8cb9c5ef4cfa67011d25479793fbe660518ee825
Martín Fernández <paziusss@gmail.com>
2020-07-26 00:41:16 +0200

    Sample, taking min brightness and min warmth into account.

--- frontend/device/android/powerd.lua
+++ frontend/device/android/powerd.lua
@@ -2,21 +2,19 @@ local BasePowerD = require("device/generic/powerd")
 local _, android = pcall(require, "android")
 
 local AndroidPowerD = BasePowerD:new{
-    fl_min = 0, fl_max = 100, -- consistant scale across android, kobo ...
---    fl_intensity = 10,
+    fl_min = 0,
+    fl_max = 100,
+    bright_diff = android.getScreenMaxBrightness() - android.getScreenMinBrightness(),
+    warm_diff = android.getScreenMaxWarmth() - android.getScreenMinWarmth(),
 }
 
 function AndroidPowerD:frontlightIntensityHW()
-    local intensity = math.floor(android.getScreenBrightness() / android.getScreenMaxBrightness() * self.fl_max)
-    if intensity < 0 then
-        intensity = 0
-    end
-    return intensity
+    return math.floor(android.getScreenBrightness() / self.bright_diff * self.fl_max)
 end
 
 function AndroidPowerD:setIntensityHW(intensity)
     self.fl_intensity = intensity
-    android.setScreenBrightness(math.floor(intensity * android.getScreenMaxBrightness() / self.fl_max))
+    android.setScreenBrightness(math.floor(intensity * self.bright_diff / self.fl_max))
 end
 
 function AndroidPowerD:init()
@@ -27,22 +25,13 @@ end
 
 function AndroidPowerD:setWarmth(warmth)
     self.fl_warmth = warmth
-    android.setScreenWarmth( warmth / AndroidPowerD:getMaxWarmth() )
+    android.setScreenWarmth(warmth / (android.getScreenWarmth() * self.warm_diff))
 end
 
 function AndroidPowerD:getWarmth()
-    local warmth = android.getScreenWarmth() * AndroidPowerD.getMaxWarmth()
+    local warmth = android.getScreenWarmth() * self.bright_diff
     return warmth
 end
-
-function AndroidPowerD:getMaxWarmth()
-    return android.getScreenMaxWarmth()
-end
-
-function AndroidPowerD:getMinWarmth()
-    return android.getScreenMinWarmth()
-end
-
 
 function AndroidPowerD:getCapacityHW()
     return android.getBatteryLevel()
