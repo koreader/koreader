991dcab27bdb0ce1f054d7a31c6584cd22a3239e
Frans de Jonge <fransdejonge@gmail.com>
2020-07-30 22:15:02 +0200

    Update tesseract data files URL (#6452)
    Reported by @lescheck on Weblate.

--- frontend/apps/reader/modules/readerhighlight.lua
+++ frontend/apps/reader/modules/readerhighlight.lua
@@ -788,7 +788,7 @@ No OCR results or no language data.
 
 KOReader has a build-in OCR engine for recognizing words in scanned PDF and DjVu documents. In order to use OCR in scanned pages, you need to install tesseract trained data for your document language.
 
-You can download language data files for version 3.04 from https://github.com/tesseract-ocr/tesseract/wiki/Data-Files
+You can download language data files for version 3.04 from https://tesseract-ocr.github.io/tessdoc/Data-Files
 
 Copy the language data files for Tesseract 3.04 (e.g., eng.traineddata for English and spa.traineddata for Spanish) into koreader/data/tessdata]])
 
