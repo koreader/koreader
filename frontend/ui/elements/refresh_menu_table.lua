683056b73a47cd414630dc91284fe0e18bc10923
NiLuJe <ninuje@gmail.com>
2020-08-20 15:24:03 +0200

    Keep the elipsi around
    Makes it clear those two go hand-in-hand (and explains the lack of
    leading capital).

--- frontend/ui/elements/refresh_menu_table.lua
+++ frontend/ui/elements/refresh_menu_table.lua
@@ -109,7 +109,7 @@ return {
             separator = true,
         },
         {
-            text = _("Always flash on chapter boundaries"),
+            text = _("Always flash on chapter boundaries…"),
             checked_func = function() return G_reader_settings:isTrue("refresh_on_chapter_boundaries") end,
             callback = function() UIManager:broadcastEvent(Event:new("ToggleFlashOnChapterBoundaries")) end,
         },
