koreader/koreader

commit f7d538b108167a6bb4e89880d2b0cf8b4c69b42f

Landscape FM / Refactor rotation (#6309)
* landscape FM / Refactor rotation

refactor and simplify the orientation handling code. the user generally cares about the rotation (what direction the device is facing) and not about if koreader is displaying in portrait or landscape mode

* bump base

update luasocket, libjpeg-turbo, curl
add logging to evernote-sdk-lua
update framebuffer for proper rotation

frontend/apps/filemanager/filemanager.lua
@@ -47,20 +47,6 @@ local C_ = _.pgettext
 local Screen = Device.screen
 local T = require("ffi/util").template
 
-local function restoreScreenMode()
-    --- @todo: Not Yet Implemented. Layout is currently broken in Landscape.
-    local screen_mode = G_reader_settings:readSetting("fm_screen_mode") or "portrait"
-    --- @note: Basically, if we were already in Portrait/Inverted Portrait, don't mess with it,
-    --         as the FM supports it.
-    --         See setScreenMode in base's ffi/framebuffer.lua for the gory details.
-    --         See also ReaderView:onSetScreenMode in apps/reader/modules/readerview.lua for a similar logic,
-    --         if we ever need to add Landscape to the mix.
-    --         c.f., https://github.com/koreader/koreader/issues/5772#issuecomment-577242365
-    if Screen:getScreenMode() ~= screen_mode then
-        Screen:setScreenMode(screen_mode)
-    end
-end
-
 local FileManager = InputContainer:extend{
     title = _("KOReader"),
     root_path = lfs.currentdir(),
@@ -70,6 +56,27 @@ local FileManager = InputContainer:extend{
     cp_bin = Device:isAndroid() and "/system/bin/cp" or "/bin/cp",
     mkdir_bin =  Device:isAndroid() and "/system/bin/mkdir" or "/bin/mkdir",
 }
+
+function FileManager:onSetRotationMode(rotation)
+    if rotation ~= nil and rotation ~= Screen:getRotationMode() then
+        Screen:setRotationMode(rotation)
+        if self.instance then
+            self:reinit(self.instance.path, self.instance.focused_file)
+            UIManager:setDirty(self.instance.banner, function()
+                return "ui", self.instance.banner.dimen
+            end)
+        end
+    end
+    return true
+end
+
+function FileManager:setRotationMode()
+    local locked = G_reader_settings:readSetting("lock_rotation")
+    local rotation_mode = G_reader_settings:readSetting("fm_rotation_mode") or 0
+    if locked then
+        self:onSetRotationMode(rotation_mode)
+    end
+end
 
 function FileManager:init()
     if Device:isTouchDevice() then
@@ -1029,7 +1036,7 @@ end
 function FileManager:showFiles(path, focused_file)
     path = path or G_reader_settings:readSetting("lastdir") or filemanagerutil.getDefaultDir()
     G_reader_settings:saveSetting("lastdir", path)
-    restoreScreenMode()
+    self:setRotationMode()
     local file_manager = FileManager:new{
         dimen = Screen:getSize(),
         covers_fullscreen = true, -- hint for UIManager:_repaint()

frontend/apps/reader/modules/readerfooter.lua
@@ -1975,7 +1975,7 @@ function ReaderFooter:onFrontlightStateChanged()
     end
 end
 
-function ReaderFooter:onChangeScreenMode()
+function ReaderFooter:onSetRotationMode()
     self:updateFooterContainer()
     self:resetLayout(true)
 end

frontend/apps/reader/modules/readergesture.lua
@@ -1,4 +1,5 @@
 local BD = require("ui/bidi")
+local bit = require("bit")
 local ConfirmBox = require("ui/widget/confirmbox")
 local DataStorage = require("datastorage")
 local Device = require("device")
@@ -1551,9 +1552,8 @@ function ReaderGesture:gestureAction(action, ges)
         self.ui:handleEvent(Event:new("RestoreZoomMode"))
         self.ui:handleEvent(Event:new("InitScrollPageStates"))
     elseif action == "toggle_rotation" then
-        local event_name = self.document.info.has_pages and "SwapScreenMode" or "ChangeScreenMode"
-        local arg = Screen:getScreenMode() == "portrait" and "landscape" or "portrait"
-        self.ui:handleEvent(Event:new(event_name, arg))
+        local arg = bit.band((Screen:getRotationMode() + 1), 3)
+        self.ui:handleEvent(Event:new("SetRotationMode", arg))
     elseif action == "toggle_wifi" then
         local NetworkMgr = require("ui/network/manager")
 

frontend/apps/reader/modules/readerrolling.lua
@@ -850,14 +850,6 @@ function ReaderRolling:onSetDimensions(dimen)
     end
 end
 
-function ReaderRolling:onChangeScreenMode(mode, rotation)
-    -- Flag it as interactive so we can properly swap to Inverted orientations
-    -- (we usurp the second argument, which usually means rotation)
-    self.ui:handleEvent(Event:new("SetScreenMode", mode, rotation or true))
-    -- (This had the above ReaderRolling:onSetDimensions() called to resize
-    -- document dimensions and keep up with current position)
-end
-
 function ReaderRolling:onColorRenderingUpdate()
     self.ui.document:updateColorRendering()
     UIManager:setDirty(self.view.dialog, "partial")

frontend/apps/reader/modules/readerview.lua
@@ -672,45 +672,18 @@ function ReaderView:restoreViewContext(ctx)
     end
 end
 
--- NOTE: This is just a shim for koptoptions, because we want to be able to pass an optional second argument to SetScreenMode...
---       This is also used as a sink for gsensor input events, because we can only send a single event per input,
---       and we need to cover both CRe & KOpt...
-function ReaderView:onSwapScreenMode(new_mode, rotation)
-    -- Don't do anything if an explicit rotation was requested, but it hasn't actually changed,
-    -- because we may be sending this event *right before* a ChangeScreenMode in CRe (gyro)
-    if rotation ~= nil and rotation ~= true and rotation == Screen:getRotationMode() then
-        return true
-    end
-    -- CRe
-    self.ui:handleEvent(Event:new("ChangeScreenMode", new_mode, rotation or true))
-    -- KOpt (On CRe, since it's redundant (RR:onChangeScreenMode already sends one), this'll get discarded early)
-    self.ui:handleEvent(Event:new("SetScreenMode", new_mode, rotation or true))
-end
-
-function ReaderView:onSetScreenMode(new_mode, rotation, noskip)
-    -- Don't do anything if an explicit rotation was requested, but it hasn't actually changed,
-    -- because we may be sending this event *right after* a ChangeScreenMode in CRe (gsensor)
-    -- We only want to let the onReadSettings one go through, otherwise the testsuite blows up...
-    if noskip == nil and rotation ~= nil and rotation ~= true and rotation == Screen:getRotationMode() then
-        return true
-    end
-    if new_mode == "landscape" or new_mode == "portrait" then
-        -- NOTE: Hacky hack! If rotation is "true", that's actually an "interactive" flag for setScreenMode
-        --- @fixme That's because we can't store nils in a table, which is what Event:new attempts to do ;).
-        --        c.f., <https://stackoverflow.com/q/7183998/> & <http://lua-users.org/wiki/VarargTheSecondClassCitizen>
-        --        With a fixed Event implementation, we'd instead stick "interactive" in a third argument,
-        --        which we could happily pass while still keeping rotation nil ;).
-        if rotation ~= nil and rotation ~= true then
-            Screen:setRotationMode(rotation)
-        else
-            Screen:setScreenMode(new_mode, rotation)
+function ReaderView:onSetRotationMode(rotation)
+    if rotation ~= nil then
+        if rotation == Screen:getRotationMode() then
+            return true
         end
-        UIManager:setDirty(self.dialog, "full")
-        local new_screen_size = Screen:getSize()
-        self.ui:handleEvent(Event:new("SetDimensions", new_screen_size))
-        self.ui:onScreenResize(new_screen_size)
-        self.ui:handleEvent(Event:new("InitScrollPageStates"))
+        Screen:setRotationMode(rotation)
     end
+    UIManager:setDirty(self.dialog, "full")
+    local new_screen_size = Screen:getSize()
+    self.ui:handleEvent(Event:new("SetDimensions", new_screen_size))
+    self.ui:onScreenResize(new_screen_size)
+    self.ui:handleEvent(Event:new("InitScrollPageStates"))
     return true
 end
 
@@ -750,15 +723,18 @@ In combination with zoom to fit page, page height, content height or content, co
 end
 
 function ReaderView:onReadSettings(config)
-    local screen_mode
     self.render_mode = config:readSetting("render_mode") or 0
-    if self.ui.document.info.has_pages then
-        screen_mode = config:readSetting("screen_mode") or G_reader_settings:readSetting("kopt_screen_mode") or "portrait"
-    else
-        screen_mode = config:readSetting("screen_mode") or G_reader_settings:readSetting("copt_screen_mode") or "portrait"
+    local locked = G_reader_settings:readSetting("lock_rotation")
+    local rotation_mode = config:readSetting("rotation_mode")
+    if not rotation_mode and locked then
+        if self.ui.document.info.has_pages then
+            rotation_mode = G_reader_settings:readSetting("kopt_rotation_mode") or 0
+        else
+            rotation_mode = G_reader_settings:readSetting("copt_rotation_mode") or 0
+        end
     end
-    if screen_mode then
-        self:onSetScreenMode(screen_mode, config:readSetting("rotation_mode"), true)
+    if rotation_mode then
+        self:onSetRotationMode(rotation_mode)
     end
     self.state.gamma = config:readSetting("gamma") or 1.0
     local full_screen = config:readSetting("kopt_full_screen") or self.document.configurable.full_screen
@@ -845,7 +821,6 @@ end
 
 function ReaderView:onSaveSettings()
     self.ui.doc_settings:saveSetting("render_mode", self.render_mode)
-    self.ui.doc_settings:saveSetting("screen_mode", Screen:getScreenMode())
     self.ui.doc_settings:saveSetting("rotation_mode", Screen:getRotationMode())
     self.ui.doc_settings:saveSetting("gamma", self.state.gamma)
     self.ui.doc_settings:saveSetting("highlight", self.highlight.saved)

frontend/device/input.lua
@@ -667,24 +667,20 @@ end
 
 --- Accelerometer on the Forma, c.f., drivers/hwmon/mma8x5x.c
 function Input:handleMiscEvNTX(ev)
-    local rotation_mode, screen_mode
+    local rotation_mode
     if ev.code == MSC_RAW then
         if ev.value == MSC_RAW_GSENSOR_PORTRAIT_UP then
             -- i.e., UR
             rotation_mode = framebuffer.ORIENTATION_PORTRAIT
-            screen_mode = 'portrait'
         elseif ev.value == MSC_RAW_GSENSOR_LANDSCAPE_RIGHT then
             -- i.e., CW
             rotation_mode = framebuffer.ORIENTATION_LANDSCAPE
-            screen_mode = 'landscape'
         elseif ev.value == MSC_RAW_GSENSOR_PORTRAIT_DOWN then
             -- i.e., UD
             rotation_mode = framebuffer.ORIENTATION_PORTRAIT_ROTATED
-            screen_mode = 'portrait'
         elseif ev.value == MSC_RAW_GSENSOR_LANDSCAPE_LEFT then
             -- i.e., CCW
             rotation_mode = framebuffer.ORIENTATION_LANDSCAPE_ROTATED
-            screen_mode = 'landscape'
         else
             -- Discard FRONT/BACK
             return
@@ -695,18 +691,10 @@ function Input:handleMiscEvNTX(ev)
     end
 
     local old_rotation_mode = self.device.screen:getRotationMode()
-    local old_screen_mode = self.device.screen:getScreenMode()
-    -- NOTE: Try to handle ScreenMode changes sanely, without wrecking the FM, which only supports Portrait/Inverted ;).
     -- NOTE: See the Oasis version just above us for a variant that's locked to the current ScreenMode.
     --       Might be nice to expose the two behaviors to the user, somehow?
-    if rotation_mode ~= old_rotation_mode then
-        if screen_mode ~= old_screen_mode then
-            return Event:new("SwapScreenMode", screen_mode, rotation_mode)
-        else
-            self.device.screen:setRotationMode(rotation_mode)
-            local UIManager = require("ui/uimanager")
-            UIManager:onRotation()
-        end
+    if rotation_mode and rotation_mode ~= old_rotation_mode then
+        return Event:new("SetRotationMode", rotation_mode)
     end
 end
 

frontend/dispatcher.lua
@@ -23,7 +23,7 @@ and optionally
 --]]--
 local settingsList = {
     --CreOptions
-    screen_mode = {category="string"},
+    rotation_mode = {category="string"},
     visible_pages = {category="string"},
     h_page_margins = {category="string"},
     sync_t_b_page_margins = {category="string"},

frontend/ui/data/creoptions.lua
@@ -1,4 +1,5 @@
 local Device = require("device")
+local Screen = Device.screen
 local S = require("ui/data/strings")
 local optionsutil = require("ui/data/optionsutil")
 local _ = require("gettext")
@@ -19,14 +20,15 @@ local CreOptions = {
         icon = "resources/icons/appbar.transform.rotate.right.large.png",
         options = {
             {
-                name = "screen_mode",
+                name = "rotation_mode",
                 name_text = S.SCREEN_MODE,
-                toggle = {S.PORTRAIT, S.LANDSCAPE},
+                toggle = {S.LANDSCAPE_ROTATED, S.PORTRAIT, S.LANDSCAPE, S.PORTRAIT_ROTATED},
                 alternate = false,
-                args = {"portrait", "landscape"},
-                default_arg = "portrait",
-                current_func = function() return Device.screen:getScreenMode() end,
-                event = "ChangeScreenMode",
+                values = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},
+                args = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},
+                default_arg = 0,
+                current_func = function() return Device.screen:getRotationMode() end,
+                event = "SetRotationMode",
                 name_text_hold_callback = optionsutil.showValues,
             },
             {

commit 62c95d5e317b0c29e3a2452b2442f72f5f443b6a

FM: rotation flush setting before reinit
when rotation was caused by a gesture the gesture settings were not flushed leading to weird state when the FM reinit reloaded the gestures plugin

discovered in #6530 caused by #6309

frontend/apps/filemanager/filemanager.lua
@@ -61,6 +61,7 @@ function FileManager:onSetRotationMode(rotation)
     if rotation ~= nil and rotation ~= Screen:getRotationMode() then
         Screen:setRotationMode(rotation)
         if self.instance then
+            UIManager:flushSettings()
             self:reinit(self.instance.path, self.instance.focused_file)
             UIManager:setDirty(self.instance.banner, function()
                 return "ui", self.instance.banner.dimen