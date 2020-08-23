09049908abd1d023e6ff592b858ad4b04eba4480
NiLuJe <ninuje@gmail.com>
2020-08-23 23:43:20 +0200

    Close suspicious fds in the WiFi script
    To prevent any and all issues w/ USBMS down the road...

--- platform/kobo/obtain-ip.sh
+++ platform/kobo/obtain-ip.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# NOTE: Close any non-standard fds, so that it doesn't come back to bite us in the ass with USBMS later...
+for fd in $(ls /proc/$$/fd); do
+    if [ "${fd}" -gt 2 ]; then
+        # NOTE: dash (meaning, in turn, busybox's ash, may also have fd 10 open to /dev/tty)
+        fd_path="$(readlink -f /proc/$$/fd/10)"
+        if [ "${fd_path}" != "/dev/tty" ]; then
+            eval "exec ${fd}>&-"
+            echo "[enable-wifi.sh] Closed fd ${fd} -> ${fd_path}"
+        fi
+    fi
+done
+
 ./release-ip.sh
 
 # NOTE: Prefer dhcpcd over udhcpc if available. That's what Nickel uses,
