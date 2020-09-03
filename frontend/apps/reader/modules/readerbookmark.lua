b545dcb64f92c63b1728a18d8bd860e902950233
Alex Pot <info@smartscripts.nl>
2020-09-03 19:37:35 +0200

    Add indentation to bookmark notes for better readability

--- frontend/apps/reader/modules/readerbookmark.lua
+++ frontend/apps/reader/modules/readerbookmark.lua
@@ -539,9 +539,46 @@ function ReaderBookmark:onShowBookmark(open_navigator)
         if not item.highlighted then
             text = T(_("Page %1 %2 @ %3"), ipage, item.notes, item.datetime)
         end
+
+        local function split(str, pat)
+            local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+            local fpat = "(.-)" .. pat
+            local last_end = 1
+            local s, e, cap = str:find(fpat, 1)
+            while s do
+                if s ~= 1 or cap ~= "" then
+                    table.insert(t, cap)
+                end
+                last_end = e + 1
+                s, e, cap = str:find(fpat, last_end)
+            end
+            if last_end <= #str then
+                cap = str:sub(last_end)
+                table.insert(t, cap)
+            end
+            return t
+        end
+        local function indent(itext)
+            local paras = split(itext, "\n")
+            local skip_next_para = false
+            for nr, para in ipairs(paras) do
+                if nr > 1 and para:match("[A-Za-z]") then
+                    if not skip_next_para then
+                        paras[nr] = "     " .. para
+                    else
+                        skip_next_para = false
+                    end
+                elseif nr > 1 then
+                    skip_next_para = true
+                end
+            end
+            return table.concat(paras, "\n")
+        end
+
         self.textviewer = TextViewer:new{
             title = title,
-            text = text,
+            -- add indentation for better readability:
+            text = indent(text),
             -- make text easier to read:
             justified = false,
             text_face = Font:getFace("xx_smallinfofont"),
