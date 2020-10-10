5e4cb4a1df62ef3fafc8087fb0cdd19e44c5f066
NiLuJe <ninuje@gmail.com>
2020-10-10 21:26:07 +0200

    Follow https://github.com/koreader/koreader-base/pull/1212

--- plugins/coverbrowser.koplugin/xutil.lua
+++ plugins/coverbrowser.koplugin/xutil.lua
@@ -32,17 +32,4 @@ function xutil.zlib_uncompress(zdata, datalen)
     return ffi.string(buf, buflen[0])
 end
 
--- Not provided by base/thirdparty/lua-ljsqlite3/init.lua
--- Add a timeout to a lua-ljsqlite3 connection
--- We need that if we have multiple processes accessing the same
--- SQLite db for reading or writting (read lock and write lock can't be
--- obtained at the same time, so waiting & retry is needed)
--- SQLite will retry getting a lock every 1ms to 100ms for
--- the timeout_ms given here
-local sql = ffi.load("sqlite3")
-function xutil.sqlite_set_timeout(conn, timeout_ms)
-    sql.sqlite3_busy_timeout(conn._ptr, timeout_ms)
-end
--- For reference, SQ3 doc at: http://scilua.org/ljsqlite3.html
-
 return xutil
