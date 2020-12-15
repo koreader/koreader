77342908e639746447c460ab5d1e1926b2e49905
NiLuJe <ninuje@gmail.com>
2020-12-15 19:13:55 +0100

    CI: Update to Busted 2.0.0

--- .ci/helper_luarocks.sh
+++ .ci/helper_luarocks.sh
@@ -11,11 +11,8 @@ echo "wrap_bin_scripts = false" >>"${HOME}/.luarocks/config.lua"
 travis_retry luarocks --local install luafilesystem
 # for verbose_print module
 travis_retry luarocks --local install ansicolors
-travis_retry luarocks --local install busted 2.0.rc13-0
+travis_retry luarocks --local install busted 2.0.0-1
 #- mv -f $HOME/.luarocks/bin/busted_bootstrap $HOME/.luarocks/bin/busted
-# Apply junit testcase time fix. This can be removed once there is a busted 2.0.rc13 or final
-# See https://github.com/Olivine-Labs/busted/commit/830f175c57ca3f9e79f95b8c4eaacf58252453d7
-sed -i 's|testcase_node.time = formatDuration(element.duration)|testcase_node:set_attrib("time", formatDuration(element.duration))|' "${HOME}/.luarocks/share/lua/5.1/busted/outputHandlers/junit.lua"
 
 travis_retry luarocks --local install luacheck
 travis_retry luarocks --local install lanes # for parallel luacheck
