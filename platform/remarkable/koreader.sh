3757ea3aa1e3cd64481aeae55c0e0eaa1d6d2543
Frans de Jonge <fransdejonge@gmail.com>
2020-12-18 23:21:34 +0100

    Minor formatting fixes
    Cf. <https://github.com/koreader/koreader/pull/6992>.

--- platform/remarkable/koreader.sh
+++ platform/remarkable/koreader.sh
@@ -7,9 +7,9 @@ KOREADER_DIR="${0%/*}"
 cd "${KOREADER_DIR}" || exit
 
 # reMarkable 2 check
-IFS= read -r MACHINE_TYPE < "/sys/devices/soc0/machine"
-if [ "reMarkable 2.0" = "$MACHINE_TYPE" ]; then
-    if [ -z "$RM2FB_SHIM" ]; then
+IFS= read -r MACHINE_TYPE <"/sys/devices/soc0/machine"
+if [ "reMarkable 2.0" = "${MACHINE_TYPE}" ]; then
+    if [ -z "${RM2FB_SHIM}" ]; then
         echo "reMarkable 2 requires RM2FB to work, visit https://github.com/ddvk/remarkable2-framebuffer for instructions how to setup"
         exit 1
     fi
