7eee3f969b7d6d1926a103ad4290ca284e68e902
NiLuJe <ninuje@gmail.com>
2020-12-23 17:37:13 +0100

    sigh.

--- frontend/device/android/device.lua
+++ frontend/device/android/device.lua
@@ -209,24 +209,6 @@ function Device:init()
         setClipboardText = function(text)
             return android.setClipboardText(text)
         end,
-
-        -- Make sure we won't ever fallback to the Lua BB...
-        local bb = require("ffi/blitbuffer")
-        bb.BB_mt.__index:canUseCbbTogether = bb.getUseCBB
-        bb.BB4_mt.__index.canUseCbbTogether = bb.getUseCBB
-        bb.BB8_mt.__index.canUseCbbTogether = bb.getUseCBB
-        bb.BB8A_mt.__index.canUseCbbTogether = bb.getUseCBB
-        bb.BBRGB16_mt.__index.canUseCbbTogether = bb.getUseCBB
-        bb.BBRGB24_mt.__index.canUseCbbTogether = bb.getUseCBB
-        bb.BBRGB32_mt.__index.canUseCbbTogether = bb.getUseCBB
-
-        bb.BB_mt.__index:canUseCbb = bb.getUseCBB
-        bb.BB4_mt.__index.canUseCbb = bb.getUseCBB
-        bb.BB8_mt.__index.canUseCbb = bb.getUseCBB
-        bb.BB8A_mt.__index.canUseCbb = bb.getUseCBB
-        bb.BBRGB16_mt.__index.canUseCbb = bb.getUseCBB
-        bb.BBRGB24_mt.__index.canUseCbb = bb.getUseCBB
-        bb.BBRGB32_mt.__index.canUseCbb = bb.getUseCBB
     }
 
     -- check if we have a keyboard
@@ -276,6 +258,24 @@ function Device:init()
     if G_reader_settings:isTrue("android_ignore_back_button") then
         android.setBackButtonIgnored(true)
     end
+
+    -- Make sure we won't ever fallback to the Lua BB...
+    local bb = require("ffi/blitbuffer")
+    bb.BB_mt.__index:canUseCbbTogether = bb.getUseCBB
+    bb.BB4_mt.__index.canUseCbbTogether = bb.getUseCBB
+    bb.BB8_mt.__index.canUseCbbTogether = bb.getUseCBB
+    bb.BB8A_mt.__index.canUseCbbTogether = bb.getUseCBB
+    bb.BBRGB16_mt.__index.canUseCbbTogether = bb.getUseCBB
+    bb.BBRGB24_mt.__index.canUseCbbTogether = bb.getUseCBB
+    bb.BBRGB32_mt.__index.canUseCbbTogether = bb.getUseCBB
+
+    bb.BB_mt.__index:canUseCbb = bb.getUseCBB
+    bb.BB4_mt.__index.canUseCbb = bb.getUseCBB
+    bb.BB8_mt.__index.canUseCbb = bb.getUseCBB
+    bb.BB8A_mt.__index.canUseCbb = bb.getUseCBB
+    bb.BBRGB16_mt.__index.canUseCbb = bb.getUseCBB
+    bb.BBRGB24_mt.__index.canUseCbb = bb.getUseCBB
+    bb.BBRGB32_mt.__index.canUseCbb = bb.getUseCBB
 
     Generic.init(self)
 end
