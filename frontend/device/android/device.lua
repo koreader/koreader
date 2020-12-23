331563f05f7bbaeb72b708b63b5e810df23f8545
NiLuJe <ninuje@gmail.com>
2020-12-23 17:34:36 +0100

    Heinous hackery to avoid the Lua BB on Android...

--- frontend/device/android/device.lua
+++ frontend/device/android/device.lua
@@ -209,6 +209,24 @@ function Device:init()
         setClipboardText = function(text)
             return android.setClipboardText(text)
         end,
+
+        -- Make sure we won't ever fallback to the Lua BB...
+        local bb = require("ffi/blitbuffer")
+        bb.BB_mt.__index:canUseCbbTogether = bb.getUseCBB
+        bb.BB4_mt.__index.canUseCbbTogether = bb.getUseCBB
+        bb.BB8_mt.__index.canUseCbbTogether = bb.getUseCBB
+        bb.BB8A_mt.__index.canUseCbbTogether = bb.getUseCBB
+        bb.BBRGB16_mt.__index.canUseCbbTogether = bb.getUseCBB
+        bb.BBRGB24_mt.__index.canUseCbbTogether = bb.getUseCBB
+        bb.BBRGB32_mt.__index.canUseCbbTogether = bb.getUseCBB
+
+        bb.BB_mt.__index:canUseCbb = bb.getUseCBB
+        bb.BB4_mt.__index.canUseCbb = bb.getUseCBB
+        bb.BB8_mt.__index.canUseCbb = bb.getUseCBB
+        bb.BB8A_mt.__index.canUseCbb = bb.getUseCBB
+        bb.BBRGB16_mt.__index.canUseCbb = bb.getUseCBB
+        bb.BBRGB24_mt.__index.canUseCbb = bb.getUseCBB
+        bb.BBRGB32_mt.__index.canUseCbb = bb.getUseCBB
     }
 
     -- check if we have a keyboard
