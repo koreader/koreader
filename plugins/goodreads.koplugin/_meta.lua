107d9bebd9bf64d629a3cab88d8f751997c765ef
Martín Fdez <paziusss@gmail.com>
2021-01-01 16:32:45 +0100

    deprecate Goodreads plugin

--- plugins/goodreads.koplugin/_meta.lua
+++ plugins/goodreads.koplugin/_meta.lua
@@ -3,4 +3,5 @@ return {
     name = "goodreads",
     fullname = _("Goodreads"),
     description = _([[Allows browsing and searching the Goodreads database of books.]]),
+    deprecated = "Goodreads API was closed. Some keys still work. This plugin will be removed soon",
 }
