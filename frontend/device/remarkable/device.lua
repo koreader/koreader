5b5d156938715dea319811ad25835f8e8d8af53f
Glen Sawyer <github@snelg.com>
2021-01-09 10:54:01 -0700

    Parameterize wifi on/of for rm1 or rm2

--- frontend/device/remarkable/device.lua
+++ frontend/device/remarkable/device.lua
@@ -18,8 +18,16 @@ local wacom_height = 20967 -- unscaled_size_check: ignore
 local wacom_scale_x = screen_width / wacom_width
 local wacom_scale_y = screen_height / wacom_height
 
-local function turnOffWifi()
-    os.execute("./disable-rm2-wifi.sh")
+local function turnOnWifi(model)
+    if model == "reMarkable 2" then
+        os.execute("./enable-rm2-wifi.sh")
+    end
+end
+
+local function turnOffWifi(model)
+    if model == "reMarkable 2" then
+        os.execute("./disable-rm2-wifi.sh")
+    end
 end
 
 local Remarkable = Generic:new{
@@ -134,16 +142,18 @@ end
 
 function Remarkable:supportsScreensaver() return true end
 
-function Remarkable2:initNetworkManager(NetworkMgr)
+function Remarkable:initNetworkManager(NetworkMgr)
+    local model = self.model
+
     function NetworkMgr:turnOnWifi(complete_callback)
-        os.execute("./enable-rm2-wifi.sh")
+        turnOnWifi(model)
         self:reconnectOrShowNetworkMenu(function()
             self:connectivityCheck(1, complete_callback)
         end)
     end
 
     function NetworkMgr:turnOffWifi(complete_callback)
-        turnOffWifi()
+        turnOffWifi(model)
         if complete_callback then
             complete_callback()
         end
@@ -177,7 +187,7 @@ end
 
 function Remarkable2:suspend()
     -- Need to remove brcmfmac kernel module before suspend
-    turnOffWifi()
+    turnOffWifi("reMarkable 2")
 
     os.execute("systemctl suspend")
     -- While device is suspended, when the user presses the power button and wakes up the device,
