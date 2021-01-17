f6b67fb8f696494d76e547ab846dfd61778440fb
NiLuJe <ninuje@gmail.com>
2021-01-18 00:35:39 +0100

    If we open something that shows the VK, forget the HL
    That usually means TextEditor, Terminal & friends.
    And the way the VK is shown, it would brak the rest of our heuristics
    anyway (VK is a modal, it's displayed in a single specific flashing
    refresh, which leans it won't coalesce with whatever else might be
    shown, making getPrevrefreshRegion useless, etc.)

--- plugins/texteditor.koplugin/main.lua
+++ plugins/texteditor.koplugin/main.lua
@@ -29,6 +29,7 @@ local TextEditor = WidgetContainer:new{
     normal_font = "x_smallinfofont",
     monospace_font = "infont",
     min_file_size_warn = 200000, -- warn/ask when opening files bigger than this
+    covers_fullscreen = true, -- Asking for a fullscreen InputDialog also flags InputDialog itself that way, but it's not enough.
 }
 
 function TextEditor:onDispatcherRegisterActions()
