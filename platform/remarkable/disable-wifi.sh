4960e3dcf8f4b15e5b321c99fcc680b76e2f5ea7
Martín Fdez <paziusss@gmail.com>
2021-01-31 15:40:49 +0100

    add wifi support to Rm1

--- platform/remarkable/disable-wifi.sh
+++ platform/remarkable/disable-wifi.sh
@@ -2,17 +2,13 @@
 
 # disable wifi and remove modules
 
-read -r MACHINE_TYPE <"/sys/devices/soc0/machine"
+# clean stop (if it's running) of main wpa_supplicant service, used by xochitl
+systemctl stop wpa_supplicant
+# clean stop of non-service wpa_supplicant, if running
+wpa_cli terminate 2>/dev/null
 
-if [ "reMarkable 2.0" = "${MACHINE_TYPE}" ]; then
-    # clean stop (if it's running) of main wpa_supplicant service, used by xochitl
-    systemctl stop wpa_supplicant
-    # clean stop of non-service wpa_supplicant, if running
-    wpa_cli terminate 2>/dev/null
+# power down wifi interface
+ifconfig wlan0 down 2>/dev/null
 
-    # power down wifi interface
-    ifconfig wlan0 down 2>/dev/null
-
-    # remove module: IMPORTANT to do this before device suspends
-    modprobe -r brcmfmac 2>/dev/null
-fi
+# remove module
+modprobe -r brcmfmac 2>/dev/null
