35f964d44637de47fb514c13fb727986369187c8
NiLuJe <ninuje@gmail.com>
2021-02-09 18:05:57 +0100

    Mimic the usual typedef style

--- frontend/ui/uimanager.lua
+++ frontend/ui/uimanager.lua
@@ -979,7 +979,7 @@ end
 --[[--
 Request events to be ignored for some duration.
 
-@param set_or_seconds either `true`, in which case a platform-specific delay is chosen, or a duration in seconds (`int`).
+@param set_or_seconds either `true`, in which case a platform-specific delay is chosen, or a duration in seconds (***int***).
 ]]
 function UIManager:discardEvents(set_or_seconds)
     if not set_or_seconds then -- remove any previously set
