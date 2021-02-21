97fdbf7241bf0775cc2573d9b5cc3e859320d55a
NiLuJe <ninuje@gmail.com>
2021-02-21 03:54:24 +0100

    But that means we can just let the kernel sort it out.
    It'll most likely block for quite a bit longer than 100ms, but, eh.

--- frontend/ui/widget/touchmenu.lua
+++ frontend/ui/widget/touchmenu.lua
@@ -27,7 +27,6 @@ local UIManager = require("ui/uimanager")
 local UnderlineContainer = require("ui/widget/container/underlinecontainer")
 local VerticalGroup = require("ui/widget/verticalgroup")
 local VerticalSpan = require("ui/widget/verticalspan")
-local ffiUtil = require("ffi/util")
 local util = require("util")
 local getMenuText = require("ui/widget/menu").getMenuText
 local _ = require("gettext")
@@ -215,11 +214,13 @@ function TouchMenuItem:onHoldSelect(arg, ges)
         UIManager:widgetInvert(self.item_frame, highlight_dimen.x, highlight_dimen.y, highlight_dimen.w)
         UIManager:setDirty(nil, "fast", highlight_dimen)
 
-        -- Stupid hack because the EPDC has decided to be race-y in this very specific case.
-        -- Empirical evidence suggests using 100ms does the job well enough.
-        --ffiUtil.usleep(100 * 1000)
         UIManager:forceRePaint()
-        ffiUtil.usleep(100 * 1000)
+        -- NOTE: These very specific circumstances appear to reliably upset the EPDC,
+        --       causing a mild variant of our racey friend the papercut refresh glitch ;).
+        --       As it appears to stem from the race between *this* refresh for the highlight and the following writes to the fb,
+        --       let the kernel take a breather. It'll yield back to us when it's done.
+        --       Expect it to block for ~150 to 350ms.
+        UIManager:waitForVSync()
 
         -- Unhighlight
         --
