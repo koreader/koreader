793281e19ffa32fac0b9e91b93800756d16ce6b7
NiLuJe <ninuje@gmail.com>
2021-02-28 23:40:39 +0100

    Tweak opacity

--- frontend/apps/reader/modules/readerdogear.lua
+++ frontend/apps/reader/modules/readerdogear.lua
@@ -82,6 +82,7 @@ function ReaderDogear:onSetPageMargins(margins)
     -- (the provided margins are not scaled, so do as ReaderTypeset)
     local margin = Screen:scaleBySize(math.max(margin_top, margin_right))
     local new_dogear_size = math.max(self.dogear_min_size, math.min(self.dogear_max_size, margin))
+    print("Dogear clamped", margin, "between", self.dogear_min_size, "and", self.dogear_max_size, "to", new_dogear_size)
     self:setupDogear(new_dogear_size)
 end
 
