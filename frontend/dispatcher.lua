koreader/koreader

commit 1820c616a17ccf43dc2a6b4b088506b0ec55cb5e

[RFC] Switch ConfigDialog's rotation toggle to custom icons (#7306)

frontend/ui/data/creoptions.lua
@@ -2,7 +2,6 @@ local Device = require("device")
 local Screen = Device.screen
 local optionsutil = require("ui/data/optionsutil")
 local _ = require("gettext")
-local C_ = _.pgettext
 
 -- Get font size numbers as a table of strings
 local tableOfNumbersToTableOfStrings = function(numbers)
@@ -22,7 +21,12 @@ local CreOptions = {
             {
                 name = "rotation_mode",
                 name_text = _("Rotation"),
-                toggle = {C_("Rotation", "⤹ 90°"), C_("Rotation", "↑ 0°"), C_("Rotation", "⤸ 90°"), C_("Rotation", "↓ 180°")},
+                item_icons = {
+                    "rotation.90CCW",
+                    "rotation.0UR",
+                    "rotation.90CW",
+                    "rotation.180UD",
+                },
                 alternate = false,
                 values = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},
                 args = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},

frontend/ui/data/koptoptions.lua
@@ -29,7 +29,12 @@ local KoptOptions = {
             {
                 name = "rotation_mode",
                 name_text = _("Rotation"),
-                toggle = {C_("Rotation", "⤹ 90°"), C_("Rotation", "↑ 0°"), C_("Rotation", "⤸ 90°"), C_("Rotation", "↓ 180°")},
+                item_icons = {
+                    "rotation.90CCW",
+                    "rotation.0UR",
+                    "rotation.90CW",
+                    "rotation.180UD",
+                },
                 alternate = false,
                 values = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},
                 args = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},

resources/icons/mdlight/appbar.rotation.svg
@@ -14,8 +14,8 @@
    enable-background="new 0 0 24.00 24.00"
    xml:space="preserve"
    id="svg4"
-   sodipodi:docname="appbar.transform.rotate.right.svg"
-   inkscape:version="1.0.1 (3bc2e813f5, 2020-09-07)"
+   sodipodi:docname="appbar.rotation.svg"
+   inkscape:version="1.0.2 (e86c870879, 2021-01-15)"
    inkscape:export-xdpi="96"
    inkscape:export-ydpi="96"><sodipodi:namedview
    pagecolor="#ffffff"
@@ -26,19 +26,19 @@
    guidetolerance="10"
    inkscape:pageopacity="0"
    inkscape:pageshadow="2"
-   inkscape:window-width="1914"
-   inkscape:window-height="986"
+   inkscape:window-width="2560"
+   inkscape:window-height="1381"
    id="namedview18"
    showgrid="false"
    showguides="true"
    inkscape:snap-page="false"
-   inkscape:zoom="1"
-   inkscape:cx="53.287889"
-   inkscape:cy="-4.9413697"
+   inkscape:zoom="5"
+   inkscape:cx="29.583019"
+   inkscape:cy="23.630059"
    inkscape:window-x="0"
-   inkscape:window-y="72"
-   inkscape:window-maximized="0"
-   inkscape:current-layer="svg4"
+   inkscape:window-y="0"
+   inkscape:window-maximized="1"
+   inkscape:current-layer="g857"
    inkscape:document-rotation="0"><inkscape:grid
      type="xygrid"
      id="grid843"
@@ -47,25 +47,30 @@
      empspacing="4" /></sodipodi:namedview><metadata
    id="metadata10"><rdf:RDF><cc:Work
        rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type
-         rdf:resource="http://purl.org/dc/dcmitype/StillImage" /><dc:title></dc:title></cc:Work></rdf:RDF></metadata><defs
+         rdf:resource="http://purl.org/dc/dcmitype/StillImage" /><dc:title /></cc:Work></rdf:RDF></metadata><defs
    id="defs8" />
 	<path
    id="rect1037"
    style="fill:none;stroke:#000000;stroke-width:2;stroke-linejoin:round"
    d="m 39,20 1.485714,-1e-6 C 41.87863,19.999998 43,21.155335 43,22.590438 V 24 m 0,3 v 6 m 0,3 v 1.409562 C 43,38.844666 41.87863,40 40.485714,40 H 39 m -3,0 h -5 m -3,0 H 22 M 25,20.000006 28,20 m 3,0 h 5"
    sodipodi:nodetypes="cssccccssccccccccc" />
-<path
-   style="fill:none;stroke:#000000;stroke-width:1.6;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-   id="path1207"
-   d="m 26.343863,5.1077763 c 6.403668,0.3263125 9.324525,2.1415081 11.18675,6.5671527"
-   sodipodi:nodetypes="cc" /><path
-   style="fill:none;stroke:#000000;stroke-width:1.6;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1;stroke-miterlimit:4;stroke-dasharray:none"
-   d="m 32.850146,10.883255 4.82549,1.035239 1.038824,-4.8422044"
-   id="path1209" /><rect
+<rect
    style="fill:none;stroke:#000000;stroke-width:2;stroke-linejoin:round"
    id="rect1443"
    width="19"
    height="30.000002"
    x="6"
    y="9.9999971"
-   ry="2.5714288" /></svg>
+   ry="2.5714288" /><g
+   id="g857"
+   transform="translate(2.3668145,0.61145338)"><path
+     style="fill:none;stroke:#000000;stroke-width:1.6;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+     id="path1207"
+     d="m 26.343863,5.1077763 c 6.403668,0.3263125 9.324525,2.1415081 11.18675,6.5671527"
+     sodipodi:nodetypes="cc" /><path
+     style="fill:none;stroke:#000000;stroke-width:1.6;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+     d="m 32.850146,10.883255 4.82549,1.035239 1.038824,-4.8422044"
+     id="path1209" /><path
+     style="fill:none;stroke:#000000;stroke-width:1.6;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+     d="m 30.427529,2.3363607 -4.083666,2.7714156 2.781015,4.09781"
+     id="path1209-3" /></g></svg>

resources/icons/mdlight/rotation.0UR.svg
@@ -1,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   id="svg4"
+   width="48"
+   height="48"
+   enable-background="new 0 0 24.00 24.00"
+   version="1.1"
+   viewBox="0 0 48 48"
+   xml:space="preserve"
+   sodipodi:docname="rotation.0UR.svg"
+   inkscape:version="1.0.2 (e86c870879, 2021-01-15)"><defs
+   id="defs10" /><sodipodi:namedview
+   pagecolor="#ffffff"
+   bordercolor="#666666"
+   borderopacity="1"
+   objecttolerance="10"
+   gridtolerance="10"
+   guidetolerance="10"
+   inkscape:pageopacity="0"
+   inkscape:pageshadow="2"
+   inkscape:window-width="2560"
+   inkscape:window-height="1381"
+   id="namedview8"
+   showgrid="true"
+   inkscape:zoom="21"
+   inkscape:cx="24"
+   inkscape:cy="24"
+   inkscape:window-x="0"
+   inkscape:window-y="0"
+   inkscape:window-maximized="1"
+   inkscape:current-layer="svg4"
+   inkscape:document-rotation="0"><inkscape:grid
+     type="xygrid"
+     id="grid833" /></sodipodi:namedview><metadata
+   id="metadata10"><rdf:RDF><cc:Work
+       rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type
+         rdf:resource="http://purl.org/dc/dcmitype/StillImage" /><dc:title /></cc:Work></rdf:RDF></metadata>
+	
+<path
+   id="path1605"
+   d="m 42.597927,16.604159 v 13.6"
+   fill="none"
+   stroke="#000000"
+   stroke-width="2"
+   style="stroke-width:1.6;stroke-miterlimit:4;stroke-dasharray:none"
+   sodipodi:nodetypes="cc" /><path
+   id="path1607"
+   d="m 38.597927,20.604159 4,-4.5 4,4.5"
+   fill="none"
+   stroke="#000000"
+   stroke-width="2"
+   style="stroke-width:1.6;stroke-miterlimit:4;stroke-dasharray:none"
+   sodipodi:nodetypes="ccc" /><g
+   id="g847"
+   transform="translate(5.5000021)"><rect
+     id="rect1443"
+     x="9"
+     y="8"
+     width="19"
+     height="31"
+     ry="2.5999999"
+     fill="none"
+     stroke="#000000"
+     stroke-linejoin="round"
+     stroke-width="2" /><path
+     style="fill:#000000;fill-opacity:1;fill-rule:evenodd;stroke:#000000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+     d="M 28,35 H 9 c 0,2 0.152585,4.046356 3,4 h 13 c 2,0 3,-1 3,-4 z"
+     id="path842"
+     sodipodi:nodetypes="ccccc" /></g></svg>

resources/icons/mdlight/rotation.180UD.svg
@@ -1,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   id="svg4"
+   width="48"
+   height="48"
+   enable-background="new 0 0 24.00 24.00"
+   version="1.1"
+   viewBox="0 0 48 48"
+   xml:space="preserve"
+   sodipodi:docname="rotation.180UD.svg"
+   inkscape:version="1.0.2 (e86c870879, 2021-01-15)"><defs
+   id="defs10" /><sodipodi:namedview
+   pagecolor="#ffffff"
+   bordercolor="#666666"
+   borderopacity="1"
+   objecttolerance="10"
+   gridtolerance="10"
+   guidetolerance="10"
+   inkscape:pageopacity="0"
+   inkscape:pageshadow="2"
+   inkscape:window-width="2560"
+   inkscape:window-height="1381"
+   id="namedview8"
+   showgrid="true"
+   inkscape:zoom="21"
+   inkscape:cx="24"
+   inkscape:cy="23.714286"
+   inkscape:window-x="0"
+   inkscape:window-y="0"
+   inkscape:window-maximized="1"
+   inkscape:current-layer="svg4"
+   inkscape:document-rotation="0"><inkscape:grid
+     type="xygrid"
+     id="grid833" /></sodipodi:namedview><metadata
+   id="metadata10"><rdf:RDF><cc:Work
+       rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type
+         rdf:resource="http://purl.org/dc/dcmitype/StillImage" /><dc:title /></cc:Work></rdf:RDF></metadata>
+	
+<path
+   id="path1605"
+   d="M 42.597927,30.5 V 16.9"
+   fill="none"
+   stroke="#000000"
+   stroke-width="2"
+   style="stroke-width:1.6;stroke-miterlimit:4;stroke-dasharray:none"
+   sodipodi:nodetypes="cc" /><path
+   id="path1607"
+   d="m 38.597927,26.5 4,4.5 4,-4.5"
+   fill="none"
+   stroke="#000000"
+   stroke-width="2"
+   style="stroke-width:1.6;stroke-miterlimit:4;stroke-dasharray:none"
+   sodipodi:nodetypes="ccc" /><g
+   id="g847"
+   transform="translate(5.5,5.8655582e-4)"><rect
+     id="rect1443"
+     x="9"
+     y="-39"
+     width="19"
+     height="31"
+     ry="2.5999999"
+     fill="none"
+     stroke="#000000"
+     stroke-linejoin="round"
+     stroke-width="2"
+     transform="scale(1,-1)" /><path
+     style="fill:#000000;fill-opacity:1;fill-rule:evenodd;stroke:#000000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+     d="M 9,12 H 28 C 28,10 27.847415,7.953644 25,8 H 12 C 10,8 9,9 9,12 Z"
+     id="path842"
+     sodipodi:nodetypes="ccccc" /></g></svg>

resources/icons/mdlight/rotation.90CCW.svg
@@ -1,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   id="svg4"
+   width="48"
+   height="48"
+   enable-background="new 0 0 24.00 24.00"
+   version="1.1"
+   viewBox="0 0 48 48"
+   xml:space="preserve"
+   sodipodi:docname="rotation.90CCW.svg"
+   inkscape:version="1.0.2 (e86c870879, 2021-01-15)"><defs
+   id="defs14" /><sodipodi:namedview
+   pagecolor="#ffffff"
+   bordercolor="#666666"
+   borderopacity="1"
+   objecttolerance="10"
+   gridtolerance="10"
+   guidetolerance="10"
+   inkscape:pageopacity="0"
+   inkscape:pageshadow="2"
+   inkscape:window-width="2560"
+   inkscape:window-height="1381"
+   id="namedview12"
+   showgrid="true"
+   inkscape:zoom="22.5"
+   inkscape:cx="21.577778"
+   inkscape:cy="24.057375"
+   inkscape:window-x="0"
+   inkscape:window-y="0"
+   inkscape:window-maximized="1"
+   inkscape:current-layer="g876"
+   inkscape:document-rotation="0"><inkscape:grid
+     type="xygrid"
+     id="grid837" /></sodipodi:namedview><metadata
+   id="metadata10"><rdf:RDF><cc:Work
+       rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type
+         rdf:resource="http://purl.org/dc/dcmitype/StillImage" /><dc:title></dc:title></cc:Work></rdf:RDF></metadata>
+
+<g
+   id="g910"
+   transform="matrix(-1,0,0,1,46.299999,2.9999387)"
+   fill="none"
+   stroke="#000000"
+   stroke-width="1.6"><path
+     id="path1207"
+     d="m 26,5.1 c 6.4,0.33 9.3,2.1 11,6.6" /><path
+     id="path1209"
+     d="m 33,11 4.8,1 1,-4.8" /></g><g
+   id="g906"
+   transform="matrix(-1,0,0,1,46.799999,1.5)"
+   fill="none"
+   stroke="#000000"
+   stroke-linejoin="round"
+   stroke-width="2"><path
+     id="rect1037"
+     d="M 2.8,6.5 2.799999,5 c -9e-7,-1.4 1.1,-2.5 2.5,-2.5 h 1.3 M 9.4,2.5 h 5.799999 M 18,2.5 h 1.3 c 1.4,0 2.5,1.1 2.5,2.5 v 1.5 m 0,2.75 v 4 m -19.000001,0 v -4"
+     sodipodi:nodetypes="cssccccssccccc"
+     style="stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none" /><path
+     id="rect1037-6"
+     d="M 15.2,33.5 H 9.5 m -2.9,0 H 5.3 C 3.9,33.5 2.8,32.4 2.8,31 v -1.5 m 0,-2.75 v -4"
+     style="stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+     sodipodi:nodetypes="cccssccc" /></g><g
+   aria-label="k"
+   id="text841"
+   style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:16px;line-height:125%;font-family:Diavlo;-inkscape-font-specification:Diavlo;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+   transform="rotate(90,31.339,30.209)" /><path
+   style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+   d="m 43.999999,17.5 v 4"
+   id="path901"
+   sodipodi:nodetypes="cc" /><path
+   style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+   d="m 24.999999,17.5 v 2.7"
+   id="path903"
+   sodipodi:nodetypes="cc" /><g
+   id="g876"><rect
+     id="rect1443"
+     transform="matrix(0,1,1,0,0,0)"
+     x="20"
+     y="3.999999"
+     width="19"
+     height="30.999996"
+     ry="2.5999999"
+     fill="none"
+     stroke="#000000"
+     stroke-linejoin="round"
+     stroke-width="2" /><path
+     style="fill:#000000;fill-opacity:1;fill-rule:evenodd;stroke:#000000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+     d="M 30.999999,39 V 20 c 2,0 4.046356,0.152585 4,3 v 13 c 0,2 -1,3 -4,3 z"
+     id="path842"
+     sodipodi:nodetypes="ccccc" /></g></svg>

resources/icons/mdlight/rotation.90CW.svg
@@ -1,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   id="svg4"
+   width="48"
+   height="48"
+   enable-background="new 0 0 24.00 24.00"
+   version="1.1"
+   viewBox="0 0 48 48"
+   xml:space="preserve"
+   sodipodi:docname="rotation.90CW.svg"
+   inkscape:version="1.0.2 (e86c870879, 2021-01-15)"><defs
+   id="defs14" /><sodipodi:namedview
+   pagecolor="#ffffff"
+   bordercolor="#666666"
+   borderopacity="1"
+   objecttolerance="10"
+   gridtolerance="10"
+   guidetolerance="10"
+   inkscape:pageopacity="0"
+   inkscape:pageshadow="2"
+   inkscape:window-width="2560"
+   inkscape:window-height="1381"
+   id="namedview12"
+   showgrid="true"
+   inkscape:zoom="22.5"
+   inkscape:cx="21.577778"
+   inkscape:cy="24.057375"
+   inkscape:window-x="0"
+   inkscape:window-y="0"
+   inkscape:window-maximized="1"
+   inkscape:current-layer="svg4"
+   inkscape:document-rotation="0"><inkscape:grid
+     type="xygrid"
+     id="grid837" /></sodipodi:namedview><metadata
+   id="metadata10"><rdf:RDF><cc:Work
+       rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type
+         rdf:resource="http://purl.org/dc/dcmitype/StillImage" /><dc:title /></cc:Work></rdf:RDF></metadata>
+
+<g
+   id="g910"
+   transform="translate(1.7,2.9999387)"
+   fill="none"
+   stroke="#000000"
+   stroke-width="1.6"><path
+     id="path1207"
+     d="m 26,5.1 c 6.4,0.33 9.3,2.1 11,6.6" /><path
+     id="path1209"
+     d="m 33,11 4.8,1 1,-4.8" /></g><g
+   id="g906"
+   transform="translate(1.2 1.5)"
+   fill="none"
+   stroke="#000"
+   stroke-linejoin="round"
+   stroke-width="2"><path
+     id="rect1037"
+     d="M 2.8,6.5 2.799999,5 c -9e-7,-1.4 1.1,-2.5 2.5,-2.5 h 1.3 M 9.4,2.5 h 5.799999 M 18,2.5 h 1.3 c 1.4,0 2.5,1.1 2.5,2.5 v 1.5 m 0,2.75 v 4 m -19.000001,0 v -4"
+     sodipodi:nodetypes="cssccccssccccc"
+     style="stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none" /><path
+     id="rect1037-6"
+     d="M 15.2,33.5 H 9.5 m -2.9,0 H 5.3 C 3.9,33.5 2.8,32.4 2.8,31 v -1.5 m 0,-2.75 v -4"
+     style="stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+     sodipodi:nodetypes="cccssccc" /></g><g
+   aria-label="k"
+   id="text841"
+   style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:16px;line-height:125%;font-family:Diavlo;-inkscape-font-specification:Diavlo;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+   transform="rotate(90,31.339,30.209)" /><path
+   style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+   d="m 4,17.5 v 4"
+   id="path901"
+   sodipodi:nodetypes="cc" /><path
+   style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+   d="m 23,17.5 v 2.7"
+   id="path903"
+   sodipodi:nodetypes="cc" /><g
+   id="g876"><rect
+     id="rect1443"
+     transform="rotate(90)"
+     x="20"
+     y="-44"
+     width="19"
+     height="30.999996"
+     ry="2.5999999"
+     fill="none"
+     stroke="#000000"
+     stroke-linejoin="round"
+     stroke-width="2" /><path
+     style="fill:#000000;fill-rule:evenodd;stroke:#000000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;fill-opacity:1"
+     d="M 17,39 V 20 c -2,0 -4.046356,0.152585 -4,3 v 13 c 0,2 1,3 4,3 z"
+     id="path842"
+     sodipodi:nodetypes="ccccc" /></g></svg>

commit f4706567a3139db18d33e7d7c9fcdb31fd267e4e

Unbreak (Reader) rotation labels in Dispatcher
Regression since #7306

frontend/dispatcher.lua
@@ -599,6 +599,7 @@ arguments are:
     3) optionally a `gestures`object
 --]]--
 function Dispatcher:execute(ui, settings, gesture)
+    print("Dispatcher:execute", ui, settings, gesture)
     for k, v in pairs(settings) do
         if settingsList[k] ~= nil and (settingsList[k].conditions == nil or settingsList[k].conditions == true) then
             if settingsList[k].category == "none" then
@@ -624,13 +625,15 @@ function Dispatcher:execute(ui, settings, gesture)
                 ui:handleEvent(Event:new(settingsList[k].event, arg))
             end
             if settingsList[k].configurable ~= nil then
-                 local value = v
-                 if type(v) ~= "number" then
-                     for i, r in ipairs(settingsList[k].args) do
+                local value = v
+                if type(v) ~= "number" then
+                    for i, r in ipairs(settingsList[k].args) do
                         if v == r then value = settingsList[k].configurable.values[i] break end
-                     end
-                 end
-                 ui.document.configurable[settingsList[k].configurable.name] = value
+                    end
+                end
+                print("Have a configurable:", settingsList[k].configurable.name, value)
+                print("Document?", ui.document)
+                ui.document.configurable[settingsList[k].configurable.name] = value
             end
         end
     end

frontend/ui/data/creoptions.lua
@@ -2,6 +2,7 @@ local Device = require("device")
 local Screen = Device.screen
 local optionsutil = require("ui/data/optionsutil")
 local _ = require("gettext")
+local C_ = _.pgettext
 
 -- Get font size numbers as a table of strings
 local tableOfNumbersToTableOfStrings = function(numbers)
@@ -27,6 +28,8 @@ local CreOptions = {
                     "rotation.90CW",
                     "rotation.180UD",
                 },
+                -- For Dispatcher's sake
+                labels = {C_("Rotation", "⤹ 90°"), C_("Rotation", "↑ 0°"), C_("Rotation", "⤸ 90°"), C_("Rotation", "↓ 180°")},
                 alternate = false,
                 values = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},
                 args = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},

frontend/ui/data/koptoptions.lua
@@ -35,6 +35,8 @@ local KoptOptions = {
                     "rotation.90CW",
                     "rotation.180UD",
                 },
+                -- For Dispatcher's sake
+                labels = {C_("Rotation", "⤹ 90°"), C_("Rotation", "↑ 0°"), C_("Rotation", "⤸ 90°"), C_("Rotation", "↓ 180°")},
                 alternate = false,
                 values = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},
                 args = {Screen.ORIENTATION_LANDSCAPE_ROTATED, Screen.ORIENTATION_PORTRAIT, Screen.ORIENTATION_LANDSCAPE, Screen.ORIENTATION_PORTRAIT_ROTATED},