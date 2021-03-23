9967ea39151839059f275b4943876eecd125b7fa
NiLuJe <ninuje@gmail.com>
2021-03-23 02:25:26 +0100

    Proper data type there
    Not that it changes much ;).

--- plugins/autoturn.koplugin/main.lua
+++ plugins/autoturn.koplugin/main.lua
@@ -15,7 +15,7 @@ local AutoTurn = WidgetContainer:new{
     autoturn_distance = 1,
     enabled = false,
     settings_id = 0,
-    last_action_tv = 0,
+    last_action_tv = TimeVal:new{},
 }
 
 function AutoTurn:_enabled()
