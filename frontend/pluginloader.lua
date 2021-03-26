koreader/koreader

commit dee72a431c1ef4d59569ffc80b876e2e91765bbf

Set extra_plugin_paths to data_dir/plugins (#6849)

frontend/pluginloader.lua
@@ -49,6 +49,11 @@ function PluginLoader:loadPlugins()
         else
             logger.err("extra_plugin_paths config only accepts string or table value")
         end
+    else
+        local data_dir = require("datastorage"):getDataDir()
+        if data_dir ~= "." then
+            G_reader_settings:saveSetting("extra_plugin_paths", { data_dir .. "/plugins/" })
+        end
     end
 
     -- keep reference to old value so they can be restored later

datastorage.lua
@@ -54,7 +54,7 @@ local function initDataDir()
     local sub_data_dirs = {
         "cache", "clipboard",
         "data", "data/dict", "data/tessdata",
-        "history", "ota",
+        "history", "plugins",
         "screenshots", "settings", "styletweaks",
     }
     for _, dir in ipairs(sub_data_dirs) do

commit f14022421e6bdaff15f3cae65759c9b01190e199

PluginLoader should update extra_paths if data_dir is not ".".
It looks like a problem introduced by https://github.com/koreader/koreader/commit/dee72a431c1ef4d59569ffc80b876e2e91765bbf#diff-b3b0e0e90ea7b43a9d97a1c7572acef3f1dc7d1d2f75fe634345a1b841c624fa. Or did I misunderstand it?

frontend/pluginloader.lua
@@ -66,7 +66,8 @@ function PluginLoader:loadPlugins()
     else
         local data_dir = require("datastorage"):getDataDir()
         if data_dir ~= "." then
-            G_reader_settings:saveSetting("extra_plugin_paths", { data_dir .. "/plugins/" })
+            extra_paths = { data_dir .. "/plugins/" }
+            G_reader_settings:saveSetting("extra_plugin_paths", extra_paths)
         end
     end
 