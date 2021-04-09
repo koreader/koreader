koreader/koreader

commit 3706196bfe106538f9876d97a5d3b8a856530dd7

Update PDF annotations when changing bookmark text (#7411)

frontend/apps/reader/modules/readerbookmark.lua
@@ -597,9 +597,11 @@ end
 function ReaderBookmark:renameBookmark(item, from_highlight)
     if from_highlight then
         -- Called by ReaderHighlight:editHighlight, we need to find the bookmark
+        local pboxes = item.pboxes
         for i=1, #self.bookmarks do
             if item.datetime == self.bookmarks[i].datetime and item.page == self.bookmarks[i].page then
                 item = self.bookmarks[i]
+                item.pboxes = pboxes
                 if item.text == nil or item.text == "" then
                     -- Make up bookmark text as done in onShowBookmark
                     local page = item.page
@@ -647,6 +649,10 @@ function ReaderBookmark:renameBookmark(item, from_highlight)
                                 if item.text == self.bookmarks[i].text and  item.pos0 == self.bookmarks[i].pos0 and
                                     item.pos1 == self.bookmarks[i].pos1 and item.page == self.bookmarks[i].page then
                                     self.bookmarks[i].text = value
+                                    local setting = G_reader_settings:readSetting("save_document")
+                                    if setting ~= "disable" then
+                                        self.ui.document:updateHighlightContents(item.page, item, value)
+                                    end
                                     UIManager:close(self.input)
                                     if not from_highlight then
                                         self.refresh()

frontend/apps/reader/modules/readerhighlight.lua
@@ -1442,6 +1442,11 @@ function ReaderHighlight:deleteHighlight(page, i, bookmark_item)
             datetime = removed.datetime,
         })
     end
+    local setting = G_reader_settings:readSetting("save_document")
+    if setting ~= "disable" then
+        logger.dbg("delete highlight from document", removed)
+        self.ui.document:deleteHighlight(page, removed)
+    end
 end
 
 function ReaderHighlight:editHighlight(page, i)
@@ -1449,6 +1454,7 @@ function ReaderHighlight:editHighlight(page, i)
     self.ui.bookmark:renameBookmark({
         page = self.ui.document.info.has_pages and page or item.pos0,
         datetime = item.datetime,
+        pboxes = item.pboxes
     }, true)
 end
 

frontend/document/document.lua
@@ -483,6 +483,14 @@ function Document:saveHighlight(pageno, item)
     return nil
 end
 
+function Document:deleteHighlight(pageno, item)
+    return nil
+end
+
+function Document:updateHighlightContents(pageno, item, contents)
+    return nil
+end
+
 --[[
 helper functions
 --]]

frontend/document/pdfdocument.lua
@@ -176,34 +176,11 @@ function PdfDocument:getPageLinks(pageno)
 end
 
 function PdfDocument:saveHighlight(pageno, item)
-    local suffix = util.getFileNameSuffix(self.file)
-    if string.lower(suffix) ~= "pdf" then return end
+    local can_write = self:_checkIfWritable()
+    if can_write ~= true then return can_write end
 
-    if self.is_writable == nil then
-        local handle = io.open(self.file, 'r+b')
-        self.is_writable = handle ~= nil
-        if handle then handle:close() end
-    end
-    if self.is_writable == false then
-        return false
-    end
     self.is_edited = true
-    -- will also need mupdf_h.lua to be evaluated once
-    -- but this is guaranteed at this point
-    local n = #item.pboxes
-    local quadpoints = ffi.new("float[?]", 8*n)
-    for i=1, n do
-        -- The order must be left bottom, right bottom, left top, right top.
-        -- https://bugs.ghostscript.com/show_bug.cgi?id=695130
-        quadpoints[8*i-8] = item.pboxes[i].x
-        quadpoints[8*i-7] = item.pboxes[i].y + item.pboxes[i].h
-        quadpoints[8*i-6] = item.pboxes[i].x + item.pboxes[i].w
-        quadpoints[8*i-5] = item.pboxes[i].y + item.pboxes[i].h
-        quadpoints[8*i-4] = item.pboxes[i].x
-        quadpoints[8*i-3] = item.pboxes[i].y
-        quadpoints[8*i-2] = item.pboxes[i].x + item.pboxes[i].w
-        quadpoints[8*i-1] = item.pboxes[i].y
-    end
+    local quadpoints, n = self:_quadpointsFromPboxes(item.pboxes)
     local page = self._document:openPage(pageno)
     local annot_type = C.PDF_ANNOT_HIGHLIGHT
     if item.drawer == "lighten" then
@@ -215,6 +192,66 @@ function PdfDocument:saveHighlight(pageno, item)
     end
     page:addMarkupAnnotation(quadpoints, n, annot_type)
     page:close()
+end
+
+function Document:deleteHighlight(pageno, item)
+    local can_write = self:_checkIfWritable()
+    if can_write ~= true then return can_write end
+
+    self.is_edited = true
+    local quadpoints, n = self:_quadpointsFromPboxes(item.pboxes)
+    local page = self._document:openPage(pageno)
+    local annot = page:getMarkupAnnotation(quadpoints, n)
+    if annot ~= nil then
+        page:deleteMarkupAnnotation(annot)
+    end
+    page:close()
+end
+
+function PdfDocument:updateHighlightContents(pageno, item, contents)
+    local can_write = self:_checkIfWritable()
+    if can_write ~= true then return can_write end
+
+    self.is_edited = true
+    local quadpoints, n = self:_quadpointsFromPboxes(item.pboxes)
+    local page = self._document:openPage(pageno)
+    local annot = page:getMarkupAnnotation(quadpoints, n)
+    if annot ~= nil then
+        page:updateMarkupAnnotation(annot, contents)
+    end
+    page:close()
+end
+
+-- returns nil if file is not a pdf, true if document is a writable pdf, false else
+function PdfDocument:_checkIfWritable()
+    local suffix = util.getFileNameSuffix(self.file)
+    if string.lower(suffix) ~= "pdf" then return nil end
+    if self.is_writable == nil then
+        local handle = io.open(self.file, 'r+b')
+        self.is_writable = handle ~= nil
+        if handle then handle:close() end
+    end
+    return self.is_writable
+end
+
+function PdfDocument:_quadpointsFromPboxes(pboxes)
+    -- will also need mupdf_h.lua to be evaluated once
+    -- but this is guaranteed at this point
+    local n = #pboxes
+    local quadpoints = ffi.new("float[?]", 8*n)
+    for i=1, n do
+        -- The order must be left bottom, right bottom, left top, right top.
+        -- https://bugs.ghostscript.com/show_bug.cgi?id=695130
+        quadpoints[8*i-8] = pboxes[i].x
+        quadpoints[8*i-7] = pboxes[i].y + pboxes[i].h
+        quadpoints[8*i-6] = pboxes[i].x + pboxes[i].w
+        quadpoints[8*i-5] = pboxes[i].y + pboxes[i].h
+        quadpoints[8*i-4] = pboxes[i].x
+        quadpoints[8*i-3] = pboxes[i].y
+        quadpoints[8*i-2] = pboxes[i].x + pboxes[i].w
+        quadpoints[8*i-1] = pboxes[i].y
+    end
+    return quadpoints, n
 end
 
 function PdfDocument:writeDocument()

commit de78a9aa3c4f09ac8e151f0dba0e3e4cbe340a56

ReaderBookmark: Don't confuse bookmarks with highlights
Attempting to update a highlight when matching on a bookmark doesn't
make sense, and no longer works since #7411

Also, switch to ipairs(), and stop piggybacking on a new local that
shadows or uses the function's item argument.
It works in Lua, but it's super weird and error-prone.

Fix #7520 (regression since #7411).

frontend/apps/reader/modules/readerbookmark.lua
@@ -595,16 +595,17 @@ function ReaderBookmark:updateBookmark(item)
 end
 
 function ReaderBookmark:renameBookmark(item, from_highlight)
+    local bookmark
     if from_highlight then
         -- Called by ReaderHighlight:editHighlight, we need to find the bookmark
         local pboxes = item.pboxes
-        for i=1, #self.bookmarks do
-            if item.datetime == self.bookmarks[i].datetime and item.page == self.bookmarks[i].page then
-                item = self.bookmarks[i]
-                item.pboxes = pboxes
-                if item.text == nil or item.text == "" then
+        for __, bm in ipairs(self.bookmarks) do
+            if item.datetime == bm.datetime and item.page == bm.page then
+                bookmark = bm
+                bookmark.pboxes = pboxes
+                if bookmark.text == nil or bookmark.text == "" then
                     -- Make up bookmark text as done in onShowBookmark
-                    local page = item.page
+                    local page = bookmark.page
                     if not self.ui.document.info.has_pages then
                         page = self.ui.document:getPageFromXPointer(page)
                         if self.ui.document:hasHiddenFlows() then
@@ -615,18 +616,20 @@ function ReaderBookmark:renameBookmark(item, from_highlight)
                             end
                         end
                     end
-                    item.text = T(_("Page %1 %2 @ %3"), page, item.notes, item.datetime)
+                    bookmark.text = T(_("Page %1 %2 @ %3"), page, bookmark.notes, bookmark.datetime)
                 end
                 break
             end
         end
-        if item.text == nil then -- bookmark not found
+        if bookmark.text == nil then -- bookmark not found
             return
         end
+    else
+        bookmark = item
     end
     self.input = InputDialog:new{
         title = _("Rename bookmark"),
-        input = item.text,
+        input = bookmark.text,
         input_type = "text",
         allow_newline = true,
         cursor_at_end = false,
@@ -645,13 +648,16 @@ function ReaderBookmark:renameBookmark(item, from_highlight)
                     callback = function()
                         local value = self.input:getInputValue()
                         if value ~= "" then
-                            for i=1, #self.bookmarks do
-                                if item.text == self.bookmarks[i].text and  item.pos0 == self.bookmarks[i].pos0 and
-                                    item.pos1 == self.bookmarks[i].pos1 and item.page == self.bookmarks[i].page then
-                                    self.bookmarks[i].text = value
-                                    local setting = G_reader_settings:readSetting("save_document")
-                                    if setting ~= "disable" then
-                                        self.ui.document:updateHighlightContents(item.page, item, value)
+                            for __, bm in ipairs(self.bookmarks) do
+                                if bookmark.text == bm.text and  bookmark.pos0 == bm.pos0 and
+                                    bookmark.pos1 == bm.pos1 and bookmark.page == bm.page then
+                                    bm.text = value
+                                    -- A bookmark isn't necessarily a highlight (it doesn't have pboxes)
+                                    if bookmark.pboxes then
+                                        local setting = G_reader_settings:readSetting("save_document")
+                                        if setting ~= "disable" then
+                                            self.ui.document:updateHighlightContents(bookmark.page, bookmark, value)
+                                        end
                                     end
                                     UIManager:close(self.input)
                                     if not from_highlight then