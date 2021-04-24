5da816229deb3d9c8d59764009a25d63570e0705
gbyl <gbyl@users.noreply.github.com>
2021-04-24 17:17:38 -0500

    add help message

--- frontend/ui/elements/disable_refresh_swipes.lua
+++ frontend/ui/elements/disable_refresh_swipes.lua
@@ -2,6 +2,7 @@ local _ = require("gettext")
 
 return {
     text = _("Disable refresh swipes"),
+    help_text = _("Disables fullscreen refreshes triggered by non-horizontal swipes. This setting applies only to in-reader swipes."),
     checked_func = function()
         return G_reader_settings:isTrue("disable_refresh_swipes")
     end,
