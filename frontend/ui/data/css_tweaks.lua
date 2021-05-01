588ce5e9003facc35e4151edb7210fb6ae8abe28
hius07 <62179190+hius07@users.noreply.github.com>
2021-05-01 11:04:37 +0300

    Style tweaks: add page break after <body>, better wording

--- frontend/ui/data/css_tweaks.lua
+++ frontend/ui/data/css_tweaks.lua
@@ -136,7 +136,7 @@ h1 + h6, h2 + h6, h3 + h6, h4 + h6, h5 + h6 { page-break-before: avoid !importan
             },
             {
                 id = "page-break-after-body";
-                title = _("New page after <body>"),
+                title = _("New page on next <body>"),
                 css = [[body { page-break-after: always !important; }]],
             },
         },
