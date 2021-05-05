50f479b55871ef266e7d6183941607be0eeecfde
NiLuJe <ninuje@gmail.com>
2021-05-05 23:42:14 +0200

    Disable debug

--- frontend/document/credocument.lua
+++ frontend/document/credocument.lua
@@ -1358,10 +1358,10 @@ function CreDocument:setupCallCache()
         return
     end
     logger.dbg("CreDocument: using cre call cache")
-    local do_stats = true -- G_reader_settings:isTrue("use_cre_call_cache_log_stats")
+    local do_stats =  G_reader_settings:isTrue("use_cre_call_cache_log_stats")
     -- Tune these when debugging
-    local do_stats_include_not_cached = true
-    local do_log = true
+    local do_stats_include_not_cached = false
+    local do_log = false
 
     -- Beware below for luacheck warnings "shadowing upvalue argument 'self'":
     -- the 'self' we got and use here, and the one we may get implicitely
