9df5e54f22048b4b79dee4c53f1b11762b2a8286
yparitcher <y@paritcher.com>
2021-05-12 22:38:52 -0400

    Cleanup for #7626

--- frontend/dispatcher.lua
+++ frontend/dispatcher.lua
@@ -417,16 +417,16 @@ function Dispatcher:addItem(caller, menu, location, settings, section)
                     end,
                     callback = function(touchmenu_instance)
                         local SpinWidget = require("ui/widget/spinwidget")
-                        local requested_precision = settingsList[k].precision
-                        if settingsList[k].step and math.floor(settingsList[k].step) ~= settingsList[k].step then
-                            requested_precision = "%0.1f"
+                        local precision
+                        if settingsList[k].step and settingsList[k].step < 1 then
+                            precision = "%0.1f"
                         end
                         local items = SpinWidget:new{
                             width = math.floor(Screen:getWidth() * 0.6),
                             value = location[settings] ~= nil and location[settings][k] or settingsList[k].default or 0,
                             value_min = settingsList[k].min,
                             value_step = settingsList[k].step or 1,
-                            precision = requested_precision,
+                            precision = precision,
                             value_hold_step = 5,
                             value_max = settingsList[k].max,
                             default_value = settingsList[k].default,
@@ -463,9 +463,9 @@ function Dispatcher:addItem(caller, menu, location, settings, section)
                     end,
                     callback = function(touchmenu_instance)
                         local _ = require("gettext")
-                        local requested_precision = settingsList[k].precision
-                        if settingsList[k].step and math.floor(settingsList[k].step) ~= settingsList[k].step then
-                            requested_precision = "%0.1f"
+                        local precision
+                        if settingsList[k].step and settingsList[k].step < 1 then
+                            precision = "%0.1f"
                         end
                         local SpinWidget = require("ui/widget/spinwidget")
                         local items = SpinWidget:new{
@@ -473,7 +473,7 @@ function Dispatcher:addItem(caller, menu, location, settings, section)
                             value = location[settings] ~= nil and location[settings][k] or 0,
                             value_min = settingsList[k].min,
                             value_step = settingsList[k].step or 1,
-                            precision = requested_precision,
+                            precision = precision,
                             value_hold_step = 5,
                             value_max = settingsList[k].max,
                             default_value = 0,
