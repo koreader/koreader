a665901c51ffb70003db0b64a54a65f038adc90a
NiLuJe <ninuje@gmail.com>
2021-05-18 20:04:02 +0200

    ZSync: Flag this plugin as deprecated.
    It's somewhat non-trivial, and has been unmaintained for years.

--- plugins/zsync.koplugin/_meta.lua
+++ plugins/zsync.koplugin/_meta.lua
@@ -3,4 +3,5 @@ return {
     name = "zsync",
     fullname = _("Zsync"),
     description = _([[Devices in the same Wi-Fi network can transfer documents between each other directly.]]),
+    deprecated = _("This plugin has been unmaintained for years. It might work for you, or it might not."),
 }
