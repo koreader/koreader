b5a68d002a7fe141f9d26c83856fd4bfd68f9f9e
zwim <martin.zwicknagl@kirchbichl.net>
2021-05-20 15:11:09 +0200

    use ui.document

--- frontend/dispatcher.lua
+++ frontend/dispatcher.lua
@@ -618,7 +618,7 @@ function Dispatcher:execute(ui, settings, gesture)
     for k, v in pairs(settings) do
         if settingsList[k] ~= nil and (settingsList[k].conditions == nil or settingsList[k].conditions == true) then
             -- Be sure we don't send a document setting event if there's not yet or no longer a document
-            if self.document or (not settingsList[k].paging and not settingsList[k].rolling) then
+            if ui.document or (not settingsList[k].paging and not settingsList[k].rolling) then
                 if settingsList[k].category == "none" then
                     if settingsList[k].arg ~= nil then
                         ui:handleEvent(Event:new(settingsList[k].event, settingsList[k].arg))
