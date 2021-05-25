44f04f80f1a1020bee8a8dd430093ada1ab1b7b7
zwim <martin.zwicknagl@kirchbichl.net>
2021-05-25 12:31:06 +0200

    Nit

--- frontend/apps/reader/modules/readerfont.lua
+++ frontend/apps/reader/modules/readerfont.lua
@@ -249,7 +249,7 @@ function ReaderFont:onSetWordSpacing(values)
     self.word_spacing = values
     self.ui.document:setWordSpacing(values)
     self.ui:handleEvent(Event:new("UpdatePos"))
-    Notification:notify(T(_("Word spacing set to %1%,  %2%."), values[1], values[2]))
+    Notification:notify(T(_("Word spacing set to %1%, %2%."), values[1], values[2]))
     return true
 end
 
