9bfb0ce365be10f51d3402dfa1510bf268ca4ac7
Frans de Jonge <fransdejonge@gmail.com>
2021-05-30 11:46:59 +0200

    bleh

--- .ci/helper_luarocks.sh
+++ .ci/helper_luarocks.sh
@@ -11,7 +11,8 @@ echo "wrap_bin_scripts = false" >>"${HOME}/.luarocks/config.lua"
 travis_retry luarocks --local install luafilesystem
 # for verbose_print module
 travis_retry luarocks --local install ansicolors
-travis_retry luarocks --local install busted scm-1
+wget https://raw.githubusercontent.com/Olivine-Labs/busted/2e4799e06b865c352baa7f7721e32aedaafd19d6/busted-scm-2.rockspec
+travis_retry luarocks --local make ./busted-scm-2.rockspec
 
 travis_retry luarocks --local install luacheck
 travis_retry luarocks --local install lanes # for parallel luacheck
