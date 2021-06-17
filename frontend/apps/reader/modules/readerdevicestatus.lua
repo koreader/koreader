b335796ac504fdb2f7208d123a0fe6c0e886bb9f
hius07 <62179190+hius07@users.noreply.github.com>
2021-06-17 14:50:42 +0300

    Update readerdevicestatus.lua

--- frontend/apps/reader/modules/readerdevicestatus.lua
+++ frontend/apps/reader/modules/readerdevicestatus.lua
@@ -42,51 +42,54 @@ function ReaderDeviceStatus:init()
         self:startBatteryChecker()
     end
 
-    self.memory_interval = G_reader_settings:readSetting("device_status_memory_interval") or 5
-    self.memory_threshold = G_reader_settings:readSetting("device_status_memory_threshold") or 100
-    self.checkHighMemory = function()
-        local statm = io.open("/proc/self/statm", "r")
-        if statm then
-            local dummy, rss = statm:read("*number", "*number")
-            statm:close()
-            rss = math.floor(rss * 4096 / 1024 / 1024)
-            if rss >= self.memory_threshold then
-                if Device:canRestart() then
-                    if G_reader_settings:isTrue("device_status_memory_auto_restart") then
-                        UIManager:show(InfoMessage:new{
-                            text = _("High memory usage!\n\nKOReader is restarting..."),
-                            icon = "notice-warning",
-                        })
-                        self.ui:handleEvent(Event:new("Restart"))
+    if not Device:isAndroid() then
+        self.memory_interval = G_reader_settings:readSetting("device_status_memory_interval") or 5
+        self.memory_threshold = G_reader_settings:readSetting("device_status_memory_threshold") or 100
+        self.checkHighMemory = function()
+            local statm = io.open("/proc/self/statm", "r")
+            if statm then
+                local dummy, rss = statm:read("*number", "*number")
+                statm:close()
+                rss = math.floor(rss * 4096 / 1024 / 1024)
+                if rss >= self.memory_threshold then
+                    if Device:canRestart() then
+                        if UIManager:getTopWidget() == "ReaderUI"
+                           and G_reader_settings:isTrue("device_status_memory_auto_restart") then
+                            UIManager:show(InfoMessage:new{
+                                text = _("High memory usage!\n\nKOReader is restarting..."),
+                                icon = "notice-warning",
+                            })
+                            self.ui:handleEvent(Event:new("Restart"))
+                        else
+                            UIManager:show(ConfirmBox:new {
+                                text = T(_("High memory usage: %1 MB\n\nRestart KOReader?"), rss),
+                                ok_text = _("Restart"),
+                                dismissable = false,
+                                ok_callback = function()
+                                    UIManager:show(InfoMessage:new{
+                                        text = _("High memory usage!\n\nKOReader is restarting..."),
+                                        icon = "notice-warning",
+                                    })
+                                    self.ui:handleEvent(Event:new("Restart"))
+                                end,
+                            })
+                        end
                     else
                         UIManager:show(ConfirmBox:new {
-                            text = T(_("High memory usage: %1 MB\n\nRestart KOReader?"), rss),
-                            ok_text = _("Restart"),
+                            text = T(_("High memory usage: %1 MB\n\nExit KOReader?"), rss),
+                            ok_text = _("Exit"),
                             dismissable = false,
                             ok_callback = function()
-                                UIManager:show(InfoMessage:new{
-                                    text = _("High memory usage!\n\nKOReader is restarting..."),
-                                    icon = "notice-warning",
-                                })
-                                self.ui:handleEvent(Event:new("Restart"))
+                                self.ui:handleEvent(Event:new("Exit"))
                             end,
                         })
                     end
-                else
-                    UIManager:show(ConfirmBox:new {
-                        text = T(_("High memory usage: %1 MB\n\nExit KOReader?"), rss),
-                        ok_text = _("Exit"),
-                        dismissable = false,
-                        ok_callback = function()
-                            self.ui:handleEvent(Event:new("Exit"))
-                        end,
-                    })
                 end
             end
+            UIManager:scheduleIn(self.memory_interval * 60, self.checkHighMemory)
         end
-        UIManager:scheduleIn(self.memory_interval * 60, self.checkHighMemory)
+        self:startMemoryChecker()
     end
-    self:startMemoryChecker()
 
     self.ui.menu:registerToMainMenu(self)
 end
@@ -94,89 +97,10 @@ end
 function ReaderDeviceStatus:addToMainMenu(menu_items)
     menu_items.device_status_alarm = {
         text = _("Devise status alarm"),
-        sub_item_table = {
-            {
-                text = _("High memory usage"),
-                checked_func = function()
-                    return G_reader_settings:isTrue("device_status_memory_alarm")
-                end,
-                callback = function()
-                    G_reader_settings:flipNilOrFalse("device_status_memory_alarm")
-                    if G_reader_settings:isTrue("device_status_memory_alarm") then
-                        self:startMemoryChecker()
-                    else
-                        self:stopMemoryChecker()
-                    end
-                end,
-            },
-            {
-                text_func = function()
-                    return T(_("Check interval (%1 min)"), self.memory_interval)
-                end,
-                enabled_func = function()
-                    return G_reader_settings:isTrue("device_status_memory_alarm")
-                end,
-                keep_menu_open = true,
-                callback = function(touchmenu_instance)
-                    UIManager:show(SpinWidget:new{
-                        width = math.floor(Screen:getWidth() * 0.6),
-                        value = self.memory_interval,
-                        value_min = 1,
-                        value_max = 60,
-                        default_value = 5,
-                        value_hold_step = 5,
-                        title_text =  _("Memory check interval"),
-                        callback = function(spin)
-                            self.memory_interval = spin.value
-                            G_reader_settings:saveSetting("device_status_memory_interval", self.memory_interval)
-                            touchmenu_instance:updateItems()
-                            UIManager:scheduleIn(self.memory_interval*60, self.checkHighMemory)
-                        end,
-                    })
-                end,
-            },
-            {
-                text_func = function()
-                    return T(_("Threshold (%1 MB)"), self.memory_threshold)
-                end,
-                enabled_func = function()
-                    return G_reader_settings:isTrue("device_status_memory_alarm")
-                end,
-                keep_menu_open = true,
-                callback = function(touchmenu_instance)
-                    UIManager:show(SpinWidget:new{
-                        width = math.floor(Screen:getWidth() * 0.6),
-                        value = self.memory_threshold,
-                        value_min = 20,
-                        value_max = 500,
-                        default_value = 100,
-                        value_step = 5,
-                        value_hold_step = 10,
-                        title_text =  _("Memory alarm threshold"),
-                        callback = function(spin)
-                            self.memory_threshold = spin.value
-                            G_reader_settings:saveSetting("device_status_memory_threshold", self.memory_threshold)
-                            touchmenu_instance:updateItems()
-                        end,
-                    })
-                end,
-            },
-            {
-                text = _("Automatic restart"),
-                enabled_func = function()
-                    return G_reader_settings:isTrue("device_status_memory_alarm") and Device:canRestart()
-                end,
-                checked_func = function()
-                    return G_reader_settings:isTrue("device_status_memory_auto_restart")
-                end,
-                callback = function()
-                    G_reader_settings:flipNilOrFalse("device_status_memory_auto_restart")
-                end,
-            },
-        },
+        sub_item_table = {},
     }
     if self.has_battery then
-        table.insert(menu_items.device_status_alarm.sub_item_table, 1,
+        table.insert(menu_items.device_status_alarm.sub_item_table,
             {
                 text = _("Low battery"),
                 checked_func = function()
@@ -192,7 +116,7 @@ function ReaderDeviceStatus:addToMainMenu(menu_items)
                     end
                 end,
             })
-        table.insert(menu_items.device_status_alarm.sub_item_table, 2,
+        table.insert(menu_items.device_status_alarm.sub_item_table,
             {
                 text_func = function()
                     return T(_("Check interval (%1 min)"), self.battery_interval)
@@ -220,7 +144,7 @@ function ReaderDeviceStatus:addToMainMenu(menu_items)
                     })
                 end,
             })
-        table.insert(menu_items.device_status_alarm.sub_item_table, 3,
+        table.insert(menu_items.device_status_alarm.sub_item_table,
             {
                 text_func = function()
                     return T(_("Threshold (%1%)"), self.battery_threshold)
@@ -247,6 +171,90 @@ function ReaderDeviceStatus:addToMainMenu(menu_items)
                     })
                 end,
                 separator = true,
+            })
+    end
+    if not Device:isAndroid() then
+        table.insert(menu_items.device_status_alarm.sub_item_table,
+            {
+                text = _("High memory usage"),
+                checked_func = function()
+                    return G_reader_settings:isTrue("device_status_memory_alarm")
+                end,
+                callback = function()
+                    G_reader_settings:flipNilOrFalse("device_status_memory_alarm")
+                    if G_reader_settings:isTrue("device_status_memory_alarm") then
+                        self:startMemoryChecker()
+                    else
+                        self:stopMemoryChecker()
+                    end
+                end,
+            })
+        table.insert(menu_items.device_status_alarm.sub_item_table,
+            {
+                text_func = function()
+                    return T(_("Check interval (%1 min)"), self.memory_interval)
+                end,
+                enabled_func = function()
+                    return G_reader_settings:isTrue("device_status_memory_alarm")
+                end,
+                keep_menu_open = true,
+                callback = function(touchmenu_instance)
+                    UIManager:show(SpinWidget:new{
+                        width = math.floor(Screen:getWidth() * 0.6),
+                        value = self.memory_interval,
+                        value_min = 1,
+                        value_max = 60,
+                        default_value = 5,
+                        value_hold_step = 5,
+                        title_text =  _("Memory check interval"),
+                        callback = function(spin)
+                            self.memory_interval = spin.value
+                            G_reader_settings:saveSetting("device_status_memory_interval", self.memory_interval)
+                            touchmenu_instance:updateItems()
+                            UIManager:scheduleIn(self.memory_interval*60, self.checkHighMemory)
+                        end,
+                    })
+                end,
+            })
+        table.insert(menu_items.device_status_alarm.sub_item_table,
+            {
+                text_func = function()
+                    return T(_("Threshold (%1 MB)"), self.memory_threshold)
+                end,
+                enabled_func = function()
+                    return G_reader_settings:isTrue("device_status_memory_alarm")
+                end,
+                keep_menu_open = true,
+                callback = function(touchmenu_instance)
+                    UIManager:show(SpinWidget:new{
+                        width = math.floor(Screen:getWidth() * 0.6),
+                        value = self.memory_threshold,
+                        value_min = 20,
+                        value_max = 500,
+                        default_value = 100,
+                        value_step = 5,
+                        value_hold_step = 10,
+                        title_text =  _("Memory alarm threshold"),
+                        callback = function(spin)
+                            self.memory_threshold = spin.value
+                            G_reader_settings:saveSetting("device_status_memory_threshold", self.memory_threshold)
+                            touchmenu_instance:updateItems()
+                        end,
+                    })
+                end,
+            })
+        table.insert(menu_items.device_status_alarm.sub_item_table,
+            {
+                text = _("Automatic restart"),
+                enabled_func = function()
+                    return G_reader_settings:isTrue("device_status_memory_alarm") and Device:canRestart()
+                end,
+                checked_func = function()
+                    return G_reader_settings:isTrue("device_status_memory_auto_restart")
+                end,
+                callback = function()
+                    G_reader_settings:flipNilOrFalse("device_status_memory_auto_restart")
+                end,
             })
     end
 end
