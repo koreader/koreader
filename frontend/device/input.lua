eebc36150efce9a69e5ec3716ad9ce13a72e5e49
NiLuJe <ninuje@gmail.com>
2021-07-17 17:43:05 +0200

    Explain why we hide that bunch of logger.dbg calls behind a branch

--- frontend/device/input.lua
+++ frontend/device/input.lua
@@ -1101,30 +1101,33 @@ function Input:waitEvent(now, deadline)
         -- We're guaranteed that ev is an array of event tables. Might be an array of *one* event, but an array nonetheless ;).
         for __, event in ipairs(ev) do
             if DEBUG.is_on then
+                -- NOTE: This is rather spammy of computationally intensive,
+                --       and we can't conditionally prevent evalutation of function arguments,
+                --       so, just hide the whole thing behind a branch ;).
                 DEBUG:logEv(event)
                 if event.type == C.EV_KEY then
                     logger.dbg(string.format(
-                        "key event => code: %d (%s), value: %s, time: %d.%d",
+                        "key event => code: %d (%s), value: %s, time: %d.%06d",
                         event.code, self.event_map[event.code] or linux_evdev_key_code_map[event.code], event.value,
                         event.time.sec, event.time.usec))
                 elseif event.type == C.EV_SYN then
                     logger.dbg(string.format(
-                        "input event => type: %d (%s), code: %d (%s), value: %s, time: %d.%d",
+                        "input event => type: %d (%s), code: %d (%s), value: %s, time: %d.%06d",
                         event.type, linux_evdev_type_map[event.type], event.code, linux_evdev_syn_code_map[event.code], event.value,
                         event.time.sec, event.time.usec))
                 elseif event.type == C.EV_ABS then
                     logger.dbg(string.format(
-                        "input event => type: %d (%s), code: %d (%s), value: %s, time: %d.%d",
+                        "input event => type: %d (%s), code: %d (%s), value: %s, time: %d.%06d",
                         event.type, linux_evdev_type_map[event.type], event.code, linux_evdev_abs_code_map[event.code], event.value,
                         event.time.sec, event.time.usec))
                 elseif event.type == C.EV_MSC then
                     logger.dbg(string.format(
-                        "input event => type: %d (%s), code: %d (%s), value: %s, time: %d.%d",
+                        "input event => type: %d (%s), code: %d (%s), value: %s, time: %d.%06d",
                         event.type, linux_evdev_type_map[event.type], event.code, linux_evdev_msc_code_map[event.code], event.value,
                         event.time.sec, event.time.usec))
                 else
                     logger.dbg(string.format(
-                        "input event => type: %d (%s), code: %d, value: %s, time: %d.%d",
+                        "input event => type: %d (%s), code: %d, value: %s, time: %d.%06d",
                         event.type, linux_evdev_type_map[event.type], event.code, event.value,
                         event.time.sec, event.time.usec))
                 end
