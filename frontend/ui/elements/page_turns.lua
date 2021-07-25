27e147dd24a83561800be8771030dda7a55c9793
hius07 <62179190+hius07@users.noreply.github.com>
2021-07-25 12:47:39 +0300

    Page turns: With taps, With swipes

--- frontend/ui/elements/page_turns.lua
+++ frontend/ui/elements/page_turns.lua
@@ -7,21 +7,21 @@ local PageTurns = {
     text = _("Page turns"),
     sub_item_table = {
         {
-            text = _("Disable taps"),
+            text = _("With taps"),
             checked_func = function()
-                return G_reader_settings:isTrue("page_turns_disable_tap")
+                return G_reader_settings:nilOrFalse("page_turns_disable_tap")
             end,
             callback = function()
-                G_reader_settings:toggle("page_turns_disable_tap")
+                G_reader_settings:flipNilOrFalse("page_turns_disable_tap")
             end,
         },
         {
-            text = _("Disable swipes"),
+            text = _("With swipes"),
             checked_func = function()
-                return G_reader_settings:isTrue("page_turns_disable_swipe")
+                return G_reader_settings:nilOrFalse("page_turns_disable_swipe")
             end,
             callback = function()
-                G_reader_settings:toggle("page_turns_disable_swipe")
+                G_reader_settings:flipNilOrFalse("page_turns_disable_swipe")
             end,
             separator = true,
         },
