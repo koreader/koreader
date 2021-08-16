bacb3e4d34c4d4b058dd3806ef3eb8c26b638baf
Grubuntu <56045316+Grubuntu@users.noreply.github.com>
2021-08-16 16:06:34 +0200

    Update device.lua
    Correct freeze behavior when we close Koreader on some old device :  https://github.com/koreader/koreader/issues/8053#issuecomment-897496109

--- frontend/device/pocketbook/device.lua
+++ frontend/device/pocketbook/device.lua
@@ -264,7 +264,7 @@ function PocketBook:notifyBookState(title, document)
         fo:write(fn)
         fo:close()
     end
-    inkview.SetSubtaskInfo(inkview.GetCurrentTask(), 0, title and (title .. " - koreader") or "koreader", fn)
+    inkview.SetSubtaskInfo(inkview.GetCurrentTask(), 0, title and (title .. " - koreader") or "koreader", fn or "[nil]")
 end
 
 function PocketBook:setDateTime(year, month, day, hour, min, sec)
