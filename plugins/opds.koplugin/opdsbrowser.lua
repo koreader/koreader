2181b10601b3ba4eea99e56b0031944e50117e7e
Frans de Jonge <fransdejonge@gmail.com>
2021-08-22 10:38:23 +0200

    [plugin] Change filetype/extension algorithm
    Fixes <https://github.com/koreader/koreader/issues/7995>.

--- plugins/opds.koplugin/opdsbrowser.lua
+++ plugins/opds.koplugin/opdsbrowser.lua
@@ -630,13 +630,13 @@ function OPDSBrowser:showDownloads(item)
             local index = (i-1)*downloadsperline + j
             local acquisition = acquisitions[index]
             if acquisition then
-                local filetype
-                if DocumentRegistry:hasProvider(nil, acquisition.type) then
+                local filetype = util.getFileNameSuffix(acquisition.href)
+                if not filetype and DocumentRegistry:hasProvider(nil, acquisition.type) then
                     filetype = DocumentRegistry:mimeToExt(acquisition.type)
-                elseif DocumentRegistry:hasProvider(acquisition.href) then
-                    filetype = string.lower(util.getFileNameSuffix(acquisition.href))
                 end
+
                 if filetype then
+                    filetype = string.lower(filetype)
                     -- append DOWNWARDS BLACK ARROW ⬇ U+2B07 to format
                     button.text = string.upper(filetype) .. "\xE2\xAC\x87"
                     button.callback = function()
