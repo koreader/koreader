5b5066d89adf7eb1dedcdb6095a53c23669d6999
hius07 <62179190+hius07@users.noreply.github.com>
2021-09-21 14:36:43 +0300

    Update virtualkeyboard.lua

--- frontend/ui/widget/virtualkeyboard.lua
+++ frontend/ui/widget/virtualkeyboard.lua
@@ -736,7 +736,7 @@ local VirtualKeyboard = FocusManager:new{
     umlautmode = false,
     layout = {},
 
-    width = Screen:scaleBySize(600),
+    width = nil, -- non-fullscreen keyboard may cause design issues (eg Text editor)
     height = nil,
     bordersize = Size.border.default,
     padding = 0,
@@ -776,6 +776,7 @@ function VirtualKeyboard:init()
     self.symbolmode_keys = keyboard.symbolmode_keys
     self.utf8mode_keys = keyboard.utf8mode_keys
     self.umlautmode_keys = keyboard.umlautmode_keys
+    self.width = self.width or Screen:getWidth()
     local keys_height = G_reader_settings:isTrue("keyboard_key_compact") and 48 or 64
     self.height = Screen:scaleBySize(keys_height * #self.KEYS)
     self.min_layer = keyboard.min_layer
