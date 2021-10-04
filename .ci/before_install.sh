d3e49bfb3a279e6b1cf308756348dd6245abb0ff
Frans de Jonge <fransdejonge@gmail.com>
2021-10-04 22:04:13 +0200

    right, that

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -2,14 +2,14 @@
 
 # don't do this for clang
 if [ "${CXX}" = "g++" ]; then
-    export CXX="g++-5" CC="gcc-5"
+    export CXX="g++-8" CC="gcc-8"
 fi
 # in case anything ignores the environment variables, override through PATH
 mkdir bin
-ln -s "$(command -v gcc-5)" bin/cc
-ln -s "$(command -v gcc-5)" bin/gcc
+ln -s "$(command -v gcc-8)" bin/cc
+ln -s "$(command -v gcc-8)" bin/gcc
 ln -s "$(command -v c++)" bin/c++
-ln -s "$(command -v g++-5)" bin/g++
+ln -s "$(command -v g++-8)" bin/g++
 
 # Travis only makes a shallow clone of --depth=50. KOReader is small enough that
 # we can just grab it all. This is necessary to generate the version number,
