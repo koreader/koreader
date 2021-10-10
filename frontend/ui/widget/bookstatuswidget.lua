3f2b2916fa59edfc4f2e1f43e6308bda4930317c
hius07 <62179190+hius07@users.noreply.github.com>
2021-10-10 10:50:58 +0300

    Update bookstatuswidget.lua

--- frontend/ui/widget/bookstatuswidget.lua
+++ frontend/ui/widget/bookstatuswidget.lua
@@ -327,12 +327,7 @@ function BookStatusWidget:genBookInfoGroup()
         }
     )
     -- progress bar
-    local read_percentage
-    if self.view.document.info.has_pages then
-        read_percentage = self.view.ui.paging:getLastPercent()
-    else
-        read_percentage = self.view.ui.rolling:getLastPercent()
-    end
+    local read_percentage = self.view.document:getCurrentPage() / self.total_pages
     local progress_bar = ProgressWidget:new{
         width = math.floor(width * 0.7),
         height = Screen:scaleBySize(10),
