a2459f7fd45eedcd08e8f8ef8370a372454f9397
ichnilatis-gr <76203123+ichnilatis-gr@users.noreply.github.com>
2021-10-10 15:22:54 +0300

    Update el_popup.lua
    Creation of a new popup menu on the Greek acute key for Greek numeral signs.

--- frontend/ui/data/keyboardlayouts/keypopup/el_popup.lua
+++ frontend/ui/data/keyboardlayouts/keypopup/el_popup.lua
@@ -9,6 +9,11 @@ return {
         west = "!",
         "·",
     },
+    acu = {
+        "´",
+		alt_label = "ʹ",
+		east = "ʹ", -- Greek Numeral Sign
+		west = "͵", -- Greek Lower Numeral Sign
     _A_el = {
         "Α",
         northeast = "Ά",
