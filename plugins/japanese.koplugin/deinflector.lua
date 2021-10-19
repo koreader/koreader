a4435ace4e1a197a77bd85d3eed3c6354c4d481f
Aleksa Sarai <cyphar@cyphar.com>
2021-10-19 22:07:40 +1100

    plugins: add Japanese Support plugin
    This plugin provides support for Japanese deinflection during lookup as
    well as making long-hold word selection actually select whole words
    properly. With this plugin, word lookups in Japanese text in KOReader
    become much easier, and no longer requires users to use special
    dictionaries that have synonym-based deinflection rules defined (which
    were always fairly annoying to use).
    
    The basic idea and deinflection data for this plugin come from
    Yomichan (which is also a GPL-3.0+ project), but everything was
    implemented specifically for KOReader.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

---
+++ plugins/japanese.koplugin/deinflector.lua
@@ -0,0 +1,344 @@
+--- Yomichan deinflector implementation in pure Lua.
+-- This is very heavily modelled after Yomichan's deinflection code, with some
+-- minor changes to make it slightly more performant in the more restricted
+-- environment KOReader tends to run in.
+--
+-- @module koplugin.japanese.deinflector
+-- @alias Deinflector
+
+-- Copyright (C) 2021 Aleksa Sarai <cyphar@cyphar.com>
+-- Licensed under the GPLv3 or later.
+--
+-- This deinflection logic is heavily modelled after Yomichan
+-- <https://github.com/FooSoft/yomichan>, up to and including the deinflection
+-- table.
+
+local JSON = require("rapidjson")
+local Utf8Proc = require("ffi/utf8proc")
+local bit = require("bit")
+local logger = require("logger")
+local util = require("util")
+
+local Deinflector = {}
+
+local ruleTypes = {}
+-- These need to be outside of the definition because "adj-i" cannot be set
+-- using {} initialisation.
+ruleTypes["v1"]    = 0x01 -- Verb ichidan (so-called ru-verb)
+ruleTypes["v5"]    = 0x02 -- Verb godan (so-called u-verb)
+ruleTypes["vs"]    = 0x04 -- Verb suru
+ruleTypes["vk"]    = 0x08 -- Verb kuru
+ruleTypes["vz"]    = 0x0A -- Verb zuru
+ruleTypes["adj-i"] = 0x10 -- Adjectival verb (i-adjective)
+ruleTypes["iru"]   = 0x20 -- Intermediate -iru endings for progressive or perfect tense
+
+local function toRuleTypes(...)
+    final = 0
+    for _, ruleType in ipairs({...}) do
+        if ruleTypes[ruleType] then
+            final = bit.bor(final, ruleTypes[ruleType])
+        end
+    end
+    return final
+end
+
+local function getSourceDir()
+    local callerSource = debug.getinfo(2, "S").source
+    if callerSource:find("^@") then
+        return callerSource:gsub("^@(.*)/[^/]*", "%1")
+    end
+end
+
+local function parsePluginJson(filename)
+    local jsonPath = getSourceDir().."/"..filename
+    local file, err = io.open(jsonPath, "r")
+    if file then
+        local contents = file:read("*all")
+        local ok, parsed = pcall(JSON.decode, contents)
+        if ok then
+            return parsed
+        else
+            logger.err("japanese.koplugin: failed to parse plugin json", filename)
+        end
+    else
+        logger.err("japanese.koplugin: failed to open plugin json", filename, err)
+    end
+    return {}
+end
+
+--- A single deinflection result.
+-- @field term Deinflected form of the term (string).
+-- @field rules Rules bitmask the term has applied (int).
+-- @field reasons Array of reasons applied to reach the term ({string,...}).
+-- @table DeinflectResult
+
+local function makeDeinflectionResult(term, rules, reasons)
+    return { term = term, rules = rules, reasons = reasons }
+end
+
+--- Deinflect some text as-is (without trying any possible conversions between
+-- types of kana or any other such modifications). You probably want to use
+-- Deinflector:deinflect() because it is more thorough.
+--
+-- @see deinflect
+-- @tparam string text Japanese text to deinflect verbatim.
+-- @treturn {DeinflectResult,...} An array of possible deinflections (including the text given).
+function Deinflector:deinflectVerbatim(text)
+    self:init() -- in case this is being called directly
+    local results = {makeDeinflectionResult(text, 0, {})}
+    local seen = {}
+    seen[text] = true
+    for _, current in ipairs(results) do
+        for reason, rules in pairs(self.rules) do
+            for _, rule in ipairs(rules) do
+                local rulesMatch = current.rules == 0 or bit.band(current.rules, rule.rulesIn) ~= 0
+                local endsWithKana = current.term:sub(-#rule.kanaIn) == rule.kanaIn
+                local longEnough = #current.term - #rule.kanaIn + #rule.kanaOut > 0
+                if rulesMatch and endsWithKana and longEnough then
+                    -- Check if we've already found this deinflection. If so,
+                    -- that means there was a shorter reason path to it and
+                    -- this deinflection is almost certainly theoretical.
+                    new_term = current.term:sub(1, -#rule.kanaIn-1) .. rule.kanaOut
+                    if not seen[new_term] then
+                        table.insert(results, makeDeinflectionResult(
+                            new_term,
+                            rule.rulesOut,
+                            {reason, unpack(current.reasons)}
+                        ))
+                        seen[new_term] = true
+                    end
+                end
+            end
+        end
+    end
+    return results
+end
+
+-- These are all in 五十音 order, but we list variants in their 五十音 order
+-- before the base kana because half-width katakana use two characters for
+-- 濁点・半濁点 and thus need to be replaced first. We could make use of NFD
+-- normalisation, only replace the base kana (leaving the combining ◌゙・◌゚ marks
+-- intact), then re-normalise with NFC but it's simpler to do it this way.
+-- @TODO Maybe add historic (ゐ, ゑ) or lingustic (う゚, か゚, さ゚, ら゚) kana too?
+
+local FULLWIDTH_HIRAGANA = {
+    -- 小書き
+    "ぁ", "ぃ", "ぅ", "ぇ", "ぉ",
+    "ゕ",             "ゖ",
+                "っ",
+    "ゃ",       "ゅ",       "ょ",
+    "ゎ",
+    -- 濁点・半濁点 つき
+    "が", "ぎ", "ぐ", "げ", "ご",
+    "ざ", "じ", "ず", "ぜ", "ぞ",
+    "だ", "ぢ", "づ", "で", "ど",
+    "ば", "び", "ぶ", "べ", "ぼ",
+    "ぱ", "ぴ", "ぷ", "ぺ", "ぽ",
+    "わ゙",       "ゔ",       "を゙",
+    -- 五十音と撥音
+    "あ", "い", "う", "え", "お",
+    "か", "き", "く", "け", "こ",
+    "さ", "し", "す", "せ", "そ",
+    "た", "ち", "つ", "て", "と",
+    "な", "に", "ぬ", "ね", "の",
+    "は", "ひ", "ふ", "へ", "ほ",
+    "ま", "み", "む", "め", "も",
+    "や",       "ゆ",       "よ",
+    "ら", "り", "る", "れ", "ろ",
+    "わ",                   "を",
+    "ん", "ー",
+}
+
+local FULLWIDTH_KATAKANA = {
+    -- 小書き
+    "ァ", "ィ", "ゥ", "ェ", "ォ",
+    "ヵ",             "ヶ",
+                "ッ",
+    "ャ",       "ュ",       "ョ",
+    "ヮ",
+    -- 濁点・半濁点 つき
+    "ガ", "ギ", "グ", "ゲ", "ゴ",
+    "ザ", "ジ", "ズ", "ゼ", "ゾ",
+    "ダ", "ヂ", "ヅ", "デ", "ド",
+    "バ", "ビ", "ブ", "ベ", "ボ",
+    "パ", "ピ", "プ", "ペ", "ポ",
+    "ヷ",       "ヴ",       "ヺ",
+    -- 五十音と撥音
+    "ア", "イ", "ウ", "エ", "オ",
+    "カ", "キ", "ク", "ケ", "コ",
+    "サ", "シ", "ス", "セ", "ソ",
+    "タ", "チ", "ツ", "テ", "ト",
+    "ナ", "ニ", "ヌ", "ネ", "ノ",
+    "ハ", "ヒ", "フ", "ヘ", "ホ",
+    "マ", "ミ", "ム", "メ", "モ",
+    "ヤ",       "ユ",       "ヨ",
+    "ラ", "リ", "ル", "レ", "ロ",
+    "ワ",                   "ヲ",
+    "ン", "ー",
+}
+
+local HALFWIDTH_KATAKANA = {
+    -- 小書き
+    "ｧ",  "ｨ",  "ｩ",  "ｪ",  "ｫ",
+    "",               "",         -- no ヵ・ヶ (small か・け)
+                "ｯ",
+    "ｬ",        "ｭ",        "ｮ",
+    "",                           -- no ゎ (small わ)
+    -- 濁点・半濁点 つき
+    "ｶﾞ", "ｷﾞ", "ｸﾞ", "ｹﾞ", "ｺﾞ",
+    "ｻﾞ", "ｼﾞ", "ｽﾞ", "ｾﾞ", "ｿﾞ",
+    "ﾀﾞ", "ﾁﾞ", "ﾂﾞ", "ﾃﾞ", "ﾄﾞ",
+    "ﾊﾞ", "ﾋﾞ", "ﾌﾞ", "ﾍﾞ", "ﾎﾞ",
+    "ﾊﾟ", "ﾋﾟ", "ﾌﾟ", "ﾍﾟ", "ﾎﾟ",
+    "ﾜﾞ",       "ｳﾞ",       "ｦﾞ",
+    -- 五十音と撥音
+    "ｱ",  "ｲ",  "ｳ",  "ｴ",  "ｵ",
+    "ｶ",  "ｷ",  "ｸ",  "ｹ",  "ｺ",
+    "ｻ",  "ｼ",  "ｽ",  "ｾ",  "ｿ",
+    "ﾀ",  "ﾁ",  "ﾂ",  "ﾃ",  "ﾄ",
+    "ﾅ",  "ﾆ",  "ﾇ",  "ﾈ",  "ﾉ",
+    "ﾊ",  "ﾋ",  "ﾌ",  "ﾍ",  "ﾎ",
+    "ﾏ",  "ﾐ",  "ﾑ",  "ﾒ",  "ﾓ",
+    "ﾔ",        "ﾕ",        "ﾖ",
+    "ﾗ",  "ﾘ",  "ﾙ",  "ﾚ",  "ﾛ",
+    "ﾜ",                    "ｦ",
+    "ﾝ", "ｰ",
+}
+
+-- Ensure all of the tables are normalised.
+for i, c in ipairs(HALFWIDTH_KATAKANA) do HALFWIDTH_KATAKANA[i] = Utf8Proc.normalize_NFC(c) end
+for i, c in ipairs(FULLWIDTH_KATAKANA) do FULLWIDTH_KATAKANA[i] = Utf8Proc.normalize_NFC(c) end
+for i, c in ipairs(FULLWIDTH_HIRAGANA) do FULLWIDTH_HIRAGANA[i] = Utf8Proc.normalize_NFC(c) end
+-- Ensure all tables are the same size.
+assert(#HALFWIDTH_KATAKANA == #FULLWIDTH_KATAKANA)
+assert(#FULLWIDTH_KATAKANA == #FULLWIDTH_HIRAGANA)
+-- Create fast conversion tables.
+local HALFWIDTH_TO_FULLWIDTH, KATAKANA_TO_HIRAGANA, HIRAGANA_TO_KATAKANA = {}, {}, {}
+for i in ipairs(FULLWIDTH_KATAKANA) do
+    KATAKANA_TO_HIRAGANA[FULLWIDTH_KATAKANA[i]] = FULLWIDTH_HIRAGANA[i]
+    HIRAGANA_TO_KATAKANA[FULLWIDTH_HIRAGANA[i]] = FULLWIDTH_KATAKANA[i]
+    -- Some entries are "" but that doesn't matter since we won't hit them during conversion.
+    HALFWIDTH_TO_FULLWIDTH[HALFWIDTH_KATAKANA[i]] = FULLWIDTH_KATAKANA[i]
+end
+
+local function kana_mapper(map)
+    return function(text)
+        local new_text = {}
+        for c in text:gmatch(util.UTF8_CHAR_PATTERN) do
+            table.insert(new_text, map[c] or c)
+        end
+        return {table.concat(new_text, "")}
+    end
+end
+
+local EMPHATIC_SYMBOLS = {
+    ["っ"] = true, ["ッ"] = true,
+    ["ー"] = true, ["〜"] = true,
+}
+
+local function collapse_emphatic(text)
+    local complete_collapse, partial_collapse = {}, {}
+    local last_char
+    for c in text:gmatch(util.UTF8_CHAR_PATTERN) do
+        if not EMPHATIC_SYMBOLS[c] then
+            table.insert(partial_collapse, c)
+            table.insert(complete_collapse, c)
+        elseif last_char ~= c then -- first instance of this emphatic marker
+            table.insert(partial_collapse, c)
+        end
+        last_char = c
+    end
+    return {
+        table.concat(partial_collapse, ""),
+        table.concat(complete_collapse, ""),
+    }
+end
+
+--- Return the set of deinflections (and the reason path taken) for the
+-- provided text. In addition to the verbatim text provided, several cleanups
+-- will be attempted on the text (conversion from half-width kana, conversion
+-- between katakana and hiragana, and collapsing of any emphatic sequences) and
+-- any valid deinflections found will also be returned.
+--
+-- @tparam string text Japanese text to deinflect.
+-- @treturn {DeinflectResult,...} An array of possible deinflections (including the text given).
+function Deinflector:deinflect(text)
+    -- Normalise the text to ensure that we handle full-width text that
+    -- inexplicably uses combining 濁点・半濁点 (◌゙・◌゚) marks.
+    text = Utf8Proc.normalize_NFC(util.fixUtf8(text, "�"))
+    local seen = {}
+    local all_results = {}
+    local map_functions = {
+        kana_mapper(HALFWIDTH_TO_FULLWIDTH),
+        kana_mapper(HIRAGANA_TO_KATAKANA),
+        kana_mapper(KATAKANA_TO_HIRAGANA),
+        collapse_emphatic,
+    }
+    -- Iterate over the powerset of map_functions by looping over every
+    -- possible bitmask for map_functions then applying the functions which
+    -- have their corresponding bit set in the mask.
+    local max_mapfn_bitmask = bit.lshift(1, #map_functions) - 1 -- (2^n - 1)
+    for mapfn_bitmask = 0, max_mapfn_bitmask do
+        local mapped_texts = {text}
+        for i, func in ipairs(map_functions) do
+            local mapfn_bit = bit.lshift(1, i-1) -- the bit for this function
+            if bit.band(mapfn_bit, mapfn_bitmask) ~= 0 then
+                local old_texts = mapped_texts
+                mapped_texts = {}
+                for _, old_text in ipairs(old_texts) do
+                    util.arrayAppend(mapped_texts, func(old_text))
+                end
+            end
+        end
+        for _, mapped_text in ipairs(mapped_texts) do
+            if not seen[mapped_text] then
+                if text ~= mapped_text then
+                    logger.dbg("japanese.koplugin deinflector: trying mapped variant", text, "->", mapped_text)
+                end
+                local results = self:deinflectVerbatim(mapped_text)
+                if results then
+                    util.arrayAppend(all_results, results)
+                end
+                seen[mapped_text] = true
+            end
+        end
+    end
+    return all_results
+end
+
+--- Initialise a Deflector instance with the set of rules defined in
+-- yomichan-deflect.json.
+function Deinflector:init()
+    if self.rules ~= nil then return end -- already loaded
+    -- @TODO Maybe make this location configurable?
+    inflections = parsePluginJson("yomichan-deinflect.json")
+    -- Normalise the reasons and convert the rules to the rule_types bitflags.
+    self.rules = {}
+    local nrules, nvariants = 0, 0
+    for reason, rules in pairs(inflections) do
+        variants = {}
+        for i, variant in ipairs(rules) do
+            variants[i] = {
+                kanaIn = variant.kanaIn,
+                kanaOut = variant.kanaOut,
+                rulesIn = toRuleTypes(unpack(variant.rulesIn)),
+                rulesOut = toRuleTypes(unpack(variant.rulesOut)),
+            }
+            nvariants = nvariants + 1
+        end
+        nrules = nrules + 1
+        self.rules[reason] = variants
+    end
+    logger.dbg("japanese.koplugin deinflector: loaded inflection table with", nrules, "rules and", nvariants, "variants")
+end
+
+--- Create a new Deflector instance.
+function Deinflector:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    o:init()
+    return o
+end
+
+return Deinflector
