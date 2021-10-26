42dcc2d4fbfebe696947539e71c3055e02c2b1e2
yparitcher <y@paritcher.com>
2021-10-26 11:01:23 -0400

    [CI] Github Actions

--- .ci/test.sh
+++ .ci/test.sh
@@ -4,9 +4,10 @@ CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
 source "${CI_DIR}/common.sh"
 
-pushd koreader-emulator-x86_64-linux-gnu/koreader && {
-    # the circleci command spits out newlines; we want spaces instead
-    BUSTED_SPEC_FILE="$(circleci tests glob "spec/front/unit/*_spec.lua" | circleci tests split --split-by=timings --timings-type=filename | tr '\n' ' ')"
-} && popd || exit
-
-make testfront BUSTED_SPEC_FILE="${BUSTED_SPEC_FILE}"
+#pushd koreader-emulator-x86_64-linux-gnu/koreader && {
+#    # the circleci command spits out newlines; we want spaces instead
+#    BUSTED_SPEC_FILE="$(circleci tests glob "spec/front/unit/*_spec.lua" | circleci tests split --split-by=timings --timings-type=filename | tr '\n' ' ')"
+#} && popd || exit
+#
+#make testfront BUSTED_SPEC_FILE="${BUSTED_SPEC_FILE}"
+make testfront
