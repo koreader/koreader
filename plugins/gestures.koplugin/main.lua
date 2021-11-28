d5bb204bab2548836c0a318ef444548964ab3e3b
hius07 <62179190+hius07@users.noreply.github.com>
2021-11-28 08:27:29 +0200

    Gesture intervals spinwidgets - edit info-text

--- plugins/gestures.koplugin/main.lua
+++ plugins/gestures.koplugin/main.lua
@@ -448,11 +448,9 @@ function Gestures:addIntervals(menu_items)
                     end
                     local items = SpinWidget:new{
                         title_text = _("Text selection rate"),
-                        info_text = T(_([[
+                        info_text = _([[
 The rate is how often screen will be refreshed per second while selecting text.
-Higher values mean faster screen updates, but also use more CPU.
-
-Default value: %1]]), Screen.low_pan_rate and 5.0 or 30.0),
+Higher values mean faster screen updates, but also use more CPU.]]),
                         width = math.floor(Screen:getWidth() * 0.75),
                         value = current_value,
                         value_min = 1.0,
@@ -478,11 +476,10 @@ Default value: %1]]), Screen.low_pan_rate and 5.0 or 30.0),
                     local GestureDetector = require("device/gesturedetector")
                     local items = SpinWidget:new{
                         title_text = _("Tap interval"),
-                        info_text = T(_([[
+                        info_text = _([[
 Any other taps made within this interval after a first tap will be considered accidental and ignored.
 
-The interval value is in milliseconds and can range from 0 (0 seconds) to 2000 (2 seconds).
-Default value: %1]]), GestureDetector.TAP_INTERVAL/1000),
+The interval value is in milliseconds and can range from 0 (0 seconds) to 2000 (2 seconds).]]),
                         width = math.floor(Screen:getWidth() * 0.75),
                         value = GestureDetector:getInterval("ges_tap_interval")/1000,
                         value_min = 0,
@@ -509,8 +506,7 @@ Default value: %1]]), GestureDetector.TAP_INTERVAL/1000),
                         info_text = _([[
 Any other taps made within this interval after a first tap will be considered accidental and ignored.
 
-The interval value is in milliseconds and can range from 0 (0 seconds) to 2000 (2 seconds).
-Default value: 0]]),
+The interval value is in milliseconds and can range from 0 (0 seconds) to 2000 (2 seconds).]]),
                         width = math.floor(Screen:getWidth() * 0.75),
                         value = (G_reader_settings:readSetting("ges_tap_interval_on_keyboard") or 0)/1000,
                         value_min = 0,
@@ -534,11 +530,10 @@ Default value: 0]]),
                     local GestureDetector = require("device/gesturedetector")
                     local items = SpinWidget:new{
                         title_text = _("Double tap interval"),
-                        info_text = T(_([[
+                        info_text = _([[
 When double tap is enabled, this sets the time to wait for the second tap. A single tap will take at least this long to be detected.
 
-The interval value is in milliseconds and can range from 100 (0.1 seconds) to 2000 (2 seconds).
-Default value: %1]]), GestureDetector.DOUBLE_TAP_INTERVAL/1000),
+The interval value is in milliseconds and can range from 100 (0.1 seconds) to 2000 (2 seconds).]]),
                         width = math.floor(Screen:getWidth() * 0.75),
                         value = GestureDetector:getInterval("ges_double_tap_interval")/1000,
                         value_min = 100,
@@ -563,11 +558,10 @@ Default value: %1]]), GestureDetector.DOUBLE_TAP_INTERVAL/1000),
                     local GestureDetector = require("device/gesturedetector")
                     local items = SpinWidget:new{
                         title_text = _("Two finger tap duration"),
-                        info_text = T(_([[
+                        info_text = _([[
 This sets the allowed duration of any of the two fingers touch/release for the combined event to be considered a two finger tap.
 
-The duration value is in milliseconds and can range from 100 (0.1 seconds) to 2000 (2 seconds).
-Default value: %1]]), GestureDetector.TWO_FINGER_TAP_DURATION/1000),
+The duration value is in milliseconds and can range from 100 (0.1 seconds) to 2000 (2 seconds).]]),
                         width = math.floor(Screen:getWidth() * 0.75),
                         value = GestureDetector:getInterval("ges_two_finger_tap_duration")/1000,
                         value_min = 100,
@@ -592,11 +586,10 @@ Default value: %1]]), GestureDetector.TWO_FINGER_TAP_DURATION/1000),
                     local GestureDetector = require("device/gesturedetector")
                     local items = SpinWidget:new{
                         title_text = _("Long-press interval"),
-                        info_text = T(_([[
+                        info_text = _([[
 If a touch is not released in this interval, it is considered a long-press. On document text, single word selection will then be triggered.
 
-The interval value is in milliseconds and can range from 100 (0.1 seconds) to 2000 (2 seconds).
-Default value: %1]]), GestureDetector.HOLD_INTERVAL/1000),
+The interval value is in milliseconds and can range from 100 (0.1 seconds) to 2000 (2 seconds).]]),
                         width = math.floor(Screen:getWidth() * 0.75),
                         value = GestureDetector:getInterval("ges_hold_interval")/1000,
                         value_min = 100,
@@ -621,11 +614,10 @@ Default value: %1]]), GestureDetector.HOLD_INTERVAL/1000),
                     local GestureDetector = require("device/gesturedetector")
                     local items = SpinWidget:new{
                         title_text = _("Swipe interval"),
-                        info_text = T(_([[
+                        info_text = _([[
 This sets the maximum delay between the start and the end of a swipe for it to be considered a swipe. Above this interval, it's considered panning.
 
-The interval value is in milliseconds and can range from 100 (0.1 seconds) to 2000 (2 seconds).
-Default value: %1]]), GestureDetector.SWIPE_INTERVAL/1000),
+The interval value is in milliseconds and can range from 100 (0.1 seconds) to 2000 (2 seconds).]]),
                         width = math.floor(Screen:getWidth() * 0.75),
                         value = GestureDetector:getInterval("ges_swipe_interval")/1000,
                         value_min = 100,
