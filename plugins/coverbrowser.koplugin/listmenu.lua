7fc4dc520feb5771288256f33d5a7cd367da8d2f
hius07 <62179190+hius07@users.noreply.github.com>
2021-12-09 19:07:26 +0200

    Update listmenu.lua

--- plugins/coverbrowser.koplugin/listmenu.lua
+++ plugins/coverbrowser.koplugin/listmenu.lua
@@ -262,8 +262,8 @@ function ListMenuItem:update()
             },
         }
     else
-        local file_selected = self.menu.filemanager.selected_files and self.menu.filemanager.selected_files[self.filepath]
-        if file_mode ~= "file" or file_selected then
+        local is_file_selected = self.menu.filemanager.selected_files and self.menu.filemanager.selected_files[self.filepath]
+        if file_mode ~= "file" or is_file_selected then
             self.file_deleted = true -- dim file
         end
         -- File
