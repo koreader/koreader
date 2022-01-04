ffe4c9f7486c0ea075891e8b065e28be9dc52412
Frans de Jonge <fransdejonge@gmail.com>
2022-01-04 21:53:21 +0100

    and remove that newline for the same reason

--- .ci/after_success.sh
+++ .ci/after_success.sh
@@ -8,7 +8,6 @@ set +e
 
 if [ -z "${CIRCLE_PULL_REQUEST}" ] && [ "${CIRCLE_BRANCH}" = 'master' ]; then
     echo "CIRCLE_NODE_INDEX: ${CIRCLE_NODE_INDEX}"
-
     if [ "${CIRCLE_NODE_INDEX}" = 1 ]; then
         echo -e "\\n${ANSI_GREEN}Running make testfront for timings."
         make testfront BUSTED_OVERRIDES="--output=junit -Xoutput junit-test-results.xml"
