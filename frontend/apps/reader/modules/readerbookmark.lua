89327a22e8fadceb215c2a0f8e3fc5e239fbd9af
hius07 <62179190+hius07@users.noreply.github.com>
2022-01-10 14:47:47 +0200

    Update readerbookmark.lua

--- frontend/apps/reader/modules/readerbookmark.lua
+++ frontend/apps/reader/modules/readerbookmark.lua
@@ -481,10 +481,6 @@ function ReaderBookmark:onShowBookmark(match_table)
     local multilines_show_more_text = G_reader_settings:isTrue("bookmarks_items_multilines_show_more_text")
     local show_separator = G_reader_settings:isTrue("bookmarks_items_show_separator")
 
-    self.bookmark_menu = CenterContainer:new{
-        dimen = Screen:getSize(),
-        covers_fullscreen = true, -- hint for UIManager:_repaint()
-    }
     local bm_menu = Menu:new{
         title = self.filtered_mode and _("Bookmarks (search results)") or _("Bookmarks"),
         item_table = item_table,
@@ -508,9 +504,7 @@ function ReaderBookmark:onShowBookmark(match_table)
                 direction = BD.flipDirectionIfMirroredUILayout("east")
             }
         },
-        show_parent = self.bookmark_menu,
     }
-    table.insert(self.bookmark_menu, bm_menu)
 
     -- buid up menu widget method as closure
     local bookmark = self
@@ -821,7 +815,7 @@ function ReaderBookmark:onShowBookmark(match_table)
     end
 
     bm_menu.close_callback = function()
-        UIManager:close(self.bookmark_menu)
+        UIManager:close(bm_menu)
     end
 
     self.refresh = function()
@@ -829,7 +823,7 @@ function ReaderBookmark:onShowBookmark(match_table)
         self:onSaveSettings()
     end
 
-    UIManager:show(self.bookmark_menu)
+    UIManager:show(bm_menu)
     return true
 end
 
