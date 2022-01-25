28bd63c2215d55a34150d2b1dbfe95a775dd0dbe
Robert-Jan de Dreu <robertjandreu@gmail.com>
2022-01-25 16:05:15 +0100

    Check NET_CONNECTED to see if wifi is really connected

--- frontend/device/pocketbook/device.lua
+++ frontend/device/pocketbook/device.lua
@@ -360,14 +360,7 @@ function PocketBook:initNetworkManager(NetworkMgr)
 
     function NetworkMgr:isWifiOn()
         local state = inkview.QueryNetwork()
-        -- Some devices (PB741) return state = 515 for connected and state = 3
-        -- when not connected. We guess the reason is deprecation of the old API
-        -- for this reason when state is higher than C.CONNECTED we try the new API
-        if state <= C.CONNECTED then
-            return band(state, C.CONNECTED) ~= 0
-        else
-            return band(inkview.GetNetState(), C.CONNECTED) ~= 0
-        end
+        return band(state, C.NET_CONNECTED) ~= 0
     end
 end
 
