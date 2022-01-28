9e0b563a0729f24b4c5ab04861549962f7240b81
zwim <martin.zwicknagl@kirchbichl.net>
2022-01-28 13:58:43 +0100

    Remove terminal.koplugin

--- plugins/terminal.koplugin/_meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("gettext")
-return {
-    name = "terminal",
-    fullname = _("Terminal"),
-    description = _([[Executes simple commands and shows their output.]]),
-    deprecated = "Will be given up in favor of KOTerm.",
-}
