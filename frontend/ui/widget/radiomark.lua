efce9315ccb295fb0e7f793d45a4b86388804ffe
hius07 <62179190+hius07@users.noreply.github.com>
2022-02-03 18:26:24 +0200

    RadioMark: down shift

--- frontend/ui/widget/radiomark.lua
+++ frontend/ui/widget/radiomark.lua
@@ -3,6 +3,8 @@ local Blitbuffer = require("ffi/blitbuffer")
 local Font = require("ui/font")
 local InputContainer = require("ui/widget/container/inputcontainer")
 local TextWidget = require("ui/widget/textwidget")
+local VerticalGroup = require("ui/widget/verticalgroup")
+local VerticalSpan = require("ui/widget/verticalspan")
 
 local RadioMark = InputContainer:new{
     checkable = true, -- empty space when false
@@ -11,16 +13,25 @@ local RadioMark = InputContainer:new{
     face = Font:getFace("smallinfofont"),
     baseline = 0,
     _mirroredUI = BD.mirroredUILayout(),
+    -- round radio mark looks a little bit upper than the button/menu text
+    -- default vertical down shift ratio (to the text height) looks good in touchmenu
+    v_shift_ratio = 0.05,
 }
 
 function RadioMark:init()
-    local widget = TextWidget:new{
+    local text_widget = TextWidget:new{
         text = self.checkable and (self.checked and "◉ " or "◯ ") or "",
         face = self.face,
         fgcolor = self.enabled and Blitbuffer.COLOR_BLACK or Blitbuffer.COLOR_DARK_GRAY,
         para_direction_rtl = self._mirroredUI,
     }
-    self.baseline = widget:getBaseline()
+    self.baseline = text_widget:getBaseline()
+    local pad = math.floor(text_widget:getSize().h * self.v_shift_ratio)
+    local widget = VerticalGroup:new{
+        align = "left",
+        VerticalSpan:new{ width = pad },
+        text_widget,
+    }
     self[1] = widget
     self.dimen = widget:getSize()
 end
