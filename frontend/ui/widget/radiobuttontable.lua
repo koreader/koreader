ec7f11b55237604b2055bf5cc4ae07d047ef357e
yparitcher <y@paritcher.com>
2022-02-26 21:29:31 -0500

    RadioButtonTable: CheckButton does not have onFocus

--- frontend/ui/widget/radiobuttontable.lua
+++ frontend/ui/widget/radiobuttontable.lua
@@ -120,7 +120,6 @@ function RadioButtonTable:init()
 
     if Device:hasDPad() or Device:hasKeyboard() then
         self.layout = self.radio_buttons_layout
-        self.layout[1][1]:onFocus()
         self.key_events.SelectByKeyPress = { {{"Press"}} }
     else
         self.key_events = {}  -- deregister all key press event listeners
