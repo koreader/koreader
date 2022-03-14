6e41db639961ac4764b6022e480ae953758c4809
Will <willharrisbraun@gmail.com>
2022-03-13 20:17:01 -0400

    Ignore hardware sleep button
    When running KOReader with a launcher, having the power button trigger the Power event conflicts with the launcher's built-in screensaver (see https://github.com/koreader/koreader/issues/8891). This commit ignores the hardware power button in KOReader and lets the launcher handle sleep and wake normally.

--- frontend/device/remarkable/event_map.lua
+++ frontend/device/remarkable/event_map.lua
@@ -2,6 +2,6 @@ return {
     [102] = "Home",
     [105] = "LPgBack",
     [106] = "RPgFwd",
-    [116] = "Power",
+    [116] = "noop",
 }
 -- TODO libremarkable has 143 as "wakeup" - don't know what that corresponds to?
