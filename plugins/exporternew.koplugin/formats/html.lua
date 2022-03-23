2cebf7a34c4187f88a32e0579418854356704a65
Utsob Roy <roy@codesign.com.bd>
2022-03-23 15:13:31 +0600

    html export

--- plugins/exporternew.koplugin/formats/html.lua
+++ plugins/exporternew.koplugin/formats/html.lua
@@ -1,9 +1,42 @@
+local logger = require("logger")
+
+local slt2 = require('template/slt2')
+
+
 local HtmlExporter = require("formats/base"):new{
     name = "html",
+    version = "html/1.0.0",
+
 }
 
-function HtmlExporter:export(t)
-    print("html export")
+
+function HtmlExporter:export(t, export_type, timestamp)
+    local path, title
+    if export_type == "single" then
+        path = self:getFilePath(timestamp, t[1].title)
+        title = t[1].title
+    else
+        path = self:getFilePath(timestamp)
+        title = "All Books"
+    end
+    local file = io.open(path, "w")
+    local template = slt2.loadfile(self.path .. "/template/note.tpl")
+    if file then
+        local content = slt2.render(template, {
+            clippings = t,
+            document_title = title,
+        })
+        file:write(content)
+        file:close()
+    end
+end
+
+function HtmlExporter:getFilePath(timestamp, title)
+    if title then
+        return self.clipping_dir .. "/" .. timestamp .. "-" .. title .. ".html"
+    else
+        return self.clipping_dir .. "/" .. timestamp .. "-all-books.html"
+    end
 end
 
 return HtmlExporter
