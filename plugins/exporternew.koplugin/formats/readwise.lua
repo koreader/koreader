33cbd84ded75604a8f06c4e9edd1a7264bd9379b
Utsob Roy <roy@codesign.com.bd>
2022-03-24 09:46:49 +0600

    readwise support

--- plugins/exporternew.koplugin/formats/readwise.lua
+++ plugins/exporternew.koplugin/formats/readwise.lua
@@ -1,10 +1,78 @@
+
+local ReadwiseClient = require("clients/ReadwiseClient")
+local _ = require("gettext")
+local InfoMessage = require("ui/widget/infomessage")
+local InputDialog = require("ui/widget/inputdialog")
+local UIManager = require("ui/uimanager")
+
+
 local ReadwiseExporter = require("formats/base"):new{
     name = "readwise",
     is_remote = true,
+    version = "readwise/1.0.0"
 }
 
-function ReadwiseExporter:export(t)
-    print("readwise export")
+
+function ReadwiseExporter:getMenuTable()
+    return {
+        text = _("Readwise") ,
+        checked_func = function() return self:isEnabled() end,
+        sub_item_table ={
+            {
+                text = _("Set authorization token"),
+                keep_menu_open = true,
+                callback = function()
+                    local auth_dialog
+                    auth_dialog = InputDialog:new{
+                        title = _("Set authorization token for Readwise"),
+                        input = self.settings.token,
+                        buttons = {
+                            {
+                                {
+                                    text = _("Cancel"),
+                                    callback = function()
+                                        UIManager:close(auth_dialog)
+                                    end
+                                },
+                                {
+                                    text = _("Set token"),
+                                    callback = function()
+                                        self.settings.token = auth_dialog:getInputText()
+                                        self:saveSettings()
+                                        UIManager:close(auth_dialog)
+                                    end
+                                }
+                            }
+                        }
+                    }
+                    UIManager:show(auth_dialog)
+                    auth_dialog:onShowKeyboard()
+                end
+            },
+            {
+                text = _("Export to Readwise"),
+                checked_func = function() return self:isEnabled() end,
+                callback = function() self:toggleEnabled() end,
+            },
+
+        }
+    }
+end
+
+function ReadwiseExporter:getClient()
+    return ReadwiseClient:new{
+            auth_token = self.settings.token
+        }
+end
+
+function ReadwiseExporter:exportOne(t)
+    local client = self:getClient()
+    client:createHighlights(t)
+end
+
+function ReadwiseExporter:exportAll(t)
+    local client = self:getClient()
+    client:createAllHighlights(t)
 end
 
 return ReadwiseExporter
