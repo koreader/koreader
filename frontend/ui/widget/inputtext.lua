koreader/koreader

commit 107156c0a851f3692106cd8a3a492be9ab43e610

[feat] Non-touch improvements (#8859)
FocusManager: fix round x use y layout
FocusManager: add tab and shift tab focus navigation support
FocusManager: handle Press key by default
FocusManager: make sure selected in instance level
FocusManager: add hold event support
FocusManager: Half move instead of edge move
FocusManager: add keymap override support
FocusManager: refocusWidget will delegate to parent FocusManager
Focusmanager: refocusWidget can execute on next tick
inputtext: can move out of focus on back
inputtext: fix cannot exit for non-touch device
inputtext: fix cannot input text with kindle dx physical keyboard
fontlightwidget: add non-touch support
datetimewidget: add non-touch support
datetimewidget: fix set date failed in kindle DX, fix datetimewidget month range to 1~23 by default
datetimewidget: make hour max value to 23
multiinputdialog: add non-touch support
checkbox: focusable and focus style
virtualkeyboard: no need to press two back to unfocus inputtext
virtualkeyboard: collect FocusManager event key names to let VirtualKeyboard disable them
openwithdialog: add non-touch support
inputdialog: can close via back button
enable all InputDialog and MultiInputDialog can be close by back
keyboardlayoutdialog: non-touch support
readertoc: non touch device can expand/collapse in toc
bookstatuswidget: non touch support
keyvaluepage: non-touch support
calendarview: non-touch support

frontend/ui/widget/inputtext.lua
@@ -1,6 +1,7 @@
 local Blitbuffer = require("ffi/blitbuffer")
 local CheckButton = require("ui/widget/checkbutton")
 local Device = require("device")
+local FocusManager = require("ui/widget/focusmanager")
 local FrameContainer = require("ui/widget/container/framecontainer")
 local Font = require("ui/font")
 local Geom = require("ui/geometry")
@@ -19,6 +20,7 @@ local _ = require("gettext")
 local Screen = Device.screen
 
 local Keyboard
+local FocusManagerInstance = FocusManager:new{}
 
 local InputText = InputContainer:new{
     text = "",
@@ -274,7 +276,6 @@ if Device:isTouchDevice() or Device:hasDPad() then
             -- used for taking a screenshot)
             return false
         end
-
     end
     if Device:hasDPad() then
         if not InputText.initEventListener then
@@ -283,14 +284,17 @@ if Device:isTouchDevice() or Device:hasDPad() then
 
         function InputText:onFocus()
             -- Event called by the focusmanager
-            self.key_events.ShowKeyboard = { {"Press"}, doc = "show keyboard" }
+            if self.parent.onSwitchFocus then
+                self.parent:onSwitchFocus(self)
+            else
+                self:onShowKeyboard()
+            end
             self:focus()
             return true
         end
 
         function InputText:onUnfocus()
             -- Event called by the focusmanager
-            self.key_events = {}
             self:unfocus()
             return true
         end
@@ -542,7 +546,11 @@ end
 -- is shown. Mostly likely to be in the emulator, but could be Android + BT
 -- keyboard, or a "coder's keyboard" Android input method.
 function InputText:onKeyPress(key)
-
+    -- only handle key on focused status, otherwise there are more than one InputText
+    -- the first one always handle key pressed
+    if not self.focused then
+        return false
+    end
     local handled = true
 
     if not key["Ctrl"] and not key["Shift"] and not key["Alt"] then
@@ -567,6 +575,10 @@ function InputText:onKeyPress(key)
             self:addChars("\n")
         elseif key["Tab"] then
             self:addChars("    ")
+        elseif key["Back"] then
+            if self.focused then
+                self:unfocus()
+            end
         else
             handled = false
         end
@@ -581,15 +593,46 @@ function InputText:onKeyPress(key)
     else
         handled = false
     end
-
+    if not handled and Device:hasDPad() then
+        -- FocusManager may turn on alternative key maps.
+        -- These key map maybe single text keys.
+        -- It will cause unexpected focus move instead of enter text to InputText
+        local is_alternative_key = FocusManagerInstance:isAlternativeKey(key)
+        if not is_alternative_key and Device:isSDL() then
+            -- SDL already insert char via TextInput event
+            -- Stop event propagate to FocusManager
+            return true
+        end
+        -- if it is single text char, insert it
+        local key_code = key.key -- is in upper case
+        if not Device.isSDL() and #key_code == 1 then
+            if not key["Shift"] then
+                key_code = string.lower(key_code)
+            end
+            for modifier, flag in pairs(key.modifiers) do
+                if modifier ~= "Shift" and flag then -- Other modifier: not a single char insert
+                    return true
+                end
+            end
+            self:addChars(key_code)
+            return true
+        end
+        if is_alternative_key then
+            return true -- Stop event propagate to FocusManager to void focus move
+        end
+    end
     return handled
 end
 
 -- Handle text coming directly as text from the Device layer (eg. soft keyboard
 -- or via SDL's keyboard mapping).
 function InputText:onTextInput(text)
-    self:addChars(text)
-    return true
+    -- for more than one InputText, let the focused one add chars
+    if self.focused then
+        self:addChars(text)
+        return true
+    end
+    return false
 end
 
 function InputText:onShowKeyboard(ignore_first_hold_release)

commit 29089465360ea17557dd8ea004d2b02790081cd7

Make sure InputText has a consistent API, regardless of hasDPad
Fix #8967
Regression since #8859

frontend/ui/widget/inputtext.lua
@@ -68,6 +68,11 @@ local InputText = InputContainer:new{
     is_keyboard_hidden = false, -- to be able to show the keyboard again when it was hidden
 }
 
+-- These may be (internally) overloaded as needed, depending on Device capabilities.
+function InputText:initEventListener() end
+function InputText:onFocus() end
+function InputText:onUnfocus() end
+
 -- only use PhysicalKeyboard if the device does not have touch screen
 if Device:isTouchDevice() or Device:hasDPad() then
     Keyboard = require("ui/widget/virtualkeyboard")
@@ -278,10 +283,6 @@ if Device:isTouchDevice() or Device:hasDPad() then
         end
     end
     if Device:hasDPad() then
-        if not InputText.initEventListener then
-            function InputText:initEventListener() end
-        end
-
         function InputText:onFocus()
             -- Event called by the focusmanager
             if self.parent.onSwitchFocus then
@@ -301,7 +302,6 @@ if Device:isTouchDevice() or Device:hasDPad() then
     end
 else
     Keyboard = require("ui/widget/physicalkeyboard")
-    function InputText:initEventListener() end
 end
 
 function InputText:checkTextEditability()