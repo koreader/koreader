5fc5720bb17d2efd50bd8c4940713acfb818169d
Utsob Roy <roy@utsob.me>
2022-04-08 21:31:16 +0600

    added migrate and discard settings

--- plugins/exporternew.koplugin/formats/html.lua
+++ plugins/exporternew.koplugin/formats/html.lua
@@ -6,8 +6,25 @@ local slt2 = require('template/slt2')
 local HtmlExporter = require("formats/base"):new{
     name = "html",
     version = "html/1.0.0",
+    settings_version = "1.0.0"
 
 }
+
+function HtmlExporter:migrateOrDiscardSettings(plugin_settings)
+    if plugin_settings[self.name] == nil then
+        if plugin_settings.html_export ~= nil then
+            local settings = {
+                version = self.settings_version,
+                enabled = plugin_settings.html_export
+            }
+            plugin_settings.html_export = nil
+            plugin_settings[self.name] = settings
+            G_reader_settings:saveSetting(self.id, plugin_settings)
+            return settings
+        end
+    end
+    return plugin_settings[self.name]
+end
 
 function HtmlExporter:export(t)
     local path, title
