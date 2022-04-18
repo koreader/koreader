d37009a62199f740d4d0db553faef8917efb968c
NiLuJe <ninuje@gmail.com>
2022-04-18 02:46:41 +0200

    One more reason ;o).

--- plugins/autosuspend.koplugin/main.lua
+++ plugins/autosuspend.koplugin/main.lua
@@ -584,7 +584,8 @@ function AutoSuspend:onAllowStandby()
         -- Make sure UIManager will consume the input events that woke us up first (in case we were woken up by user input,
         -- as opposed to an rtc wake alarm)!
         -- (This ensures we'll use an up to date last_action_tv, and that it only ever gets updated from *user* input).
-        -- NOTE: UIManager consumes scheduled tasks before input, so make sure we delay by a significant amount...
+        -- NOTE: UIManager consumes scheduled tasks before input events, so make sure we delay by a significant amount,
+        --       especially given that this delay will likely be used as the next input polling loop timeout...
         UIManager:scheduleIn(1, self.leave_standby_task)
     end
     -- We don't reschedule standby here, as this will interfere with suspend.
