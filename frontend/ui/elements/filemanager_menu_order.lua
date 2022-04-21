53e1b680e868d0f8531d88eff680c78c53075b3e
zwim <martin.zwicknagl@kirchbichl.net>
2022-04-21 10:48:27 +0200

    1st try

--- frontend/ui/elements/filemanager_menu_order.lua
+++ frontend/ui/elements/filemanager_menu_order.lua
@@ -90,6 +90,7 @@ local order = {
         "----------------------------",
         "screen_dpi",
         "screen_eink_opt",
+        "autodim",
         "autowarmth",
         "color_rendering",
         "----------------------------",
