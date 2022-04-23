e09e37eacba93e7a4f9b6aa4380ff9f9ae6f38c5
Utsob Roy <roy@utsob.me>
2022-04-23 21:34:58 +0600

    fixed text export

--- plugins/exporter.koplugin/formats/text.lua
+++ plugins/exporter.koplugin/formats/text.lua
@@ -17,31 +17,34 @@ function TextExporter:export(t)
     local path = self:getFilePath()
     local file = io.open(path, "a")
     if file then
-        file:write(title .. "\n" .. wide_space .. "\n")
-        for _ignore1, chapter in ipairs(t) do
-            if chapter.title then
-                file:write(wide_space .. chapter.title .. "\n" .. wide_space .. "\n")
+        for _ignore, booknotes in ipairs(t) do
+            if booknotes.title then
+                file:write(wide_space .. booknotes.title .. "\n" .. wide_space .. "\n")
             end
-            for _ignore2, clipping in ipairs(chapter) do
-                file:write(wide_space .. wide_space ..
-                            T(_("-- Page: %1, added on %2\n"),
-                                clipping.page, os.date("%c", clipping.time)))
-                if clipping.text then
-                    file:write(clipping.text)
+            for _ignore1, entry in ipairs(booknotes) do
+                for _ignore2, clipping in ipairs(entry) do
+                    if clipping.chapter then
+                        file:write(wide_space .. clipping.chapter .. "\n" .. wide_space .. "\n")
+                    end
+                    file:write(wide_space .. wide_space ..
+                    T(_("-- Page: %1, added on %2\n"),
+                        clipping.page, os.date("%c", clipping.time)))
+                    if clipping.text then
+                        file:write(clipping.text)
+                    end
+                    if clipping.note then
+                        file:write("\n---\n" .. clipping.note)
+                    end
+                    if clipping.image then
+                        file:write(_("<An image>"))
+                    end
+                    file:write("\n-=-=-=-=-=-\n")
                 end
-                if clipping.note then
-                    file:write("\n---\n" .. clipping.note)
-                end
-                if clipping.image then
-                    file:write(_("<An image>"))
-                end
-                file:write("\n-=-=-=-=-=-\n")
             end
         end
-
         file:write("\n")
         file:close()
-    end        
+    end
 end
 
 return TextExporter
