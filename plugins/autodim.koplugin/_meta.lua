0de1c247ba37c6bd7437dc05007ea6ff1a037e6b
zwim <martin.zwicknagl@kirchbichl.net>
2022-04-24 02:18:13 +0200

    Better a separate plugin

---
+++ plugins/autodim.koplugin/_meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "autodim",
+    fullname = _("Automatic dimmer"),
+    description = _([[This plugin allows to dim the frontlight after an idle period.]]),
+}
