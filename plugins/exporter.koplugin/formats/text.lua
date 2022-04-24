81a05372c47c8e88503e6102949a6047df8e6044
Martín Fdez <paziusss@gmail.com>
2022-04-24 15:25:22 +0200

    text: bool export, rename unused var in for loop

--- plugins/exporter.koplugin/formats/text.lua
+++ plugins/exporter.koplugin/formats/text.lua
@@ -12,35 +12,34 @@ function TextExporter:export(t)
     local wide_space = "\227\128\128"
     local path = self:getFilePath()
     local file = io.open(path, "a")
-    if file then
-        for _ignore, booknotes in ipairs(t) do
-            if booknotes.title then
-                file:write(wide_space .. booknotes.title .. "\n" .. wide_space .. "\n")
-            end
-            for _ignore1, entry in ipairs(booknotes) do
-                for _ignore2, clipping in ipairs(entry) do
-                    if clipping.chapter then
-                        file:write(wide_space .. clipping.chapter .. "\n" .. wide_space .. "\n")
-                    end
-                    file:write(wide_space .. wide_space ..
-                    T(_("-- Page: %1, added on %2\n"),
-                        clipping.page, os.date("%c", clipping.time)))
-                    if clipping.text then
-                        file:write(clipping.text)
-                    end
-                    if clipping.note then
-                        file:write("\n---\n" .. clipping.note)
-                    end
-                    if clipping.image then
-                        file:write(_("<An image>"))
-                    end
-                    file:write("\n-=-=-=-=-=-\n")
+    if not file then return false end
+    for __, booknotes in ipairs(t) do
+        if booknotes.title then
+            file:write(wide_space .. booknotes.title .. "\n" .. wide_space .. "\n")
+        end
+        for __, entry in ipairs(booknotes) do
+            for __, clipping in ipairs(entry) do
+                if clipping.chapter then
+                    file:write(wide_space .. clipping.chapter .. "\n" .. wide_space .. "\n")
                 end
+                local text = T(_("-- Page: %1, added on %2\n"), clipping.page, os.date("%c", clipping.time))
+                file:write(wide_space .. wide_space .. text)
+                if clipping.text then
+                    file:write(clipping.text)
+                end
+                if clipping.note then
+                    file:write("\n---\n" .. clipping.note)
+                end
+                if clipping.image then
+                    file:write(_("<An image>"))
+                end
+                file:write("\n-=-=-=-=-=-\n")
             end
         end
         file:write("\n")
-        file:close()
     end
+    file:close()
+    return true
 end
 
 return TextExporter
