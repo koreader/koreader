0b49cd7db9fadeafb7e4a461b5c2b11b6f9f8e33
zwim <martin.zwicknagl@kirchbichl.net>
2022-05-07 22:23:21 +0200

    Monkey-Patches

--- reader.lua
+++ reader.lua
@@ -205,6 +205,33 @@ end
 
 local UIManager = require("ui/uimanager")
 
+-- Run KOReader patches placed in the patches directory (flat, no subdir).
+-- (https://github.com/koreader/koreader/pull/9058#issuecomment-1120240470, thanks to @poire-z)
+local patch_dir = DataStorage:getDataDir() .. "/patches"
+local patch_dir_attributes = lfs.attributes(patch_dir)
+if patch_dir_attributes and patch_dir_attributes.mode == "directory" then
+    local logger = require("logger")
+    logger.info("Patch directory found")
+    for entry in lfs.dir(patch_dir) do
+        local patch = patch_dir .. "/" .. entry
+        local patch_attributes = lfs.attributes(patch)
+        if patch_attributes and patch_attributes.mode == "file" and patch:match("%.lua$") then
+            local ok, err = pcall(dofile, patch)
+            if not ok then
+                -- Only developers (advanced users) will use this mechanism.
+                -- A warning on a patch failure after an OTA update will simplify troubleshooting.
+                logger.warn("Patch", err)
+                local InfoMessage = require("ui/widget/infomessage")
+                UIManager:show(InfoMessage:new{text=_("Error loading patch:\n") .. patch})
+            else
+                logger.info("Patch applied:", patch)
+            end
+        elseif patch_attributes and patch_attributes.mode == "directory" and not patch:match("%.%.?$") then
+            logger.warn("Patch subdirectories are not supported", patch)
+        end
+    end
+end
+
 -- Inform once about color rendering on newly supported devices
 -- (there are some android devices that may not have a color screen,
 -- and we are not (yet?) able to guess that fact)
