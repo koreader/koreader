caa9ef2c7e405ee4a16b36a9c428dd0feece2134
zwim <martin.zwicknagl@kirchbichl.net>
2022-05-08 09:38:22 +0200

    Typo

--- frontend/ui/screensaver.lua
+++ frontend/ui/screensaver.lua
@@ -393,7 +393,7 @@ function Screensaver:setStretchLimit(touchmenu_instance)
         value_max = 25,
         default_value = 8,
         unit = "%",
-        precission = "%1d",
+        precision = "%1d",
         title_text = _("Set maximum stretch limit"),
         ok_text = _("Set"),
         ok_always_enabled = true,
