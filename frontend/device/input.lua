koreader/koreader

commit 86c35ad066c72a6e52a5a935ab2fc4a133d829d6

A host of low power states related tweaks (#9036)
* Disable all non power management related input during suspend. (This prevents wonky touch events from being tripped when closing a sleep cover on an already-in-suspend device, among other things).
* Kobo: Use our WakeupMgr instance, not the class.
* WakupMgr: split `removeTask` in two: 
* `removeTask`, which *only* takes a queue index as input, and only removes a single task. Greatly simplifies the function (i.e., it's just a `table.remove`).
* `removeTasks`, which takes an epoch or a cb ref, and removes *every* task that matches.
* Both of these will also *always* re-schedule the next task (if any) on exit, since we can have multiple WakeupMgr tasks queued, but we can only have a single RTC wake alarm set ;).
* `wakeupAction` now takes a `proximity` argument, which it passes on to its `validateWakeupAlarmByProximity` call, allowing call sites to avoir having to duplicate that call themselves when they want to use a custom proximity window.
* `wakeupAction` now re-schedules the next task (if any) on exit.
* Simplify `Kobo:checkUnexpectedWakeup`, by removing the duplicate `WakerupMgr:validateWakeupAlarmByProximity` call, now that we can pass a proximity window to `WakeuoMgr:wakeupAction`.
* The various network activity timeouts are now halved when autostandby is enabled.
* Autostandby: get rid of the dummy deadline_guard task, as it's no longer necessary since #9009.
* UIManager: The previous change allows us to simplify `getNextTaskTimes` into a simpler `getNextTaskTime` variant, getting rid of a table & a loop.
* ReaderFooter & ReaderHeader: Make sure we only perform a single refresh when exiting standby.
* Kobo: Rewrite sysfs writes to use ANSI C via FFI instead of stdio via Lua, as it obscured some common error cases (e.g., EBUSY on /sys/power/state).
* Kobo: Simplify `suspend`, now that we have sane error handling in sysfs writes.

frontend/device/input.lua
@@ -578,6 +578,57 @@ function Input:handleKeyBoardEv(ev)
     end
 end
 
+-- Mangled variant of handleKeyBoardEv that will only handle power management related keys.
+-- (Used when blocking input during suspend via sleep cover).
+function Input:handlePowerManagementOnlyEv(ev)
+    local keycode = self.event_map[ev.code]
+    if not keycode then
+        -- Do not handle keypress for keys we don't know
+        return
+    end
+
+    -- We'll need to parse the synthetic event map, because SleepCover* events are synthetic.
+    if self.event_map_adapter[keycode] then
+        keycode = self.event_map_adapter[keycode](ev)
+    end
+
+    -- Power management synthetic events
+    if keycode == "SleepCoverClosed" or keycode == "SleepCoverOpened"
+    or keycode == "Suspend" or keycode == "Resume" then
+        return keycode
+    end
+
+    -- Fake events
+    if keycode == "IntoSS" or keycode == "OutOfSS"
+    or keycode == "UsbPlugIn" or keycode == "UsbPlugOut"
+    or keycode == "Charging" or keycode == "NotCharging" then
+        return keycode
+    end
+
+    if keycode == "Power" then
+        -- Kobo generates Power keycode only, we need to decide whether it's
+        -- power-on or power-off ourselves.
+        if ev.value == EVENT_VALUE_KEY_PRESS then
+            return "PowerPress"
+        elseif ev.value == EVENT_VALUE_KEY_RELEASE then
+            return "PowerRelease"
+        end
+    end
+
+    -- Nothing to see, move along!
+    return
+end
+
+-- Empty event handler used to send input to the void
+function Input:voidEv(ev)
+    return
+end
+
+-- Generic event handler for unhandled input events
+function Input:handleGenericEv(ev)
+    return Event:new("GenericInput", ev)
+end
+
 function Input:handleMiscEv(ev)
     -- should be handled by a misc event protocol plugin
 end
@@ -892,13 +943,13 @@ function Input:toggleMiscEvNTX(toggle)
     elseif toggle == false then
         -- Ignore Gyro events
         if self.isNTXAccelHooked then
-            self.handleMiscEv = function() end
+            self.handleMiscEv = self.voidEv
             self.isNTXAccelHooked = false
         end
     else
         -- Toggle it
         if self.isNTXAccelHooked then
-            self.handleMiscEv = function() end
+            self.handleMiscEv = self.voidEv
         else
             self.handleMiscEv = self.handleMiscEvNTX
         end
@@ -1204,7 +1255,10 @@ function Input:waitEvent(now, deadline)
                 end
             else
                 -- Received some other kind of event that we do not know how to specifically handle yet
-                table.insert(handled, Event:new("GenericInput", event))
+                local handled_ev = self:handleGenericEv(event)
+                if handled_ev then
+                    table.insert(handled, handled_ev)
+                end
             end
         end
         return handled
@@ -1217,6 +1271,66 @@ function Input:waitEvent(now, deadline)
         return {
             Event:new("InputError", "Catastrophic")
         }
+    end
+end
+
+-- Allow toggling the handling of most every kind of input, except for power management related events.
+function Input:inhibitInput(toggle)
+    if toggle then
+        -- Only handle power management events
+        if not self._key_ev_handler then
+            logger.info("Inhibiting user input")
+            self._key_ev_handler = self.handleKeyBoardEv
+            self.handleKeyBoardEv = self.handlePowerManagementOnlyEv
+        end
+        -- And send everything else to the void
+        if not self._oasis_ev_handler then
+            self._oasis_ev_handler = self.handleOasisOrientationEv
+            self.handleOasisOrientationEv = self.voidEv
+        end
+        if not self._abs_ev_handler then
+            self._abs_ev_handler = self.handleTouchEv
+            self.handleTouchEv = self.voidEv
+        end
+        if not self._msc_ev_handler then
+            self._msc_ev_handler = self.handleMiscEv
+            self.handleMiscEv = self.voidEv
+        end
+        if not self._sdl_ev_handler then
+            self._sdl_ev_handler = self.handleSdlEv
+            self.handleSdlEv = self.voidEv
+        end
+        if not self._generic_ev_handler then
+            self._generic_ev_handler = self.handleGenericEv
+            self.handleGenericEv = self.voidEv
+        end
+    else
+        -- Restore event handlers, if any
+        if self._key_ev_handler then
+            logger.info("Restoring user input handling")
+            self.handleKeyBoardEv = self._key_ev_handler
+            self._key_ev_handler = nil
+        end
+        if self._oasis_ev_handler then
+            self.handleOasisOrientationEv = self._oasis_ev_handler
+            self._oasis_ev_handler = nil
+        end
+        if self._abs_ev_handler then
+            self.handleTouchEv = self._abs_ev_handler
+            self._abs_ev_handler = nil
+        end
+        if self._msc_ev_handler then
+            self.handleMiscEv = self._msc_ev_handler
+            self._msc_ev_handler = nil
+        end
+        if self._sdl_ev_handler then
+            self.handleSdlEv = self._sdl_ev_handler
+            self._sdl_ev_handler = nil
+        end
+        if self._generic_ev_handler then
+            self.handleGenericEv = self._generic_ev_handler
+            self._generic_ev_handler = nil
+        end
     end
 end
 

frontend/apps/reader/modules/readercoptlistener.lua
@@ -130,7 +130,7 @@ function ReaderCoptListener:rescheduleHeaderRefreshIfNeeded()
     end
 end
 
--- Schedule or stop scheluding on these events, as they may change what is shown:
+-- Schedule or stop scheduling on these events, as they may change what is shown:
 ReaderCoptListener.onSetStatusLine = ReaderCoptListener.rescheduleHeaderRefreshIfNeeded
     -- configurable.status_line is set before this event is triggered
 ReaderCoptListener.onSetViewMode = ReaderCoptListener.rescheduleHeaderRefreshIfNeeded
@@ -145,11 +145,12 @@ function ReaderCoptListener:onResume()
     end
 
     self:headerRefresh()
+    self:rescheduleHeaderRefreshIfNeeded()
 end
 
 function ReaderCoptListener:onLeaveStandby()
-    self:onResume()
-    self:onOutOfScreenSaver()
+    self:headerRefresh()
+    self:rescheduleHeaderRefreshIfNeeded()
 end
 
 function ReaderCoptListener:onOutOfScreenSaver()
@@ -159,6 +160,7 @@ function ReaderCoptListener:onOutOfScreenSaver()
 
     self._delayed_screensaver = nil
     self:headerRefresh()
+    self:rescheduleHeaderRefreshIfNeeded()
 end
 
 -- Unschedule on these events

frontend/apps/reader/modules/readerfooter.lua
@@ -191,13 +191,13 @@ local footerTextGeneratorMap = {
                 batt_lvl = main_batt_lvl + aux_batt_lvl
                 -- But average 'em to compute the icon...
                 if symbol_type == "icons" or symbol_type == "compact_items" then
-                    prefix = powerd:getBatterySymbol(is_charging, batt_lvl / 2)
+                    prefix = powerd:getBatterySymbol(powerd:isAuxCharged(), is_charging, batt_lvl / 2)
                 end
             else
                 is_charging = powerd:isCharging()
                 batt_lvl = main_batt_lvl
                 if symbol_type == "icons" or symbol_type == "compact_items" then
-                   prefix = powerd:getBatterySymbol(is_charging, main_batt_lvl)
+                   prefix = powerd:getBatterySymbol(powerd:isCharged(), is_charging, main_batt_lvl)
                 end
             end
         end
@@ -2448,8 +2448,8 @@ function ReaderFooter:onOutOfScreenSaver()
 end
 
 function ReaderFooter:onLeaveStandby()
-    self:onResume()
-    self:onOutOfScreenSaver()
+    self:maybeUpdateFooter()
+    self:rescheduleFooterAutoRefreshIfNeeded()
 end
 
 function ReaderFooter:onSuspend()

frontend/device/generic/powerd.lua
@@ -54,8 +54,12 @@ function BasePowerD:getAuxCapacityHW() return 0 end
 function BasePowerD:isAuxBatteryConnectedHW() return false end
 function BasePowerD:getDismissBatteryStatus() return self.battery_warning end
 function BasePowerD:setDismissBatteryStatus(status) self.battery_warning = status end
+--- @note: Should ideally return true as long as the device is plugged in, even once the battery is full...
 function BasePowerD:isChargingHW() return false end
+--- @note: ...at which point this should start returning true (i.e., plugged in & fully charged).
+function BasePowerD:isChargedHW() return false end
 function BasePowerD:isAuxChargingHW() return false end
+function BasePowerD:isAuxChargedHW() return false end
 function BasePowerD:frontlightIntensityHW() return 0 end
 function BasePowerD:isFrontlightOnHW() return self.fl_intensity > self.fl_min end
 function BasePowerD:turnOffFrontlightHW() self:setIntensityHW(self.fl_min) end
@@ -231,6 +235,10 @@ function BasePowerD:isCharging()
     return self:isChargingHW()
 end
 
+function BasePowerD:isCharged()
+    return self:isChargedHW()
+end
+
 function BasePowerD:getAuxCapacity()
     local now_btv
 
@@ -261,6 +269,10 @@ function BasePowerD:isAuxCharging()
     return self:isAuxChargingHW()
 end
 
+function BasePowerD:isAuxCharged()
+    return self:isAuxChargedHW()
+end
+
 function BasePowerD:isAuxBatteryConnected()
     return self:isAuxBatteryConnectedHW()
 end
@@ -274,8 +286,10 @@ function BasePowerD:stateChanged()
 end
 
 -- Silly helper to avoid code duplication ;).
-function BasePowerD:getBatterySymbol(is_charging, capacity)
-    if is_charging then
+function BasePowerD:getBatterySymbol(is_charged, is_charging, capacity)
+    if is_charged then
+        return ""
+    elseif is_charging then
         return ""
     else
         if capacity >= 100 then

frontend/device/kindle/device.lua
@@ -757,6 +757,7 @@ function KindleOasis2:init()
         fl_intensity_file = "/sys/class/backlight/max77796-bl/brightness",
         batt_capacity_file = "/sys/class/power_supply/max77796-battery/capacity",
         is_charging_file = "/sys/class/power_supply/max77796-charger/charging",
+        batt_status_file = "/sys/class/power_supply/max77796-charger/status",
     }
 
     self.input = require("device/input"):new{
@@ -832,6 +833,7 @@ function KindleOasis3:init()
         warmth_intensity_file = "/sys/class/backlight/lm3697-bl0/brightness",
         batt_capacity_file = "/sys/class/power_supply/max77796-battery/capacity",
         is_charging_file = "/sys/class/power_supply/max77796-charger/charging",
+        batt_status_file = "/sys/class/power_supply/max77796-charger/status",
     }
 
     self.input = require("device/input"):new{
@@ -845,13 +847,7 @@ function KindleOasis3:init()
     }
 
 
-    --- @fixme When starting KOReader with the device upside down ("D"), touch input is registered wrong
-    --        (i.e., probably upside down).
-    --        If it's started upright ("U"), everything's okay, and turning it upside down after that works just fine.
-    --        See #2206 & #2209 for the original KOA implementation, which obviously doesn't quite cut it here...
-    --        See also <https://www.mobileread.com/forums/showthread.php?t=298302&page=5>
-    -- NOTE: It'd take some effort to actually start KOReader while in a LANDSCAPE orientation,
-    --       since they're only exposed inside the stock reader, and not the Home/KUAL Booklets.
+    --- @fixme The same quirks as on the Oasis 2 apply ;).
     local haslipc, lipc = pcall(require, "liblipclua")
     if haslipc and lipc then
         local lipc_handle = lipc.init("com.github.koreader.screen")
@@ -906,6 +902,7 @@ function KindleBasic2:init()
         device = self,
         batt_capacity_file = "/sys/class/power_supply/bd7181x_bat/capacity",
         is_charging_file = "/sys/class/power_supply/bd7181x_bat/charging",
+        batt_status_file = "/sys/class/power_supply/bd7181x_bat/status",
     }
 
     Kindle.init(self)
@@ -921,6 +918,7 @@ function KindlePaperWhite4:init()
         fl_intensity_file = "/sys/class/backlight/bl/brightness",
         batt_capacity_file = "/sys/class/power_supply/bd71827_bat/capacity",
         is_charging_file = "/sys/class/power_supply/bd71827_bat/charging",
+        batt_status_file = "/sys/class/power_supply/bd71827_bat/status",
     }
 
     Kindle.init(self)
@@ -946,6 +944,7 @@ function KindleBasic3:init()
         fl_intensity_file = "/sys/class/backlight/bl/brightness",
         batt_capacity_file = "/sys/class/power_supply/bd71827_bat/capacity",
         is_charging_file = "/sys/class/power_supply/bd71827_bat/charging",
+        batt_status_file = "/sys/class/power_supply/bd71827_bat/status",
     }
 
     Kindle.init(self)
@@ -963,6 +962,7 @@ function KindlePaperWhite5:init()
         warmth_intensity_file = "/sys/class/backlight/fp9966-bl0/brightness",
         batt_capacity_file = "/sys/class/power_supply/bd71827_bat/capacity",
         is_charging_file = "/sys/class/power_supply/bd71827_bat/charging",
+        batt_status_file = "/sys/class/power_supply/bd71827_bat/status",
     }
 
     -- Enable the so-called "fast" mode, so as to prevent the driver from silently promoting refreshes to REAGL.

frontend/device/kindle/powerd.lua
@@ -148,6 +148,15 @@ function KindlePowerD:isChargingHW()
     return is_charging == 1
 end
 
+function KindlePowerD:isChargedHW()
+    -- Older kernels don't necessarily have this...
+    if self.batt_status_file then
+        return self:read_str_file(self.batt_status_file) == "Full"
+    end
+
+    return false
+end
+
 function KindlePowerD:_readFLIntensity()
     return self:read_int_file(self.fl_intensity_file)
 end

commit 837a41cac16afcd8dda155f775b404d3e5c3f267

PowketBook: Don't break synthetic power input events on suspend
Regression since #9036

frontend/device/input.lua
@@ -1292,8 +1292,12 @@ function Input:inhibitInput(toggle)
             self.handleTouchEv = self.voidEv
         end
         if not self._msc_ev_handler then
-            self._msc_ev_handler = self.handleMiscEv
-            self.handleMiscEv = self.voidEv
+            if not self.device:isPocketBook() then
+                -- NOTE: PocketBook is a special snowflake, synthetic Power events are sent as EV_MSC.
+                --       Thankfully, that's all that EV_MSC is used for on that platform.
+                self._msc_ev_handler = self.handleMiscEv
+                self.handleMiscEv = self.voidEv
+            end
         end
         if not self._sdl_ev_handler then
             self._sdl_ev_handler = self.handleSdlEv