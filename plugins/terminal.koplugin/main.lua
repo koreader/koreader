cdc1eb11cdd4b2b42657f7fe5a39cae5c81ac30b
zwim <martin.zwicknagl@kirchbichl.net>
2022-05-21 10:47:05 +0200

    Fix crash

--- plugins/terminal.koplugin/main.lua
+++ plugins/terminal.koplugin/main.lua
@@ -350,9 +350,7 @@ function Terminal:generateInputDialog()
             {
             text = "⎚", --clear
             callback = function()
-                self.history = ""
-                self.input = {}
-                self.input_dialog:setInputText("$ ")
+                self:transmit("clear\n") -- this could happen during a running program (e.g. top)
             end,
             },
             {
