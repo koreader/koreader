koreader/koreader

commit 9de0df81da522168bafc6fa4718eee69f40105b3

Cloudstorage WebDAV: fix leading slash for root folder (#9061)

frontend/apps/cloudstorage/webdavapi.lua
@@ -122,7 +122,7 @@ function WebDavApi:listFolder(address, user, pass, folder_path)
             local is_not_collection = item:find("<[^:]*:resourcetype/>") or
                 item:find("<[^:]*:resourcetype></[^:]*:resourcetype>")
 
-            local item_path = path .. "/" .. item_name
+            local item_path = path == "" and item_name or path .. "/" .. item_name
             if item:find("<[^:]*:collection/>") then
                 item_name = item_name .. "/"
                 if not is_current_dir then

commit e336983fd2e109a9119c1a42aeef20f588e44d46

Cloudstorage WebDAV: more fix slash in server address (#9128)
Honor server addresses with or without trailing slash.
Regression since #9061. Closes #9125.

frontend/apps/cloudstorage/webdavapi.lua
@@ -122,7 +122,7 @@ function WebDavApi:listFolder(address, user, pass, folder_path)
             local is_not_collection = item:find("<[^:]*:resourcetype/>") or
                 item:find("<[^:]*:resourcetype></[^:]*:resourcetype>")
 
-            local item_path = path == "" and item_name or path .. "/" .. item_name
+            local item_path = (path == "" and has_trailing_slash) and item_name or path .. "/" .. item_name
             if item:find("<[^:]*:collection/>") then
                 item_name = item_name .. "/"
                 if not is_current_dir then