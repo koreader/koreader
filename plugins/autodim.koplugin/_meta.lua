beff1b5d34f81cc7434a415628d3d8643664c6e0
zwim <36999612+zwim@users.noreply.github.com>
2022-06-01 16:22:42 +0200

    AutoDim: Allow to dim the frontlight on idle (#9028)

---
+++ plugins/autodim.koplugin/_meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "autodim",
+    fullname = _("Automatic dimmer"),
+    description = _("This plugin allows dimming the frontlight after a period of inactivity."),
+}
