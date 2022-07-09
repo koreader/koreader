koreader/koreader

commit 9b9cfe29a49423f8ee850cd8b3a6d05edba1a270

[feat] Replace TimeVal (RIP) with time, fixed point time seconds (#8999)

frontend/apps/reader/modules/readerpaging.lua
@@ -5,15 +5,14 @@ local Geom = require("ui/geometry")
 local InputContainer = require("ui/widget/container/inputcontainer")
 local Math = require("optmath")
 local ReaderZooming = require("apps/reader/modules/readerzooming")
-local TimeVal = require("ui/timeval")
 local UIManager = require("ui/uimanager")
 local bit = require("bit")
 local logger = require("logger")
 local util = require("util")
+local time = require("ui/time")
 local _ = require("gettext")
 local Input = Device.input
 local Screen = Device.screen
-
 
 local function copyPageState(page_state)
     return {
@@ -98,7 +97,7 @@ function ReaderPaging:init()
             {"0"}, doc = "go to end", event = "GotoPercent", args = 100,
         }
     end
-    self.pan_interval = TimeVal:new{ usec = 1000000 / self.pan_rate }
+    self.pan_interval = time.s(1 / self.pan_rate)
     self.number_of_pages = self.ui.document.info.number_of_pages
 end
 
@@ -321,9 +320,9 @@ function ReaderPaging:bookmarkFlipping(flipping_page, flipping_ges)
     UIManager:setDirty(self.view.dialog, "partial")
 end
 
-function ReaderPaging:onScrollSettingsUpdated(scroll_method, inertial_scroll_enabled, scroll_activation_delay)
+function ReaderPaging:onScrollSettingsUpdated(scroll_method, inertial_scroll_enabled, scroll_activation_delay_ms)
     self.scroll_method = scroll_method
-    self.scroll_activation_delay = TimeVal:new{ usec = scroll_activation_delay * 1000 }
+    self.scroll_activation_delay = time.ms(scroll_activation_delay_ms)
     if inertial_scroll_enabled then
         self.ui.scrolling:setInertialScrollCallbacks(
             function(distance) -- do_scroll_callback
@@ -408,7 +407,7 @@ function ReaderPaging:onPan(_, ges)
                 self._pan_has_scrolled = false
                 self._pan_prev_relative_y = 0
                 self._pan_to_scroll_later = 0
-                self._pan_real_last_time = TimeVal.zero
+                self._pan_real_last_time = 0
                 if ges.mousewheel_direction then
                     self._pan_activation_time = false
                 else

frontend/device/sdl/device.lua
@@ -168,7 +168,6 @@ function Device:init()
         event_map = require("device/sdl/event_map_sdl2"),
         handleSdlEv = function(device_input, ev)
             local Geom = require("ui/geometry")
-            local TimeVal = require("ui/timeval")
             local UIManager = require("ui/uimanager")
 
             -- SDL events can remain cdata but are almost completely transparent
@@ -191,8 +190,6 @@ function Device:init()
                     y = 0,
                     w = 0, h = 0,
                 }
-
-                setmetatable(ev.time, TimeVal)
 
                 local fake_ges = {
                     ges = "pan",

frontend/device/generic/device.lua
@@ -6,7 +6,6 @@ This module defines stubs for common methods.
 
 local DataStorage = require("datastorage")
 local Geom = require("ui/geometry")
-local TimeVal = require("ui/timeval")
 local logger = require("logger")
 local util = require("util")
 local _ = require("gettext")
@@ -71,10 +70,10 @@ local Device = {
     canSuspend = yes,
     canStandby = no,
     canPowerSaveWhileCharging = no,
-    total_standby_tv = TimeVal.zero, -- total time spent in standby
-    last_standby_tv = TimeVal.zero,
-    total_suspend_tv = TimeVal.zero, -- total time spent in suspend
-    last_suspend_tv = TimeVal.zero,
+    total_standby_time = 0, -- total time spent in standby
+    last_standby_time = 0,
+    total_suspend_time = 0, -- total time spent in suspend
+    last_suspend_time = 0,
     canReboot = no,
     canPowerOff = no,
     canAssociateFileExtensions = no,

frontend/device/kobo/device.lua
@@ -824,14 +824,14 @@ function Kobo:standby(max_duration)
         self.wakeup_mgr:addTask(max_duration, standby_alarm)
     end
 
-    local TimeVal = require("ui/timeval")
+    local time = require("ui/time")
     logger.info("Kobo standby: asking to enter standby . . .")
-    local standby_time_tv = TimeVal:boottime_or_realtime_coarse()
+    local standby_time = time.boottime_or_realtime_coarse()
 
     local ret = writeToSys("standby", "/sys/power/state")
 
-    self.last_standby_tv = TimeVal:boottime_or_realtime_coarse() - standby_time_tv
-    self.total_standby_tv = self.total_standby_tv + self.last_standby_tv
+    self.last_standby_time = time.boottime_or_realtime_coarse() - standby_time
+    self.total_standby_time = self.total_standby_time + self.last_standby_time
 
     if ret then
         logger.info("Kobo standby: zZz zZz zZz zZz... And woke up!")
@@ -925,16 +925,16 @@ function Kobo:suspend()
     end
     --]]
 
-    local TimeVal = require("ui/timeval")
+    local time = require("ui/time")
     logger.info("Kobo suspend: asking for a suspend to RAM . . .")
-    local suspend_time_tv = TimeVal:boottime_or_realtime_coarse()
+    local suspend_time = time.boottime_or_realtime_coarse()
 
     ret = writeToSys("mem", "/sys/power/state")
 
     -- NOTE: At this point, we *should* be in suspend to RAM, as such,
     --       execution should only resume on wakeup...
-    self.last_suspend_tv = TimeVal:boottime_or_realtime_coarse() - suspend_time_tv
-    self.total_suspend_tv = self.total_suspend_tv + self.last_suspend_tv
+    self.last_suspend_time = time.boottime_or_realtime_coarse() - suspend_time
+    self.total_suspend_time = self.total_suspend_time + self.last_suspend_time
 
     if ret then
         logger.info("Kobo suspend: ZzZ ZzZ ZzZ... And woke up!")

frontend/device/remarkable/device.lua
@@ -1,7 +1,7 @@
 local Generic = require("device/generic/device") -- <= look at this file!
-local TimeVal = require("ui/timeval")
 local PluginShare = require("pluginshare")
 local logger = require("logger")
+local time = require("ui/time")
 local ffi = require("ffi")
 local C = ffi.C
 require("ffi/linux_input_h")
@@ -95,7 +95,7 @@ function Remarkable2:adjustTouchEvent(ev, by)
     -- Inject CLOCK_MONOTONIC timestamps at the end of every input frame in order to have consistent gesture detection across input devices.
     -- c.f., #7536
     if ev.type == C.EV_SYN and ev.code == C.SYN_REPORT then
-       ev.time = TimeVal:now()
+       ev.time = time.now()
     end
 end
 

frontend/apps/reader/modules/readerdevicestatus.lua
@@ -16,7 +16,7 @@ memory_confirm_box = nil,
 
 function ReaderDeviceStatus:init()
     if Device:hasBattery() then
-        self.battery_interval = G_reader_settings:readSetting("device_status_battery_interval", 10)
+        self.battery_interval_m = G_reader_settings:readSetting("device_status_battery_interval_minutes", 10)
         self.battery_threshold = G_reader_settings:readSetting("device_status_battery_threshold", 20)
         self.battery_threshold_high = G_reader_settings:readSetting("device_status_battery_threshold_high", 100)
         self.checkLowBatteryLevel = function()
@@ -45,13 +45,13 @@ function ReaderDeviceStatus:init()
                     UIManager:show(self.battery_confirm_box)
                 end
             end
-            UIManager:scheduleIn(self.battery_interval * 60, self.checkLowBatteryLevel)
+            UIManager:scheduleIn(self.battery_interval_m * 60, self.checkLowBatteryLevel)
         end
         self:startBatteryChecker()
     end
 
     if not Device:isAndroid() then
-        self.memory_interval = G_reader_settings:readSetting("device_status_memory_interval", 5)
+        self.memory_interval_m = G_reader_settings:readSetting("device_status_memory_interval_minutes", 5)
         self.memory_threshold = G_reader_settings:readSetting("device_status_memory_threshold", 100)
         self.checkHighMemoryUsage = function()
             local statm = io.open("/proc/self/statm", "r")
@@ -103,7 +103,7 @@ function ReaderDeviceStatus:init()
                     end
                 end
             end
-            UIManager:scheduleIn(self.memory_interval * 60, self.checkHighMemoryUsage)
+            UIManager:scheduleIn(self.memory_interval_m * 60, self.checkHighMemoryUsage)
         end
         self:startMemoryChecker()
     end
@@ -136,7 +136,7 @@ function ReaderDeviceStatus:addToMainMenu(menu_items)
         table.insert(menu_items.device_status_alarm.sub_item_table,
             {
                 text_func = function()
-                    return T(_("Check interval: %1 min"), self.battery_interval)
+                    return T(_("Check interval: %1 min"), self.battery_interval_m)
                 end,
                 enabled_func = function()
                     return G_reader_settings:isTrue("device_status_battery_alarm")
@@ -144,18 +144,18 @@ function ReaderDeviceStatus:addToMainMenu(menu_items)
                 keep_menu_open = true,
                 callback = function(touchmenu_instance)
                     UIManager:show(SpinWidget:new{
-                        value = self.battery_interval,
+                        value = self.battery_interval_m,
                         value_min = 1,
                         value_max = 60,
                         default_value = 10,
                         value_hold_step = 5,
                         title_text = _("Battery check interval"),
                         callback = function(spin)
-                            self.battery_interval = spin.value
-                            G_reader_settings:saveSetting("device_status_battery_interval", self.battery_interval)
+                            self.battery_interval_m = spin.value
+                            G_reader_settings:saveSetting("device_status_battery_interval_minutes", self.battery_interval_m)
                             touchmenu_instance:updateItems()
                             powerd:setDismissBatteryStatus(false)
-                            UIManager:scheduleIn(self.battery_interval * 60, self.checkLowBatteryLevel)
+                            UIManager:scheduleIn(self.battery_interval_m * 60, self.checkLowBatteryLevel)
                         end,
                     })
                 end,
@@ -228,7 +228,7 @@ High level threshold is checked when the device is charging.]]),
         table.insert(menu_items.device_status_alarm.sub_item_table,
             {
                 text_func = function()
-                    return T(_("Check interval: %1 min"), self.memory_interval)
+                    return T(_("Check interval: %1 min"), self.memory_interval_m)
                 end,
                 enabled_func = function()
                     return G_reader_settings:isTrue("device_status_memory_alarm")
@@ -236,17 +236,17 @@ High level threshold is checked when the device is charging.]]),
                 keep_menu_open = true,
                 callback = function(touchmenu_instance)
                     UIManager:show(SpinWidget:new{
-                        value = self.memory_interval,
+                        value = self.memory_interval_m,
                         value_min = 1,
                         value_max = 60,
                         default_value = 5,
                         value_hold_step = 5,
                         title_text = _("Memory check interval"),
                         callback = function(spin)
-                            self.memory_interval = spin.value
-                            G_reader_settings:saveSetting("device_status_memory_interval", self.memory_interval)
+                            self.memory_interval_m = spin.value
+                            G_reader_settings:saveSetting("device_status_memory_interval_minutes", self.memory_interval_m)
                             touchmenu_instance:updateItems()
-                            UIManager:scheduleIn(self.memory_interval * 60, self.checkHighMemoryUsage)
+                            UIManager:scheduleIn(self.memory_interval_m * 60, self.checkHighMemoryUsage)
                         end,
                     })
                 end,

frontend/apps/reader/modules/readerdictionary.lua
@@ -13,11 +13,11 @@ local LuaData = require("luadata")
 local MultiConfirmBox = require("ui/widget/multiconfirmbox")
 local NetworkMgr = require("ui/network/manager")
 local SortWidget = require("ui/widget/sortwidget")
-local TimeVal = require("ui/timeval")
 local Trapper = require("ui/trapper")
 local UIManager = require("ui/uimanager")
 local ffiUtil  = require("ffi/util")
 local logger = require("logger")
+local time = require("ui/time")
 local util  = require("util")
 local _ = require("gettext")
 local T = ffiUtil.template
@@ -111,7 +111,7 @@ function ReaderDictionary:init()
     -- Allow quick interruption or dismiss of search result window
     -- with tap if done before this delay. After this delay, the
     -- result window is shown and dismiss prevented for a few 100ms
-    self.quick_dismiss_before_delay = TimeVal:new{ sec = 3, usec = 0 }
+    self.quick_dismiss_before_delay = time.s(3)
 
     -- Gather info about available dictionaries
     if not available_ifos then
@@ -931,9 +931,10 @@ function ReaderDictionary:stardictLookup(word, dict_names, fuzzy_search, boxes, 
 
     self:showLookupInfo(word, self.lookup_msg_delay)
 
-    self._lookup_start_tv = UIManager:getTime()
+    self._lookup_start_time = UIManager:getTime()
     local results = self:startSdcv(word, dict_names, fuzzy_search)
-    if results and results.lookup_cancelled and TimeVal:now() - self._lookup_start_tv <= self.quick_dismiss_before_delay then
+    if results and results.lookup_cancelled
+        and (time.now() - self._lookup_start_time) <= self.quick_dismiss_before_delay then
         -- If interrupted quickly just after launch, don't display anything
         -- (this might help avoiding refreshes and the need to dismiss
         -- after accidental long-press when holding a device).
@@ -991,7 +992,8 @@ function ReaderDictionary:showDict(word, results, boxes, link)
     self:dismissLookupInfo()
     if results and results[1] then
         UIManager:show(self.dict_window)
-        if not results.lookup_cancelled and self._lookup_start_tv and TimeVal:now() - self._lookup_start_tv > self.quick_dismiss_before_delay then
+        if not results.lookup_cancelled and self._lookup_start_time
+            and (time.now() - self._lookup_start_time) > self.quick_dismiss_before_delay then
             -- If the search took more than a few seconds to be done, discard
             -- queued and coming up events to avoid a voluntary dismissal
             -- (because the user felt the result would not come) to kill the

frontend/apps/reader/modules/readerhighlight.lua
@@ -7,7 +7,6 @@ local InfoMessage = require("ui/widget/infomessage")
 local InputContainer = require("ui/widget/container/inputcontainer")
 local Notification = require("ui/widget/notification")
 local TextViewer = require("ui/widget/textviewer")
-local TimeVal = require("ui/timeval")
 local Translator = require("ui/translator")
 local UIManager = require("ui/uimanager")
 local dbg = require("dbg")
@@ -15,6 +14,7 @@ local logger = require("logger")
 local util = require("util")
 local Size = require("ui/size")
 local ffiUtil = require("ffi/util")
+local time = require("ui/time")
 local _ = require("gettext")
 local C_ = _.pgettext
 local T = require("ffi/util").template
@@ -51,7 +51,7 @@ function ReaderHighlight:init()
     self._start_indicator_highlight = false
     self._current_indicator_pos = nil
     self._previous_indicator_pos = nil
-    self._last_indicator_move_args = {dx = 0, dy = 0, distance = 0, time = TimeVal:now()}
+    self._last_indicator_move_args = {dx = 0, dy = 0, distance = 0, time = time:now()}
 
     if Device:hasDPad() then
         -- Used for text selection with dpad/keys
@@ -897,9 +897,9 @@ dbg:guard(ReaderHighlight, "onShowHighlightMenu",
 
 function ReaderHighlight:_resetHoldTimer(clear)
     if clear then
-        self.hold_last_tv = nil
+        self.hold_last_time = nil
     else
-        self.hold_last_tv = UIManager:getTime()
+        self.hold_last_time = UIManager:getTime()
     end
 end
 
@@ -1423,14 +1423,14 @@ function ReaderHighlight:onHoldRelease()
     end
 
     local long_final_hold = false
-    if self.hold_last_tv then
-        local hold_duration = TimeVal:now() - self.hold_last_tv
-        local long_hold_threshold = G_reader_settings:readSetting("highlight_long_hold_threshold", 3)
-        if hold_duration > TimeVal:new{ sec = long_hold_threshold, usec = 0 } then
+    if self.hold_last_time then
+        local hold_duration = time.now() - self.hold_last_time
+        local long_hold_threshold_s = G_reader_settings:readSetting("highlight_long_hold_threshold", 3) -- seconds
+        if hold_duration > time.s(long_hold_threshold_s) then
             -- We stayed 3 seconds before release without updating selection
             long_final_hold = true
         end
-        self.hold_last_tv = nil
+        self.hold_last_time = nil
     end
     if self.is_word_selection then -- single-word selection
         if long_final_hold or G_reader_settings:isTrue("highlight_action_on_single_word") then
@@ -1947,14 +1947,14 @@ function ReaderHighlight:onMoveHighlightIndicator(args)
             rect.x = rect.x + quick_move_distance_dx * dx
             rect.y = rect.y + quick_move_distance_dy * dy
         else
-            local now = TimeVal:now()
+            local now = time:now()
             if dx == self._last_indicator_move_args.dx and dy == self._last_indicator_move_args.dy then
                 local diff = now - self._last_indicator_move_args.time
                 -- if press same arrow key in 1 second, speed up
                 -- double press: 4 single move distances, usually move to next word or line
                 -- triple press: 16 single distances, usually skip several words or lines
                 -- quadruple press: 54 single distances, almost move to screen edge
-                if diff < TimeVal:new{ sec = 1, usec = 0 } then
+                if diff < time.s(1) then
                     move_distance = self._last_indicator_move_args.distance * 4
                 end
             end
@@ -1998,7 +1998,7 @@ function ReaderHighlight:_createHighlightGesture(gesture)
     return {
         ges = gesture,
         pos = point,
-        time = TimeVal:realtime(),
+        time = time.realtime(),
     }
 end
 

commit 22fe3933c34481169fec04796363142773b576c8

[fix, SDL] Time: fix crash when scrolling in continuous mode (#9314)
Introduced with #8999.

Fixes #9311.

frontend/apps/reader/modules/readerpaging.lua
@@ -349,7 +349,7 @@ function ReaderPaging:onSwipe(_, ges)
         -- We did some panning but released after a short amount of time,
         -- so this gesture ended up being a Swipe - and this swipe was
         -- not handled by the other modules (so, not opening the menus).
-        -- Do as :onPanRelese() and ignore this swipe.
+        -- Do as :onPanRelease() and ignore this swipe.
         self:onPanRelease() -- no arg, so we know there we come from here
         return true
     else

frontend/device/sdl/device.lua
@@ -3,6 +3,7 @@ local Generic = require("device/generic/device")
 local SDL = require("ffi/SDL2_0")
 local ffi = require("ffi")
 local logger = require("logger")
+local time = require("ui/time")
 
 -- SDL computes WM_CLASS on X11/Wayland based on process's binary name.
 -- Some desktop environments rely on WM_CLASS to name the app and/or to assign the proper icon.
@@ -212,7 +213,7 @@ function Device:init()
                         y = 100*scrolled_y,
                     },
                     pos = pos,
-                    time = ev.time,
+                    time = time.timeval(ev.time),
                     mousewheel_direction = scrolled_y,
                 }
                 local fake_ges_release = {
@@ -220,7 +221,7 @@ function Device:init()
                     distance = fake_ges.distance,
                     relative = fake_ges.relative,
                     pos = pos,
-                    time = ev.time,
+                    time = time.timeval(ev.time),
                     from_mousewheel = true,
                 }
                 local fake_pan_ev = Event:new("Pan", nil, fake_ges)