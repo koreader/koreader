b7df2ee136e06a6a4676a06af5855b845108dd3e
hasezoey <hasezoey@gmail.com>
2022-09-10 19:38:17 +0200

    refactor(tolino/event_map): refactor to use android eventmap and only modify changed values

--- frontend/device/tolino/event_map.lua
+++ frontend/device/tolino/event_map.lua
@@ -1,35 +1,5 @@
-return {
-    -- the following has mostly been copy-pasted from "device/android/event_map.lua"
-    [29] = "A", [30] = "B", [31] = "C", [32] = "D", [33] = "E", [34] = "F",
-    [35] = "G", [36] = "H", [37] = "I", [38] = "J", [39] = "K", [40] = "L",
-    [41] = "M", [42] = "N", [43] = "O", [44] = "P", [45] = "Q", [46] = "R",
-    [47] = "S", [48] = "T", [49] = "U", [50] = "V", [51] = "W", [52] = "X",
-    [53] = "Y", [54] = "Z", [ 7] = "0", [ 8] = "1", [ 9] = "2", [10] = "3",
-    [11] = "4", [12] = "5", [13] = "6", [14] = "7", [15] = "8", [16] = "9",
+local android_eventmap = require("device/android/event_map")
+android_eventmap[21] = "LPgBack" -- changed for Tolino Buttons (up key)
+android_eventmap[22] = "LPgFwd" -- changed for Tolino Buttons (down key)
 
-    [4] = "Back",   -- BACK
-    [19] = "Up",    -- DPAD_UP
-    [20] = "Down",  -- DPAD_UP
-    [21] = "LPgBack",  -- DPAD_LEFT -- changed for Tolino Buttons (up key)
-    [22] = "LPgFwd", -- DPAD_RIGHT -- changed for Tolino Buttons (down key)
-    [23] = "Press", -- DPAD_CENTER
-    [24] = "LPgBack", -- VOLUME_UP
-    [25] = "LPgFwd",  -- VOLUME_DOWN
-    [27] = "Camera",  -- CAMERA
-    [56] = ".",     -- PERIOD
-    [59] = "Shift", -- SHIFT_LEFT
-    [60] = "Shift", -- SHIFT_RIGHT
-    [62] = " ",     -- SPACE
-    [63] = "Sym",   -- SYM
-    [66] = "Press", -- ENTER
-    [67] = "Del",   -- DEL
-    [76] = "/",     -- SLASH
-    [82] = "Menu",  -- MENU
-    [84] = "Search",--SEARCH
-    [92] = "LPgBack", -- PAGE_UP
-    [93] = "LPgFwd",  -- PAGE_DOWN
-
-    [104] = "LPgBack", -- T68 PageUp
-    [109] = "LPgFwd",  -- T68 PageDown
-    [139] = "Menu",    -- T68 Menu
-}
+return android_eventmap
