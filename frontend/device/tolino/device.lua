3c5d42ded7e1ceba3b7468cf6e076e8bf4f22d97
hasezoey <hasezoey@gmail.com>
2022-09-10 17:21:36 +0200

    style(tolino/device.lua): remove commented-out code

--- frontend/device/tolino/device.lua
+++ frontend/device/tolino/device.lua
@@ -1,20 +1,6 @@
--- local Generic = require("device/generic/device")
--- local Geom = require("ui/geometry")
--- local WakeupMgr = require("device/wakeupmgr")
--- local ffiUtil = require("ffi/util")
--- local lfs = require("libs/libkoreader-lfs")
 local logger = require("logger")
--- local util = require("util")
--- local _ = require("gettext")
 local A, android = pcall(require, "android")  -- luacheck: ignore
 local AndroidDevice = require("device/android/device")
-
--- We're going to need a few <linux/fb.h> & <linux/input.h> constants...
--- local ffi = require("ffi")
--- local C = ffi.C
--- require("ffi/linux_fb_h")
--- require("ffi/linux_input_h")
--- require("ffi/posix_h")
 
 local function yes() return true end
 local function no() return false end
