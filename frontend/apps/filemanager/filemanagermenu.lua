45e4f38c41bdbc476c1a3db38c4b0b7a202c902b
hius07 <62179190+hius07@users.noreply.github.com>
2022-09-20 12:54:57 +0300

    CloudStorage: remove cloudmgr (#9532)
    Use downloadmgr instead.

--- frontend/apps/filemanager/filemanagermenu.lua
+++ frontend/apps/filemanager/filemanagermenu.lua
@@ -1,6 +1,5 @@
 local BD = require("ui/bidi")
 local CenterContainer = require("ui/widget/container/centercontainer")
-local CloudStorage = require("apps/cloudstorage/cloudstorage")
 local ConfirmBox = require("ui/widget/confirmbox")
 local Device = require("device")
 local Event = require("ui/event")
@@ -713,7 +712,7 @@ To:
     self.menu_items.cloud_storage = {
         text = _("Cloud storage"),
         callback = function()
-            local cloud_storage = CloudStorage:new{}
+            local cloud_storage = require("apps/cloudstorage/cloudstorage"):new{}
             UIManager:show(cloud_storage)
             local filemanagerRefresh = function() self.ui:onRefresh() end
             function cloud_storage:onClose()
