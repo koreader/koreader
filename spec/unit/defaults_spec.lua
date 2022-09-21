684f5ffe4b88c021af969d6b65b96c0055487998
NiLuJe <ninuje@gmail.com>
2022-09-21 02:45:45 +0200

    Let's hope we'll luck out with the defaults tests...

--- spec/unit/defaults_spec.lua
+++ spec/unit/defaults_spec.lua
@@ -8,7 +8,7 @@ describe("defaults module", function()
 
     it("should load all defaults from defaults.lua", function()
         Defaults:init()
-        assert.is_same(99, #Defaults.defaults_name)
+        assert.is_same(98, #Defaults.defaults_name)
     end)
 
     it("should save changes to defaults.persistent.lua", function()
@@ -16,18 +16,18 @@ describe("defaults module", function()
         os.remove(persistent_filename)
 
         -- To see indices and help updating this when new settings are added:
-        -- for i=1, 99 do print(i.." ".. Defaults.defaults_name[i]) end
+        -- for i=1, 98 do print(i.." ".. Defaults.defaults_name[i]) end
 
         -- not in persistent but checked in defaults
-        Defaults.changed[18] = true
-        Defaults.changed[48] = true
-        Defaults.changed[54] = true
-        Defaults.changed[83] = true
+        Defaults.changed[17] = true
+        Defaults.changed[47] = true
+        Defaults.changed[53] = true
+        Defaults.changed[82] = true
         Defaults:saveSettings()
-        assert.is_same(99, #Defaults.defaults_name)
-        assert.is_same("DTAP_ZONE_BACKWARD", Defaults.defaults_name[84])
-        assert.is_same("DCREREADER_CONFIG_WORD_SPACING_LARGE", Defaults.defaults_name[48])
-        assert.is_same("DCREREADER_CONFIG_H_MARGIN_SIZES_XXX_LARGE", Defaults.defaults_name[18])
+        assert.is_same(98, #Defaults.defaults_name)
+        assert.is_same("DTAP_ZONE_BACKWARD", Defaults.defaults_name[83])
+        assert.is_same("DCREREADER_CONFIG_WORD_SPACING_LARGE", Defaults.defaults_name[47])
+        assert.is_same("DCREREADER_CONFIG_H_MARGIN_SIZES_XXX_LARGE", Defaults.defaults_name[17])
         dofile(persistent_filename)
         assert.is_same(DCREREADER_CONFIG_WORD_SPACING_LARGE, { [1] = 100, [2] = 90 })
         assert.is_same(DTAP_ZONE_BACKWARD, { ["y"] = 0, ["x"] = 0, ["h"] = 1, ["w"] = 0.25 })
@@ -36,15 +36,15 @@ describe("defaults module", function()
 
         -- in persistent
         Defaults:init()
-        Defaults.changed[54] = true
-        Defaults.defaults_value[54] = {
+        Defaults.changed[53] = true
+        Defaults.defaults_value[53] = {
             y = 0,
             x = 0,
             h = 0.25,
             w = 0.75
         }
-        Defaults.changed[84] = true
-        Defaults.defaults_value[84] = {
+        Defaults.changed[83] = true
+        Defaults.defaults_value[83] = {
             y = 10,
             x = 10.125,
             h = 20.25,
