2420557085ecd34a330b786437c296121bcfc4a0
NiLuJe <ninuje@gmail.com>
2022-09-28 01:19:00 +0200

    DocCache: Unbreak on !Linux platforms (#9566)
    Regression since #9529
    Fix #9565

--- frontend/document/doccache.lua
+++ frontend/document/doccache.lua
@@ -15,7 +15,8 @@ local DHINTCOUNT = G_defaults:readSetting("DHINTCOUNT")
 local function calcCacheMemSize()
     local min = G_defaults:readSetting("DGLOBAL_CACHE_SIZE_MINIMUM")
     local max = G_defaults:readSetting("DGLOBAL_CACHE_SIZE_MAXIMUM")
-    local calc = util.calcFreeMem() * (G_defaults:readSetting("DGLOBAL_CACHE_FREE_PROPORTION") or 0)
+    local memfree, _ = util.calcFreeMem() or 0, 0
+    local calc = memfree * G_defaults:readSetting("DGLOBAL_CACHE_FREE_PROPORTION")
     return math.min(max, math.max(min, calc))
 end
 local doccache_size = calcCacheMemSize()
