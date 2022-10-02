c558ff6d137192a9006f045e223ab0d94b1bb773
NiLuJe <ninuje@gmail.com>
2022-10-02 23:13:19 +0200

    Do a micro review pass over the toplevel stuff in frontend

--- frontend/pluginshare.lua
+++ frontend/pluginshare.lua
@@ -1,5 +1,6 @@
--- PluginShare is a table for plugins to exchange data between each other. Plugins should maintain
--- their own protocols.
+-- PluginShare is a table for plugins to exchange data between each other.
+-- It is a singleton, which will persist across views (and, as such, across plugins lifecycle).
+-- Plugins should maintain their own protocols.
 return {
     backgroundJobs = {},
 }
