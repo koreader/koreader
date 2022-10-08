a769f7446037b5668b555ef854fb96acf0da2034
spike <spike@w530>
2022-10-08 16:30:42 +0200

    add md5 checksum of the file while exporting highlights
    - Add a menu with an export option to add md5sum of selected books in export
      The option is disabled by default in the Json export menu.
    
    - This allows interoperability between different readers who want to
      import highlights on different devices/file systems. All that is
      needed then to import highlights is to match the local books and their
      path based on the checksum.
    
    - I needed this to import highlights on sioyek book reader from
      koreader, I use koreader on tablets and sioyek on my laptop.

--- plugins/exporter.koplugin/target/json.lua
+++ plugins/exporter.koplugin/target/json.lua
@@ -1,13 +1,47 @@
 local Device = require("device")
 local rapidjson = require("rapidjson")
+local _ = require("gettext")
+local md5 = require("ffi/MD5")
 
 -- json exporter
 local JsonExporter = require("base"):new {
     name = "json",
     shareable = Device:canShareText(),
+    init_callback = function(self, settings)
+        local changed = false
+        if settings.bookChecksum == nil then
+            settings.bookChecksum = false
+        end
+        changed = true
+        return changed, settings
+    end
 }
 
-local function format(booknotes)
+function JsonExporter:getMenuTable()
+    local menu = {
+        text = _("Json"),
+        checked_func = function() return self:isEnabled() end,
+        sub_item_table = {
+            {
+                text = _("Export to Json"),
+                checked_func = function() return self:isEnabled() end,
+                callback = function() self:toggleEnabled() end,
+            },
+            {
+                text = _("Show book checksum in export"),
+                checked_func = function() return self.settings.bookChecksum end,
+                callback = function()
+                    self.settings.bookChecksum = not self.settings.bookChecksum 
+                    self:saveSettings()
+                end,
+            }
+        }
+    }
+
+    return menu
+end
+
+local function format(booknotes, settings)
     local t = {
         title = booknotes.title,
         author = booknotes.author,
@@ -16,6 +50,10 @@ local function format(booknotes)
         file = booknotes.file,
         number_of_pages = booknotes.number_of_pages
     }
+    if settings.bookChecksum then
+        t.file_md5sum = md5.sumFile(booknotes.file)
+
+    end
     for _, entry in ipairs(booknotes) do
         table.insert(t.entries, entry[1])
     end
@@ -27,13 +65,13 @@ function JsonExporter:export(t)
     local timestamp = self.timestamp or os.time()
     local path = self:getFilePath(t)
     if #t == 1 then
-        exportable = format(t[1])
+        exportable = format(t[1], self.settings)
         exportable.created_on = timestamp
         exportable.version = self:getVersion()
     else
         local documents = {}
         for _, booknotes in ipairs(t) do
-            table.insert(documents, format(booknotes))
+            table.insert(documents, format(booknotes, self.settings))
         end
         exportable = {
             created_on = timestamp,
