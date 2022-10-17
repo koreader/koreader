eae3ae985d85ecbfbe4686919f235fc88709bbbb
hius07 <62179190+hius07@users.noreply.github.com>
2022-10-16 20:49:51 -0400

    Update reader_menu_order.lua

--- frontend/ui/elements/reader_menu_order.lua
+++ frontend/ui/elements/reader_menu_order.lua
@@ -41,7 +41,7 @@ local order = {
         "bookmarks_settings",
     },
     typeset = {
-        "manage_document_settings",
+        "document_settings",
         "----------------------------",
         "set_render_style",
         "style_tweaks",
