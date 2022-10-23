koreader/koreader

commit 4969811c08312dfedd65462ce001cfb0b93d6ccd

Optimization: Use constant folding for divisions not a power of two (#9609)

frontend/device/kobo/powerd.lua
@@ -42,7 +42,7 @@ function KoboPowerD:_syncKoboLightOnStart()
                     -- ColorSetting is stored as a color temperature scale in Kelvin,
                     -- from 1500 to 6400
                     -- so normalize this to [0, 100] on our end.
-                    new_warmth = (100 - Math.round((new_color - 1500) / 49))
+                    new_warmth = (100 - Math.round((new_color - 1500) * (1/49)))
                 end
             end
             if is_frontlight_on == nil then
@@ -303,7 +303,7 @@ function KoboPowerD:turnOffFrontlightHW()
     end
     ffiUtil.runInSubProcess(function()
         for i = 1,5 do
-            self:setIntensityHW(math.floor(self.fl_intensity - ((self.fl_intensity / 5) * i)))
+            self:setIntensityHW(math.floor(self.fl_intensity - ((self.fl_intensity * (1/5)) * i)))
             --- @note: Newer devices appear to block slightly longer on FL ioctls/sysfs, so only sleep on older devices,
             ---        otherwise we get a jump and not a ramp ;).
             if not self.device:hasNaturalLight() then
@@ -336,7 +336,7 @@ function KoboPowerD:turnOnFrontlightHW()
     end
     ffiUtil.runInSubProcess(function()
         for i = 1,5 do
-            self:setIntensityHW(math.ceil(self.fl_min + ((self.fl_intensity / 5) * i)))
+            self:setIntensityHW(math.ceil(self.fl_min + ((self.fl_intensity * (1/5)) * i)))
             --- @note: Newer devices appear to block slightly longer on FL ioctls/sysfs, so only sleep on older devices,
             ---        otherwise we get a jump and not a ramp ;).
             if not self.device:hasNaturalLight() then

frontend/apps/filemanager/filemanagermenu.lua
@@ -889,10 +889,10 @@ function FileManagerMenu:_getTabIndexFromLocation(ges)
     if not ges then
         return last_tab_index
     -- if the start position is far right
-    elseif ges.pos.x > 2 * Screen:getWidth() / 3 then
+    elseif ges.pos.x > Screen:getWidth() * (2/3) then
         return BD.mirroredUILayout() and 1 or #self.tab_item_table
     -- if the start position is far left
-    elseif ges.pos.x < Screen:getWidth() / 3 then
+    elseif ges.pos.x < Screen:getWidth() * (1/3) then
         return BD.mirroredUILayout() and #self.tab_item_table or 1
     -- if center return the last index
     else

frontend/apps/reader/modules/readerdevicestatus.lua
@@ -64,7 +64,7 @@ function ReaderDeviceStatus:init()
             if statm then
                 local dummy, rss = statm:read("*number", "*number")
                 statm:close()
-                rss = math.floor(rss * 4096 / 1024 / 1024)
+                rss = math.floor(rss * (4096 / 1024 / 1024))
                 if rss >= self.memory_threshold then
                     if self.memory_confirm_box then
                         UIManager:close(self.memory_confirm_box)

frontend/apps/reader/modules/readerdogear.lua
@@ -17,8 +17,8 @@ function ReaderDogear:init()
     -- to not overwrite the book text.
     -- For other documents, there is no easy way to know if valuable content
     -- may be hidden by the icon (kopt's page_margin is quite obscure).
-    self.dogear_min_size = math.ceil(math.min(Screen:getWidth(), Screen:getHeight()) / 40)
-    self.dogear_max_size = math.ceil(math.min(Screen:getWidth(), Screen:getHeight()) / 32)
+    self.dogear_min_size = math.ceil(math.min(Screen:getWidth(), Screen:getHeight()) * (1/40))
+    self.dogear_max_size = math.ceil(math.min(Screen:getWidth(), Screen:getHeight()) * (1/32))
     self.dogear_size = nil
     self.dogear_y_offset = 0
     self.top_pad = nil

frontend/apps/reader/modules/readerfooter.lua
@@ -352,7 +352,7 @@ local footerTextGeneratorMap = {
             local dummy, rss = statm:read("*number", "*number")
             statm:close()
             -- we got the nb of 4Kb-pages used, that we convert to MiB
-            rss = math.floor(rss * 4096 / 1024 / 1024)
+            rss = math.floor(rss * (4096 / 1024 / 1024))
             return (prefix .. " %d"):format(rss)
         end
         return ""
@@ -390,7 +390,7 @@ local footerTextGeneratorMap = {
             local title = doc_info.title:gsub(" ", "\xC2\xA0") -- replace space with no-break-space
             local title_widget = TextWidget:new{
                 text = title,
-                max_width = footer._saved_screen_width * footer.settings.book_title_max_width_pct / 100,
+                max_width = footer._saved_screen_width * footer.settings.book_title_max_width_pct * (1/100),
                 face = Font:getFace(footer.text_font_face, footer.settings.text_font_size),
                 bold = footer.settings.text_font_bold,
             }
@@ -410,7 +410,7 @@ local footerTextGeneratorMap = {
             chapter_title = chapter_title:gsub(" ", "\xC2\xA0") -- replace space with no-break-space
             local chapter_widget = TextWidget:new{
                 text = chapter_title,
-                max_width = footer._saved_screen_width * footer.settings.book_chapter_max_width_pct / 100,
+                max_width = footer._saved_screen_width * footer.settings.book_chapter_max_width_pct * (1/100),
                 face = Font:getFace(footer.text_font_face, footer.settings.text_font_size),
                 bold = footer.settings.text_font_bold,
             }
@@ -2168,7 +2168,7 @@ function ReaderFooter:_updateFooterText(force_repaint, force_recompute)
             self.footer_text.height = 0
         else
             -- Alongside a progress bar, it's the bar's width plus whatever's left.
-            local text_max_available_ratio = (100 - self.settings.progress_bar_min_width_pct) / 100
+            local text_max_available_ratio = (100 - self.settings.progress_bar_min_width_pct) * (1/100)
             self.footer_text:setMaxWidth(math.floor(text_max_available_ratio * self._saved_screen_width - 2 * self.settings.progress_margin_width - self.horizontal_margin))
             -- Add some spacing between the text and the bar
             self.text_width = self.footer_text:getSize().w + self.horizontal_margin

frontend/apps/reader/modules/readerhighlight.lua
@@ -2022,8 +2022,8 @@ end
 function ReaderHighlight:onMoveHighlightIndicator(args)
     if self.view.visible_area and self._current_indicator_pos then
         local dx, dy, quick_move = unpack(args)
-        local quick_move_distance_dx = self.view.visible_area.w / 5 -- quick move distance: fifth of visible_area
-        local quick_move_distance_dy = self.view.visible_area.h / 5
+        local quick_move_distance_dx = self.view.visible_area.w * (1/5) -- quick move distance: fifth of visible_area
+        local quick_move_distance_dy = self.view.visible_area.h * (1/5)
         -- single move distance, small and capable to move on word with small font size and narrow line height
         local move_distance = Size.item.height_default / 4
         local rect = self._current_indicator_pos:copy()

frontend/apps/reader/modules/readermenu.lua
@@ -433,10 +433,10 @@ function ReaderMenu:_getTabIndexFromLocation(ges)
     if not ges then
         return self.last_tab_index
     -- if the start position is far right
-    elseif ges.pos.x > 2 * Screen:getWidth() / 3 then
+    elseif ges.pos.x > Screen:getWidth() * (2/3) then
         return BD.mirroredUILayout() and 1 or #self.tab_item_table
     -- if the start position is far left
-    elseif ges.pos.x < Screen:getWidth() / 3 then
+    elseif ges.pos.x < Screen:getWidth() * (1/3) then
         return BD.mirroredUILayout() and #self.tab_item_table or 1
     -- if center return the last index
     else

frontend/apps/reader/modules/readerpaging.lua
@@ -542,7 +542,7 @@ end
 
 function ReaderPaging:onGotoPercent(percent)
     logger.dbg("goto document offset in percent:", percent)
-    local dest = math.floor(self.number_of_pages * percent / 100)
+    local dest = math.floor(self.number_of_pages * percent * (1/100))
     if dest < 1 then dest = 1 end
     if dest > self.number_of_pages then
         dest = self.number_of_pages
@@ -908,8 +908,8 @@ function ReaderPaging:onGotoPageRel(diff)
     local x_pan_off, y_pan_off = 0, 0
     local right_to_left = self.ui.document.configurable.writing_direction and self.ui.document.configurable.writing_direction > 0
     local bottom_to_top = self.ui.zooming.zoom_bottom_to_top
-    local h_progress = 1 - self.ui.zooming.zoom_overlap_h / 100
-    local v_progress = 1 - self.ui.zooming.zoom_overlap_v / 100
+    local h_progress = 1 - self.ui.zooming.zoom_overlap_h * (1/100)
+    local v_progress = 1 - self.ui.zooming.zoom_overlap_v * (1/100)
     local old_va = self.visible_area
     local old_page = self.current_page
     local x, y, w, h = "x", "y", "w", "h"

commit 30a3b42e34d1695da9fffcfb50967f69e4322298

Kobo: Unbreak frontlight toggle for some specific values
Because of floating point computery math stuff.

Regression since #9609
c.f., https://github.com/koreader/koreader/pull/9609#issuecomment-1288187080

frontend/device/kobo/powerd.lua
@@ -302,8 +302,12 @@ function KoboPowerD:turnOffFrontlightHW()
         return
     end
     ffiUtil.runInSubProcess(function()
-        for i = 1,5 do
-            self:setIntensityHW(math.floor(self.fl_intensity - ((self.fl_intensity * (1/5)) * i)))
+        for i = 1, 5 do
+            -- NOTE: Do *not* switch to (self.fl_intensity * (1/5) * i) here, it may lead to rounding errors,
+            --       which is problematic paired w/ math.floor because it doesn't round towards zero,
+            --       which means we may end up passing -1 to setIntensityHW, which will fail,
+            --       because we're bypassing the clamping usually done by setIntensity...
+            self:setIntensityHW(math.floor(self.fl_intensity - (self.fl_intensity / 5 * i)))
             --- @note: Newer devices appear to block slightly longer on FL ioctls/sysfs, so only sleep on older devices,
             ---        otherwise we get a jump and not a ramp ;).
             if not self.device:hasNaturalLight() then
@@ -335,8 +339,8 @@ function KoboPowerD:turnOnFrontlightHW()
         return
     end
     ffiUtil.runInSubProcess(function()
-        for i = 1,5 do
-            self:setIntensityHW(math.ceil(self.fl_min + ((self.fl_intensity * (1/5)) * i)))
+        for i = 1, 5 do
+            self:setIntensityHW(math.ceil(self.fl_min + (self.fl_intensity / 5 * i)))
             --- @note: Newer devices appear to block slightly longer on FL ioctls/sysfs, so only sleep on older devices,
             ---        otherwise we get a jump and not a ramp ;).
             if not self.device:hasNaturalLight() then