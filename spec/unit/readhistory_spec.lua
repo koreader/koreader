4d4b04359cb8dc9d17886e2b39d435aa8582d252
hius07 <62179190+hius07@users.noreply.github.com>
2022-10-25 06:15:59 -0400

    ReadHistory: refactoring with binary search (#9603)
    Get rid of indexing and sorting, reduce flushing.

--- spec/unit/readhistory_spec.lua
+++ spec/unit/readhistory_spec.lua
@@ -43,7 +43,6 @@ describe("ReadHistory module", function()
 
     local function assert_item_is(h, i, name, fileRemoved)
         assert.is.same(name, h.hist[i].text)
-        assert.is.same(i, h.hist[i].index)
         assert.is.same(joinPath(realpath(test_data_dir()), name), h.hist[i].file)
         if fileRemoved then
             assert.is_nil(realpath(test_file(name)))
@@ -258,28 +257,6 @@ describe("ReadHistory module", function()
         rm(test_file("c"))
         rm(test_file("d"))
         rm(test_file("e"))
-    end)
-
-    it("should remove duplicate entry", function()
-        rm(file("history.lua"))
-        touch(test_file("a"))
-        touch(test_file("b"))
-        local h = reload()
-        now = now + 61
-        h:addItem(test_file("b"), now)
-        now = now + 61
-        h:addItem(test_file("b"), now)
-        touch(legacy_history_file("a"))
-        now = now + 61
-        h:addItem(test_file("a"), now)  -- ensure a is before b
-        h = reload()
-        assert.is.same(2, #h.hist)
-        assert_item_is(h, 1, "a")
-        assert_item_is(h, 2, "b")
-
-        rm(legacy_history_file("a"))
-        rm(test_file("a"))
-        rm(test_file("b"))
     end)
 
     it("should reduce the total count", function()
