koreader/koreader

commit 62059f8d68fa884209ab0de75d190614bbc7a4f1

Misc: Get rid of the legacy defaults.lua globals (#9546)
* This removes support for the following deprecated constants: `DTAP_ZONE_FLIPPING`, `DTAP_ZONE_BOOKMARK`, `DCREREADER_CONFIG_DEFAULT_FONT_GAMMA`
* The "Advanced settings" panel now highlights modified values in bold (think about:config in Firefox ;)).
* LuaData: Isolate global table lookup shenanigans, and fix a few issues in unused-in-prod codepaths.
* CodeStyle: Require module locals for Lua/C modules, too.
* ScreenSaver: Actually garbage collect our widget on close (ScreenSaver itself is not an instantiated object).
* DateTimeWidget: Code cleanups to ensure child widgets can be GC'ed.

frontend/ui/data/onetime_migration.lua
@@ -7,7 +7,7 @@ local lfs = require("libs/libkoreader-lfs")
 local logger = require("logger")
 
 -- Date at which the last migration snippet was added
-local CURRENT_MIGRATION_DATE = 20220914
+local CURRENT_MIGRATION_DATE = 20220922
 
 -- Retrieve the date of the previous migration, if any
 local last_migration_date = G_reader_settings:readSetting("last_migration_date", 0)
@@ -443,6 +443,35 @@ if last_migration_date < 20220914 then
     local ok, err = os.remove(cache_path .. "/fontinfo.dat")
     if not ok then
        logger.warn("os.remove:", err)
+    end
+end
+
+-- The great defaults.persistent.lua migration to LuaDefaults
+if last_migration_date < 20220922 then
+    logger.info("Performing one-time migration for 20220922")
+
+    local defaults_path = DataStorage:getDataDir() .. "/defaults.persistent.lua"
+    local defaults = {}
+    local load_defaults, err = loadfile(defaults_path, "t", defaults)
+    if not load_defaults then
+        logger.warn("loadfile:", err)
+    else
+        load_defaults()
+    end
+
+    for k, v in pairs(defaults) do
+        -- Don't migrate deprecated settings
+        if G_defaults:has(k) then
+            G_defaults:saveSetting(k, v)
+        end
+    end
+    G_defaults:flush()
+
+    local archived_path = DataStorage:getDataDir() .. "/defaults.legacy.lua"
+    local ok
+    ok, err = os.rename(defaults_path, archived_path)
+    if not ok then
+       logger.warn("os.rename:", err)
     end
 end
 

commit 12de6667dbc1ab0154b9372581d47242a6b5ae43

OTM: Handle syntax errors at *both* stages when migrating defaults.persistent.lua
We were pcall'ing the parsing, but not the execution...
The funky Lua syntax quirks means that it is possible to pass the former
but not the latter ;).

Fix #9700, de-facto regression since #9546

frontend/ui/data/onetime_migration.lua
@@ -456,7 +456,14 @@ if last_migration_date < 20220930 then
     if not load_defaults then
         logger.warn("loadfile:", err)
     else
-        load_defaults()
+        -- User input, there may be syntax errors, go through pcall like we used to.
+        local ok, err = pcall(load_defaults)
+        if not ok then
+            logger.warn("Failed to execute defaults.persistent.lua:", err)
+            -- Don't keep *anything* around, to make it more obvious that something went screwy...
+            logger.warn("/!\\ YOU WILL HAVE TO MIGRATE YOUR CUSTOM defaults.lua SETTINGS MANUALLY /!\\")
+            defaults = {}
+        end
     end
 
     for k, v in pairs(defaults) do