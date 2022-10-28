af0c57557f173d4368c9b2b2f61ca4bc4b8d2998
NiLuJe <ninuje@gmail.com>
2022-10-28 23:08:20 +0200

    This duplicates the lfs check done in the plugin

--- plugins/externalkeyboard.koplugin/setup-debugfs.sh
+++ plugins/externalkeyboard.koplugin/setup-debugfs.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
 # This script is safe to run multiple times
-
-if ! [ -d /sys/kernel/debug ]; then
-    echo "The kernel does not support debugfs. It must be built with CONFIG_DEBUG_FS set."
-    exit 1
-fi
-
 if ! grep -q "^none /sys/kernel/debug debugfs" "/proc/mounts"; then
     if ! mount -t debugfs none /sys/kernel/debug; then
         echo "Failed to mount debugfs"
