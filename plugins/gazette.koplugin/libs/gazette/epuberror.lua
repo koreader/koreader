135b866675d5cce46a5270a045aa59f91d46ebe8
Scarlett <social@scarlettmcallister.com>
2022-10-29 21:42:47 -0300

    Move tables to declaration

--- plugins/gazette.koplugin/libs/gazette/epuberror.lua
+++ plugins/gazette.koplugin/libs/gazette/epuberror.lua
@@ -2,23 +2,21 @@ local _ = require("gettext")
 local T = require("ffi/util").template
 
 local EpubError = {
-
+    EPUB_INVALID_CONTENTS = _("Contents invalid"),
+    EPUBWRITER_INVALID_PATH = _("The path couldn't be opened."),
+    ITEMFACTORY_UNSUPPORTED_TYPE = _("Item type is not supported."),
+    ITEMFACTORY_NONEXISTENT_CONSTRUCTOR = _("Item type is supported but ItemFactory doesn't have a constructor for it."),
+    RESOURCE_WEBPAGE_INVALID_URL = _(""),
+    ITEM_MISSNG_ID = _("Item missing id"),
+    ITEM_MISSING_MEDIA_TYPE = _("Item missing media type"),
+    ITEM_MISSING_PATH = _("Item missing path"),
+    ITEM_NONSPECIFIC_ERROR = _("Something's wrong with your item. That's all I know"),
+    IMAGE_UNSUPPORTED_FORMAT = _("Image format is not supported."),
+    MANIFEST_BUILD_ERROR = _("Could not build manifest part for item."),
+    MANIFEST_ITEM_ALREADY_EXISTS = _("Item already exists in manifest"),
+    MANIFEST_ITEM_NIL = _("Can't add a nil item to the manifest."),
+    SPINE_BUILD_ERROR = _("Could not build spine part for item."),
 }
-
-EpubError.EPUB_INVALID_CONTENTS = _("Contents invalid")
-EpubError.EPUBWRITER_INVALID_PATH = _("The path couldn't be opened.")
-EpubError.ITEMFACTORY_UNSUPPORTED_TYPE = _("Item type is not supported.")
-EpubError.ITEMFACTORY_NONEXISTENT_CONSTRUCTOR = _("Item type is supported but ItemFactory doesn't have a constructor for it.")
-EpubError.RESOURCE_WEBPAGE_INVALID_URL = _("")
-EpubError.ITEM_MISSNG_ID = _("Item missing id")
-EpubError.ITEM_MISSING_MEDIA_TYPE = _("Item missing media type")
-EpubError.ITEM_MISSING_PATH = _("Item missing path")
-EpubError.ITEM_NONSPECIFIC_ERROR = _("Something's wrong with your item. That's all I know")
-EpubError.IMAGE_UNSUPPORTED_FORMAT = _("Image format is not supported.")
-EpubError.MANIFEST_BUILD_ERROR = _("Could not build manifest part for item.")
-EpubError.MANIFEST_ITEM_ALREADY_EXISTS = _("Item already exists in manifest")
-EpubError.MANIFEST_ITEM_NIL = _("Can't add a nil item to the manifest.")
-EpubError.SPINE_BUILD_ERROR = _("Could not build spine part for item.")
 
 function EpubError:provideFromEpubWriter(epubwriter)
 
