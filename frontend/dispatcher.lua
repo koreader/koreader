koreader/koreader

commit d37803a8e7a5039f80580b82e1650e7d4619f2ba

Dispatcher: preserve profile name, show actions count (#9682)
(1) Preserve profile name when selecting the "Nothing" action.
(2) Show number of actions instead of "Many".

frontend/dispatcher.lua
@@ -38,6 +38,7 @@ local UIManager = require("ui/uimanager")
 local util = require("util")
 local _ = require("gettext")
 local C_ = _.pgettext
+local N_ = _.ngettext
 local T = require("ffi/util").template
 
 local Dispatcher = {
@@ -488,6 +489,17 @@ local function iter_func(settings)
     end
 end
 
+-- Returns the number of items present in the settings table
+function Dispatcher:_itemsCount(settings)
+    if settings then
+        local count = util.tableSize(settings)
+        if count > 0 and settings.settings ~= nil then
+            count = count - 1
+        end
+        return count
+    end
+end
+
 -- Returns a display name for the item.
 function Dispatcher:getNameFromItem(item, settings)
     if settingsList[item] == nil then
@@ -551,17 +563,14 @@ end
 function Dispatcher:menuTextFunc(settings)
     local action_name = _("Pass through")
     if settings then
-        local count = util.tableSize(settings)
+        local count = Dispatcher:_itemsCount(settings)
         if count == 0 then return _("Nothing") end
-        if count > 1 and settings.settings ~= nil then
-            count = count - 1
-        end
         if count == 1 then
             local item = next(settings)
             if item == "settings" then item = next(settings, item) end
             action_name = Dispatcher:getNameFromItem(item, settings)
         else
-            action_name = _("Many")
+            action_name = T(N_("", "%1 actions", count), count)
         end
     end
     return action_name
@@ -798,10 +807,14 @@ function Dispatcher:addSubMenu(caller, menu, location, settings)
         text = _("Nothing"),
         separator = true,
         checked_func = function()
-            return location[settings] ~= nil and next(location[settings]) == nil
+            return location[settings] ~= nil and Dispatcher:_itemsCount(location[settings]) == 0
         end,
         callback = function(touchmenu_instance)
+            local name = location[settings] and location[settings].settings and location[settings].settings.name
             location[settings] = {}
+            if name then
+                location[settings].settings = { name = name }
+            end
             caller.updated = true
             if touchmenu_instance then touchmenu_instance:updateItems() end
         end,
@@ -931,7 +944,7 @@ function Dispatcher:execute(settings, gesture)
     if settings.settings ~= nil and settings.settings.show_as_quickmenu == true then
         return Dispatcher:_showAsMenu(settings)
     end
-    local has_many = util.tableSize(settings) > (settings.settings ~= nil and 2 or 1)
+    local has_many = Dispatcher:_itemsCount(settings) > 1
     if has_many then
         UIManager:broadcastEvent(Event:new("BatchedUpdate"))
     end

commit c8e01327de6fc26798ed5e4bec479f4229f65532

Fix broken translation metadata
Should fix https://github.com/koreader/koreader/discussions/9719

Regression introduced in https://github.com/koreader/koreader/pull/9682 by my fault.

See the xgettext error message:

```
frontend/dispatcher.lua:573: warning: Empty msgid.  It is reserved by GNU gettext:
                                      gettext("") returns the header entry with
                                      meta information, not the empty string.
xgettext: warning: msgid '' is used without plural and with plural.
                   xgettext.c:1975: Here is the occurrence without plural.
                   frontend/dispatcher.lua:573: Here is the occurrence with plural.
                   Workaround: If the msgid is a sentence, change the wording of the sentence; otherwise, use contexts for disambiguation.
```

frontend/dispatcher.lua
@@ -38,7 +38,7 @@ local UIManager = require("ui/uimanager")
 local util = require("util")
 local _ = require("gettext")
 local C_ = _.pgettext
-local N_ = _.ngettext
+local NC_ = _.npgettext
 local T = require("ffi/util").template
 
 local Dispatcher = {
@@ -570,7 +570,7 @@ function Dispatcher:menuTextFunc(settings)
             if item == "settings" then item = next(settings, item) end
             action_name = Dispatcher:getNameFromItem(item, settings)
         else
-            action_name = T(N_("", "%1 actions", count), count)
+            action_name = T(NC_("Dispatcher", "", "%1 actions", count), count)
         end
     end
     return action_name