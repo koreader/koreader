3c699885f8d46d9ac13f59ce7f0c74f0037075f2
Scarlett <social@scarlettmcallister.com>
2022-11-17 19:03:35 -0400

    Slim down subscription class
    Get rid of things that are not used, don't make sense.

--- plugins/gazette.koplugin/subscription/subscription.lua
+++ plugins/gazette.koplugin/subscription/subscription.lua
@@ -1,41 +1,21 @@
+local _ = require("gettext")
+
 local State = require("subscription/state")
 
-local Subscription = State:extend{
-   last_fetch = nil
-}
+local Subscription = State:extend{}
 
 function Subscription:new(o)
    o = o or {}
    setmetatable(o, self)
    self.__index = self
 
-   o:_init(o)
    o = o:load()
 
    return o
 end
 
-function Subscription:sync()
-   return true
-end
-
-function Subscription:_init(o)
-   self.id = o.id
-   self.last_updated = o.last_updated
-   self.last_fetch = o.last_fetch
-   self.subscription_type = o.subscription_type
-end
-
 function Subscription:onSuccessfulSync()
    self.last_fetch = os.date()
-end
-
-function Subscription:getTitle()
-   return "Untitled"
-end
-
-function Subscription:getDescription()
-   return ""
 end
 
 return Subscription
