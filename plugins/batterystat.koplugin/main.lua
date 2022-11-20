c8cf1ca7c8a71969f515e83bea9edc82be0d8793
Melik <10296053+melyux@users.noreply.github.com>
2022-11-19 23:15:14 -0800

    BatteryStat: Show current battery percentage

--- plugins/batterystat.koplugin/main.lua
+++ plugins/batterystat.koplugin/main.lua
@@ -212,7 +212,7 @@ function BatteryStat:showStatistics()
                                 end)
                             end})
     self.kv_page = KeyValuePage:new{
-        title = _("Battery statistics"),
+        title = string.format(_("Battery statistics (now %d%%)"), self.awake_state.percentage),
         kv_pairs = kv_pairs,
         single_page = true,
     }
