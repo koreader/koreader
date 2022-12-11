koreader/koreader

commit 31b16ba3e19dcbab0cf8fe503af5c96aa0b4762b

UIManager: get rid of self._running and self._run_forever (#9669)

frontend/ui/uimanager.lua
@@ -29,7 +29,6 @@ local UIManager = {
 
     event_handlers = nil,
 
-    _running = true,
     _now = time.now(),
     _window_stack = {},
     _task_queue = {},
@@ -68,7 +67,11 @@ function UIManager:init()
         self:nextTick(function()
             Device:saveSettings()
             Device:powerOff()
-            self:quit(Device:isKobo() and 88)
+            if Device:isKobo() then
+                self:quit(88)
+            else
+                self:quit()
+            end
         end)
     end
     self.reboot_action = function()
@@ -81,11 +84,19 @@ function UIManager:init()
         self:nextTick(function()
             Device:saveSettings()
             Device:reboot()
-            self:quit(Device:isKobo() and 88)
+            if Device:isKobo() then
+                self:quit(88)
+            else
+                self:quit()
+            end
         end)
     end
 
     Device:_setEventHandlers(self)
+
+    -- A simple wrapper for UIManager:quit()
+    -- This may be overwritten by setRunForeverMode(); for testing purposes
+    self._gated_quit = self.quit
 end
 
 --[[--
@@ -116,7 +127,6 @@ function UIManager:show(widget, refreshtype, refreshregion, x, y, refreshdither)
     end
     logger.dbg("show widget:", widget.id or widget.name or tostring(widget))
 
-    self._running = true
     local window = {x = x or 0, y = y or 0, widget = widget}
     -- put this window on top of the topmost non-modal window
     for i = #self._window_stack, 0, -1 do
@@ -728,13 +738,16 @@ function UIManager:isWidgetShown(widget)
 end
 
 --- Signals to quit.
+-- An exit_code of false is not allowed.
 function UIManager:quit(exit_code)
-    if not self._running then return end
-    logger.info("quitting uimanager with exit code:", exit_code or 0)
-    self._exit_code = exit_code
+    if exit_code == false then
+        logger.err("UIManager:quit() called with false")
+        return
+    end
+    -- Also honor older exit codes; default to 0
+    self._exit_code = exit_code or self._exit_code or 0
+    logger.info("quitting uimanager with exit code:", self._exit_code)
     self._task_queue_dirty = false
-    self._running = false
-    self._run_forever = nil
     for i = #self._window_stack, 1, -1 do
         table.remove(self._window_stack, i)
     end
@@ -749,6 +762,21 @@ function UIManager:quit(exit_code)
         self.looper:close()
         self.looper = nil
     end
+    return self._exit_code
+end
+dbg:guard(UIManager, 'quit',
+    function(self, exit_code)
+        assert(exit_code ~= false, "exit_code == false is not supported")
+    end)
+
+-- Disable automatic UIManager quit; for testing purposes
+function UIManager:setRunForeverMode()
+    self._gated_quit = function() return false end
+end
+
+-- Enable automatic UIManager quit; for testing purposes
+function UIManager:unsetRunForeverMode()
+    self._gated_quit = self.quit
 end
 
 --[[--
@@ -1345,10 +1373,11 @@ function UIManager:handleInput()
         --]]
 
         -- stop when we have no window to show
-        if not self._window_stack[1] and not self._run_forever then
+        if not self._window_stack[1] then
             logger.info("no dialog left to show")
-            self:quit()
-            return nil
+            if self:_gated_quit() ~= false then
+                return nil
+            end
         end
 
         self:_repaint()
@@ -1446,8 +1475,6 @@ This is the main loop of the UI controller.
 It is intended to manage input events and delegate them to dialogs.
 --]]
 function UIManager:run()
-    self._running = true
-
     -- Tell PowerD that we're ready
     Device:getPowerDevice():readyUI()
 
@@ -1455,21 +1482,15 @@ function UIManager:run()
     -- currently there is no Turbo support for Windows
     -- use our own main loop
     if not self.looper then
-        while self._running do
+        repeat
             self:handleInput()
-        end
+        until self._exit_code
     else
         self.looper:add_callback(function() self:handleInput() end)
         self.looper:start()
     end
 
     return self._exit_code
-end
-
--- run uimanager forever for testing purpose
-function UIManager:runForever()
-    self._run_forever = true
-    return self:run()
 end
 
 --[[--

spec/unit/autofrontlight_spec.lua
@@ -45,7 +45,7 @@ describe("AutoFrontlight widget tests", function()
             close()
 
         UIManager = require("ui/uimanager")
-        UIManager._run_forever = true
+        UIManager:setRunForeverMode()
 
         requireBackgroundRunner()
         class = dofile("plugins/autofrontlight.koplugin/main.lua")

spec/unit/autosuspend_spec.lua
@@ -13,7 +13,7 @@ describe("AutoSuspend", function()
             Device.input.waitEvent = function() end
             local UIManager = require("ui/uimanager")
             stub(UIManager, "suspend")
-            UIManager._run_forever = true
+            UIManager:setRunForeverMode()
             G_reader_settings:saveSetting("auto_suspend_timeout_seconds", 10)
             require("mock_time"):install()
             -- Reset UIManager:getTime()
@@ -71,7 +71,7 @@ describe("AutoSuspend", function()
             Device.input.waitEvent = function() end
             local UIManager = require("ui/uimanager")
             stub(UIManager, "poweroff_action")
-            UIManager._run_forever = true
+            UIManager:setRunForeverMode()
             G_reader_settings:saveSetting("autoshutdown_timeout_seconds", 10)
             require("mock_time"):install()
             -- Reset UIManager:getTime()

spec/unit/background_runner_spec.lua
@@ -12,7 +12,7 @@ describe("BackgroundRunner widget tests", function()
         MockTime = require("mock_time")
         MockTime:install()
         UIManager = require("ui/uimanager")
-        UIManager._run_forever = true
+        UIManager:setRunForeverMode()
         requireBackgroundRunner()
     end)
 

spec/unit/background_task_plugin_spec.lua
@@ -9,7 +9,7 @@ describe("BackgroundTaskPlugin", function()
         MockTime:install()
         local Device = require("device")
         Device.input.waitEvent = function() end
-        UIManager._run_forever = true
+        UIManager:setRunForeverMode()
         requireBackgroundRunner()
         -- Monkey patch this method to notify BackgroundRunner
         -- as it is not accessible to UIManager in these tests

spec/unit/httpclient_spec.lua
@@ -35,6 +35,7 @@ describe("HTTP client module #notest #nocov", function()
                 url = url,
             }, response_callback)
         end
-        UIManager:runForever()
+        UIManager:setRunForeverMode()
+        UIManager:run()
     end)
 end)

spec/unit/readerlink_spec.lua
@@ -28,6 +28,7 @@ describe("ReaderLink module", function()
 
     it("should jump to links in pdf page mode", function()
         UIManager:quit()
+        UIManager._exit_code = nil
         local readerui = ReaderUI:new{
             dimen = Screen:getSize(),
             document = DocumentRegistry:openDocument(sample_pdf),
@@ -44,6 +45,7 @@ describe("ReaderLink module", function()
 
     it("should jump to links in pdf scroll mode", function()
         UIManager:quit()
+        UIManager._exit_code = nil
         local readerui = ReaderUI:new{
             dimen = Screen:getSize(),
             document = DocumentRegistry:openDocument(sample_pdf),
@@ -78,6 +80,7 @@ describe("ReaderLink module", function()
 
     it("should be able to go back after link jump in pdf page mode", function()
         UIManager:quit()
+        UIManager._exit_code = nil
         local readerui = ReaderUI:new{
             dimen = Screen:getSize(),
             document = DocumentRegistry:openDocument(sample_pdf),
@@ -96,6 +99,7 @@ describe("ReaderLink module", function()
 
     it("should be able to go back after link jump in pdf scroll mode", function()
         UIManager:quit()
+        UIManager._exit_code = nil
         local readerui = ReaderUI:new{
             dimen = Screen:getSize(),
             document = DocumentRegistry:openDocument(sample_pdf),
@@ -116,6 +120,7 @@ describe("ReaderLink module", function()
 
     it("should be able to go back to the same position after link jump in pdf scroll mode", function()
         UIManager:quit()
+        UIManager._exit_code = nil
         local expected_page_states = {
             {
                 gamma = 1,

spec/unit/spore_spec.lua
@@ -102,7 +102,8 @@ describe("Lua Spore modules with async http request #notest #nocov", function()
         client:enable("Format.JSON")
         client:enable("AsyncHTTP", {thread = co})
         coroutine.resume(co)
-        UIManager:runForever()
+        UIManager:setRunForeverMode()
+        UIManager:run()
     end)
 
     it("should complete POST request", function()
@@ -117,6 +118,7 @@ describe("Lua Spore modules with async http request #notest #nocov", function()
         client:enable("Format.JSON")
         client:enable("AsyncHTTP", {thread = co})
         coroutine.resume(co)
-        UIManager:runForever()
+        UIManager:setRunForeverMode()
+        UIManager:run()
     end)
 end)

commit c11d1f55c6a6612fc06e41f19265a3b8fedd790c

Startup: Unbreak last_file when the file is missing
Fix #9887, likely a regression since #9669

frontend/ui/uimanager.lua
@@ -825,6 +825,23 @@ function UIManager:unsetRunForeverMode()
     self._gated_quit = self.quit
 end
 
+-- Ignore an empty window stack *once*; for startup w/ a missing last_file shenanigans...
+function UIManager:runForOneFrame()
+    -- We don't actually want to call self.quit, and we need to deal with a bit of trickery in there anyway...
+    self._gated_quit = function()
+        -- We need this set to break the loop in UIManager:run()
+        self._exit_code = 0
+        -- And this is to break the loop in UIManager:handleInput()
+        return true
+    end
+    -- The idea being that we want to *return* from this run call, but *without* quitting.
+    -- NOTE: This implies that calling run multiple times across a single session *needs* to be safe.
+    self:run()
+    -- Restore standard behavior
+    self:unsetRunForeverMode()
+    self._exit_code = nil
+end
+
 --[[--
 Transmits an @{ui.event.Event|Event} to active widgets, top to bottom.
 Stops at the first handler that returns `true`.

reader.lua
@@ -271,15 +271,11 @@ end
 local exit_code
 if file then
     local ReaderUI = require("apps/reader/readerui")
-    UIManager:nextTick(function()
-        ReaderUI:showReader(file)
-    end)
+    ReaderUI:showReader(file)
     exit_code = UIManager:run()
 elseif directory then
     local FileManager = require("apps/filemanager/filemanager")
-    UIManager:nextTick(function()
-        FileManager:showFiles(directory)
-    end)
+    FileManager:showFiles(directory)
     exit_code = UIManager:run()
 else
     local QuickStart = require("ui/quickstart")
@@ -290,47 +286,38 @@ else
 
     if start_with == "last" and last_file and lfs.attributes(last_file, "mode") ~= "file" then
         UIManager:show(retryLastFile())
-        -- no exit code as something else will be run after this.
-        UIManager:run()
+        -- We'll want to return from this without actually quitting,
+        -- so this is a slightly mangled UIManager:run() call to coerce the main loop into submission...
+        -- We'll call :run properly in either of the following branches once returning.
+        UIManager:runForOneFrame()
     end
     if start_with == "last" and last_file then
         local ReaderUI = require("apps/reader/readerui")
-        UIManager:nextTick(function()
-            -- Instantiate RD
-            ReaderUI:showReader(last_file)
-        end)
+        -- Instantiate RD
+        ReaderUI:showReader(last_file)
         exit_code = UIManager:run()
     else
         local FileManager = require("apps/filemanager/filemanager")
-        local home_dir =
-            G_reader_settings:readSetting("home_dir") or Device.home_dir or lfs.currentdir()
-        UIManager:nextTick(function()
-            -- Instantiate FM
-            FileManager:showFiles(home_dir)
-        end)
+        local home_dir = G_reader_settings:readSetting("home_dir") or Device.home_dir or lfs.currentdir()
+        -- Instantiate FM
+        FileManager:showFiles(home_dir)
         -- Always open FM modules on top of filemanager, so closing 'em doesn't result in an exit
         -- because of an empty widget stack, and so they can interact with the FM instance as expected.
         if start_with == "history" then
             local FileManagerHistory = require("apps/filemanager/filemanagerhistory")
-            UIManager:nextTick(function()
-                FileManagerHistory:new{
-                    ui = FileManager.instance,
-                }:onShowHist()
-            end)
+            FileManagerHistory:new{
+                ui = FileManager.instance,
+            }:onShowHist()
         elseif start_with == "favorites" then
             local FileManagerCollection = require("apps/filemanager/filemanagercollection")
-            UIManager:nextTick(function()
-                FileManagerCollection:new{
-                    ui = FileManager.instance,
-                }:onShowColl("favorites")
-            end)
+            FileManagerCollection:new{
+                ui = FileManager.instance,
+            }:onShowColl("favorites")
         elseif start_with == "folder_shortcuts" then
             local FileManagerShortcuts = require("apps/filemanager/filemanagershortcuts")
-            UIManager:nextTick(function()
-                FileManagerShortcuts:new{
-                    ui = FileManager.instance,
-                }:onShowFolderShortcutsDialog()
-            end)
+            FileManagerShortcuts:new{
+                ui = FileManager.instance,
+            }:onShowFolderShortcutsDialog()
         end
         exit_code = UIManager:run()
     end
@@ -342,8 +329,10 @@ local function exitReader()
     local ko_exit = os.getenv("KO_EXIT_CODE")
     if ko_exit then
         local fo = io.open(ko_exit, "w+")
-        fo:write(tostring(exit_code))
-        fo:close()
+        if fo then
+            fo:write(tostring(exit_code))
+            fo:close()
+        end
     end
 
     local ReaderActivityIndicator =