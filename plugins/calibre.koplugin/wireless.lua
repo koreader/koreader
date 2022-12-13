70032971495b0f687fd7e1e75cf270f40506d6b5
NiLuJe <ninuje@gmail.com>
2022-12-13 21:23:37 +0100

    Calibre: Log errors on wireless connection failures
    Also, get rid of the weird and clunky failed_connect_callback thingy,
    because it makes no sense?

--- plugins/calibre.koplugin/wireless.lua
+++ plugins/calibre.koplugin/wireless.lua
@@ -122,13 +122,13 @@ function CalibreWireless:checkCalibreServer(host, port)
     local socket = require("socket")
     local tcp = socket.tcp()
     tcp:settimeout(5)
-    local client = tcp:connect(host, port)
     -- In case of error, the method returns nil followed by a string describing the error. In case of success, the method returns 1.
-    if client then
+    local ok, err = tcp:connect(host, port)
+    if ok then
         tcp:close()
         return true
     end
-    return false
+    return false, err
 end
 
 -- Standard JSON/control opcodes receive callback
@@ -160,10 +160,6 @@ function CalibreWireless:JSONReceiveCallback(host, port)
             end
             this.connect_message = true
             UIManager:scheduleIn(1, this.password_check_callback)
-            if this.failed_connect_callback then
-                -- Don't disconnect if we connect in 10 seconds
-                UIManager:unschedule(this.failed_connect_callback)
-            end
         end
     end
 end
@@ -234,26 +230,32 @@ function CalibreWireless:connect()
     end
 
     self.connect_message = false
-    local host, port
+    local address_type, host, port, ok, err
     if G_reader_settings:hasNot("calibre_wireless_url") then
         host, port = self:find_calibre_server()
+        if host and port then
+            address_type = "discovered"
+        else
+            ok = false
+            err = "Couldn't discover a calibre instance on the local network"
+            address_type = "unavailable"
+        end
     else
         local calibre_url = G_reader_settings:readSetting("calibre_wireless_url")
         host, port = calibre_url["address"], calibre_url["port"]
-        if not self:checkCalibreServer(host, port) then
-            host = nil
-        else
-            self.failed_connect_callback = function()
-                UIManager:show(InfoMessage:new{
-                    text = _("Cannot connect to calibre server."),
-                })
-                self:disconnect()
-            end
-            -- wait 10 seconds to connect to calibre
-            UIManager:scheduleIn(10, self.failed_connect_callback)
-        end
+        address_type = "specified"
     end
+
     if host and port then
+        ok, err = self:checkCalibreServer(host, port)
+    end
+
+    if not ok then
+        logger.warn(string.format("Cannot connect to %s calibre server at %s:%s (%s)", address_type, host or "????", port or "??", err or "N/A"))
+        UIManager:show(InfoMessage:new{
+            text = _("Cannot connect to calibre server."),
+        })
+    else
         local inbox_dir = G_reader_settings:readSetting("inbox_dir")
         if inbox_dir then
             CalibreMetadata:init(inbox_dir)
@@ -261,12 +263,6 @@ function CalibreWireless:connect()
         else
             self:setInboxDir(host, port)
         end
-    else
-        logger.info("cannot connect to calibre server")
-        UIManager:show(InfoMessage:new{
-            text = _("Cannot connect to calibre server."),
-        })
-        return
     end
 end
 
