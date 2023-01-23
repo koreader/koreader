5c4fd9c6765c622bdb624aa46ff70a2a71776f48
NiLuJe <ninuje@gmail.com>
2023-01-23 19:55:46 +0100

    AutoTurn: Unbreak when paired w/ the Dimmer plugin

--- plugins/autoturn.koplugin/main.lua
+++ plugins/autoturn.koplugin/main.lua
@@ -32,7 +32,8 @@ function AutoTurn:_schedule()
     local delay = self.last_action_time + time.s(self.autoturn_sec) - UIManager:getTime()
 
     if delay <= 0 then
-        if UIManager:getNthTopWidget().name == "ReaderUI" then
+        local top_wg = UIManager:getNthTopWidget() or {}
+        if top_wg.name == "ReaderUI" or top_wg.invisible then
             logger.dbg("AutoTurn: go to next page")
             self.ui:handleEvent(Event:new("GotoViewRel", self.autoturn_distance))
             self.last_action_time = UIManager:getTime()
