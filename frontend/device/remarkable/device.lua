fc936447709d99273371d2c4e283bb9b67250056
yparitcher <y@paritcher.com>
2023-01-31 15:28:32 -0500

    NetworkManager: isWifiOn isConnected consistent usage.
    cervantes kindle kobo remarkable: use sysfs carrier file to determine connection state
    
    cleanup hasWifiManager checks
    
    gateway check: use ip if available
    
    Fixes: #10087
    Closes: #10092

--- frontend/device/remarkable/device.lua
+++ frontend/device/remarkable/device.lua
@@ -194,9 +194,8 @@ function Remarkable:initNetworkManager(NetworkMgr)
 
     NetworkMgr:setWirelessBackend("wpa_supplicant", {ctrl_interface = "/var/run/wpa_supplicant/wlan0"})
 
-    NetworkMgr.isWifiOn = function()
-        return NetworkMgr:isConnected()
-    end
+    NetworkMgr.isWifiOn = NetworkMgr.sysfsWifiOn
+    NetworkMgr.isConnected = NetworkMgr.sysfsCarrierConnected
 end
 
 function Remarkable:setDateTime(year, month, day, hour, min, sec)
