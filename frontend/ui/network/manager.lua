2c081cbe9611f0504bc1ed33b31063f6ab7a252c
yparitcher <y@paritcher.com>
2023-01-31 23:49:37 -0500

    android

--- frontend/ui/network/manager.lua
+++ frontend/ui/network/manager.lua
@@ -248,23 +248,19 @@ end
 
 -- This function be overridden by a device specific implementation.
 function NetworkMgr:isConnected()
-    if Device:isAndroid() then
-        return self:isWifiOn()
-    else
-        -- Pull the default gateway first, so we don't even try to ping anything if there isn't one...
-        local default_gw
-        local std_out = io.popen([[/sbin/route -n | awk '$4 == "UG" {print $2}' | tail -n 1]], "r")
-        if std_out then
-            default_gw = std_out:read("*all")
-            std_out:close()
-            if not default_gw or default_gw == "" then
-                return false
-            end
+    -- Pull the default gateway first, so we don't even try to ping anything if there isn't one...
+    local default_gw
+    local std_out = io.popen([[/sbin/route -n | awk '$4 == "UG" {print $2}' | tail -n 1]], "r")
+    if std_out then
+        default_gw = std_out:read("*all")
+        std_out:close()
+        if not default_gw or default_gw == "" then
+            return false
         end
-
-        -- `-c1` try only once; `-w2` wait 2 seconds
-        return 0 == os.execute("ping -c1 -w2 " .. default_gw)
     end
+
+    -- `-c1` try only once; `-w2` wait 2 seconds
+    return 0 == os.execute("ping -c1 -w2 " .. default_gw)
 end
 
 function NetworkMgr:isOnline()
