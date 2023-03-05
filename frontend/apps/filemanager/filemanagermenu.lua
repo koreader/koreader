5fce41d7d9b9e86c28dad747d5b5adad15853312
Frans de Jonge <fransdejonge@gmail.com>
2023-03-05 21:10:03 +0100

    [i18n] FileManagerMenu: remove stray newline from translated sting (#10174)

--- frontend/apps/filemanager/filemanagermenu.lua
+++ frontend/apps/filemanager/filemanagermenu.lua
@@ -852,8 +852,8 @@ function FileManagerMenu:moveBookMetadata()
                 UIManager:show(ConfirmBox:new{
                     text = T(N_("1 book with metadata not in your preferred location found.",
                               "%1 books with metadata not in your preferred location found.",
-                              books_to_move_nb), books_to_move_nb) ..
-                              _("\nMove book metadata to your preferred location?"),
+                              books_to_move_nb), books_to_move_nb) .. "\n" ..
+                              _("Move book metadata to your preferred location?"),
                     ok_text = _("Move"),
                     ok_callback = function()
                         UIManager:close(self.menu_container)
