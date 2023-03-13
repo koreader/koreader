d861e0d4296624a6c487d29899e5de26e7991032
zwim <martin.zwicknagl@kirchbichl.net>
2023-03-13 19:33:56 +0100

    Initial commit

--- frontend/ui/uimanager.lua
+++ frontend/ui/uimanager.lua
@@ -243,6 +243,16 @@ function UIManager:close(widget, refreshtype, refreshregion, refreshdither)
     end
 end
 
+--- Shift the execution times of all scheduled tasks.
+-- UIManager uses CLOCK_MONOTONIC (which doesn't tick during standby), so shifting the execution
+-- time by a negative value will lead to an execution at the expected time.
+-- @param time if positive execute the tasks later, if negative the should be executed earlier
+function UIManager:shiftScheduledTasksBy(shift_time)
+    for i, v in ipairs(self._task_queue) do
+        v.time = v.time + shift_time
+    end
+end
+
 -- Schedule an execution task; task queue is in descending order
 function UIManager:schedule(sched_time, action, ...)
     local lo, hi = 1, #self._task_queue
