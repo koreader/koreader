ce243f513882798ece05f403ea8867eecd815626
hius07 <62179190+hius07@users.noreply.github.com>
2023-03-18 12:19:08 +0200

    filemanagerfilesearcher: consistent results view

--- frontend/apps/filemanager/filemanagerfilesearcher.lua
+++ frontend/apps/filemanager/filemanagerfilesearcher.lua
@@ -16,38 +16,135 @@ local lfs = require("libs/libkoreader-lfs")
 local util = require("util")
 local _ = require("gettext")
 local Screen = require("device").screen
-local T = require("ffi/util").template
+local T = BaseUtil.template
 
 local FileSearcher = WidgetContainer:extend{
-    dirs = nil, -- table
-    files = nil, -- table
-    results = nil, -- table
-
     case_sensitive = false,
     include_subfolders = true,
-}
-
-local sys_folders = { -- do not search in sys_folders
-    ["/dev"] = true,
-    ["/proc"] = true,
-    ["/sys"] = true,
+    include_metadata = false,
 }
 
 function FileSearcher:init()
-    self.dirs = {}
-    self.files = {}
-    self.results = {}
 end
 
-function FileSearcher:readDir()
-    local ReaderUI = require("apps/reader/readerui")
+function FileSearcher:onShowFileSearch(search_string)
+    local search_dialog
+    local check_button_case, check_button_subfolders, check_button_metadata
+    search_dialog = InputDialog:new{
+        title = _("Enter text to search for in filename"),
+        input = search_string or self.search_value,
+        buttons = {
+            {
+                {
+                    text = _("Cancel"),
+                    id = "close",
+                    callback = function()
+                        UIManager:close(search_dialog)
+                    end,
+                },
+                {
+                    text = _("Home folder"),
+                    enabled = G_reader_settings:has("home_dir"),
+                    callback = function()
+                        self.search_value = search_dialog:getInputText()
+                        if self.search_value == "" then return end
+                        UIManager:close(search_dialog)
+                        self.path = G_reader_settings:readSetting("home_dir")
+                        self:doSearch()
+                    end,
+                },
+                {
+                    text = self.ui.file_chooser and _("Current folder") or _("Book folder"),
+                    is_enter_default = true,
+                    callback = function()
+                        self.search_value = search_dialog:getInputText()
+                        if self.search_value == "" then return end
+                        UIManager:close(search_dialog)
+                        self.path = self.ui.file_chooser and self.ui.file_chooser.path or self.ui:getLastDirFile()
+                        self:doSearch()
+                    end,
+                },
+            },
+        },
+    }
+
+    check_button_case = CheckButton:new{
+        text = _("Case sensitive"),
+        checked = self.case_sensitive,
+        parent = search_dialog,
+        callback = function()
+            self.case_sensitive = check_button_case.checked
+        end,
+    }
+    search_dialog:addWidget(check_button_case)
+    check_button_subfolders = CheckButton:new{
+        text = _("Include subfolders"),
+        checked = self.include_subfolders,
+        parent = search_dialog,
+        callback = function()
+            self.include_subfolders = check_button_subfolders.checked
+        end,
+    }
+    search_dialog:addWidget(check_button_subfolders)
+    check_button_metadata = CheckButton:new{
+        text = _("Search in book metadata (slow)"),
+        checked = self.include_metadata,
+        parent = search_dialog,
+        callback = function()
+            self.include_metadata = check_button_metadata.checked
+        end,
+    }
+    search_dialog:addWidget(check_button_metadata)
+
+    UIManager:show(search_dialog)
+    search_dialog:onShowKeyboard()
+end
+
+function FileSearcher:doSearch()
+    local results
+    local dirs, files = self:getList()
+    -- If we have a FileChooser instance, use it, to be able to make use of its natsort cache
+    if self.ui.file_chooser then
+        results = self.ui.file_chooser:genItemTable(dirs, files)
+    else
+        results = FileChooser:genItemTable(dirs, files)
+    end
+    if #results > 0 then
+        self:showSearchResults(results)
+    else
+        UIManager:show(InfoMessage:new{
+            text = T(_("No results for '%1'."), self.search_value),
+        })
+    end
+end
+
+function FileSearcher:getList()
+    local sys_folders = { -- do not search in sys_folders
+        ["/dev"] = true,
+        ["/proc"] = true,
+        ["/sys"] = true,
+    }
     local show_unsupported = G_reader_settings:isTrue("show_unsupported")
-    self.dirs = {self.path}
-    self.files = {}
-    while #self.dirs ~= 0 do
+    local collate = G_reader_settings:readSetting("collate")
+    local keywords = self.search_value
+    if keywords ~= "*" then -- one * to show all files
+        if not self.case_sensitive then
+            keywords = Utf8Proc.lowercase(util.fixUtf8(keywords, "?"))
+        end
+        -- replace '.' with '%.'
+        keywords = keywords:gsub("%.","%%%.")
+        -- replace '*' with '.*'
+        keywords = keywords:gsub("%*","%.%*")
+        -- replace '?' with '.'
+        keywords = keywords:gsub("%?","%.")
+    end
+
+    local dirs, files = {}, {}
+    local scan_dirs = {self.path}
+    while #scan_dirs ~= 0 do
         local new_dirs = {}
         -- handle each dir
-        for __, d in pairs(self.dirs) do
+        for _, d in ipairs(scan_dirs) do
             -- handle files in d
             local ok, iter, dir_obj = pcall(lfs.dir, d)
             if ok then
@@ -59,180 +156,70 @@ function FileSearcher:readDir()
                     local attributes = lfs.attributes(fullpath) or {}
                     -- Don't traverse hidden folders if we're not showing them
                     if attributes.mode == "directory" and f ~= "." and f ~= ".."
-                        and (G_reader_settings:isTrue("show_hidden") or not util.stringStartsWith(f, "."))
-                        and FileChooser:show_dir(f)
-                    then
+                            and (G_reader_settings:isTrue("show_hidden") or not util.stringStartsWith(f, "."))
+                            and FileChooser:show_dir(f) then
                         if self.include_subfolders and not sys_folders[fullpath] then
                             table.insert(new_dirs, fullpath)
                         end
-                        table.insert(self.files, {
-                            dir = d,
-                            name = f,
-                            text = f.."/",
-                            attr = attributes,
-                            callback = function()
-                                self:showFolder(fullpath .. "/")
-                            end,
-                        })
+                        if self:isFileMatch(f, fullpath, keywords) then
+                            table.insert(dirs, FileChooser:getListItem(f, fullpath, attributes))
+                        end
                     -- Always ignore macOS resource forks, too.
                     elseif attributes.mode == "file" and not util.stringStartsWith(f, "._")
-                        and (show_unsupported or DocumentRegistry:hasProvider(fullpath))
-                        and FileChooser:show_file(f)
-                    then
-                        table.insert(self.files, {
-                            dir = d,
-                            name = f,
-                            text = f,
-                            mandatory = util.getFriendlySize(attributes.size or 0),
-                            attr = attributes,
-                            callback = function()
-                                ReaderUI:showReader(fullpath)
-                            end,
-                        })
-                    end
-                end
-            end
-        end
-        self.dirs = new_dirs
-    end
-end
-
-function FileSearcher:setSearchResults()
-    local keywords = self.search_value
-    self.results = {}
-    if keywords == "*" then -- one * to show all files
-        self.results = self.files
-    else
-        if not self.case_sensitive then
-            keywords = Utf8Proc.lowercase(util.fixUtf8(keywords, "?"))
-        end
-        -- replace '.' with '%.'
-        keywords = keywords:gsub("%.","%%%.")
-        -- replace '*' with '.*'
-        keywords = keywords:gsub("%*","%.%*")
-        -- replace '?' with '.'
-        keywords = keywords:gsub("%?","%.")
-        for i, f in ipairs(self.files) do
-            local f_name = self.case_sensitive and f.name or Utf8Proc.lowercase(util.fixUtf8(f.name, "?"))
-            if string.find(f_name, keywords) then
-                table.insert(self.results, f)
-            elseif self.include_metadata and f.mandatory then -- files only
-                local filepath = f.dir.."/"..f.name
-                if DocumentRegistry:hasProvider(filepath) then
-                    local book_props = FileManagerBookInfo:getBookProps(filepath)
-                    if next(book_props) ~= nil then
-                        for k, v in pairs(book_props) do
-                            if k ~= "pages" then
-                                local prop = self.case_sensitive and v or Utf8Proc.lowercase(util.fixUtf8(v, "?"))
-                                if k == "description" then
-                                    prop = util.htmlToPlainTextIfHtml(prop)
-                                end
-                                if string.find(prop, keywords) then
-                                    table.insert(self.results, f)
-                                    break
-                                end
-                            end
+                            and (show_unsupported or DocumentRegistry:hasProvider(fullpath))
+                            and FileChooser:show_file(f) then
+                        if self:isFileMatch(f, fullpath, keywords, true) then
+                            table.insert(files, FileChooser:getListItem(f, fullpath, attributes, collate))
                         end
                     end
                 end
             end
         end
+        scan_dirs = new_dirs
+    end
+    return dirs, files
+end
+
+function FileSearcher:isFileMatch(filename, fullpath, keywords, is_file)
+    if keywords == "*" then
+        return true
+    end
+    if not self.case_sensitive then
+        filename = Utf8Proc.lowercase(util.fixUtf8(filename, "?"))
+    end
+    if string.find(filename, keywords) then
+        return true
+    end
+    if self.include_metadata and is_file and DocumentRegistry:hasProvider(fullpath) then
+        local book_props = FileManagerBookInfo:getBookProps(fullpath)
+        book_props.pages = nil
+        if next(book_props) ~= nil then
+            for name, prop in pairs(book_props) do
+                if not self.case_sensitive then
+                    prop = Utf8Proc.lowercase(util.fixUtf8(prop, "?"))
+                end
+                if name == "description" then
+                    prop = util.htmlToPlainTextIfHtml(prop)
+                end
+                if string.find(prop, keywords) then
+                    return true
+                end
+            end
+        end
     end
 end
 
-function FileSearcher:close()
-    UIManager:close(self.search_dialog)
-    self:readDir() --- @todo this probably doesn't need to be repeated once it's been done
-    self:setSearchResults() --- @todo doesn't have to be repeated if the search term is the same
-    if #self.results > 0 then
-        self:showSearchResults()
-    else
-        UIManager:show(
-            InfoMessage:new{
-                text = BaseUtil.template(_("No results for '%1'."),
-                                     self.search_value)
-            }
-        )
-    end
-end
-
-function FileSearcher:onShowFileSearch(search_string)
-    self.search_dialog = InputDialog:new{
-        title = _("Enter filename to search for"),
-        input = search_string or self.search_value,
-        buttons = {
-            {
-                {
-                    text = _("Cancel"),
-                    id = "close",
-                    callback = function()
-                        UIManager:close(self.search_dialog)
-                    end,
-                },
-                {
-                    text = _("Home folder"),
-                    enabled = G_reader_settings:has("home_dir"),
-                    callback = function()
-                        self.search_value = self.search_dialog:getInputText()
-                        if self.search_value == "" then return end
-                        self.path = G_reader_settings:readSetting("home_dir")
-                        self:close()
-                    end,
-                },
-                {
-                    text = _("Current folder"),
-                    is_enter_default = true,
-                    callback = function()
-                        self.search_value = self.search_dialog:getInputText()
-                        if self.search_value == "" then return end
-                        self.path = self.ui.file_chooser and self.ui.file_chooser.path or self.ui:getLastDirFile()
-                        self:close()
-                    end,
-                },
-            },
-        },
-    }
-
-    self.check_button_case = CheckButton:new{
-        text = _("Case sensitive"),
-        checked = self.case_sensitive,
-        parent = self.search_dialog,
-        callback = function()
-            self.case_sensitive = self.check_button_case.checked
-        end,
-    }
-    self.search_dialog:addWidget(self.check_button_case)
-    self.check_button_subfolders = CheckButton:new{
-        text = _("Include subfolders"),
-        checked = self.include_subfolders,
-        parent = self.search_dialog,
-        callback = function()
-            self.include_subfolders = self.check_button_subfolders.checked
-        end,
-    }
-    self.search_dialog:addWidget(self.check_button_subfolders)
-    self.check_button_metadata = CheckButton:new{
-        text = _("In book metadata (slow)"),
-        checked = self.include_metadata,
-        parent = self.search_dialog,
-        callback = function()
-            self.include_metadata = self.check_button_metadata.checked
-        end,
-    }
-    self.search_dialog:addWidget(self.check_button_metadata)
-
-    UIManager:show(self.search_dialog)
-    self.search_dialog:onShowKeyboard()
-end
-
-function FileSearcher:showSearchResults()
+function FileSearcher:showSearchResults(results)
     local menu_container = CenterContainer:new{
         dimen = Screen:getSize(),
     }
     self.search_menu = Menu:new{
-        width = Screen:getWidth() - (Size.margin.fullscreen_popout * 2),
-        height = Screen:getHeight() - (Size.margin.fullscreen_popout * 2),
+        width = Screen:getWidth(),
+        height = Screen:getHeight(),
+        is_borderless = true,
+        is_popout = false,
         show_parent = menu_container,
+        onMenuSelect = self.onMenuSelect,
         onMenuHold = self.onMenuHold,
         _manager = self,
     }
@@ -240,60 +227,66 @@ function FileSearcher:showSearchResults()
     self.search_menu.close_callback = function()
         UIManager:close(menu_container)
     end
-
-    local collate = G_reader_settings:readSetting("collate") or "strcoll"
-    local reverse_collate = G_reader_settings:isTrue("reverse_collate")
-    -- If we have a FileChooser instance, use it, to be able to make use of its natsort cache
-    local sorting
-    if self.ui.file_chooser then
-        sorting = self.ui.file_chooser:getSortingFunction(collate, reverse_collate)
-    else
-        sorting = FileChooser:getSortingFunction(collate, reverse_collate)
-    end
-
-    table.sort(self.results, sorting)
-    self.search_menu:switchItemTable(T(_("Search results (%1)"), #self.results), self.results)
+    self.search_menu:switchItemTable(T(_("Search results (%1)"), #results), results)
     UIManager:show(menu_container)
 end
 
-function FileSearcher:onMenuHold(item)
-    local ReaderUI = require("apps/reader/readerui")
-    local is_file = item.attr.mode == "file"
-    local fullpath = item.dir .. "/" .. item.name .. (is_file and "" or "/")
-    local buttons = {
-        {
+function FileSearcher:onMenuSelect(item)
+    local dialog
+    local buttons = {}
+    if item.is_file then
+        table.insert(buttons, {
             {
-                text = _("Cancel"),
+                text = _("Book information"),
                 callback = function()
-                    UIManager:close(self.results_dialog)
+                    UIManager:close(dialog)
+                    FileManagerBookInfo:show(item.path)
                 end,
             },
             {
-                text = _("Show folder"),
+                text = _("Open"),
+                enabled = DocumentRegistry:hasProvider(item.path),
                 callback = function()
-                    UIManager:close(self.results_dialog)
+                    UIManager:close(dialog)
                     self.close_callback()
-                    self._manager:showFolder(fullpath)
+                    require("apps/reader/readerui"):showReader(item.path)
                 end,
             },
-        },
-    }
-    if is_file then
-        table.insert(buttons[1], {
-            text = _("Open"),
-            callback = function()
-                UIManager:close(self.results_dialog)
-                self.close_callback()
-                ReaderUI:showReader(fullpath)
-            end,
         })
     end
-
-    self.results_dialog = ButtonDialogTitle:new{
-        title = fullpath,
+    table.insert(buttons, {
+        {
+            text = _("Cancel"),
+            callback = function()
+                UIManager:close(dialog)
+            end,
+        },
+        {
+            text = _("Show folder"),
+            callback = function()
+                UIManager:close(dialog)
+                self.close_callback()
+                self._manager:showFolder(item.path)
+            end,
+        },
+    })
+    dialog = ButtonDialogTitle:new{
+        title = item.path,
         buttons = buttons,
     }
-    UIManager:show(self.results_dialog)
+    UIManager:show(dialog)
+end
+
+function FileSearcher:onMenuHold(item)
+    if item.is_file then
+        if DocumentRegistry:hasProvider(item.path) then
+            self.close_callback()
+            require("apps/reader/readerui"):showReader(item.path)
+        end
+    else
+        self.close_callback()
+        self._manager:showFolder(item.path)
+    end
     return true
 end
 
