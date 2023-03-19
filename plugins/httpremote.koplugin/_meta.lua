28aa516eae9315cf387dd6431ddce217e588f70d
Bryan Neo <bryanncy1999@gmail.com>
2023-03-19 14:30:45 +0800

    update meta comment

--- plugins/httpremote.koplugin/_meta.lua
+++ plugins/httpremote.koplugin/_meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     name = "httpremote",
     fullname = _("HttpRemote"),
-    description = _([[Control KOReader with by sending requests to a HTTP server.]]),
+    description = _([[Control KOReader by sending requests to a HTTP server.]]),
 }
