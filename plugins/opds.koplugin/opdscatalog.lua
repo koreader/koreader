45e955a761186129d5d834e4bd26691337bf0a79
hius07 <62179190+hius07@users.noreply.github.com>
2023-03-24 10:35:14 +0200

    OPDSCatalog: remove has_close_button

--- plugins/opds.koplugin/opdscatalog.lua
+++ plugins/opds.koplugin/opdscatalog.lua
@@ -20,7 +20,6 @@ function OPDSCatalog:init()
         show_parent = self,
         is_popout = false,
         is_borderless = true,
-        has_close_button = true,
         close_callback = function() return self:onClose() end,
         file_downloaded_callback = function(downloaded_file)
             UIManager:show(ConfirmBox:new{
