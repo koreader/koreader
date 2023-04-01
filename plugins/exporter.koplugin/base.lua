de86f3dda63e993838f8b601e14cb829354a2de9
Mochitto <mochittodeveloper@gmail.com>
2023-04-01 13:48:33 +0200

    Fix: files that have the same extension don't collide anymore

--- plugins/exporter.koplugin/base.lua
+++ plugins/exporter.koplugin/base.lua
@@ -93,7 +93,7 @@ function BaseExporter:getFilePath(t)
     if not self.is_remote then
         local filename = string.format("%s-%s.%s",
             self:getTimeStamp(),
-            #t == 1 and t[1].exportable_title or "all-books",
+            #t == 1 and t[1].exportable_title or self.all_books_title or "all-books",
             self.extension)
         return self.clipping_dir .. "/" .. getSafeFilename(filename)
     end
