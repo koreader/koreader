4136f97256ce0e864b57765222fc10d426fb28a4
hius07 <62179190+hius07@users.noreply.github.com>
2023-04-15 09:13:25 +0300

    coverbrowser covermenu: add custom covers

--- plugins/coverbrowser.koplugin/covermenu.lua
+++ plugins/coverbrowser.koplugin/covermenu.lua
@@ -339,8 +339,11 @@ function CoverMenu:updateItems(select_number)
 
                 -- Replace the "Book information" button callback to use directly our bookinfo
                 button = self.file_dialog.button_table:getButtonById("book_information")
+                local function refresh_cached_book_info()
+                    BookInfoManager:deleteBookInfo(file)
+                end
                 button.callback = function()
-                    FileManagerBookInfo:show(file, bookinfo)
+                    FileManagerBookInfo:show(file, bookinfo, refresh_cached_book_info)
                     UIManager:close(self.file_dialog)
                 end
 
@@ -464,10 +467,14 @@ function CoverMenu:onHistoryMenuHold(item)
         end
     end
 
-    -- Replace Book information callback to use directly our bookinfo
+    -- Replace the "Book information" button callback to use directly our bookinfo
     button = self.histfile_dialog.button_table:getButtonById("book_information")
+    local function refresh_cached_book_info()
+        BookInfoManager:deleteBookInfo(file)
+        self:updateItems()
+    end
     button.callback = function()
-        FileManagerBookInfo:show(file, bookinfo)
+        FileManagerBookInfo:show(file, bookinfo, refresh_cached_book_info)
         UIManager:close(self.histfile_dialog)
     end
 
@@ -584,10 +591,14 @@ function CoverMenu:onCollectionsMenuHold(item)
         end
     end
 
-    -- Replace Book information callback to use directly our bookinfo
+    -- Replace the "Book information" button callback to use directly our bookinfo
     button = self.collfile_dialog.button_table:getButtonById("book_information")
+    local function refresh_cached_book_info()
+        BookInfoManager:deleteBookInfo(file)
+        self:updateItems()
+    end
     button.callback = function()
-        FileManagerBookInfo:show(file, bookinfo)
+        FileManagerBookInfo:show(file, bookinfo, refresh_cached_book_info)
         UIManager:close(self.collfile_dialog)
     end
 
