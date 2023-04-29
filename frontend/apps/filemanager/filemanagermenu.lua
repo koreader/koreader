koreader/koreader

commit 4d26650ad6bb3670e48b82aa04267eb6a4bfbd3d

Filesearcher: add search in book metadata (#10198)

frontend/apps/filemanager/filemanagermenu.lua
@@ -396,7 +396,7 @@ To:
                     UIManager:show(items)
                 end,
             },
-        }
+        },
     }
 
     for _, widget in pairs(self.registered_widgets) do
@@ -406,19 +406,48 @@ To:
         end
     end
 
-    self.menu_items.sort_by = self.ui:getSortingMenuTable()
+    self.menu_items.sort_by = self:getSortingMenuTable()
     self.menu_items.reverse_sorting = {
         text = _("Reverse sorting"),
-        checked_func = function() return self.ui.file_chooser.reverse_collate end,
-        callback = function() self.ui:toggleReverseCollate() end
+        checked_func = function()
+            return G_reader_settings:isTrue("reverse_collate")
+        end,
+        callback = function()
+            G_reader_settings:flipNilOrFalse("reverse_collate")
+            self.ui.file_chooser:refreshPath()
+        end,
     }
-    self.menu_items.start_with = self.ui:getStartWithMenuTable()
+    self.menu_items.sort_mixed = {
+        text = _("Folders and files mixed"),
+        enabled_func = function()
+            local collate = G_reader_settings:readSetting("collate")
+            return collate ~= "size" and
+                   collate ~= "type" and
+                   collate ~= "percent_unopened_first" and
+                   collate ~= "percent_unopened_last"
+        end,
+        checked_func = function()
+            local collate = G_reader_settings:readSetting("collate")
+            return G_reader_settings:isTrue("collate_mixed") and
+                   collate ~= "size" and
+                   collate ~= "type" and
+                   collate ~= "percent_unopened_first" and
+                   collate ~= "percent_unopened_last"
+        end,
+        callback = function()
+            G_reader_settings:flipNilOrFalse("collate_mixed")
+            self.ui.file_chooser:refreshPath()
+        end,
+    }
+    self.menu_items.start_with = self:getStartWithMenuTable()
+
     if Device:supportsScreensaver() then
         self.menu_items.screensaver = {
             text = _("Screensaver"),
             sub_item_table = require("ui/elements/screensaver_menu"),
         }
     end
+
     -- insert common settings
     for id, common_setting in pairs(dofile("frontend/ui/elements/common_settings_menu_table.lua")) do
         self.menu_items[id] = common_setting
@@ -501,7 +530,7 @@ To:
                     end
                 end,
             },
-        }
+        },
     }
     if Device:isKobo() and not Device:isSunxi() then
         table.insert(self.menu_items.developer_options.sub_item_table, {
@@ -665,8 +694,8 @@ To:
                     G_reader_settings:flipNilOrFalse("dev_reverse_ui_text_direction")
                     UIManager:askForRestart()
                 end
-            }
-        }
+            },
+        },
     })
     table.insert(self.menu_items.developer_options.sub_item_table, {
         text_func = function()
@@ -799,6 +828,77 @@ dbg:guard(FileManagerMenu, 'setUpdateItemTable',
             widget:addToMainMenu(mock_menu_items)
         end
     end)
+
+function FileManagerMenu:getSortingMenuTable()
+    local collates = {
+        { _("name"), "strcoll" },
+        { _("name (natural sorting)"), "natural" },
+        { _("last read date"), "access" },
+        { _("date added"), "change" },
+        { _("date modified"), "modification" },
+        { _("size"), "size" },
+        { _("type"), "type" },
+        { _("percent – unopened first"), "percent_unopened_first" },
+        { _("percent – unopened last"), "percent_unopened_last" },
+    }
+    local sub_item_table = {}
+    for i, v in ipairs(collates) do
+        table.insert(sub_item_table, {
+            text = v[1],
+            checked_func = function()
+                return v[2] == G_reader_settings:readSetting("collate", "strcoll")
+            end,
+            callback = function()
+                G_reader_settings:saveSetting("collate", v[2])
+                self.ui.file_chooser:refreshPath()
+            end,
+        })
+    end
+    return {
+        text_func = function()
+            local collate = G_reader_settings:readSetting("collate")
+            for i, v in ipairs(collates) do
+                if v[2] == collate then
+                    return T(_("Sort by: %1"), v[1])
+                end
+            end
+        end,
+        sub_item_table = sub_item_table,
+    }
+end
+
+function FileManagerMenu:getStartWithMenuTable()
+    local start_withs = {
+        { _("file browser"), "filemanager" },
+        { _("history"), "history" },
+        { _("favorites"), "favorites" },
+        { _("folder shortcuts"), "folder_shortcuts" },
+        { _("last file"), "last" },
+    }
+    local sub_item_table = {}
+    for i, v in ipairs(start_withs) do
+        table.insert(sub_item_table, {
+            text = v[1],
+            checked_func = function()
+                return v[2] == G_reader_settings:readSetting("start_with", "filemanager")
+            end,
+            callback = function()
+                G_reader_settings:saveSetting("start_with", v[2])
+            end,
+        })
+    end
+    return {
+        text_func = function()
+            local start_with = G_reader_settings:readSetting("start_with")
+            for i, v in ipairs(start_withs) do
+                if v[2] == start_with then
+                    return T(_("Start with: %1"), v[1])
+                end
+            end
+        end,
+        sub_item_table = sub_item_table,
+    }
+end
 
 function FileManagerMenu:moveBookMetadata()
     local DocSettings = require("docsettings")

frontend/apps/filemanager/filemanager.lua
@@ -134,15 +134,12 @@ function FileManager:setupLayout()
         -- remember to adjust the height when new item is added to the group
         path = self.root_path,
         focused_path = self.focused_file,
-        collate = G_reader_settings:readSetting("collate") or "strcoll",
-        reverse_collate = G_reader_settings:isTrue("reverse_collate"),
         show_parent = self.show_parent,
-        show_hidden = show_hidden,
         width = Screen:getWidth(),
         height = Screen:getHeight() - self.title_bar:getHeight(),
         is_popout = false,
         is_borderless = true,
-        has_close_button = true,
+        show_hidden = show_hidden,
         show_unsupported = show_unsupported,
         file_filter = function(filename)
             if DocumentRegistry:hasProvider(filename) then
@@ -793,16 +790,6 @@ function FileManager:toggleUnsupportedFiles()
     G_reader_settings:saveSetting("show_unsupported", self.file_chooser.show_unsupported)
 end
 
-function FileManager:setCollate(collate)
-    self.file_chooser:setCollate(collate)
-    G_reader_settings:saveSetting("collate", self.file_chooser.collate)
-end
-
-function FileManager:toggleReverseCollate()
-    self.file_chooser:toggleReverseCollate()
-    G_reader_settings:saveSetting("reverse_collate", self.file_chooser.reverse_collate)
-end
-
 function FileManager:onClose()
     logger.dbg("close filemanager")
     PluginLoader:finalize()
@@ -876,7 +863,6 @@ function FileManager:openRandomFile(dir)
                 self:openRandomFile(dir)
             end,
         })
-        UIManager:close(self.file_dialog)
     else
         UIManager:show(InfoMessage:new {
             text = _("File not found"),
@@ -1136,106 +1122,6 @@ function FileManager:renameFile(file, basename, is_file)
     else
         doRenameFile()
     end
-end
-
-function FileManager:getSortingMenuTable()
-    local fm = self
-    local collates = {
-        strcoll = {_("filename"), _("Sort by filename")},
-        natural = {_("natural"), _("Sort by filename (natural sorting)")},
-        strcoll_mixed = {_("name mixed"), _("Sort by name – mixed files and folders")},
-        access = {_("date read"), _("Sort by last read date")},
-        change = {_("date added"), _("Sort by date added")},
-        modification = {_("date modified"), _("Sort by date modified")},
-        size = {_("size"), _("Sort by size")},
-        type = {_("type"), _("Sort by type")},
-        percent_unopened_first = {_("percent – unopened first"), _("Sort by percent – unopened first")},
-        percent_unopened_last = {_("percent – unopened last"), _("Sort by percent – unopened last")},
-    }
-    local set_collate_table = function(collate)
-        return {
-            text = collates[collate][2],
-            checked_func = function()
-                return fm.file_chooser.collate == collate
-            end,
-            callback = function() fm:setCollate(collate) end,
-        }
-    end
-    local get_collate_percent = function()
-        local collate_type = G_reader_settings:readSetting("collate")
-        if collate_type == "percent_unopened_first" or collate_type == "percent_unopened_last" then
-            return collates[collate_type][2]
-        else
-            return _("Sort by percent")
-        end
-    end
-    return {
-        text_func = function()
-            return T(
-                _("Sort by: %1"),
-                collates[fm.file_chooser.collate][1]
-            )
-        end,
-        sub_item_table = {
-            set_collate_table("strcoll"),
-            set_collate_table("natural"),
-            set_collate_table("strcoll_mixed"),
-            set_collate_table("access"),
-            set_collate_table("change"),
-            set_collate_table("modification"),
-            set_collate_table("size"),
-            set_collate_table("type"),
-            {
-                text_func =  get_collate_percent,
-                checked_func = function()
-                    return fm.file_chooser.collate == "percent_unopened_first"
-                        or fm.file_chooser.collate == "percent_unopened_last"
-                end,
-                sub_item_table = {
-                    set_collate_table("percent_unopened_first"),
-                    set_collate_table("percent_unopened_last"),
-                }
-            },
-        }
-    }
-end
-
-function FileManager:getStartWithMenuTable()
-    local start_with_setting = G_reader_settings:readSetting("start_with") or "filemanager"
-    local start_withs = {
-        filemanager = {_("file browser"), _("Start with file browser")},
-        history = {_("history"), _("Start with history")},
-        favorites = {_("favorites"), _("Start with favorites")},
-        folder_shortcuts = {_("folder shortcuts"), _("Start with folder shortcuts")},
-        last = {_("last file"), _("Start with last file")},
-    }
-    local set_sw_table = function(start_with)
-        return {
-            text = start_withs[start_with][2],
-            checked_func = function()
-                return start_with_setting == start_with
-            end,
-            callback = function()
-                start_with_setting = start_with
-                G_reader_settings:saveSetting("start_with", start_with)
-            end,
-        }
-    end
-    return {
-        text_func = function()
-            return T(
-                _("Start with: %1"),
-                start_withs[start_with_setting][1]
-            )
-        end,
-        sub_item_table = {
-            set_sw_table("filemanager"),
-            set_sw_table("history"),
-            set_sw_table("favorites"),
-            set_sw_table("folder_shortcuts"),
-            set_sw_table("last"),
-        }
-    }
 end
 
 --- @note: This is the *only* safe way to instantiate a new FileManager instance!

frontend/apps/filemanager/filemanagerbookinfo.lua
@@ -157,7 +157,7 @@ function BookInfo:show(file, book_props)
     UIManager:show(widget)
 end
 
-function BookInfo:getBookProps(file, book_props)
+function BookInfo:getBookProps(file, book_props, no_open_document)
     if DocSettings:hasSidecarFile(file) then
         local doc_settings = DocSettings:open(file)
         if not book_props then
@@ -185,7 +185,7 @@ function BookInfo:getBookProps(file, book_props)
     end
 
     -- If still no book_props (book never opened or empty "stats"), open the document to get them
-    if not book_props then
+    if not book_props and not no_open_document then
         local document = DocumentRegistry:openDocument(file)
         if document then
             local loaded = true

commit ff38dd96028c4effcbd49217c06c8e2dbed3d434

[fix] Reintroduce "Start with" default to prevent nil string on startup
Regression from #10198.

Fixes #10368.

frontend/apps/filemanager/filemanagermenu.lua
@@ -889,7 +889,7 @@ function FileManagerMenu:getStartWithMenuTable()
     end
     return {
         text_func = function()
-            local start_with = G_reader_settings:readSetting("start_with")
+            local start_with = G_reader_settings:readSetting("start_with") or "filemanager"
             for i, v in ipairs(start_withs) do
                 if v[2] == start_with then
                     return T(_("Start with: %1"), v[1])