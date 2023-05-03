bed5782e78827fd64f46d05731a8916a94cfb653
yparitcher <y@paritcher.com>
2023-05-03 10:24:15 -0400

    Update frontend/ui/elements/common_settings_menu_table.lua
    Co-authored-by: Frans de Jonge <fransdejonge@gmail.com>

--- frontend/ui/elements/common_settings_menu_table.lua
+++ frontend/ui/elements/common_settings_menu_table.lua
@@ -241,7 +241,7 @@ if Device:isKindle() and PowerD:hasHallSensor() then
     common_settings.cover_events = {
         text = _("Disable Kindle cover events"),
         help_text = _([[Toggle the Hall effect sensor.
-This is used to detect if the cover is closed to auto sleep and wake the device. If there is no cover present the sensor may cause spurious wakeups when located next to a magnetic source.]]),
+This is used to detect if the cover is closed, which will automatically sleep and wake the device. If there is no cover present the sensor may cause spurious wakeups when located next to a magnetic source.]]),
         keep_menu_open = true,
         checked_func = function() return not PowerD:isHallSensorEnabled() end,
         callback = function() PowerD:onToggleHallSensor() end,
