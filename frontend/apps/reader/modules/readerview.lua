0a8058a9f5181c19b5f56c5cec39d23d191dddc3
hius07 <62179190+hius07@users.noreply.github.com>
2023-05-07 16:25:12 +0300

    Size.line.medium instead of 2

--- frontend/apps/reader/modules/readerview.lua
+++ frontend/apps/reader/modules/readerview.lua
@@ -213,7 +213,7 @@ function ReaderView:paintTo(bb, x, y)
             self.arrow:paintTo(bb, 0, self.dim_area.y == 0 and self.dim_area.h - center_offset or self.dim_area.y - center_offset)
         elseif self.page_overlap_style == "line" then
             bb:paintRect(0, self.dim_area.y == 0 and self.dim_area.h or self.dim_area.y,
-                self.dim_area.w, 2, Blitbuffer.COLOR_BLACK)
+                self.dim_area.w, Size.line.medium, Blitbuffer.COLOR_BLACK)
         end
     end
     -- draw saved highlight
@@ -619,19 +619,19 @@ function ReaderView:drawHighlightRect(bb, _x, _y, rect, drawer, draw_note_mark)
     if drawer == "lighten" then
         bb:lightenRect(x, y, w, h, self.highlight.lighten_factor)
     elseif drawer == "underscore" then
-        bb:paintRect(x, y + h - 1, w, 2, Blitbuffer.COLOR_GRAY)
+        bb:paintRect(x, y + h - 1, w, Size.line.medium, Blitbuffer.COLOR_GRAY)
     elseif drawer == "strikeout" then
         local line_y = y + math.floor(h / 2) + 1
         if self.ui.paging then
             line_y = line_y + 2
         end
-        bb:paintRect(x, line_y, w, 2, Blitbuffer.COLOR_BLACK)
+        bb:paintRect(x, line_y, w, Size.line.medium, Blitbuffer.COLOR_BLACK)
     elseif drawer == "invert" then
         bb:invertRect(x, y, w, h)
     end
     if draw_note_mark then
         if self.highlight.note_mark == "underline" then
-            bb:paintRect(x, y + h - 1, w, 2, Blitbuffer.COLOR_BLACK)
+            bb:paintRect(x, y + h - 1, w, Size.line.medium, Blitbuffer.COLOR_BLACK)
         else
             local note_mark_pos_x
             if self.ui.paging or
