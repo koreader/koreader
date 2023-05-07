46f0bba74b789d6269964b6487c96a7f2935340e
Frans de Jonge <fransdejonge@gmail.com>
2023-05-07 18:36:28 +0200

    ReaderLink: fix capitalization typo in "Go Forward" (#10399)

--- frontend/apps/reader/modules/readerlink.lua
+++ frontend/apps/reader/modules/readerlink.lua
@@ -308,12 +308,12 @@ function ReaderLink:addToMainMenu(menu_items)
         end,
     }
     menu_items.go_to_next_location = {
-        text = _("Go Forward to next location"),
+        text = _("Go forward to next location"),
         enabled_func = function() return self.forward_location_stack and #self.forward_location_stack > 0 end,
         callback = function() self:onGoForwardLink() end,
         hold_callback = function(touchmenu_instance)
             UIManager:show(ConfirmBox:new{
-                text = _("Clear Forward location history?"),
+                text = _("Clear forward location history?"),
                 ok_text = _("Clear"),
                 ok_callback = function()
                     self:onClearForwardLocationStack()
