ab6ba64605e913a48e0edf91304648f22865036f
zwim <martin.zwicknagl@kirchbichl.net>
2023-05-11 11:42:41 +0200

    clean up

--- doc/events.lua
+++ doc/events.lua
@@ -1,35 +1,5 @@
 -- You can call me with something like `ldoc --filter events.filter .`
 
-local function output_source(events_and_handlers, name_len, module_len)
-    io.write("```\n") -- start code tag
-
-    -- Write header line.
-    io.write("Event/Handler" .. string.rep(" ", name_len - #"Event/Handler") .. "   " ..
-        "Module Name" .. string.rep(" ", module_len - #"Module Name") .. "   " ..
-        "Line#" .. "   " ..
-        "File" .. "\n\n")
-
-    local event_plus_handler_nb = 0
-    -- Write events and handlers.
-    for _,v in pairs(events_and_handlers) do
-        local name = v.name
-        if name then
-            if name:sub(1, 2) ~= "on" then
-                name = "  " .. name
-            end
-            io.write(name .. string.rep(" ", name_len - #name) .. "   " ..
-                v.module_name .. string.rep(" ", module_len - #v.module_name) .. "   " ..
-                string.format("%5d", v.lineno) .. "   " ..
-                v.module_file .. "\n")
-            event_plus_handler_nb = event_plus_handler_nb + 1
-        else
-            io.write("\n")
-        end
-    end
-    io.write("```\n") -- end code tag
-    return event_plus_handler_nb
-end
-
 local function output_table(events_and_handlers, name_len, module_len)
     local empty_table_line = "<tr><td bgcolor=\"#FFFFFF\" style=\"line-height:0.8;\" colspan=3>&nbsp;</td></tr>\n"
 
@@ -41,7 +11,7 @@ local function output_table(events_and_handlers, name_len, module_len)
     local function make_source_link(path, lineno)
         -- Turn the symbolic link from `/frontend/plugins` to `/plugins/`.
         path = path:gsub("/frontend/plugins/","/plugins/")
-        local text = path:gsub("^.*koreader", "koreader")
+        local text = path:gsub("^.*koreader/", "")
         local link = path:gsub("^.*koreader", "https://github.com/koreader/koreader/tree/master/") .. "#L" .. lineno
         return "<a href=\""..link.."\">" .. text .. "</a>"
     end
@@ -72,8 +42,8 @@ local function output_table(events_and_handlers, name_len, module_len)
             elseif name:sub(1, 2) == "on" then
                 name = "&nbsp;" .. name
             end
-            name = name:gsub("[_]+_key_event", "&nbsp;&nbsp; key_event")
-            name = name:gsub("[_]+_ges_event", "&nbsp;&nbsp; gesture_event")
+            name = name:gsub("[_]+_key_event", "&nbsp;&nbsp; (key_event)")
+            name = name:gsub("[_]+_ges_event", "&nbsp;&nbsp; (gesture_event)")
             io.write("<tr>")
             io.write("<td><code>" .. name .. string.rep(" ", name_len - #name) .. "&nbsp;</code></td>")
             io.write("<td>" .. make_modules_link(v.module_name) .. "&nbsp;</td>")
@@ -132,25 +102,21 @@ return {
             return text
         end
 
+        local function get_base_name(name)
+            if name:sub(1, 2) == "on" then
+                name = name:sub(3)
+            elseif name:sub(1, 3) == "_on" then
+                name = name:sub(4)
+            else
+                name = name
+            end
+            name = drop_key_and_ges_event(name)
+            return name
+        end
+
         local function sort_events_and_handlers(a, b)
-            local a_name
-            local b_name
-            if a.name:sub(1, 2) == "on" then
-                a_name = a.name:sub(3)
-            elseif a.name:sub(1, 3) == "_on" then
-                a_name = a.name:sub(4)
-            else
-                a_name = a.name
-            end
-            if b.name:sub(1, 2) == "on" then
-                b_name = b.name:sub(3)
-            elseif b.name:sub(1, 3) == "_on" then
-                b_name = b.name:sub(4)
-            else
-                b_name = b.name
-            end
-            a_name = drop_key_and_ges_event(a_name)
-            b_name = drop_key_and_ges_event(b_name)
+            local a_name = get_base_name(a.name)
+            local b_name = get_base_name(b.name)
 
             if a_name < b_name then
                 return true
@@ -168,12 +134,9 @@ return {
                 else
                     return a_name < b_name
                 end
-
             end
         end
-        local function sort_events_and_handlers1(a,b)
-            return a.name < b.name
-        end
+
         table.sort(events_and_handlers, sort_events_and_handlers)
 
         local name_len=15
