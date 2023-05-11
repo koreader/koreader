79026267972eed26816b83b9fd70d772a0c2e76b
zwim <martin.zwicknagl@kirchbichl.net>
2023-05-11 12:07:41 +0200

    one step more on ges/key_events
    remove quirks

--- doc/events_helper/document_events.sh
+++ doc/events_helper/document_events.sh
@@ -2,27 +2,12 @@
 
 # usage: bash document_events.sh [0|1] path1, path2, path3 ...
 
-if [[ "$1" == "0" ]]; then
-    FILENAME=undocumentedEvents.txt
-    rm -f ${FILENAME}
-elif [[ "$1" == "1" ]]; then
-    FILENAME=documentedEvents.txt
-    rm -f ${FILENAME}
-elif [[ "$1" == "2" ]]; then
-    FILENAME="/dev/null"
-else
-    echo "usage: bash documented_events.sh [0|1|2] path1, path2, path3 ..."
-    echo "    0 find undocumented; 1 find documented; 2 document undocumented"
+if [[ "$?" == "1" ]]; then
+    echo "usage: bash documented_events.sh path1, path2, path3 ..."
     exit 1
 fi
 
-MODE=$1
 LUA_SCRIPT=${0//\.sh/\.lua}
 
-shift 1
-find -L "$@" -maxdepth 50 -name "*.lua" -exec lua "${LUA_SCRIPT}" "${MODE}" {} \; >>${FILENAME}
-
-if [[ "${FILENAME}" =~ "/dev/null" ]]; then
-    echo results can be found in ${FILENAME}
-fi
+find -L "$@" -maxdepth 50 -name "*.lua" -exec lua "${LUA_SCRIPT}" {} \;
 
