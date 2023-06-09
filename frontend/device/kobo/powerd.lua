koreader/koreader

commit 7e98b9de4b5917a2568779d7281cfc67e770c73d

PM: Minor refactor to suspend/resume code flow (#10426)
Make sure we only send Suspend/Resume events when we *actually* suspend/resume. This is done via the Device `_beforeSuspend`/`_afterResume` methods, and those were called by the *input handlers*, not the PM logic; which means they would fire, while the PM logic could actually take a smarter decision and *not* do what the event just sent implied ;).

(i.e., sleep with a cover -> suspend + actual suspend, OK; but if you then resume with a button -> input assumes resume, but PM will actually suspend again!).

Existing design issue made more apparent by #9448 ;).

Also fixes/generalizes a few corner-cases related to screen_saver_lock handling (e.g., don't allow USBMS during a lock).

And deal with the fallout of the main change to the Kobo frontlight ramp behavior ;).

frontend/device/kobo/powerd.lua
@@ -1,8 +1,8 @@
-local UIManager = nil -- will be updated when available
 local BasePowerD = require("device/generic/powerd")
 local Math = require("optmath")
 local NickelConf = require("device/kobo/nickel_conf")
 local SysfsLight = require ("device/sysfs_light")
+local UIManager
 local RTC = require("ffi/rtc")
 
 -- Here, we only deal with the real hw intensity.
@@ -415,26 +415,44 @@ end
 
 -- Turn off front light before suspend.
 function KoboPowerD:beforeSuspend()
-    if self.fl == nil then return end
-    -- Remember the current frontlight state
-    self.fl_was_on = self.is_fl_on
-    -- Turn off the frontlight
-    self:turnOffFrontlight()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+
+    -- Handle the frontlight last,
+    -- to prevent as many things as we can from interfering with the smoothness of the ramp
+    if self.fl then
+        -- Remember the current frontlight state
+        self.fl_was_on = self.is_fl_on
+        -- Turn off the frontlight
+        -- NOTE: Funky delay mainly to yield to the EPDC's refresh on UP systems.
+        --       (Neither yieldToEPDC nor nextTick & friends quite cut it here)...
+        UIManager:scheduleIn(0.001, self.turnOffFrontlight, self)
+    end
 end
 
 -- Restore front light state after resume.
 function KoboPowerD:afterResume()
-    if self.fl == nil then return end
-    -- Don't bother if the light was already off on suspend
-    if not self.fl_was_on then return end
-    -- Turn the frontlight back on
-    self:turnOnFrontlight()
-
     -- Set the system clock to the hardware clock's time.
     RTC:HCToSys()
+
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+
+    -- There's a whole bunch of stuff happening before us in Generic:onPowerEvent,
+    -- so we'll delay this ever so slightly so as to appear as smooth as possible...
+    if self.fl then
+        -- Don't bother if the light was already off on suspend
+        if self.fl_was_on then
+            -- Turn the frontlight back on
+            -- NOTE: There's quite likely *more* resource contention than on suspend here :/.
+            UIManager:scheduleIn(0.001, self.turnOnFrontlight, self)
+        end
+    end
 end
 
-function KoboPowerD:readyUIHW(uimgr)
+function KoboPowerD:UIManagerReadyHW(uimgr)
     UIManager = uimgr
 end
 

frontend/device/android/powerd.lua
@@ -1,4 +1,6 @@
 local BasePowerD = require("device/generic/powerd")
+local Event = require("ui/event")
+local UIManager
 local _, android = pcall(require, "android")
 
 local AndroidPowerD = BasePowerD:new{
@@ -8,9 +10,7 @@ local AndroidPowerD = BasePowerD:new{
 
 -- Let the footer know of the change
 local function broadcastLightChanges()
-    if package.loaded["ui/uimanager"] ~= nil then
-        local Event = require("ui/event")
-        local UIManager = require("ui/uimanager")
+    if UIManager then
         UIManager:broadcastEvent(Event:new("FrontlightStateChanged"))
     end
 end
@@ -81,6 +81,10 @@ function AndroidPowerD:turnOnFrontlightHW()
 
     self.is_fl_on = true
     broadcastLightChanges()
+end
+
+function AndroidPowerD:UIManagerReadyHW(uimgr)
+    UIManager = uimgr
 end
 
 return AndroidPowerD

frontend/device/cervantes/powerd.lua
@@ -132,19 +132,29 @@ function CervantesPowerD:isChargingHW()
 end
 
 function CervantesPowerD:beforeSuspend()
-    if self.fl == nil then return end
-    -- just turn off frontlight without remembering its state
-    self.fl:setBrightness(0)
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+
+    if self.fl then
+        -- just turn off frontlight without remembering its state
+        self.fl:setBrightness(0)
+    end
 end
 
 function CervantesPowerD:afterResume()
-    if self.fl == nil then return end
-    -- just re-set it to self.hw_intensity that we haven't change on Suspend
-    if not self.device:hasNaturalLight() then
-        self.fl:setBrightness(self.hw_intensity)
-    else
-        self.fl:setNaturalBrightness(self.hw_intensity, self.fl_warmth)
+    if self.fl then
+        -- just re-set it to self.hw_intensity that we haven't changed on Suspend
+        if not self.device:hasNaturalLight() then
+            self.fl:setBrightness(self.hw_intensity)
+        else
+            self.fl:setNaturalBrightness(self.hw_intensity, self.fl_warmth)
+        end
     end
+
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
 end
 
 return CervantesPowerD

frontend/device/generic/powerd.lua
@@ -1,5 +1,6 @@
-local UIManager = nil -- will be updated when available
+local Event = require("ui/event")
 local Math = require("optmath")
+local UIManager
 local logger = require("logger")
 local time = require("ui/time")
 local BasePowerD = {
@@ -41,11 +42,6 @@ function BasePowerD:new(o)
     return o
 end
 
-function BasePowerD:readyUI()
-    UIManager = require("ui/uimanager")
-    self:readyUIHW(UIManager)
-end
-
 function BasePowerD:init() end
 function BasePowerD:setIntensityHW(intensity) end
 --- @note: Unlike the "public" setWarmth, this one takes a value in the *native* scale!
@@ -66,13 +62,30 @@ function BasePowerD:isFrontlightOnHW() return self.fl_intensity > self.fl_min en
 function BasePowerD:turnOffFrontlightHW(done_callback) self:setIntensityHW(self.fl_min) end
 function BasePowerD:turnOnFrontlightHW(done_callback) self:setIntensityHW(self.fl_intensity) end --- @fixme: what if fl_intensity == fl_min (c.f., kindle)?
 function BasePowerD:frontlightWarmthHW() return 0 end
-function BasePowerD:readyUIHW(uimgr) end
 -- Anything that needs to be done before doing a real hardware suspend.
 -- (Such as turning the front light off).
-function BasePowerD:beforeSuspend() end
+-- Do *not* omit calling Device's _beforeSuspend method! This default implementation passes `false` so as *not* to disable input events during PM.
+function BasePowerD:beforeSuspend() self.device:_beforeSuspend(false) end
 -- Anything that needs to be done after doing a real hardware resume.
 -- (Such as restoring front light state).
-function BasePowerD:afterResume() end
+-- Do *not* omit calling Device's _afterResume method!
+function BasePowerD:afterResume()
+    -- MONOTONIC doesn't tick during suspend,
+    -- invalidate the last battery capacity pull time so that we get up to date data immediately.
+    self:invalidateCapacityCache()
+
+    self.device:_afterResume(false)
+end
+
+-- Update our UIManager reference once it's ready
+function BasePowerD:UIManagerReady(uimgr)
+    -- Our own ref
+    UIManager = uimgr
+    -- Let implementations do the same thing, too
+    self:UIManagerReadyHW(uimgr)
+end
+-- Ditto, but for implementations
+function BasePowerD:UIManagerReadyHW(uimgr) end
 
 function BasePowerD:isFrontlightOn()
     return self.is_fl_on
@@ -275,7 +288,6 @@ end
 function BasePowerD:stateChanged()
     -- BasePowerD is loaded before UIManager. So we cannot broadcast events before UIManager has been loaded.
     if UIManager then
-        local Event = require("ui/event")
         UIManager:broadcastEvent(Event:new("FrontlightStateChanged"))
     end
 end

frontend/device/kindle/powerd.lua
@@ -1,4 +1,5 @@
 local BasePowerD = require("device/generic/powerd")
+local UIManager
 local WakeupMgr = require("device/wakeupmgr")
 local logger = require("logger")
 local util = require("util")
@@ -185,7 +186,6 @@ function KindlePowerD:afterResume()
     if not self.device:hasFrontlight() then
         return
     end
-    local UIManager = require("ui/uimanager")
     if self:isFrontlightOn() then
         -- The Kindle framework should turn the front light back on automatically.
         -- The following statement ensures consistency of intensity, but should basically always be redundant,
@@ -249,7 +249,6 @@ function KindlePowerD:initWakeupMgr()
         -- This filters out user input resumes -> device will resume to active
         -- Also the Kindle stays in Ready to suspend for 10 seconds
         -- so the alarm may fire 10 seconds early
-        local UIManager = require("ui/uimanager")
         UIManager:scheduleIn(15, self.checkUnexpectedWakeup, self)
     end
 
@@ -282,6 +281,22 @@ function KindlePowerD:resetT1Timeout()
     else
         os.execute("lipc-set-prop -i com.lab126.powerd touchScreenSaverTimeout 1")
     end
+end
+
+function KindlePowerD:beforeSuspend()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+end
+
+function KindlePowerD:afterResume()
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+end
+
+function KindlePowerD:UIManagerReadyHW(uimgr)
+    UIManager = uimgr
 end
 
 --- @fixme: This won't ever fire on its own, as KindlePowerD is already a metatable on a plain table.

frontend/device/pocketbook/powerd.lua
@@ -73,4 +73,16 @@ function PocketBookPowerD:isChargingHW()
     end
 end
 
+function PocketBookPowerD:beforeSuspend()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+end
+
+function PocketBookPowerD:afterResume()
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+end
+
 return PocketBookPowerD

frontend/device/remarkable/powerd.lua
@@ -22,4 +22,16 @@ function Remarkable_PowerD:isChargingHW()
     return self:read_str_file(self.status_file) == "Charging"
 end
 
+function Remarkable_PowerD:beforeSuspend()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+end
+
+function Remarkable_PowerD:afterResume()
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+end
+
 return Remarkable_PowerD

frontend/device/sdl/powerd.lua
@@ -37,4 +37,16 @@ function SDLPowerD:isChargingHW()
     return false
 end
 
+function SDLPowerD:beforeSuspend()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+end
+
+function SDLPowerD:afterResume()
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+end
+
 return SDLPowerD

commit 1ce1fe1780fbeeeddf2d8e7caa14393ebb0ce6cd

[plugin] AutoWarmth: Fix resume and frontlight issue (#10557)
Fixes a regression introduced in #10426, when suspending during night and resuming during daylight.

frontend/device/kobo/powerd.lua
@@ -403,9 +403,8 @@ function KoboPowerD:turnOnFrontlightHW(done_callback)
         -- We've got nothing to do if we're already ramping up
         if not self.fl_ramp_up_running then
             self:_stopFrontlightRamp()
-            self.fl_ramp_up_running = true
-
             self:turnOnFrontlightRamp(self.fl_min, self.fl_intensity, done_callback)
+            self.fl_ramp_up_running = true
         end
     else
         -- If UIManager is not initialized yet, just turn it on immediately

plugins/autowarmth.koplugin/main.lua
@@ -142,6 +142,11 @@ function AutoWarmth:_onResume()
 
     local resume_date = os.date("*t")
 
+    if self.fl_off_during_day then
+    -- We handle the frontlight state ourself.
+        Powerd.fl_was_on = false
+    end
+
     -- check if resume and suspend are done on the same day
     if resume_date.day == SunTime.date.day and resume_date.month == SunTime.date.month
         and resume_date.year == SunTime.date.year then
@@ -163,6 +168,7 @@ function AutoWarmth:_onSuspend()
     UIManager:unschedule(self.scheduleMidnightUpdate)
     UIManager:unschedule(self.setWarmth)
     UIManager:unschedule(self.setFrontlight)
+    UIManager:unschedule(self.scheduleNextWarmthChange)
 end
 
 function AutoWarmth:_onToggleNightMode()
@@ -203,8 +209,10 @@ end
 function AutoWarmth:_onToggleFrontlight()
     logger.dbg("AutoWarmth: onToggleFrontlight")
     local now_s = SunTime:getTimeInSec()
-    AutoWarmth.fl_turned_off = now_s >= self.current_times_h[5]*3600 + self.fl_off_during_day_offset_s and
-        now_s < self.current_times_h[7]*3600 - self.fl_off_during_day_offset_s
+    local sunrise_in_s = self.current_times_h[5] * 3600 + self.fl_off_during_day_offset_s - now_s
+    local sunset_in_s = self.current_times_h[7] * 3600 - self.fl_off_during_day_offset_s - now_s
+
+    AutoWarmth.fl_turned_off = sunrise_in_s < 0 and sunset_in_s > 0
 end
 
 function AutoWarmth:setEventHandlers()
@@ -309,8 +317,8 @@ function AutoWarmth:scheduleMidnightUpdate(from_resume)
         self.current_times_h[2] = nil   -- Astronomical dawn
         self.current_times_h[3] = nil   -- Nautical dawn
         self.current_times_h[6] = nil   -- Solar noon
-        self.current_times_h[9] = nil   -- Nautical dust
-        self.current_times_h[10] = nil  -- Astronomical dust
+        self.current_times_h[9] = nil   -- Nautical dusk
+        self.current_times_h[10] = nil  -- Astronomical dusk
         self.current_times_h[11] = nil  -- Solar midnight
     end
 
@@ -362,10 +370,10 @@ function AutoWarmth:scheduleToggleFrontlight(now_s)
     -- Reset user fl toggles at sunset or sunrise with offset, as `scheduleNextWarmthChange` gets called only
     -- on scheduled warmth changes.
     local sunset_in_s = self.current_times_h[7] * 3600 - self.fl_off_during_day_offset_s - now_s
-    if sunset_in_s >= 0 then
+    if sunset_in_s >= 0 then -- first check if we are before sunset
         UIManager:scheduleIn(sunset_in_s, self.setFrontlight, self, true)
         local sunrise_in_s = self.current_times_h[5] * 3600 + self.fl_off_during_day_offset_s - now_s
-        if sunrise_in_s >= 0 then
+        if sunrise_in_s >= 0 then -- second check if we are before sunrise
             UIManager:scheduleIn(sunrise_in_s, self.setFrontlight, self, false)
         end
     end
@@ -385,21 +393,23 @@ function AutoWarmth:setFrontlight(enable)
 end
 
 -- toggles Frontlight on or off, only depending on the time
-function AutoWarmth:forceToggleFrontlight()
+function AutoWarmth:forceToggleFrontlight(now_s)
     if not self.fl_off_during_day then
         return
     end
 
-    local now_s = SunTime:getTimeInSec()
-    local is_fl_on = now_s < self.current_times_h[5] * 3600 + self.fl_off_during_day_offset_s
-                  or now_s > self.current_times_h[7] * 3600 - self.fl_off_during_day_offset_s
+    now_s = now_s or SunTime:getTimeInSec()
+    local sunrise_in_s = self.current_times_h[5] * 3600 + self.fl_off_during_day_offset_s - now_s
+    local sunset_in_s = self.current_times_h[7] * 3600 - self.fl_off_during_day_offset_s - now_s
 
-    self:setFrontlight(is_fl_on)
+    self:setFrontlight(sunrise_in_s < 0 or sunset_in_s > 0)
 end
 
 -- toggles Frontlight on or off, depending on `now_s`
 -- decide with the help of `Autowarmth.fl_turned_off` if the fl should be on or off
 function AutoWarmth:toggleFrontlight(now_s)
+    logger.dbg("AutoWarmth: toggleFrontlight", now_s)
+
     if self.fl_off_during_day then
         if now_s >= self.current_times_h[5]*3600 + self.fl_off_during_day_offset_s
             and now_s < self.current_times_h[7]*3600 - self.fl_off_during_day_offset_s then
@@ -1035,6 +1045,17 @@ function AutoWarmth:getWarmthMenu()
             end,
             enabled_func = function()
                 return Device:hasNaturalLight()
+            end,
+            hold_callback = function()
+                if Device:hasNaturalLight() then
+                    UIManager:show(InfoMessage:new{
+                        text = _("Tapping here chooses between the different AutoWarmth modes: 'warmth only', 'warmth and night mode', 'night mode only'."),
+                    })
+                else
+                    UIManager:show(InfoMessage:new{
+                        text = _("Your device supports 'night mode' control only."),
+                    })
+                end
             end,
             callback = function(touchmenu_instance)
                 if self.control_warmth and self.control_nightmode then