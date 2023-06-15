1d7cc529a3398ef34b7ec564c07b2807cf9c2a53
hius07 <62179190+hius07@users.noreply.github.com>
2023-06-15 18:48:26 +0300

    Update plugins/arcviewer.koplugin/_meta.lua
    Co-authored-by: Frans de Jonge <fransdejonge@gmail.com>

--- plugins/arcviewer.koplugin/_meta.lua
+++ plugins/arcviewer.koplugin/_meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     name = "arcviewer",
     fullname = _("Archive viewer"),
-    description = _([[Archives viewer.]]),
+    description = _([[Enables exploring the content of zip archives.]]),
 }
