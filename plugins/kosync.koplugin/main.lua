5e1e0748dd058742cf567a6be96da5406fb773de
NiLuJe <ninuje@gmail.com>
2023-06-26 19:58:12 +0200

    Don't skip onResume if auto_restore_wifi would not, actually, restore wifi ;o)

--- plugins/kosync.koplugin/main.lua
+++ plugins/kosync.koplugin/main.lua
@@ -862,7 +862,7 @@ function KOSync:_onResume()
     logger.dbg("KOSync: onResume")
     -- If we have auto_restore_wifi enabled, skip this to prevent both the "Connecting..." UI to pop-up,
     -- *and* a duplicate NetworkConnected event from firing...
-    if Device:hasWifiManager() and G_reader_settings:isTrue("auto_restore_wifi") then
+    if Device:hasWifiManager() and NetworkMgr.wifi_was_on and G_reader_settings:isTrue("auto_restore_wifi") then
         return
     end
 
