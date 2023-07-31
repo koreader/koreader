d6365f9966e7d03abc61a38ac4c60638ab3ee691
NiLuJe <ninuje@gmail.com>
2023-07-31 21:37:28 +0200

    Oops, forgot to disable that properly ;p

--- frontend/device/kobo/device.lua
+++ frontend/device/kobo/device.lua
@@ -575,12 +575,7 @@ end
 
 function Kobo:init()
     -- Check if we need to disable MXCFB_WAIT_FOR_UPDATE_COMPLETE ioctls...
-    local mxcfb_bypass_wait_for
-    if G_reader_settings:has("mxcfb_bypass_wait_for") then
-        mxcfb_bypass_wait_for = G_reader_settings:isTrue("mxcfb_bypass_wait_for")
-    else
-        mxcfb_bypass_wait_for = not self:hasReliableMxcWaitFor()
-    end
+    local mxcfb_bypass_wait_for = G_reader_settings:isTrue("mxcfb_bypass_wait_for")
 
     if self:isSunxi() then
         self.screen = require("ffi/framebuffer_sunxi"):new{
