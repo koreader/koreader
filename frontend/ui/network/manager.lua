73c70e2844fbed63711c6e7aaffcfd80c3edfa33
NiLuJe <ninuje@gmail.com>
2023-08-17 11:08:12 +0200

    NetworkManager: Always flip wifi_was_on when interactively tearing down Wi-Fi
    Fix #10823
    
    (For reference, we *enable* wifi_was_on no matter *how* wifi is enabled,
    but we only oggle it off when it' killed by a *direct* user interaction,
    the intent being that if *something* non-interactive enabled wifi,
    you'll probably silently need it on resume too).

--- frontend/ui/network/manager.lua
+++ frontend/ui/network/manager.lua
@@ -266,7 +266,7 @@ function NetworkMgr:enableWifi(wifi_cb, connectivity_cb, connectivity_widget, in
     return true
 end
 
-function NetworkMgr:disableWifi(cb)
+function NetworkMgr:disableWifi(cb, interactive)
     local complete_callback = function()
         UIManager:broadcastEvent(Event:new("NetworkDisconnected"))
         if cb then
@@ -275,6 +275,11 @@ function NetworkMgr:disableWifi(cb)
     end
     UIManager:broadcastEvent(Event:new("NetworkDisconnecting"))
     self:turnOffWifi(complete_callback)
+
+    if interactive then
+        self.wifi_was_on = false
+        G_reader_settings:makeFalse("wifi_was_on")
+    end
 end
 
 function NetworkMgr:toggleWifiOn(complete_callback, long_press, interactive)
@@ -284,8 +289,6 @@ function NetworkMgr:toggleWifiOn(complete_callback, long_press, interactive)
     UIManager:show(toggle_im)
     UIManager:forceRePaint()
 
-    self.wifi_was_on = true
-    G_reader_settings:makeTrue("wifi_was_on")
     self.wifi_toggle_long_press = long_press
 
     self:enableWifi(complete_callback, nil, nil, interactive)
@@ -293,17 +296,14 @@ function NetworkMgr:toggleWifiOn(complete_callback, long_press, interactive)
     UIManager:close(toggle_im)
 end
 
-function NetworkMgr:toggleWifiOff(complete_callback)
+function NetworkMgr:toggleWifiOff(complete_callback, interactive)
     local toggle_im = InfoMessage:new{
         text = _("Turning off Wi-Fi…"),
     }
     UIManager:show(toggle_im)
     UIManager:forceRePaint()
 
-    self.wifi_was_on = false
-    G_reader_settings:makeFalse("wifi_was_on")
-
-    self:disableWifi(complete_callback)
+    self:disableWifi(complete_callback, interactive)
 
     UIManager:close(toggle_im)
 end
