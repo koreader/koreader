10489979a864de24967aba6426bc5307351d7df3
hius07 <62179190+hius07@users.noreply.github.com>
2023-09-04 10:37:20 +0300

    Update clip.lua

--- plugins/exporter.koplugin/clip.lua
+++ plugins/exporter.koplugin/clip.lua
@@ -115,9 +115,10 @@ function MyClipping:parseTitleFromPath(line)
     elseif extensions[line:sub(-5):lower()] then
         line = line:sub(1, -6)
     end
-    local _i, _j, title, author = line:find("(.-)%s*%((.*)%)")
+    local dummy, title, author
+    dummy, dummy, title, author = line:find("(.-)%s*%((.*)%)")
     if not author then
-        _i, _j, title, author = line:find("(.-)%s*-%s*(.*)")
+        dummy, dummy, title, author = line:find("(.-)%s*-%s*(.*)")
     end
     title = title or line:match("^%s*(.-)%s*$")
     return isEmpty(title) and _("Unknown Book") or title,
