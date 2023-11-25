koreader/koreader

commit 7e98b9de4b5917a2568779d7281cfc67e770c73d

PM: Minor refactor to suspend/resume code flow (#10426)
Make sure we only send Suspend/Resume events when we *actually* suspend/resume. This is done via the Device `_beforeSuspend`/`_afterResume` methods, and those were called by the *input handlers*, not the PM logic; which means they would fire, while the PM logic could actually take a smarter decision and *not* do what the event just sent implied ;).

(i.e., sleep with a cover -> suspend + actual suspend, OK; but if you then resume with a button -> input assumes resume, but PM will actually suspend again!).

Existing design issue made more apparent by #9448 ;).

Also fixes/generalizes a few corner-cases related to screen_saver_lock handling (e.g., don't allow USBMS during a lock).

And deal with the fallout of the main change to the Kobo frontlight ramp behavior ;).

frontend/device/android/powerd.lua
@@ -1,4 +1,6 @@
 local BasePowerD = require("device/generic/powerd")
+local Event = require("ui/event")
+local UIManager
 local _, android = pcall(require, "android")
 
 local AndroidPowerD = BasePowerD:new{
@@ -8,9 +10,7 @@ local AndroidPowerD = BasePowerD:new{
 
 -- Let the footer know of the change
 local function broadcastLightChanges()
-    if package.loaded["ui/uimanager"] ~= nil then
-        local Event = require("ui/event")
-        local UIManager = require("ui/uimanager")
+    if UIManager then
         UIManager:broadcastEvent(Event:new("FrontlightStateChanged"))
     end
 end
@@ -81,6 +81,10 @@ function AndroidPowerD:turnOnFrontlightHW()
 
     self.is_fl_on = true
     broadcastLightChanges()
+end
+
+function AndroidPowerD:UIManagerReadyHW(uimgr)
+    UIManager = uimgr
 end
 
 return AndroidPowerD

frontend/device/generic/powerd.lua
@@ -1,5 +1,6 @@
-local UIManager = nil -- will be updated when available
+local Event = require("ui/event")
 local Math = require("optmath")
+local UIManager
 local logger = require("logger")
 local time = require("ui/time")
 local BasePowerD = {
@@ -41,11 +42,6 @@ function BasePowerD:new(o)
     return o
 end
 
-function BasePowerD:readyUI()
-    UIManager = require("ui/uimanager")
-    self:readyUIHW(UIManager)
-end
-
 function BasePowerD:init() end
 function BasePowerD:setIntensityHW(intensity) end
 --- @note: Unlike the "public" setWarmth, this one takes a value in the *native* scale!
@@ -66,13 +62,30 @@ function BasePowerD:isFrontlightOnHW() return self.fl_intensity > self.fl_min en
 function BasePowerD:turnOffFrontlightHW(done_callback) self:setIntensityHW(self.fl_min) end
 function BasePowerD:turnOnFrontlightHW(done_callback) self:setIntensityHW(self.fl_intensity) end --- @fixme: what if fl_intensity == fl_min (c.f., kindle)?
 function BasePowerD:frontlightWarmthHW() return 0 end
-function BasePowerD:readyUIHW(uimgr) end
 -- Anything that needs to be done before doing a real hardware suspend.
 -- (Such as turning the front light off).
-function BasePowerD:beforeSuspend() end
+-- Do *not* omit calling Device's _beforeSuspend method! This default implementation passes `false` so as *not* to disable input events during PM.
+function BasePowerD:beforeSuspend() self.device:_beforeSuspend(false) end
 -- Anything that needs to be done after doing a real hardware resume.
 -- (Such as restoring front light state).
-function BasePowerD:afterResume() end
+-- Do *not* omit calling Device's _afterResume method!
+function BasePowerD:afterResume()
+    -- MONOTONIC doesn't tick during suspend,
+    -- invalidate the last battery capacity pull time so that we get up to date data immediately.
+    self:invalidateCapacityCache()
+
+    self.device:_afterResume(false)
+end
+
+-- Update our UIManager reference once it's ready
+function BasePowerD:UIManagerReady(uimgr)
+    -- Our own ref
+    UIManager = uimgr
+    -- Let implementations do the same thing, too
+    self:UIManagerReadyHW(uimgr)
+end
+-- Ditto, but for implementations
+function BasePowerD:UIManagerReadyHW(uimgr) end
 
 function BasePowerD:isFrontlightOn()
     return self.is_fl_on
@@ -275,7 +288,6 @@ end
 function BasePowerD:stateChanged()
     -- BasePowerD is loaded before UIManager. So we cannot broadcast events before UIManager has been loaded.
     if UIManager then
-        local Event = require("ui/event")
         UIManager:broadcastEvent(Event:new("FrontlightStateChanged"))
     end
 end

frontend/device/kindle/powerd.lua
@@ -1,4 +1,5 @@
 local BasePowerD = require("device/generic/powerd")
+local UIManager
 local WakeupMgr = require("device/wakeupmgr")
 local logger = require("logger")
 local util = require("util")
@@ -185,7 +186,6 @@ function KindlePowerD:afterResume()
     if not self.device:hasFrontlight() then
         return
     end
-    local UIManager = require("ui/uimanager")
     if self:isFrontlightOn() then
         -- The Kindle framework should turn the front light back on automatically.
         -- The following statement ensures consistency of intensity, but should basically always be redundant,
@@ -249,7 +249,6 @@ function KindlePowerD:initWakeupMgr()
         -- This filters out user input resumes -> device will resume to active
         -- Also the Kindle stays in Ready to suspend for 10 seconds
         -- so the alarm may fire 10 seconds early
-        local UIManager = require("ui/uimanager")
         UIManager:scheduleIn(15, self.checkUnexpectedWakeup, self)
     end
 
@@ -282,6 +281,22 @@ function KindlePowerD:resetT1Timeout()
     else
         os.execute("lipc-set-prop -i com.lab126.powerd touchScreenSaverTimeout 1")
     end
+end
+
+function KindlePowerD:beforeSuspend()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+end
+
+function KindlePowerD:afterResume()
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+end
+
+function KindlePowerD:UIManagerReadyHW(uimgr)
+    UIManager = uimgr
 end
 
 --- @fixme: This won't ever fire on its own, as KindlePowerD is already a metatable on a plain table.

frontend/device/pocketbook/powerd.lua
@@ -73,4 +73,16 @@ function PocketBookPowerD:isChargingHW()
     end
 end
 
+function PocketBookPowerD:beforeSuspend()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+end
+
+function PocketBookPowerD:afterResume()
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+end
+
 return PocketBookPowerD

frontend/device/sdl/powerd.lua
@@ -37,4 +37,16 @@ function SDLPowerD:isChargingHW()
     return false
 end
 
+function SDLPowerD:beforeSuspend()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+end
+
+function SDLPowerD:afterResume()
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+end
+
 return SDLPowerD

frontend/device/cervantes/powerd.lua
@@ -132,19 +132,29 @@ function CervantesPowerD:isChargingHW()
 end
 
 function CervantesPowerD:beforeSuspend()
-    if self.fl == nil then return end
-    -- just turn off frontlight without remembering its state
-    self.fl:setBrightness(0)
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+
+    if self.fl then
+        -- just turn off frontlight without remembering its state
+        self.fl:setBrightness(0)
+    end
 end
 
 function CervantesPowerD:afterResume()
-    if self.fl == nil then return end
-    -- just re-set it to self.hw_intensity that we haven't change on Suspend
-    if not self.device:hasNaturalLight() then
-        self.fl:setBrightness(self.hw_intensity)
-    else
-        self.fl:setNaturalBrightness(self.hw_intensity, self.fl_warmth)
+    if self.fl then
+        -- just re-set it to self.hw_intensity that we haven't changed on Suspend
+        if not self.device:hasNaturalLight() then
+            self.fl:setBrightness(self.hw_intensity)
+        else
+            self.fl:setNaturalBrightness(self.hw_intensity, self.fl_warmth)
+        end
     end
+
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
 end
 
 return CervantesPowerD

frontend/device/kobo/powerd.lua
@@ -1,8 +1,8 @@
-local UIManager = nil -- will be updated when available
 local BasePowerD = require("device/generic/powerd")
 local Math = require("optmath")
 local NickelConf = require("device/kobo/nickel_conf")
 local SysfsLight = require ("device/sysfs_light")
+local UIManager
 local RTC = require("ffi/rtc")
 
 -- Here, we only deal with the real hw intensity.
@@ -415,26 +415,44 @@ end
 
 -- Turn off front light before suspend.
 function KoboPowerD:beforeSuspend()
-    if self.fl == nil then return end
-    -- Remember the current frontlight state
-    self.fl_was_on = self.is_fl_on
-    -- Turn off the frontlight
-    self:turnOffFrontlight()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+
+    -- Handle the frontlight last,
+    -- to prevent as many things as we can from interfering with the smoothness of the ramp
+    if self.fl then
+        -- Remember the current frontlight state
+        self.fl_was_on = self.is_fl_on
+        -- Turn off the frontlight
+        -- NOTE: Funky delay mainly to yield to the EPDC's refresh on UP systems.
+        --       (Neither yieldToEPDC nor nextTick & friends quite cut it here)...
+        UIManager:scheduleIn(0.001, self.turnOffFrontlight, self)
+    end
 end
 
 -- Restore front light state after resume.
 function KoboPowerD:afterResume()
-    if self.fl == nil then return end
-    -- Don't bother if the light was already off on suspend
-    if not self.fl_was_on then return end
-    -- Turn the frontlight back on
-    self:turnOnFrontlight()
-
     -- Set the system clock to the hardware clock's time.
     RTC:HCToSys()
+
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+
+    -- There's a whole bunch of stuff happening before us in Generic:onPowerEvent,
+    -- so we'll delay this ever so slightly so as to appear as smooth as possible...
+    if self.fl then
+        -- Don't bother if the light was already off on suspend
+        if self.fl_was_on then
+            -- Turn the frontlight back on
+            -- NOTE: There's quite likely *more* resource contention than on suspend here :/.
+            UIManager:scheduleIn(0.001, self.turnOnFrontlight, self)
+        end
+    end
 end
 
-function KoboPowerD:readyUIHW(uimgr)
+function KoboPowerD:UIManagerReadyHW(uimgr)
     UIManager = uimgr
 end
 

frontend/device/remarkable/powerd.lua
@@ -22,4 +22,16 @@ function Remarkable_PowerD:isChargingHW()
     return self:read_str_file(self.status_file) == "Charging"
 end
 
+function Remarkable_PowerD:beforeSuspend()
+    -- Inhibit user input and emit the Suspend event.
+    self.device:_beforeSuspend()
+end
+
+function Remarkable_PowerD:afterResume()
+    self:invalidateCapacityCache()
+
+    -- Restore user input and emit the Resume event.
+    self.device:_afterResume()
+end
+
 return Remarkable_PowerD

commit 132c8b340c725ca089b54c8b70a0e181ee1235ba

Kindle: Fix a smattering of frontlight bugs
* afterResume had *two* different implementations, so the historical one
  that handled frontlight fixups no longer ran
  (regression since #10426)
* isFrontlightOn was completely broken, for a couple of reasons:
  * There was no is isFrontlightOnHW implementation, so when it ran, it
    mostly always thought the frontlight was on, because
    self.fl_intensity doesn't change on toggle off.
  * _decideFrontlightState was never called on Kindle,
    so isFrontlightOnHW was never really called, making isFrontlightOn
    completely useless. Call it in setIntensityHW's coda, as it ought to
    be. And properly document that.

Generic *was* calling _decideFrontlightState is setIntensity, but
*before* actually setting the frontlight, which makes no goddamn sense,
so get rid of that, too.

* Also fix frontlight toggle notifications (regression since #10305)

TL;DR: The PowerD API being a mess strikes again.

frontend/device/android/powerd.lua
@@ -60,7 +60,7 @@ function AndroidPowerD:turnOffFrontlightHW()
     android.setScreenBrightness(self.fl_min)
 end
 
-function AndroidPowerD:turnOnFrontlightHW()
+function AndroidPowerD:turnOnFrontlightHW(done_callback)
     if self:isFrontlightOn() and self:isFrontlightOnHW() then
         return
     end
@@ -68,6 +68,8 @@ function AndroidPowerD:turnOnFrontlightHW()
     android.enableFrontlightSwitch()
 
     android.setScreenBrightness(math.floor(self.fl_intensity * self.bright_diff / self.fl_max))
+
+    return false
 end
 
 return AndroidPowerD

frontend/device/devicelistener.lua
@@ -187,7 +187,7 @@ if Device:hasFrontlight() then
     function DeviceListener:onToggleFrontlight()
         local powerd = Device:getPowerDevice()
         local new_text
-        if powerd.is_fl_on then
+        if powerd:isFrontlightOn() then
             new_text = _("Frontlight disabled.")
         else
             new_text = _("Frontlight enabled.")

frontend/device/generic/powerd.lua
@@ -43,7 +43,10 @@ function BasePowerD:new(o)
 end
 
 function BasePowerD:init() end
-function BasePowerD:setIntensityHW(intensity) end
+--- @note: This should *always* call self:_decideFrontlightState() in its coda (unless you have a custom isFrontlightOn implementation)!
+function BasePowerD:setIntensityHW(intensity)
+    self:_decideFrontlightState()
+end
 --- @note: Unlike the "public" setWarmth, this one takes a value in the *native* scale!
 function BasePowerD:setWarmthHW(warmth) end
 function BasePowerD:getCapacityHW() return 0 end
@@ -207,7 +210,6 @@ function BasePowerD:setIntensity(intensity)
     if not self.device:hasFrontlight() then return false end
     if intensity == self:frontlightIntensity() then return false end
     self.fl_intensity = self:normalizeIntensity(intensity)
-    self:_decideFrontlightState()
     logger.dbg("set light intensity", self.fl_intensity)
     self:setIntensityHW(self.fl_intensity)
     self:stateChanged()

frontend/device/kindle/powerd.lua
@@ -29,17 +29,22 @@ end
 
 -- If we start with the light off (fl_intensity is fl_min), ensure a toggle will set it to the lowest "on" step,
 -- and that we update fl_intensity (by using setIntensity and not setIntensityHW).
-function KindlePowerD:turnOnFrontlightHW()
+function KindlePowerD:turnOnFrontlightHW(done_callback)
     self:setIntensity(self.fl_intensity == self.fl_min and self.fl_min + 1 or self.fl_intensity)
+
+    return false
 end
 -- Which means we need to get rid of the insane fl_intensity == fl_min shortcut in turnOnFrontlight, too...
 -- That dates back to #2941, and I have no idea what it's supposed to help with.
-function BasePowerD:turnOnFrontlight()
+function KindlePowerD:turnOnFrontlight(done_callback)
     if not self.device:hasFrontlight() then return end
     if self:isFrontlightOn() then return false end
-    self:turnOnFrontlightHW()
+    local cb_handled = self:turnOnFrontlightHW(done_callback)
     self.is_fl_on = true
     self:stateChanged()
+    if not cb_handled and done_callback then
+        done_callback()
+    end
     return true
 end
 
@@ -79,6 +84,14 @@ function KindlePowerD:frontlightIntensityHW()
     end
 end
 
+-- Make sure isFrontlightOn reflects the actual HW state,
+-- as self.fl_intensity is kept as-is when toggling the light off,
+-- in order to be able to toggle it back on at the right intensity.
+function KindlePowerD:isFrontlightOnHW()
+    local hw_intensity = self:frontlightIntensityHW()
+    return hw_intensity > self.fl_min
+end
+
 function KindlePowerD:setIntensityHW(intensity)
     -- Handle the synthetic step switcheroo on ! canTurnFrontlightOff devices...
     local turn_it_off = false
@@ -108,6 +121,9 @@ function KindlePowerD:setIntensityHW(intensity)
             ffiUtil.writeToSysfs(intensity, self.warmth_intensity_file)
         end
     end
+
+    -- The state might have changed, make sure we don't break isFrontlightOn
+    self:_decideFrontlightState()
 end
 
 function KindlePowerD:frontlightWarmthHW()
@@ -180,22 +196,6 @@ end
 
 function KindlePowerD:_readFLIntensity()
     return self:read_int_file(self.fl_intensity_file)
-end
-
-function KindlePowerD:afterResume()
-    if not self.device:hasFrontlight() then
-        return
-    end
-    if self:isFrontlightOn() then
-        -- The Kindle framework should turn the front light back on automatically.
-        -- The following statement ensures consistency of intensity, but should basically always be redundant,
-        -- since we set intensity via lipc and not sysfs ;).
-        -- NOTE: This is race-y, and we want to *lose* the race, hence the use of the scheduler (c.f., #4392)
-        UIManager:tickAfterNext(function() self:turnOnFrontlightHW() end)
-    else
-        -- But in the off case, we *do* use sysfs, so this one actually matters.
-        UIManager:tickAfterNext(function() self:turnOffFrontlightHW() end)
-    end
 end
 
 function KindlePowerD:toggleSuspend()
@@ -293,6 +293,20 @@ function KindlePowerD:afterResume()
 
     -- Restore user input and emit the Resume event.
     self.device:_afterResume()
+
+    if not self.device:hasFrontlight() then
+        return
+    end
+    if self:isFrontlightOn() then
+        -- The Kindle framework should turn the front light back on automatically.
+        -- The following statement ensures consistency of intensity, but should basically always be redundant,
+        -- since we set intensity via lipc and not sysfs ;).
+        -- NOTE: This is race-y, and we want to *lose* the race, hence the use of the scheduler (c.f., #4392)
+        UIManager:tickAfterNext(function() self:turnOnFrontlightHW() end)
+    else
+        -- But in the off case, we *do* use sysfs, so this one actually matters.
+        UIManager:tickAfterNext(function() self:turnOffFrontlightHW() end)
+    end
 end
 
 function KindlePowerD:UIManagerReadyHW(uimgr)

frontend/device/pocketbook/powerd.lua
@@ -41,6 +41,9 @@ function PocketBookPowerD:setIntensityHW(intensity)
     else
         inkview.SetFrontlightState(intensity)
     end
+
+    -- We have a custom isFrontlightOn implementation, so this is redundant
+    self:_decideFrontlightState()
 end
 
 function PocketBookPowerD:isFrontlightOn()

frontend/device/sdl/powerd.lua
@@ -18,6 +18,7 @@ end
 function SDLPowerD:setIntensityHW(intensity)
     require("logger").info("set brightness to", intensity)
     self.hw_intensity = intensity or self.hw_intensity
+    self:_decideFrontlightState()
 end
 
 function SDLPowerD:frontlightWarmthHW()

spec/unit/frontlight_spec.lua
@@ -23,6 +23,7 @@ describe("Frontlight function in PowerD", function()
         end
         PowerD.setIntensityHW = function(self, intensity)
             self.frontlight = intensity
+            self:_decideFrontlightState()
         end
     end)
 