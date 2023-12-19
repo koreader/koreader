b43e54d362a1a9661a1bec5897950804a634c439
hius07 <62179190+hius07@users.noreply.github.com>
2023-12-19 09:04:42 +0200

    filemanagercollection: comment

--- frontend/apps/filemanager/filemanagercollection.lua
+++ frontend/apps/filemanager/filemanagercollection.lua
@@ -126,7 +126,7 @@ function FileManagerCollection:onShowColl(collection_name)
         is_borderless = true,
         is_popout = false,
         -- item and book cover thumbnail dimensions in Mosaic and Detailed list display modes
-        -- are equal in File manager, History and Collection windows
+        -- must be equal in File manager, History and Collection windows to avoid image scaling
         title_bar_fm_style = true,
         title_bar_left_icon = "appbar.menu",
         onLeftButtonTap = function() self:showCollDialog() end,
