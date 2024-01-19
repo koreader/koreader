koreader/koreader

commit 68aa209a6ca6315f2bc1413d869430f328887326

Open with... improvement (#11056)
Allows associating filetypes with non-document providers (like ImageViewer or TextViewer) to "open" (view) these files by a tap in file browser.

frontend/ui/screensaver.lua
@@ -23,6 +23,7 @@ local VerticalGroup = require("ui/widget/verticalgroup")
 local VerticalSpan = require("ui/widget/verticalspan")
 local datetime = require("datetime")
 local ffiUtil = require("ffi/util")
+local filemanagerutil = require("apps/filemanager/filemanagerutil")
 local lfs = require("libs/libkoreader-lfs")
 local logger = require("logger")
 local util = require("util")
@@ -78,35 +79,12 @@ if Device:isEmulator() then
     Screensaver.default_screensaver_message = Screensaver.default_screensaver_message .. "\n" .. _("(Press F2 to resume)")
 end
 
-function Screensaver:_getRandomImage(dir)
-    if not dir then
-        return nil
+local function _getRandomImage(dir)
+    if not dir then return end
+    local match_func = function(file) -- images, ignore macOS resource forks
+        return not util.stringStartsWith(file, "._") and DocumentRegistry:isImageFile(file)
     end
-
-    if string.sub(dir, string.len(dir)) ~= "/" then
-       dir = dir .. "/"
-    end
-    local pics = {}
-    local i = 0
-    math.randomseed(os.time())
-    local ok, iter, dir_obj = pcall(lfs.dir, dir)
-    if ok then
-        for f in iter, dir_obj do
-            -- Always ignore macOS resource forks, too.
-            if lfs.attributes(dir .. f, "mode") == "file" and not util.stringStartsWith(f, "._")
-                    and DocumentRegistry:isImageFile(f) then
-                i = i + 1
-                pics[i] = f
-            end
-        end
-        if i == 0 then
-            return nil
-        end
-    else
-        return nil
-    end
-
-    return dir .. pics[math.random(i)]
+    return filemanagerutil.getRandomFile(dir, match_func)
 end
 
 -- This is implemented by the Statistics plugin
@@ -573,7 +551,7 @@ function Screensaver:setup(event, event_message)
     if self.screensaver_type == "random_image" then
         local screensaver_dir = G_reader_settings:readSetting(self.prefix .. "screensaver_dir")
                              or G_reader_settings:readSetting("screensaver_dir")
-        self.image_file = self:_getRandomImage(screensaver_dir) or "resources/koreader.png" -- Fallback image
+        self.image_file = _getRandomImage(screensaver_dir) or "resources/koreader.png" -- Fallback image
     end
 
     -- Use the right background setting depending on the effective mode, now that fallbacks have kicked in.

frontend/apps/filemanager/filemanager.lua
@@ -166,8 +166,7 @@ function FileManager:setupLayout()
             end
             self:refreshPath()
         else
-            local ReaderUI = require("apps/reader/readerui")
-            ReaderUI:showReader(file)
+            file_manager:openFile(file)
         end
         return true
     end
@@ -268,43 +267,7 @@ function FileManager:setupLayout()
                     text = _("Open with…"),
                     callback = function()
                         UIManager:close(self.file_dialog)
-                        local one_time_providers = {}
-                        if DocumentRegistry:isImageFile(file) then
-                            table.insert(one_time_providers, {
-                                provider_name = _("Image viewer"),
-                                callback = function()
-                                    local ImageViewer = require("ui/widget/imageviewer")
-                                    UIManager:show(ImageViewer:new{
-                                        file = file,
-                                        fullscreen = true,
-                                        with_title_bar = false,
-                                    })
-                                end,
-                            })
-                        end
-                        table.insert(one_time_providers, {
-                            provider_name = _("Text viewer"),
-                            callback = function()
-                                file_manager:openTextViewer(file)
-                            end,
-                        })
-                        if file_manager.texteditor then
-                            table.insert(one_time_providers, {
-                                provider_name = _("Text editor"),
-                                callback = function()
-                                    file_manager.texteditor:checkEditFile(file)
-                                end,
-                            })
-                        end
-                        if file_manager.archiveviewer and file_manager.archiveviewer:isSupported(file) then
-                            table.insert(one_time_providers, {
-                                provider_name = _("Archive viewer"),
-                                callback = function()
-                                    file_manager.archiveviewer:openArchiveViewer(file)
-                                end,
-                            })
-                        end
-                        self:showSetProviderButtons(file, one_time_providers)
+                        file_manager:showOpenWithDialog(file)
                     end,
                 },
                 filemanagerutil.genBookInformationButton(file, close_dialog_callback),
@@ -410,7 +373,7 @@ function FileManager:init()
     self.active_widgets = {}
 
     self:registerModule("screenshot", Screenshoter:new{
-        prefix = 'FileManager',
+        prefix = "FileManager",
         ui = self,
     }, true)
 
@@ -838,7 +801,10 @@ function FileManager:setHome(path)
 end
 
 function FileManager:openRandomFile(dir)
-    local random_file = DocumentRegistry:getRandomFile(dir, false)
+    local match_func = function(file) -- documents, not yet opened
+        return DocumentRegistry:hasProvider(file) and not DocSettings:hasSidecarFile(file)
+    end
+    local random_file = filemanagerutil.getRandomFile(dir, match_func)
     if random_file then
         UIManager:show(MultiConfirmBox:new{
             text = T(_("Do you want to open %1?"), BD.filename(BaseUtil.basename(random_file))),
@@ -1139,36 +1105,6 @@ function FileManager:showFiles(path, focused_file)
     UIManager:show(file_manager)
 end
 
-function FileManager:openTextViewer(file_path)
-    local function _openTextViewer(filepath)
-        local file = io.open(filepath, "rb")
-        if not file then return end
-        local file_content = file:read("*all")
-        file:close()
-        UIManager:show(require("ui/widget/textviewer"):new{
-            title = filepath,
-            title_multilines = true,
-            justified = false,
-            text = file_content,
-        })
-    end
-    local attr = lfs.attributes(file_path)
-    if attr then
-        if attr.size > 400000 then
-            UIManager:show(ConfirmBox:new{
-                text = T(_("This file is %2:\n\n%1\n\nAre you sure you want to open it?\n\nOpening big files may take some time."),
-                    BD.filepath(file_path), util.getFriendlySize(attr.size)),
-                ok_text = _("Open"),
-                ok_callback = function()
-                    _openTextViewer(file_path)
-                end,
-            })
-        else
-            _openTextViewer(file_path)
-        end
-    end
-end
-
 --- A shortcut to execute mv.
 -- @treturn boolean result of mv command
 function FileManager:moveFile(from, to)
@@ -1302,6 +1238,170 @@ function FileManager:showSelectedFilesList()
     table.insert(menu_container, menu)
     menu:switchItemTable(T(_("Selected files (%1)"), #selected_files), selected_files)
     UIManager:show(menu_container)
+end
+
+function FileManager:showOpenWithDialog(file)
+    local file_associated_provider_key = DocumentRegistry:getAssociatedProviderKey(file, false)
+    local type_associated_provider_key = DocumentRegistry:getAssociatedProviderKey(file, true)
+    local file_provider_key = file_associated_provider_key
+                           or type_associated_provider_key
+                           or DocumentRegistry:getProvider(file).provider
+
+    -- radio buttons (all providers)
+    local function genRadioButton(provider, is_unsupported)
+        return {{
+            -- @translators %1 is the provider name, such as Cool Reader Engine or MuPDF.
+            text = is_unsupported and T(_("%1 ~Unsupported"), provider.provider_name) or provider.provider_name,
+            checked = provider.provider == file_provider_key,
+            provider = provider,
+        }}
+    end
+    local radio_buttons = {}
+    local providers = DocumentRegistry:getProviders(file) -- document providers
+    if providers then
+        for _, provider in ipairs(providers) do
+            table.insert(radio_buttons, genRadioButton(provider.provider))
+        end
+    else
+        local provider = DocumentRegistry:getFallbackProvider()
+        table.insert(radio_buttons, genRadioButton(provider, true))
+    end
+    for _, provider in ipairs(DocumentRegistry:getAuxProviders()) do -- auxiliary providers
+        local is_filetype_supported
+        if provider.enabled_func then -- module
+            is_filetype_supported = provider.enabled_func(file)
+        else -- plugin
+            is_filetype_supported = self[provider.provider]:isFileTypeSupported(file)
+        end
+        if is_filetype_supported then
+            table.insert(radio_buttons, genRadioButton(provider))
+        end
+    end
+
+    -- buttons
+    local __, filename_pure = util.splitFilePathName(file)
+    filename_pure = BD.filename(filename_pure)
+    local filename_suffix = util.getFileNameSuffix(file):lower()
+    local dialog
+    local buttons = {}
+    -- row: wide button
+    if file_associated_provider_key then
+        table.insert(buttons, {{
+            text = _("Reset default for this file"),
+            callback = function()
+                DocumentRegistry:setProvider(file, nil, false)
+                UIManager:close(dialog)
+            end,
+        }})
+    end
+    -- row: wide button
+    if type_associated_provider_key then
+        table.insert(buttons, {{
+            text = T(_("Reset default for %1 files"), filename_suffix),
+            callback = function()
+                DocumentRegistry:setProvider(file, nil, true)
+                UIManager:close(dialog)
+            end,
+        }})
+    end
+    -- row: wide button
+    local associated_providers = DocumentRegistry:getAssociatedProviderKey() -- hash table
+    if next(associated_providers) ~= nil then
+        table.insert(buttons, {{
+            text = _("View defaults for file types"),
+            callback = function()
+                local max_len = 0 -- align extensions
+                for extension in pairs(associated_providers) do
+                    if max_len < #extension then
+                        max_len = #extension
+                    end
+                end
+                local t = {}
+                for extension, provider_key in BaseUtil.orderedPairs(associated_providers) do
+                    local provider = DocumentRegistry:getProviderFromKey(provider_key)
+                    if provider then
+                        local space = string.rep(" ", max_len - #extension)
+                        table.insert(t, T("%1%2: %3", extension, space, provider.provider_name))
+                    end
+                end
+                UIManager:show(InfoMessage:new{
+                    text = table.concat(t, "\n"),
+                    monospace_font = true,
+                })
+            end,
+        }})
+    end
+    -- row: 2 buttons
+    table.insert(buttons, {
+        {
+            text = _("Cancel"),
+            callback = function()
+                UIManager:close(dialog)
+            end,
+        },
+        {
+            text = _("Open"),
+            is_enter_default = true,
+            callback = function()
+                local provider = dialog.radio_button_table.checked_button.provider
+                if dialog._check_file_button.checked then -- set this file associated provider
+                    UIManager:show(ConfirmBox:new{
+                        text = T(_("Always open '%2' with %1?"), provider.provider_name, filename_pure),
+                        ok_text = _("Always"),
+                        ok_callback = function()
+                            DocumentRegistry:setProvider(file, provider, false)
+                            self:openFile(file, provider)
+                            UIManager:close(dialog)
+                        end,
+                    })
+                elseif dialog._check_global_button.checked then -- set file type associated provider
+                    UIManager:show(ConfirmBox:new{
+                        text = T(_("Always open %2 files with %1?"), provider.provider_name, filename_suffix),
+                        ok_text = _("Always"),
+                        ok_callback = function()
+                            DocumentRegistry:setProvider(file, provider, true)
+                            self:openFile(file, provider)
+                            UIManager:close(dialog)
+                        end,
+                    })
+                else -- open just once
+                    self:openFile(file, provider)
+                    UIManager:close(dialog)
+                end
+            end,
+        },
+    })
+
+    local OpenWithDialog = require("ui/widget/openwithdialog")
+    dialog = OpenWithDialog:new{
+        title = T(_("Open %1 with:"), filename_pure),
+        radio_buttons = radio_buttons,
+        buttons = buttons,
+    }
+    UIManager:show(dialog)
+end
+
+function FileManager:openFile(file, provider, doc_caller_callback, aux_caller_callback)
+    if not provider then -- check associated
+        local provider_key = DocumentRegistry:getAssociatedProviderKey(file)
+        provider = provider_key and DocumentRegistry:getProviderFromKey(provider_key)
+    end
+    if provider and provider.order then -- auxiliary
+        if aux_caller_callback then
+            aux_caller_callback()
+        end
+        if provider.callback then -- module
+            provider.callback(file)
+        else -- plugin
+            self[provider.provider]:openFile(file)
+        end
+    else -- document
+        if doc_caller_callback then
+            doc_caller_callback()
+        end
+        local ReaderUI = require("apps/reader/readerui")
+        ReaderUI:showReader(file, provider)
+    end
 end
 
 return FileManager

frontend/apps/filemanager/filemanagercollection.lua
@@ -161,7 +161,7 @@ function FileManagerCollection:showCollDialog()
                     path = G_reader_settings:readSetting("home_dir"),
                     select_directory = false,
                     file_filter = function(file)
-                        return DocumentRegistry:getProviders(file) ~= nil
+                        return DocumentRegistry:hasProvider(file)
                     end,
                     onConfirm = function(file)
                         if not ReadCollection:checkItemExist(file) then

frontend/apps/filemanager/filemanagerfilesearcher.lua
@@ -269,7 +269,6 @@ end
 
 function FileSearcher:onMenuSelect(item)
     local file = item.path
-    local has_provider = false
     local dialog
     local function close_dialog_callback()
         UIManager:close(dialog)
@@ -281,8 +280,7 @@ function FileSearcher:onMenuSelect(item)
     local buttons = {}
     if item.is_file then
         local is_currently_opened = self.ui.document and self.ui.document.file == file
-        has_provider = DocumentRegistry:hasProvider(file)
-        if has_provider or DocSettings:hasSidecarFile(file) then
+        if DocumentRegistry:hasProvider(file) or DocSettings:hasSidecarFile(file) then
             local doc_settings_or_file = is_currently_opened and self.ui.doc_settings or file
             table.insert(buttons, filemanagerutil.genStatusButtonsRow(doc_settings_or_file, close_dialog_callback))
             table.insert(buttons, {}) -- separator
@@ -317,11 +315,11 @@ function FileSearcher:onMenuSelect(item)
         filemanagerutil.genShowFolderButton(file, close_dialog_menu_callback),
         {
             text = _("Open"),
-            enabled = has_provider,
+            enabled = DocumentRegistry:hasProvider(file, nil, true), -- allow auxiliary providers
             callback = function()
-                close_dialog_menu_callback()
-                local ReaderUI = require("apps/reader/readerui")
-                ReaderUI:showReader(file)
+                close_dialog_callback()
+                local FileManager = require("apps/filemanager/filemanager")
+                FileManager.openFile(self.ui, file, nil, self.close_callback)
             end,
         },
     })
@@ -334,10 +332,9 @@ end
 
 function FileSearcher:onMenuHold(item)
     if item.is_file then
-        if DocumentRegistry:hasProvider(item.path) then
-            self.close_callback()
-            local ReaderUI = require("apps/reader/readerui")
-            ReaderUI:showReader(item.path)
+        if DocumentRegistry:hasProvider(item.path, nil, true) then
+            local FileManager = require("apps/filemanager/filemanager")
+            FileManager.openFile(self.ui, item.path, nil, self.close_callback)
         end
     else
         self.close_callback()

frontend/apps/filemanager/filemanagerutil.lua
@@ -8,6 +8,7 @@ local DocSettings = require("docsettings")
 local Event = require("ui/event")
 local UIManager = require("ui/uimanager")
 local ffiutil = require("ffi/util")
+local lfs = require("libs/libkoreader-lfs")
 local util = require("util")
 local _ = require("gettext")
 local T = ffiutil.template
@@ -47,6 +48,26 @@ function filemanagerutil.splitFileNameType(filepath)
         end
     end
     return filename_without_suffix, filetype
+end
+
+function filemanagerutil.getRandomFile(dir, match_func)
+    if not dir:match("/$") then
+        dir = dir .. "/"
+    end
+    local files = {}
+    local ok, iter, dir_obj = pcall(lfs.dir, dir)
+    if ok then
+        for entry in iter, dir_obj do
+            local file = dir .. entry
+            if lfs.attributes(file, "mode") == "file" and match_func(file) then
+                table.insert(files, entry)
+            end
+        end
+        if #files > 0 then
+            math.randomseed(os.time())
+            return dir .. files[math.random(#files)]
+        end
+    end
 end
 
 -- Purge doc settings except kept

frontend/apps/reader/modules/readerlink.lua
@@ -6,6 +6,7 @@ local BD = require("ui/bidi")
 local ButtonDialogTitle = require("ui/widget/buttondialogtitle")
 local ConfirmBox = require("ui/widget/confirmbox")
 local Device = require("device")
+local DocumentRegistry = require("document/documentregistry")
 local Event = require("ui/event")
 local InfoMessage = require("ui/widget/infomessage")
 local InputContainer = require("ui/widget/container/inputcontainer")
@@ -822,9 +823,7 @@ function ReaderLink:onGotoLink(link, neglect_current_location, allow_footnote_po
     linked_filename  = ffiutil.joinPath(self.document_dir, linked_filename) -- get full path
     linked_filename = ffiutil.realpath(linked_filename) -- clean full path from ./ or ../
     if linked_filename and lfs.attributes(linked_filename, "mode") == "file" then
-        local DocumentRegistry = require("document/documentregistry")
-        local provider = DocumentRegistry:getProvider(linked_filename)
-        if provider then
+        if DocumentRegistry:hasProvider(linked_filename) then
             -- Display filename with anchor or query string, so the user gets
             -- this information and can manually go to the appropriate place
             local display_filename = linked_filename

commit 5f7f60c59960a9381e5561691f29f11cc5b5987f

ScreenSaver: Properly exclude macOS resource forks again
The previous check was inlined in the dir walk, so it always saw a
relative path.
Here, it gets an absolute one instead, so act accordingly ;).

Fix #11390
Regression since #11056

frontend/ui/screensaver.lua
@@ -79,7 +79,7 @@ end
 local function _getRandomImage(dir)
     if not dir then return end
     local match_func = function(file) -- images, ignore macOS resource forks
-        return not util.stringStartsWith(file, "._") and DocumentRegistry:isImageFile(file)
+        return not util.stringStartsWith(ffiUtil.basename(file), "._") and DocumentRegistry:isImageFile(file)
     end
     return filemanagerutil.getRandomFile(dir, match_func)
 end