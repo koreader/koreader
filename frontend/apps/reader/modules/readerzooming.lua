f4c485e79945c2d87bdaffa7c57f5ea318f837f2
hugleo <hugocanalli@gmail.com>
2024-02-19 08:46:51 -0300

    better but may not be read yet

--- frontend/apps/reader/modules/readerzooming.lua
+++ frontend/apps/reader/modules/readerzooming.lua
@@ -230,23 +230,15 @@ function ReaderZooming:onReadSettings(config)
         -- Otherwise, build it from the split genus & type settings
         local zoom_mode_genus = config:readSetting("kopt_zoom_mode_genus")
                              or G_reader_settings:readSetting("kopt_zoom_mode_genus")
+                             or 3 -- autocrop is default then pagewidth will be the default as well
         local zoom_mode_type = config:readSetting("kopt_zoom_mode_type")
                             or G_reader_settings:readSetting("kopt_zoom_mode_type")
-        if zoom_mode_genus or zoom_mode_type then
-            zoom_mode = self:combo_to_mode(zoom_mode_genus, zoom_mode_type)
-        end
+        zoom_mode = self:combo_to_mode(zoom_mode_genus, zoom_mode_type)
 
         -- Validate it
         zoom_mode = self.zoom_mode_label[zoom_mode] and zoom_mode or self.DEFAULT_ZOOM_MODE
     end
-
-    -- auto crop needs to be page content
-    local trim_page = config:readSetting("kopt_trim_page")
-                             or G_reader_settings:readSetting("kopt_trim_page")
-                             or G_defaults:readSetting("DKOPTREADER_CONFIG_TRIM_PAGE")
-    local temp_zoom_mode = zoom_mode:match("page(.*)")
-    zoom_mode = trim_page == 1 and temp_zoom_mode and "content"..temp_zoom_mode or zoom_mode
-
+    
     -- Import legacy zoom_factor settings
     if config:has("zoom_factor") and config:hasNot("kopt_zoom_factor") then
         config:saveSetting("kopt_zoom_factor", config:readSetting("zoom_factor"))
