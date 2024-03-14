6526aa1b6340bcba32c4d6925f44551c054d6d45
SomeGuy <97603719+Commodore64user@users.noreply.github.com>
2024-03-14 00:50:33 +0000

    Update screensaver.lua

--- frontend/ui/screensaver.lua
+++ frontend/ui/screensaver.lua
@@ -315,7 +315,7 @@ function Screensaver:setMessage()
     input_dialog = InputDialog:new{
         title = _("Screensaver message"),
         description = _([[
-Enter the message to be displayed by the screensaver. The following escape sequences can be used:
+Enter your custom message to be displayed on the lock screen. The following escape sequences are available:
   %T title
   %A author(s)
   %S series
