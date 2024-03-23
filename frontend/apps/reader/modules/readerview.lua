4473c615c84c2e64bdd69b18290ba0db6ac6b755
SomeGuy <97603719+Commodore64user@users.noreply.github.com>
2024-03-23 23:16:12 +0000

    Update readerview.lua
    improved underline

--- frontend/apps/reader/modules/readerview.lua
+++ frontend/apps/reader/modules/readerview.lua
@@ -632,7 +632,7 @@ function ReaderView:drawHighlightRect(bb, _x, _y, rect, drawer, draw_note_mark)
     if drawer == "lighten" then
         bb:lightenRect(x, y, w, h, self.highlight.lighten_factor)
     elseif drawer == "underscore" then
-        bb:paintRect(x, y + h - 1, w, h / 8, Blitbuffer.COLOR_GRAY)
+        bb:paintRect(x, y + h - math.floor(h / 16), w, math.floor(h / 8), Blitbuffer.COLOR_GRAY_4)
     elseif drawer == "strikeout" then
         local line_y = y + math.floor(h / 2) + 1
         if self.ui.paging then
