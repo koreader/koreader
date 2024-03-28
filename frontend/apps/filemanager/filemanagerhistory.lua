cf9e256cb3ce92621da4515efeb0a48fea5f84ff
Denis Malinovsky <dmalinovsky@gmail.com>
2024-03-28 14:22:19 -0400

    Do `flashui` refresh for first display of History page
    This fixes ghosting for color Pocketbook devices
    
    Fixes #11602.

--- frontend/apps/filemanager/filemanagerhistory.lua
+++ frontend/apps/filemanager/filemanagerhistory.lua
@@ -270,7 +270,7 @@ function FileManagerHistory:onShowHist(search_info)
         self.hist_menu = nil
         G_reader_settings:saveSetting("history_filter", self.filter)
     end
-    UIManager:show(self.hist_menu)
+    UIManager:show(self.hist_menu, "flashui")
     return true
 end
 
