b31284fced370aeda7d464cd3732d7987930b572
SomeGuy <97603719+Commodore64user@users.noreply.github.com>
2024-04-11 01:05:54 +0100

    ~~alternate~~ alternative status bar

--- frontend/apps/reader/modules/readercoptlistener.lua
+++ frontend/apps/reader/modules/readercoptlistener.lua
@@ -211,7 +211,7 @@ function ReaderCoptListener:getAltStatusBarMenu()
         separator = true,
         sub_item_table = {
             {
-                text = _("About alternate status bar"),
+                text = _("About alternative status bar"),
                 keep_menu_open = true,
                 callback = function()
                     UIManager:show(InfoMessage:new{
