6fb4f49e526586eff83b1b622d309c0f91c387c1
SomeGuy <97603719+Commodore64user@users.noreply.github.com>
2024-05-06 14:05:54 +0100

    Update main.lua

--- plugins/gestures.koplugin/main.lua
+++ plugins/gestures.koplugin/main.lua
@@ -760,7 +760,7 @@ function Gestures:addToMainMenu(menu_items)
             sub_item_table = self:genSubItemTable({"spread_gesture", "pinch_gesture"}),
         })
         table.insert(menu_items.gesture_manager.sub_item_table, {
-            text = _("Two-finger half-moon swipe"),
+            text = _("Two-finger rotation"),
             sub_item_table = self:genSubItemTable({"rotate_cw", "rotate_ccw"}),
         })
     end
