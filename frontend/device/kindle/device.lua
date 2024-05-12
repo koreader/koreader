0a6f22828d814eae2001ef02d844c3685f9d94ee
NiLuJe <ninuje@gmail.com>
2024-05-12 20:48:23 +0200

    Forgot about MVP Touch's lone home button.

--- frontend/device/kindle/device.lua
+++ frontend/device/kindle/device.lua
@@ -213,8 +213,8 @@ function Kindle:openInputDevices()
     -- Auto-detect input devices (via FBInk's fbink_input_scan)
     local FBInkInput = ffi.load("fbink_input")
     local dev_count = ffi.new("size_t[1]")
-    -- We care about: the touchscreen, a properly scaled stylus and pagination buttons.
-    local match_mask = bit.bor(C.INPUT_TOUCHSCREEN, C.INPUT_SCALED_TABLET, C.INPUT_PAGINATION_BUTTONS)
+    -- We care about: the touchscreen, a properly scaled stylus, pagination buttons and a home button.
+    local match_mask = bit.bor(C.INPUT_TOUCHSCREEN, C.INPUT_SCALED_TABLET, C.INPUT_PAGINATION_BUTTONS, C.INPUT_HOME_BUTTON)
     local devices = FBInkInput.fbink_input_scan(match_mask, 0, C.SCAN_ONLY, dev_count)
     if devices ~= nil then
         for i = 0, tonumber(dev_count[0]) - 1 do
