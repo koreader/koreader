602fee665f509144575036a5fdbf66966b1f4e31
NiLuJe <ninuje@gmail.com>
2024-05-14 02:35:12 +0200

    Explain why that happens.
    We still need this wrapper to clear the hash, as we can't do that from
    the C module.

--- frontend/device/input.lua
+++ frontend/device/input.lua
@@ -336,6 +336,7 @@ function Input.close(path)
         if ok or err == C.ENODEV then
             -- Either the call succeeded,
             -- or the backend had already caught an ENODEV in waitForInput and closed the fd internally.
+            -- (Because the EvdevInputRemove Event comes from an UsbDevicePlugOut uevent forwarded as an... *input* EV_KEY event ;)).
             -- Regardless, that device is gone, so clear its spot in the hashmap.
             Input.opened_devices[path] = nil
         end
