27e8ce08ca7143b55818c4fa58cb685286ed637b
SomeGuy <97603719+Commodore64user@users.noreply.github.com>
2024-05-20 10:04:32 +0100

    duplicated Home key_event removed

--- frontend/apps/filemanager/filemanager.lua
+++ frontend/apps/filemanager/filemanager.lua
@@ -380,7 +380,6 @@ function FileManager:registerKeyEvents()
         self.key_events.Home = { { "Home" } }
         -- Override the menu.lua way of handling the back key
         self.file_chooser.key_events.Back = { { Device.input.group.Back } }
-        self.key_events.Home = { { "Home" } }
         if Device:hasFiveWay() and not Device:hasKeyboard() then
             self.key_events.KeyToggleWifi = { { "ScreenKB", "Home" } }
         end
