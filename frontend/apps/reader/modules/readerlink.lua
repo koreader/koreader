ffc43030ece47dd5836b945660ee4c1c28690c68
Frans de Jonge <fransdejonge@gmail.com>
2024-05-27 09:06:51 +0200

    ReaderLink: change (hasScreenKB or hasSymKey) add to notification stack shortcut to Press instead of Down (#11905)
    See https://github.com/koreader/koreader/pull/11900/files/316c6a0624a504e1ef8645dfbcfb068a73384d3b#r1615314584

--- frontend/apps/reader/modules/readerlink.lua
+++ frontend/apps/reader/modules/readerlink.lua
@@ -236,11 +236,11 @@ function ReaderLink:registerKeyEvents()
     if Device:hasScreenKB() or Device:hasSymKey() then
         self.key_events.GotoSelectedPageLink = { { "Press" }, event = "GotoSelectedPageLink" }
         if Device:hasKeyboard() then
-            self.key_events.AddCurrentLocationToStack = { { "Shift", "Down" } }
+            self.key_events.AddCurrentLocationToStack = { { "Shift", "Press" } }
             self.key_events.SelectNextPageLink = { { "Shift", "LPgFwd" }, event = "SelectNextPageLink" }
             self.key_events.SelectPrevPageLink = { { "Shift", "LPgBack" }, event = "SelectPrevPageLink" }
         else
-            self.key_events.AddCurrentLocationToStack = { { "ScreenKB", "Down" } }
+            self.key_events.AddCurrentLocationToStack = { { "ScreenKB", "Press" } }
             self.key_events.SelectNextPageLink = { { "ScreenKB", "LPgFwd" }, event = "SelectNextPageLink" }
             self.key_events.SelectPrevPageLink = { { "ScreenKB", "LPgBack" }, event = "SelectPrevPageLink" }
         end
