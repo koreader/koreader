bf7131e884e35475e93349b40320c8aea13a6c04
Piotrek Marciniak <piotrek@ptrm.eu>
2024-05-27 14:43:46 +0200

    Don't send empty string as note in readwise export
    Recently Readwise started complaining about note reuired to be filled when being sent "note": "", now nil is sent instead of "".

--- plugins/exporter.koplugin/target/readwise.lua
+++ plugins/exporter.koplugin/target/readwise.lua
@@ -104,7 +104,7 @@ function ReadwiseExporter:createHighlights(booknotes)
                 author = booknotes.author ~= "" and booknotes.author:gsub("\n", ", ") or nil, -- optional author
                 source_type = "koreader",
                 category = "books",
-                note = clipping.note,
+                note = clipping.note ~= "" and clipping.note or nil,
                 location = clipping.page,
                 location_type = "order",
                 highlighted_at = os.date("!%Y-%m-%dT%TZ", clipping.time),
