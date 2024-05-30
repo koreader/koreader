8761b19febedff7bb55934fa1773cd841bc03097
SomeGuy <97603719+Commodore64user@users.noreply.github.com>
2024-05-30 12:36:01 +0100

    remove "if" and "args="

--- frontend/apps/reader/modules/readerlink.lua
+++ frontend/apps/reader/modules/readerlink.lua
@@ -236,11 +236,11 @@ function ReaderLink:registerKeyEvents()
     if Device:hasScreenKB() or Device:hasSymKey() then
         self.key_events.GotoSelectedPageLink = { { "Press" }, event = "GotoSelectedPageLink" }
         if Device:hasKeyboard() then
-            self.key_events.AddCustomCurrentLocationToStack = { { "Shift", "Press" }, args = true, }
+            self.key_events.AddCustomCurrentLocationToStack = { { "Shift", "Press" } }
             self.key_events.SelectNextPageLink = { { "Shift", "LPgFwd" }, event = "SelectNextPageLink" }
             self.key_events.SelectPrevPageLink = { { "Shift", "LPgBack" }, event = "SelectPrevPageLink" }
         else
-            self.key_events.AddCustomCurrentLocationToStack = { { "ScreenKB", "Press" }, args = true, }
+            self.key_events.AddCustomCurrentLocationToStack = { { "ScreenKB", "Press" } }
             self.key_events.SelectNextPageLink = { { "ScreenKB", "LPgFwd" }, event = "SelectNextPageLink" }
             self.key_events.SelectPrevPageLink = { { "ScreenKB", "LPgBack" }, event = "SelectPrevPageLink" }
         end
@@ -723,11 +723,7 @@ end
 -- to be used on shortcuts for non-touch devices
 function ReaderLink:onAddCustomCurrentLocationToStack(show_notification)
     self:addCurrentLocationToStack()
-    if show_notification then
-        Notification:setNotifySource(Notification.SOURCE_ALWAYS_SHOW)
-        Notification:notify(_("Current location added to history."))
-        Notification:resetNotifySource()
-    end
+    Notification:notify(_("Current location added to history."), Notification.SOURCE_ALWAYS_SHOW)
 end
 
 -- Remember current location so we can go back to it
