bed86780221778ada55edd81dbaf9a361ca3b508
David <97603719+Commodore64user@users.noreply.github.com>
2024-06-05 13:50:32 +0100

    ~~no delay~~ never

--- frontend/ui/elements/screensaver_menu.lua
+++ frontend/ui/elements/screensaver_menu.lua
@@ -69,7 +69,7 @@ return {
             {
                 text = _("Postpone screen update after wake-up"),
                 sub_item_table = {
-                    genMenuItem(_("No delay"), "screensaver_delay", "disable"),
+                    genMenuItem(_("Never"), "screensaver_delay", "disable"),
                     genMenuItem(_("1 second"), "screensaver_delay", "1"),
                     genMenuItem(_("3 seconds"), "screensaver_delay", "3"),
                     genMenuItem(_("5 seconds"), "screensaver_delay", "5"),
