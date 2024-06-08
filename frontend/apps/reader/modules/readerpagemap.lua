cca191e726a8eeb5d7b379d35133bbe50ed0adaf
David <97603719+Commodore64user@users.noreply.github.com>
2024-06-08 16:15:17 +0100

    reference pages wording changes

--- frontend/apps/reader/modules/readerpagemap.lua
+++ frontend/apps/reader/modules/readerpagemap.lua
@@ -217,7 +217,7 @@ function ReaderPageMap:onShowPageList()
     local items_with_dots = G_reader_settings:nilOrTrue("toc_items_with_dots")
 
     local pl_menu = Menu:new{
-        title = _("Reference page numbers list"),
+        title = _("Conversion table (hardback to KOReader pages)"),
         item_table = page_list,
         is_borderless = true,
         is_popout = false,
@@ -317,15 +317,15 @@ end
 
 function ReaderPageMap:addToMainMenu(menu_items)
     menu_items.page_map = {
-        -- @translators This and the other related ones refer to alternate page numbers provided in some EPUB books, that usually reference page numbers in a specific hardcopy edition of the book.
+        -- @translators This and the other related ones refer to alternative page numbers provided in some EPUB books, that usually reference page numbers in a specific hardback edition of the book.
         text = _("Reference pages"),
         sub_item_table ={
             {
-                -- @translators This shows the <dc:source> in the EPUB that usually tells which hardcopy edition the reference page numbers refers to.
+                -- @translators This shows the <dc:source> in the EPUB that usually tells which hardback edition the reference page numbers refers to.
                 text = _("Reference source info"),
                 enabled_func = function() return self.ui.document:getPageMapSource() ~= nil end,
                 callback = function()
-                    local text = T(_("Source (book hardcopy edition) of reference page numbers:\n\n%1"),
+                    local text = T(_("Reference pages sourced from hardback edition:\n\n%1"),
                                     self.ui.document:getPageMapSource())
                     local InfoMessage = require("ui/widget/infomessage")
                     local infomsg = InfoMessage:new{
@@ -336,13 +336,13 @@ function ReaderPageMap:addToMainMenu(menu_items)
                 keep_menu_open = true,
             },
             {
-                text = _("Reference page numbers list"),
+                text = _("Conversion table (hardback to KOReader pages)"),
                 callback = function()
                     self:onShowPageList()
                 end,
             },
             {
-                text = _("Use reference page numbers"),
+                text = _("Use reference page numbers instead"),
                 checked_func = function() return self.use_page_labels end,
                 callback = function()
                     self.use_page_labels = not self.use_page_labels
