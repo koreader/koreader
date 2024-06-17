0e28340963cb93ac6e934926a7c0967bd67c25fb
zwim <36999612+zwim@users.noreply.github.com>
2024-06-17 14:42:54 +0200

    fix initializing and os.time() quirks

--- plugins/readtimer.koplugin/main.lua
+++ plugins/readtimer.koplugin/main.lua
@@ -7,6 +7,7 @@ local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local logger = require("logger")
 local datetime = require("datetime")
+local time = require("ui/time")
 local _ = require("gettext")
 local T = require("ffi/util").template
 
@@ -19,9 +20,6 @@ local ReadTimer = WidgetContainer:extend{
 function ReadTimer:init()
     self.timer_symbol = "\u{23F2}"  -- ⏲ timer symbol
     self.timer_letter = "T"
-
-    self.show_value_in_header = G_reader_settings:readSetting("readtimer_show_value_in_header")
-    self.show_value_in_footer = G_reader_settings:readSetting("readtimer_show_value_in_footer")
 
     self.alarm_callback = function()
         -- Don't do anything if we were unscheduled
@@ -81,6 +79,21 @@ function ReadTimer:init()
         return
     end
 
+    self.show_value_in_header = G_reader_settings:readSetting("readtimer_show_value_in_header")
+    self.show_value_in_footer = G_reader_settings:readSetting("readtimer_show_value_in_footer")
+
+    if self.show_value_in_header then
+        self:addAdditionalHeaderContent()
+    else
+        self:removeAdditionalHeaderContent()
+    end
+
+    if self.show_value_in_footer then
+        self:addAdditionalFooterContent()
+    else
+        self:removeAdditionalFooterContent()
+    end
+
     self.ui.menu:registerToMainMenu(self)
 end
 
@@ -107,9 +120,10 @@ end
 
 function ReadTimer:remaining()
     if self:scheduled() then
-        local td = os.difftime(self.time, os.time())
-        if td > 0 then
-            return td
+        -- Resolution: time.now() subsecond, os.time() two seconds
+        local remaining_s = time.to_s(self.time - time.now())
+        if remaining_s > 0 then
+            return remaining_s
         else
             return 0
         end
@@ -169,7 +183,8 @@ function ReadTimer:unschedule()
 end
 
 function ReadTimer:rescheduleIn(seconds)
-    self.time = os.time() + seconds
+    -- Resolution: time.now() subsecond, os.time() two seconds
+    self.time = time.now() + time.s(seconds)
     UIManager:scheduleIn(seconds, self.alarm_callback)
     if self.show_value_in_header or self.show_value_in_footer then
         self:update_status_bars(seconds)
@@ -237,12 +252,12 @@ function ReadTimer:addToMainMenu(menu_items)
                         ok_text = _("Set alarm"),
                         title_text =  _("New alarm"),
                         info_text = _("Enter a time in hours and minutes."),
-                        callback = function(time)
+                        callback = function(alarm_time)
                             self.last_interval_time = 0
                             self:unschedule()
                             local then_t = now_t
-                            then_t.hour = time.hour
-                            then_t.min = time.min
+                            then_t.hour = alarm_time.hour
+                            then_t.min = alarm_time.min
                             then_t.sec = 0
                             local seconds = os.difftime(os.time(then_t), os.time())
                             if seconds > 0 then
@@ -251,7 +266,7 @@ function ReadTimer:addToMainMenu(menu_items)
                                 UIManager:show(InfoMessage:new{
                                     -- @translators %1:%2 is a clock time (HH:MM), %3 is a duration
                                     text = T(_("Timer set for %1:%2.\n\nThat's %3 from now."),
-                                        string.format("%02d", time.hour), string.format("%02d", time.min),
+                                        string.format("%02d", alarm_time.hour), string.format("%02d", alarm_time.min),
                                         datetime.secondsToClockDuration(user_duration_format, seconds, false)),
                                     timeout = 5,
                                 })
@@ -288,9 +303,9 @@ function ReadTimer:addToMainMenu(menu_items)
                         ok_text = _("Set timer"),
                         title_text =  _("Set reader timer"),
                         info_text = _("Enter a time in hours and minutes."),
-                        callback = function(time)
+                        callback = function(timer_time)
                             self:unschedule()
-                            local seconds = time.hour * 3600 + time.min * 60
+                            local seconds = timer_time.hour * 3600 + timer_time.min * 60
                             if seconds > 0 then
                                 self.last_interval_time = seconds
                                 self:rescheduleIn(seconds)
@@ -301,7 +316,7 @@ function ReadTimer:addToMainMenu(menu_items)
                                              datetime.secondsToClockDuration(user_duration_format, seconds, true)),
                                     timeout = 5,
                                 })
-                                remain_time = {time.hour, time.min}
+                                remain_time = {timer_time.hour, timer_time.min}
                                 G_reader_settings:saveSetting("reader_timer_remain_time", remain_time)
                                 if touchmenu_instance then touchmenu_instance:updateItems() end
                             end
