fa4654cdfa56e7452f6e6ae43d19cd65c105401c
hius07 <62179190+hius07@users.noreply.github.com>
2024-06-19 22:15:43 +0300

    readerbookmark: fix date sort (#12053)

--- frontend/apps/reader/modules/readerbookmark.lua
+++ frontend/apps/reader/modules/readerbookmark.lua
@@ -643,7 +643,8 @@ function ReaderBookmark:onShowBookmark()
     end
     local curr_page_datetime
     if self.sorting_mode == "date" then
-        curr_page_datetime = item_table[curr_page_index_filtered].datetime
+        local idx = math.max(1, math.min(curr_page_index_filtered, #item_table))
+        curr_page_datetime = item_table[idx].datetime
         local sort_func = self.is_reverse_sorting and function(a, b) return a.datetime > b.datetime end
                                                    or function(a, b) return a.datetime < b.datetime end
         table.sort(item_table, sort_func)
