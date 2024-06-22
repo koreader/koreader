bc7bba4e1cec03a392e56315bdffc1501bb69653
ziz57 <mbays+ziz57@sdf.org>
2024-06-22 22:43:29 +0100

    Version: fix outdated documentation of version number (#12074)

--- frontend/version.lua
+++ frontend/version.lua
@@ -25,7 +25,7 @@ end
 
 --- Returns normalized version of KOReader git-rev input string.
 -- @string rev full KOReader git-rev such as `v2015.11-982-g704d4238`
--- @treturn int version in the form of a 10 digit number such as `2015110982`
+-- @treturn int version in the form of a 12 digit number such as `201511000982`
 -- @treturn string short git commit version hash such as `704d4238`
 function Version:getNormalizedVersion(rev)
     if not rev then return end
@@ -42,7 +42,7 @@ function Version:getNormalizedVersion(rev)
 end
 
 --- Returns current version of KOReader.
--- @treturn int version in the form of a 10 digit number such as `2015110982`
+-- @treturn int version in the form of a 12 digit number such as `201511000982`
 -- @treturn string short git commit version hash such as `704d4238`
 -- @see getNormalizedVersion
 function Version:getNormalizedCurrentVersion()
