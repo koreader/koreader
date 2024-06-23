22b29909388bdbd334b5701801c9ca980a3440cd
NiLuJe <ninuje@gmail.com>
2024-06-24 00:06:00 +0200

    Linebreak for clarity

--- frontend/device/kindle/device.lua
+++ frontend/device/kindle/device.lua
@@ -414,7 +414,8 @@ function Kindle:initNetworkManager(NetworkMgr)
                 signal_level = 0,
                 signal_quality = qualities[network.signal],
                 -- See comment above about netid being unfortunately optional...
-                connected = (current_profile.netid and current_profile.netid ~= -1 and current_profile.netid == network.netid) or (current_profile.netid == nil and current_profile.essid ~= "" and current_profile.essid == network.essid),
+                connected = (current_profile.netid and current_profile.netid ~= -1 and current_profile.netid == network.netid)
+                         or (current_profile.netid == nil and current_profile.essid ~= "" and current_profile.essid == network.essid),
                 flags = network.key_mgmt,
                 ssid = network.essid ~= "" and network.essid,
                 password = password,
