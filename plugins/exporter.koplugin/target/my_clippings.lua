b48c972aed6574809b2a5523c28ec8eb416c6a14
Arpan Ghosh <26424944+arpanghosh8453@users.noreply.github.com>
2024-06-28 16:32:35 +0530

    Remove whitespaces lines

--- plugins/exporter.koplugin/target/my_clippings.lua
+++ plugins/exporter.koplugin/target/my_clippings.lua
@@ -16,22 +16,17 @@ local function format(booknotes)
     for ___, entry in ipairs(booknotes) do
         for ____, clipping in ipairs(entry) do
             if booknotes.title and clipping.text then
-
                 local title_str = booknotes.title .. " (" .. (booknotes.author or "Unknown") .. ")"
                 table.insert(tbl, title_str)
-
                 local header = T(_("- Your highlight on page %1 | Added on %2"), clipping.page,
                     os.date("%A, %B %d, %Y %I:%M:%S %p", clipping.time))
                 table.insert(tbl, header)
-
                 table.insert(tbl, "")
                 table.insert(tbl, clipping.text)
                 table.insert(tbl, "==========")
 
                 if clipping.note then
-                    
                     table.insert(tbl, title_str)
-
                     header = T(_("- Your note on page %1 | Added on %2"), clipping.page,
                         os.date("%A, %B %d, %Y %I:%M:%S %p", clipping.time))
                     table.insert(tbl, header)
