e4aeb38e41d5ad77183f6f35a37d2ef88a8d4d53
Benoit Pierre <benoit.pierre@gmail.com>
2024-07-31 01:55:35 +0200

    make: minor cleanups

--- make/sony-prstux.mk
+++ make/sony-prstux.mk
@@ -4,7 +4,7 @@ SONY_PRSTUX_PACKAGE_OTA = koreader-sony-prstux$(KODEDUG_SUFFIX)-$(VERSION).targz
 
 update: all
 	# ensure that the binaries were built for ARM
-	file --dereference $(INSTALL_DIR)/koreader/luajit | grep ARM || exit 1
+	file --dereference $(INSTALL_DIR)/koreader/luajit | grep ARM
 	# remove old package if any
 	rm -f $(SONY_PRSTUX_PACKAGE)
 	# Sony PRSTUX launching scripts
