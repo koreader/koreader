f0708c2015fdd87bdfb806f0990c5e55bbba4908
David <97603719+Commodore64user@users.noreply.github.com>
2024-08-04 02:35:30 +0100

    fix CI

--- frontend/apps/reader/modules/readerhighlight.lua
+++ frontend/apps/reader/modules/readerhighlight.lua
@@ -510,9 +510,9 @@ function ReaderHighlight:addToMainMenu(menu_items)
         },
     }
     -- actions
-    local sub_item_table = {}
+    local sub_item_table_actions = {}
     for i, v in ipairs(long_press_action) do
-        table.insert(sub_item_table, {
+        table.insert(sub_item_table_actions, {
             text = v[1],
             checked_func = function()
                 return G_reader_settings:readSetting("default_highlight_action", "ask") == v[2]
@@ -525,14 +525,14 @@ function ReaderHighlight:addToMainMenu(menu_items)
     end
     table.insert(menu_items.long_press.sub_item_table, {
         text_func = function()
-            local multi_word = G_reader_settings:readSetting("default_highlight_action")
+            local action = G_reader_settings:readSetting("default_highlight_action")
             for __, v in ipairs(long_press_action) do
-                if v[2] == multi_word then
+                if v[2] == action then
                     return T(_("Multi-word selection: %1"), v[1]:lower())
                 end
             end
         end,
-        sub_item_table = sub_item_table,
+        sub_item_table = sub_item_table_actions,
     })
     -- highlight dialog position
     local sub_item_table = {}
