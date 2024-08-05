a6663202b7220e8069a7d1aa9b8d9431acab8e91
Benoit Pierre <benoit.pierre@gmail.com>
2024-08-05 17:09:47 +0000

    NetworkMgr: fix debug trace (#12297)
    Prevent crash:
    ```
    ./luajit: frontend/util.lua:1092: attempt to get length of local 'str' (a nil value)
    stack traceback:
            frontend/util.lua:1092: in function 'fixUtf8'
            frontend/ui/network/manager.lua:1109: in function 'requestToTurnOnWifi'
    […]
    ```

--- frontend/ui/network/manager.lua
+++ frontend/ui/network/manager.lua
@@ -1106,7 +1106,7 @@ function NetworkMgr:reconnectOrShowNetworkMenu(complete_callback, interactive)
             if network.password then
                 -- If we hit a preferred network and we're not already connected,
                 -- attempt to connect to said preferred network....
-                logger.dbg("NetworkMgr: Attempting to authenticate on preferred network", util.fixUtf8(ssid, "�"))
+                logger.dbg("NetworkMgr: Attempting to authenticate on preferred network", util.fixUtf8(network.ssid, "�"))
                 success, err_msg = self:authenticateNetwork(network)
                 if success then
                     ssid = network.ssid
