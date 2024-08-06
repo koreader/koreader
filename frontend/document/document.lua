e0ae5871620f29656a58439c5f64326ae9d393a8
NiLuJe <ninuje@gmail.com>
2024-08-06 20:36:45 +0200

    More comment tweaks

--- frontend/document/document.lua
+++ frontend/document/document.lua
@@ -510,7 +510,7 @@ function Document:renderPage(pageno, rect, zoom, rotation, gamma, hinting)
     -- by pointing at the rotated origin via coordinates offsets.
     -- NOTE: We rotate our *Screen* bb on rotation (SetRotationMode), not the document,
     --       so we hardly ever exercize this codepath...
-    --       AFAICT, the only thing that will *ever* rotate the document is ReaderRotation's key bindings (RotationUpdate).
+    --       AFAICT, the only thing that will *ever* (attempt to) rotate the document is ReaderRotation's key bindings (RotationUpdate).
     --- @fixme: And whaddayano, it's broken ;). The aptly named key binds in question are J/K, I shit you not.
     if rotation == 90 then
         dc:setOffset(page_size.w, 0)
