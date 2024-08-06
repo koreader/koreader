fc7dc9ad6d905b2f4dcaed3b96b3ecbb48df4c0f
NiLuJe <ninuje@gmail.com>
2024-08-06 21:15:54 +0200

    Bis

--- frontend/document/document.lua
+++ frontend/document/document.lua
@@ -433,7 +433,7 @@ function Document:renderPage(pageno, rect, zoom, rotation, gamma, hinting)
 
         tile = DocCache:check(hash, TileCacheItem)
 
-        -- In the is_prescaled case above, we're *already* only rendering part of the page
+        -- In the is_prescaled branch above, we're *already* only rendering part of the page
         if not tile and rect then
             hash_excerpt = hash.."|"..tostring(rect)
             tile = DocCache:check(hash_excerpt)
