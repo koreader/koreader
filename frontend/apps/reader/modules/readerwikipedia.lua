8f473c48a895c9dad1ce31c7907f76ef8daffb1f
David <97603719+Commodore64user@users.noreply.github.com>
2024-08-15 22:40:37 +0100

    making poire-z happy
    rearrange if statement

--- frontend/apps/reader/modules/readerwikipedia.lua
+++ frontend/apps/reader/modules/readerwikipedia.lua
@@ -38,12 +38,14 @@ function ReaderWikipedia:init()
 end
 
 function ReaderWikipedia:registerKeyEvents()
-    if Device:hasKeyboard() and Device.model == "Kindle3" then
-        -- Any user facing documentation should advertise this shortcut as Alt+W, as that is what
-        -- the user must input in order to use it.
-        self.key_events.ShowWikipediaLookup = { { "2" } }
-    elseif Device:hasKeyboard() then
+    if Device:hasKeyboard() then
         self.key_events.ShowWikipediaLookup = { { "Alt", "W" }, { "Ctrl", "W" } }
+        if Device.model == "Kindle3" then
+            -- Override previous shortcut on kindle3 as the kernel designates Alt+W as "2".
+            -- Any user-facing documentation should _still_ advertise this shortcut as Alt+W, as
+            -- that is what the user must input in order to use it.
+            self.key_events.ShowWikipediaLookup = { { "2" } }
+        end
     end
 end
 
