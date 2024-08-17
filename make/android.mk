4ba106a6a33f884f6c9539815333989c848979cd
Benoit Pierre <benoit.pierre@gmail.com>
2024-08-17 22:23:21 +0200

    android: improve update rule
    Remove old in-tree build artifacts that could conflict (`assets/libs` & `assets/module`).

--- make/android.mk
+++ make/android.mk
@@ -68,6 +68,8 @@ update: all
 	# Note: do not remove the module directory so there's no need
 	# for `mk7z.sh` to always recreate `assets.7z` from scratch.
 	rm -rf $(ANDROID_LIBS)
+	# Remove old in-tree build artifacts that could conflict.
+	rm -rf $(ANDROID_LAUNCHER_DIR)/assets/{libs,module}
 	# APK version
 	mkdir -p $(ANDROID_ASSETS)/module $(ANDROID_LIBS)
 	echo $(VERSION) >$(ANDROID_ASSETS)/module/version.txt
