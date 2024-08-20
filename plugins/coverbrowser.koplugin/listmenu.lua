0a3f2007b8fed1f91741b5307b5ea9f0b056f0f0
NiLuJe <ninuje@gmail.com>
2024-08-20 05:15:19 +0200

    Ofc if I'm not looking at the right class, shit makes no sense.
    -_-".
    
    There, fixed item height computation in ListMenu.

--- plugins/coverbrowser.koplugin/listmenu.lua
+++ plugins/coverbrowser.koplugin/listmenu.lua
@@ -899,7 +899,9 @@ function ListMenu:_recalculateDimen()
             self.others_height = self.others_height + 2
         end
         if not self.no_title then
-            self.others_height = self.others_height + self.header_padding
+            if not self.title_bar_fm_style then
+                self.others_height = self.others_height + self.header_padding
+            end
             self.others_height = self.others_height + self.title_bar.dimen.h
         end
         if self.page_info then
