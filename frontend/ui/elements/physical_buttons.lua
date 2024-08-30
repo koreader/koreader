8e6c5914115e900c24ce41b5fd94321d934a8a10
David <97603719+Commodore64user@users.noreply.github.com>
2024-08-30 13:48:07 +0100

    broadcast event on page-turn inversions

--- frontend/ui/elements/physical_buttons.lua
+++ frontend/ui/elements/physical_buttons.lua
@@ -8,7 +8,7 @@ local PhysicalButtons = {
     text = _("Physical buttons"), -- Mainly so as to differentiate w/ "Page Turns" when in readermenu...
     sub_item_table = {
         {
-            text = _("Invert page turn buttons"),
+            text = _("Invert page-turn buttons"),
             enabled_func = function()
                 return not (G_reader_settings:isTrue("input_invert_left_page_turn_keys") or G_reader_settings:isTrue("input_invert_right_page_turn_keys"))
             end,
@@ -16,6 +16,7 @@ local PhysicalButtons = {
                 return G_reader_settings:isTrue("input_invert_page_turn_keys")
             end,
             callback = function()
+                G_reader_settings:flipNilOrFalse("input_invert_page_turn_keys")
                 UIManager:broadcastEvent(Event:new("SwapPageTurnButtons"))
             end,
             separator = true,
@@ -25,7 +26,7 @@ local PhysicalButtons = {
 
 if Device:hasDPad() and Device:useDPadAsActionKeys() then
     table.insert(PhysicalButtons.sub_item_table, {
-        text = _("Invert left-side page turn buttons"),
+        text = _("Invert left-side page-turn buttons"),
         enabled_func = function()
             return not G_reader_settings:isTrue("input_invert_page_turn_keys")
         end,
@@ -34,11 +35,11 @@ if Device:hasDPad() and Device:useDPadAsActionKeys() then
         end,
         callback = function()
             G_reader_settings:flipNilOrFalse("input_invert_left_page_turn_keys")
-            Device:invertButtonsLeft()
+            UIManager:broadcastEvent(Event:new("SwapPageTurnButtons", false, "left"))
         end,
     })
     table.insert(PhysicalButtons.sub_item_table, {
-        text = _("Invert right-side page turn buttons"),
+        text = _("Invert right-side page-turn buttons"),
         enabled_func = function()
             return not G_reader_settings:isTrue("input_invert_page_turn_keys")
         end,
@@ -47,7 +48,7 @@ if Device:hasDPad() and Device:useDPadAsActionKeys() then
         end,
         callback = function()
             G_reader_settings:flipNilOrFalse("input_invert_right_page_turn_keys")
-            Device:invertButtonsRight()
+            UIManager:broadcastEvent(Event:new("SwapPageTurnButtons", false, "right"))
         end,
         separator = true,
     })
