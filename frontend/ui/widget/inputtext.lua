koreader/koreader

commit 9cd305177e223853b9af1fe1ba4e325e2520a24e

FocusManager: Fix focus_flags check in moveFocusTo, and deal with the fallout (#12361)
* FocusManager: Fix `focus_flags` check in `moveFocusTo` (0 is truthy in Lua, can't do AND checks like in C ;).)
* FileManager+FileChooser: Pass our custom title bar directly to FileChooser (which also means we can now use FC's FocusManager layout directly).
* FileChooser/Menu: Get rid of the weird `outer_title_bar` hack, and simply take a `custom_title_bar` pointer to an actual TitleBar instance instead.
* FileManager/Menu/ListMenu/CoverMenu: Fix content height computations in `_recalculateDimen` (all the non-FM cases were including an old and now unused padding value, `self.header_padding`, leading to more blank space at the bottom than necessary, and, worse, leading to different item heights between FM views, possibly leading to unnecessary thumbnail scaling !)
* ButtonDialog: Proper focus management when the ButtonTable is wrapped in a ScrollableContainer.
* ConfigDialog: Implement a stupid workaround for a weird FocusManager issue when going back from `[⋮]` buttons.
* ConfigDialog: Don't move the visual focus in `update` (i.e., we use `NOT_FOCUS` now that it works as intended).
* DictQuickLookup: Ensures the `Menu` key bind does the exact same thing as the hamburger icon.
* DictQuickLookup: Ensure we refocus after having mangled the FocusManager layout (prevents an old focus highlight from lingering on the wrong button).
* FileChooser: Stop flagging it as no_title, because it is *never* without a title. (This behavior was a remnant of the previous FM-specific title bar hacks, which are no longer a thing).
* FileChooser: Stop calling `mergeTitleBarIntoLayout` twice in `updateItems`. We already call Menu's, which handles it. (Prevents the title bar from being added twice to the FocusManager layout).

frontend/ui/widget/inputtext.lua
@@ -142,6 +142,8 @@ local function initTouchEvents()
                 if self.keyboard then
                     self.keyboard:showKeyboard()
                 end
+                -- Make sure we're flagged as in focus again
+                self:focus()
             end
             if self._frame_textwidget.dimen ~= nil -- zh keyboard with candidates shown here has _frame_textwidget.dimen = nil
                     and #self.charlist > 0 then -- do not move cursor within a hint
@@ -297,13 +299,15 @@ end
 local function initDPadEvents()
     if Device:hasDPad() then
         function InputText:onFocus()
-            -- Event called by the focusmanager
+            -- Event sent by focusmanager
             if self.parent.onSwitchFocus then
                 self.parent:onSwitchFocus(self)
             elseif (Device:hasKeyboard() or Device:hasScreenKB()) and G_reader_settings:isFalse("virtual_keyboard_enabled") then
                 do end -- luacheck: ignore 541
             else
-                self:onShowKeyboard()
+                if not self:isKeyboardVisible() then
+                    self:onShowKeyboard()
+                end
             end
             self:focus()
             return true

frontend/apps/filemanager/filemanager.lua
@@ -32,7 +32,6 @@ local ReaderWikipedia = require("apps/reader/modules/readerwikipedia")
 local ReadHistory = require("readhistory")
 local Screenshoter = require("ui/widget/screenshoter")
 local TitleBar = require("ui/widget/titlebar")
-local VerticalGroup = require("ui/widget/verticalgroup")
 local UIManager = require("ui/uimanager")
 local filemanagerutil = require("apps/filemanager/filemanagerutil")
 local lfs = require("libs/libkoreader-lfs")
@@ -146,11 +145,10 @@ function FileManager:setupLayout()
     self:updateTitleBarPath(self.root_path)
 
     local file_chooser = FileChooser:new{
-        -- remember to adjust the height when new item is added to the group
         path = self.root_path,
         focused_path = self.focused_file,
         show_parent = self.show_parent,
-        height = Screen:getHeight() - self.title_bar:getHeight(),
+        height = Screen:getHeight(),
         is_popout = false,
         is_borderless = true,
         file_filter = function(filename) return DocumentRegistry:hasProvider(filename) end,
@@ -159,8 +157,8 @@ function FileManager:setupLayout()
         return_arrow_propagation = true,
         -- allow Menu widget to delegate handling of some gestures to GestureManager
         filemanager = self,
-        -- let Menu widget merge our title_bar into its own TitleBar's FocusManager layout
-        outer_title_bar = self.title_bar,
+        -- Tell FileChooser (i.e., Menu) to use our own title bar instead of Menu's default one
+        custom_title_bar = self.title_bar,
     }
     self.file_chooser = file_chooser
     self.focused_file = nil -- use it only once
@@ -348,16 +346,11 @@ function FileManager:setupLayout()
         return true
     end
 
-    self.layout = VerticalGroup:new{
-        self.title_bar,
-        file_chooser,
-    }
-
     local fm_ui = FrameContainer:new{
         padding = 0,
         bordersize = 0,
         background = Blitbuffer.COLOR_WHITE,
-        self.layout,
+        file_chooser,
     }
 
     self[1] = fm_ui
@@ -365,6 +358,9 @@ function FileManager:setupLayout()
     self.menu = FileManagerMenu:new{
         ui = self
     }
+
+    -- No need to reinvent the wheel, use FileChooser's layout
+    self.layout = file_chooser.layout
 
     self:registerKeyEvents()
 end

frontend/apps/reader/modules/readerbookmark.lua
@@ -1241,7 +1241,9 @@ function ReaderBookmark:setBookmarkNote(item_or_index, is_new_note, new_note, ca
                     text = _("Cancel"),
                     id = "close",
                     callback = function()
-                        UIManager:close(input_dialog)
+                        -- NOTE: We'll want a full refresh on close, as the CRe highlight may extend past our own dimensions,
+                        --       especially if we're closed separately from our VirtualKeyboard.
+                        UIManager:close(input_dialog, "flashui")
                         if is_new_note then -- "Add note" called from highlight dialog and cancelled, remove saved highlight
                             self:removeItemByIndex(index)
                         end

frontend/ui/widget/buttondialog.lua
@@ -44,10 +44,10 @@ local ButtonTable = require("ui/widget/buttontable")
 local CenterContainer = require("ui/widget/container/centercontainer")
 local Device = require("device")
 local Font = require("ui/font")
+local FocusManager = require("ui/widget/focusmanager")
 local FrameContainer = require("ui/widget/container/framecontainer")
 local Geom = require("ui/geometry")
 local GestureRange = require("ui/gesturerange")
-local InputContainer = require("ui/widget/container/inputcontainer")
 local LineWidget = require("ui/widget/linewidget")
 local MovableContainer = require("ui/widget/container/movablecontainer")
 local ScrollableContainer = require("ui/widget/container/scrollablecontainer")
@@ -59,7 +59,7 @@ local VerticalSpan = require("ui/widget/verticalspan")
 local Screen = Device.screen
 local util = require("util")
 
-local ButtonDialog = InputContainer:extend{
+local ButtonDialog = FocusManager:extend{
     buttons = nil,
     width = nil,
     width_factor = nil, -- number between 0 and 1, factor to the smallest of screen width and height
@@ -152,6 +152,7 @@ function ButtonDialog:init()
         title_widget = VerticalSpan:new{}
         title_widget_height = 0
     end
+    self.top_to_content_offset = Size.padding.buttontable + Size.margin.default + title_widget_height
 
     -- If the ButtonTable ends up being taller than the screen, wrap it inside a ScrollableContainer.
     -- Ensure some small top and bottom padding, so the scrollbar stand out, and some outer margin
@@ -233,6 +234,11 @@ function ButtonDialog:init()
         }
     }
 
+    -- No need to reinvent the wheel, ButtonTable's layout is perfect as-is
+    self.layout = self.buttontable.layout
+    -- But we'll want to control focus in its place, though
+    self.buttontable.layout = nil
+
     self[1] = CenterContainer:new{
         dimen = Screen:getSize(),
         self.movable,
@@ -294,8 +300,30 @@ function ButtonDialog:onTapClose(arg, ges)
 end
 
 function ButtonDialog:paintTo(...)
-    InputContainer.paintTo(self, ...)
+    FocusManager.paintTo(self, ...)
     self.dimen = self.movable.dimen
+end
+
+function ButtonDialog:onFocusMove(args)
+    local ret = FocusManager.onFocusMove(self, args)
+
+    -- If we're using a ScrollableContainer, ask it to scroll to the focused item
+    if self.cropping_widget then
+        local focus = self:getFocusItem()
+        if self.dimen and focus and focus.dimen then
+            local button_y_offset = focus.dimen.y - self.dimen.y - self.top_to_content_offset
+            -- NOTE: The final argument ensures we'll always keep the neighboring item visible.
+            --       (i.e., the top/bottom of the scrolled view is actually the previous/next item).
+            self.cropping_widget:_scrollBy(0, button_y_offset, true)
+        end
+    end
+
+    return ret
+end
+
+function ButtonDialog:_onPageScrollToRow(row)
+    -- ScrollableContainer will pass us the row number of the top widget at the current scroll offset
+    self:moveFocusTo(1, row)
 end
 
 return ButtonDialog

frontend/ui/widget/buttontable.lua
@@ -222,7 +222,7 @@ function ButtonTable:getStepScrollGrid()
         local row_num = 1
         while idx <= #self.container do
             local row = {
-                row_num = row_num, -- (not used, but may help with debugging)
+                row_num = row_num, -- (used by ScrollableContainer, and generally helpful for debugging)
                 top = offsets[idx].y, -- top of our vspan above text
                 content_top = offsets[idx+1].y, -- top of our text widget
                 content_bottom = offsets[idx+2].y - 1, -- bottom of our text widget

frontend/ui/widget/configdialog.lua
@@ -687,7 +687,7 @@ function ConfigOption:_itemGroupToLayoutLine(option_items_group)
             if v.layout and v.disableFocusManagement then -- it is a FocusManager
                 -- merge child layout to one row layout
                 -- currently child widgets are all one row
-                -- need improved if two or more rows widget existed
+                -- needs improvement if we ever implement widgets with two or more rows
                 for _, row in ipairs(v.layout) do
                     for _, widget in ipairs(row) do
                         layout_line[j] = widget
@@ -910,7 +910,7 @@ end
 function ConfigDialog:updateConfigPanel(index) end
 
 function ConfigDialog:update()
-    self:moveFocusTo(1, 1) -- reset selected for re-created layout
+    self:moveFocusTo(1, 1, FocusManager.NOT_FOCUS) -- reset selected for re-created layout
     self.layout = {}
 
     if self.config_menubar then
@@ -1134,6 +1134,11 @@ function ConfigDialog:onConfigMoreChoose(values, default_value_orig, name, event
                 UIManager:setDirty(self, function()
                     return "ui", self.dialog_frame.dimen
                 end)
+                -- FocusManager loses its marbles (we can only navigate on the row of the selected option) if we don't update the widget *again*...
+                -- (possibly because of the layout nastiness happening in ConfigOption:init)
+                if Device:hasDPad() then
+                    self:update()
+                end
             end
         end
         local hide_on_picker_show = more_options_param.hide_on_picker_show

frontend/ui/widget/container/scrollablecontainer.lua
@@ -632,11 +632,20 @@ function ScrollableContainer:onScrollablePanRelease(_, ges)
     return false
 end
 
+function ScrollableContainer:_notifyParentOfPageScroll()
+    -- For ButtonDialog's focus shenanigans, as we ourselves are not a FocusManager
+    if self.show_parent and self.show_parent._onPageScrollToRow then
+        local top_row = self:_getStepScrollRowAtY(self._scroll_offset_y, true)
+        self.show_parent:_onPageScrollToRow(top_row and top_row.row_num or 1)
+    end
+end
+
 function ScrollableContainer:onScrollPageUp()
     if not self._is_scrollable then
         return false
     end
     self:_scrollBy(0, -self._crop_h, true)
+    self:_notifyParentOfPageScroll()
     return true
 end
 
@@ -645,6 +654,7 @@ function ScrollableContainer:onScrollPageDown()
         return false
     end
     self:_scrollBy(0, self._crop_h, true)
+    self:_notifyParentOfPageScroll()
     return true
 end
 

frontend/ui/widget/dictquicklookup.lua
@@ -104,7 +104,7 @@ function DictQuickLookup:init()
         self.key_events.ReadPrevResult = { { Input.group.PgBack } }
         self.key_events.ReadNextResult = { { Input.group.PgFwd } }
         self.key_events.Close = { { Input.group.Back } }
-        self.key_events.ShowResultsMenu = { { "Menu" } }
+        self.key_events.MenuKeyPress = { { "Menu" } }
         if Device:hasKeyboard() then
             self.key_events.ChangeToPrevDict = { { "Shift", "Left" } }
             self.key_events.ChangeToNextDict = { { "Shift", "Right" } }
@@ -726,10 +726,12 @@ function DictQuickLookup:init()
 
     -- NT: add dict_title.left_button and lookup_edit_button to FocusManager.
     -- It is better to add these two buttons into self.movable, but it is not a FocusManager.
-    -- Only self.button_table is a FocusManager, so workaground is inserting these two buttons into self.button_table.layout.
+    -- Only self.button_table is a FocusManager, so the workaround is inserting these two buttons into self.button_table.layout.
     if Device:hasDPad() then
-        table.insert(self.button_table.layout, 1, { self.dict_title.left_button });
-        table.insert(self.button_table.layout, 2, { lookup_edit_button });
+        table.insert(self.button_table.layout, 1, { self.dict_title.left_button })
+        table.insert(self.button_table.layout, 2, { lookup_edit_button })
+        -- Refocus on the updated layout
+        self.button_table:refocusWidget()
     end
 
     -- We're a new window
@@ -1096,6 +1098,10 @@ function DictQuickLookup:onReadPrevResult()
         self.definition_widget[1]:scrollToRatio(1) -- 1 = 100% = bottom
     end
     return true
+end
+
+function DictQuickLookup:onMenuKeyPress()
+    return self.dict_title.left_icon_tap_callback()
 end
 
 function DictQuickLookup:onTap(arg, ges_ev)

commit 283f4fcd07908d5f5513fbf00ef5ba29aecbfc9f

InputText: Unbreak onTapTextBox on an already in-focus field
The drive-by fix for the original issue noticed late in #12361 turned
out to be a bit heavy-handed ;).

Fix #12444
Regression since #12361

frontend/ui/widget/inputtext.lua
@@ -142,8 +142,11 @@ local function initTouchEvents()
                 if self.keyboard then
                     self.keyboard:showKeyboard()
                 end
-                -- Make sure we're flagged as in focus again
-                self:focus()
+                -- Make sure we're flagged as in focus again.
+                -- NOTE: self:focus() does a full free/reinit cycle, which is completely unnecessary to begin with,
+                --       *and* resets cursor position, which is problematic when tapping on an already in-focus field (#1244).
+                --       So, just flip our own focused flag, that's the only thing we need ;).
+                self.focused = true
             end
             if self._frame_textwidget.dimen ~= nil -- zh keyboard with candidates shown here has _frame_textwidget.dimen = nil
                     and #self.charlist > 0 then -- do not move cursor within a hint