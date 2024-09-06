6dc8144178c9f5339d21b469681ce057dc69c51d
David <97603719+Commodore64user@users.noreply.github.com>
2024-09-07 00:11:00 +0100

    Hold on, revert that

--- frontend/device/devicelistener.lua
+++ frontend/device/devicelistener.lua
@@ -332,7 +332,6 @@ function DeviceListener:onSwapPageTurnButtons(side)
         Device:invertButtonsLeft()
         if G_reader_settings:isTrue("input_invert_left_page_turn_keys") then
             new_text = _("Left-side page-turn buttons inverted.")
-            G_reader_settings:makeFalse("input_invert_page_turn_keys")
         else
             new_text = _("Left-side page-turn buttons no longer inverted.")
         end
@@ -341,7 +340,6 @@ function DeviceListener:onSwapPageTurnButtons(side)
         Device:invertButtonsRight()
         if G_reader_settings:isTrue("input_invert_right_page_turn_keys") then
             new_text = _("Right-side page-turn buttons inverted.")
-            G_reader_settings:makeFalse("input_invert_page_turn_keys")
         else
             new_text = _("Right-side page-turn buttons no longer inverted.")
         end
@@ -350,8 +348,6 @@ function DeviceListener:onSwapPageTurnButtons(side)
         Device:invertButtons()
         if G_reader_settings:isTrue("input_invert_page_turn_keys") then
             new_text = _("Page-turn buttons inverted.")
-            G_reader_settings:makeFalse("input_invert_left_page_turn_keys")
-            G_reader_settings:makeFalse("input_invert_right_page_turn_keys")
         else
             new_text = _("Page-turn buttons no longer inverted.")
         end
