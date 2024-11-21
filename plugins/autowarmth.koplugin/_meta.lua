7af2e97fc6363ea8653b27abbb311ffa6c86156e
mergen3107 <6279855+mergen3107@users.noreply.github.com>
2024-11-21 11:45:43 -0500

    Update plugins/autowarmth.koplugin/_meta.lua
    Co-authored-by: Frans de Jonge <fransdejonge@gmail.com>

--- plugins/autowarmth.koplugin/_meta.lua
+++ plugins/autowarmth.koplugin/_meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     name = "autowarmth",
     fullname = require("device"):hasNaturalLight() and _("Auto warmth and night mode") or _("Auto night mode"),
-    description = require("device"):hasNaturalLight() and _([[This plugin allows to set the frontlight warmth and night mode automagically.]]) or _([[This plugin allows to set night mode automagically.]]),
+    description = require("device"):hasNaturalLight() and _([[This plugin allows to set the frontlight warmth and night mode automagically.]]) or _([[This plugin allows to enable night mode automagically.]]),
 }
