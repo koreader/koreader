fc74ead740bfabc573a7c03d57b14435188e15bb
Benoit Pierre <benoit.pierre@gmail.com>
2024-11-24 23:05:37 +0100

    ci/circle: speed up tests / coverage
    Instead of running the testsuite twice when coverage is need,
    only run it once (without filtering out `nocov` tests): with
    `cluacov` installed, it's only 50% slower than a normal run.

--- .ci/after_success.sh
+++ .ci/after_success.sh
@@ -7,11 +7,9 @@ source "${CI_DIR}/common.sh"
 set +e
 
 if [ -z "${CIRCLE_PULL_REQUEST}" ] && [ "${CIRCLE_BRANCH}" = 'master' ]; then
-    travis_retry make --assume-old=all coverage
-    pushd install/koreader && {
+    echo -e "\\n${ANSI_GREEN}Uploading coverage."
+    cd install/koreader && {
         # see https://github.com/codecov/example-lua
         bash <(curl -s https://codecov.io/bash)
-    } && popd || exit
-else
-    echo -e "\\n${ANSI_GREEN}Not on official master branch. Skipping coverage."
+    }
 fi
--- .ci/test.sh
+++ .ci/test.sh
@@ -4,6 +4,14 @@ CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
 source "${CI_DIR}/common.sh"
 
-make testfront --assume-old=all T="-o '${PWD}/test-results.xml'"
+if [[ -z "${CIRCLE_PULL_REQUEST}" ]] && [[ "${CIRCLE_BRANCH}" == 'master' ]]; then
+    # We're on master: do a full testsuite run with coverage.
+    target='coverage'
+else
+    # Pull request / not on master: do a regular testsuite run.
+    target='testfront'
+fi
+
+make "${target}" --assume-old=all T="-o '${PWD}/test-results.xml'"
 
 # vim: sw=4
