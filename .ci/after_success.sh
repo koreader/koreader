ea5d42486868aeb2754804571ce54fcfd68e4e99
Benoit Pierre <benoit.pierre@gmail.com>
2024-11-24 23:05:36 +0100

    tests: use new runner

--- .ci/after_success.sh
+++ .ci/after_success.sh
@@ -7,19 +7,11 @@ source "${CI_DIR}/common.sh"
 set +e
 
 if [ -z "${CIRCLE_PULL_REQUEST}" ] && [ "${CIRCLE_BRANCH}" = 'master' ]; then
-    echo "CIRCLE_NODE_INDEX: ${CIRCLE_NODE_INDEX}"
-    if [ "${CIRCLE_NODE_INDEX}" = 1 ]; then
-        echo -e "\\n${ANSI_GREEN}Running make testfront for timings."
-        make --assume-old=all testfront BUSTED_OVERRIDES="--output=junit -Xoutput junit-test-results.xml"
-    fi
-
-    if [ "${CIRCLE_NODE_INDEX}" = 0 ]; then
-        travis_retry make --assume-old=all coverage
-        pushd install/koreader && {
-            # see https://github.com/codecov/example-lua
-            bash <(curl -s https://codecov.io/bash)
-        } && popd || exit
-    fi
+    travis_retry make --assume-old=all coverage
+    pushd install/koreader && {
+        # see https://github.com/codecov/example-lua
+        bash <(curl -s https://codecov.io/bash)
+    } && popd || exit
 else
     echo -e "\\n${ANSI_GREEN}Not on official master branch. Skipping coverage."
 fi
