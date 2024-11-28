18a17a7455b2f9fad0e377444bc11b1d27ad2e70
Martín Fernández <paziusss@gmail.com>
2024-11-28 21:05:52 +0100

    remove background-runner

--- plugins/backgroundrunner.koplugin/_meta.lua
+++
@@ -1,6 +0,0 @@
-local _ = require("gettext")
-return {
-    name = "backgroundrunner",
-    fullname = _("Background runner"),
-    description = _([[Service to other plugins: allows tasks to run regularly in the background.]]),
-}
