cf927f53e07b4408f1004ad760772782622a7be6
David <97603719+Commodore64user@users.noreply.github.com>
2024-12-03 22:08:15 +0000

    Update plugins/profiles.koplugin/main.lua

--- plugins/profiles.koplugin/main.lua
+++ plugins/profiles.koplugin/main.lua
@@ -435,7 +435,7 @@ function Profiles:updateProfiles(action_old_name, action_new_name)
     end
     if self.ui.gestures then -- search and update the profile action in assigned gestures
         self.ui.gestures:updateProfiles(action_old_name, action_new_name)
-    elseif self.ui.hotkeyshortcuts then -- search and update the profile action in assigned keyboard shortcuts
+    elseif self.ui.hotkeys then -- search and update the profile action in assigned keyboard shortcuts
         self.ui.hotkeyshortcuts:updateProfiles(action_old_name, action_new_name)
     end
 end
