a11e6ede58585d5189f029c33bd4104833046726
Frans de Jonge <frans@clevercast.com>
2024-12-04 22:47:40 +0100

    [plugin] Patch management: fix disabling plugin
    Fixes #12841.

--- plugins/patchmanagement.koplugin/_meta.lua
+++ plugins/patchmanagement.koplugin/_meta.lua
@@ -1,6 +1,6 @@
 local _ = require("gettext")
 return {
-    name = "patch_management",
+    name = "patchmanagement",
     fullname = _("Patch management"),
     description = _("This plugin allows enabling, disabling or editing user patches."),
 }
