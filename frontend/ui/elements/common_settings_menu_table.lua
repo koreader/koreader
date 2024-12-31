b1940eb4d634e316fa06d487f90b5e0b4d1e2bf9
hius07 <62179190+hius07@users.noreply.github.com>
2024-12-31 08:54:05 +0200

    End of document menu: minor fix (#12962)

--- frontend/ui/elements/common_settings_menu_table.lua
+++ frontend/ui/elements/common_settings_menu_table.lua
@@ -283,7 +283,16 @@ if Device:isTouchDevice() then
             UIManager:broadcastEvent(Event:new("IgnoreHoldCorners"))
         end,
     }
-    common_settings.screen_disable_double_tab = dofile("frontend/ui/elements/screen_disable_double_tap_table.lua")
+    common_settings.screen_disable_double_tap = {
+        text = _("Disable double tap"),
+        checked_func = function()
+            return G_reader_settings:nilOrTrue("disable_double_tap")
+        end,
+        callback = function()
+            G_reader_settings:flipNilOrTrue("disable_double_tap")
+            UIManager:askForRestart()
+        end,
+    }
     common_settings.menu_activate = dofile("frontend/ui/elements/menu_activate.lua")
 end
 
@@ -685,7 +694,8 @@ common_settings.document_end_action = {
         {
             text = _("Open next file"),
             enabled_func = function()
-                return G_reader_settings:readSetting("collate") ~= "access"
+                local collate = G_reader_settings:readSetting("collate")
+                return collate ~= "access" and collate ~= "date"
             end,
             checked_func = function()
                 return G_reader_settings:readSetting("end_document_action") == "next_file"
