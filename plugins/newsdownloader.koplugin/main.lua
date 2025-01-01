d18650ae96f9e06be97e42d66957432db245c7a8
Frans de Jonge <frans@clevercast.com>
2025-01-01 10:55:33 +0100

    [plugin] NewsDownloader: reduce HTML idiosyncracies

--- plugins/newsdownloader.koplugin/main.lua
+++ plugins/newsdownloader.koplugin/main.lua
@@ -610,11 +610,18 @@ function NewsDownloader:createFromDescription(feed, title, content, feed_output_
         logger.dbg("NewsDownloader: News file will be stored to :", news_file_path)
         local article_message = T(_("%1\n%2"), message, title_with_date)
         local footer = _("If this is only a summary, the full article can be downloaded by going to the News Downloader settings and changing 'Download full article' to 'true'.")
-        local html = string.format([[<!DOCTYPE html>
+        local html = string.format([[
+<!DOCTYPE html>
 <html>
-<head><meta charset='UTF-8'><title>%s</title></head>
-<body><header><h2>%s</h2></header><article>%s</article>
-<br><footer><small>%s</small></footer>
+<head>
+<meta charset="UTF-8">
+<title>%s</title>
+</head>
+<body>
+<header><h1>%s</h1></header>
+<article>%s</article>
+<br>
+<footer><small>%s</small></footer>
 </body>
 </html>]], title, title, content, footer)
         local link = getFeedLink(feed.link)
