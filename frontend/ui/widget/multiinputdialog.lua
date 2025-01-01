d294a807aab3ce22ac0f12818b7f5122a26fe8a0
Commodore64user <97603719+Commodore64user@users.noreply.github.com>
2025-01-01 13:31:59 +0000

    improve comment

--- frontend/ui/widget/multiinputdialog.lua
+++ frontend/ui/widget/multiinputdialog.lua
@@ -110,7 +110,8 @@ function MultiInputDialog:init()
     self:onCloseKeyboard()
     self._input_widget:onCloseWidget()
 
-    -- Update keyboard_visible
+    -- Update self.keyboard_visible, this is needed because InputDialog:onCloseKeyboard updates it regardless of whether
+    -- G_reader_settings("virtual_keyboard_enabled") is disabled, which can lead to an incorrect keyboard visibility state.
     if (Device:hasKeyboard() or Device:hasScreenKB()) and G_reader_settings:isFalse("virtual_keyboard_enabled") then
         self.keyboard_visible = false
     end
