e3f25f42359fd8c1cdf9789b6e84141d9933e0ae
hius07 <62179190+hius07@users.noreply.github.com>
2025-02-03 09:08:20 +0200

    readerview: fix page boxes cache

--- frontend/apps/reader/modules/readerview.lua
+++ frontend/apps/reader/modules/readerview.lua
@@ -1167,6 +1167,20 @@ function ReaderView:resetHighlightBoxesCache(items)
                 end
             end
         end
+        if items.index_modified then -- annotation added or removed, shift annotations indexes
+            local index, delta = items.index_modified, 1
+            if index < 0 then
+                index, delta = -index, -1
+            end
+            for _, page_boxes in pairs(self.highlight.page_boxes) do
+                for i, box in ipairs(page_boxes) do
+                    if box.index > index or
+                            (box.index == index and items[1].datetime ~= self.ui.annotation.annotations[box.index].datetime) then
+                        box.index = box.index + delta
+                    end
+                end
+            end
+        end
     end
 end
 
