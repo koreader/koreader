dfc59307914199a74fde80f9b7ca20c1a1894a49
hius07 <62179190+hius07@users.noreply.github.com>
2025-02-03 10:12:53 +0200

    Update readerview.lua

--- frontend/apps/reader/modules/readerview.lua
+++ frontend/apps/reader/modules/readerview.lua
@@ -1174,9 +1174,7 @@ function ReaderView:resetHighlightBoxesCache(items)
             end
             for _, page_boxes in pairs(self.highlight.page_boxes) do
                 for i, box in ipairs(page_boxes) do
-                    if box.index > index or
-                            (box.index == index and delta > 0 and self.ui.annotation.annotations[index + 1] and
-                                items[1].datetime ~= self.ui.annotation.annotations[index + 1].datetime) then
+                    if box.index >= index then
                         box.index = box.index + delta
                     end
                 end
