636f79ab945179843e36da315bbbe9da7d51f662
Commodore64user <97603719+Commodore64user@users.noreply.github.com>
2025-02-04 14:19:47 +0000

    use self.value_step directly

--- frontend/ui/widget/spinwidget.lua
+++ frontend/ui/widget/spinwidget.lua
@@ -348,9 +348,8 @@ This method updates the widget's value based on the direction of the spin.
 ]]
 function SpinWidget:onSpinButtonPressed(direction)
     local widget = self.value_widget
-    local step = self.value_step or 1
     -- Use the widget's changeValue method but don't update the stored value directly
-    local new_value = widget:changeValue(widget:getValue(), direction * step, widget.value_max, widget.value_min, false)
+    local new_value = widget:changeValue(widget:getValue(), self.value_step * direction, widget.value_max, widget.value_min, false)
     widget.value = new_value
     widget:update()
     return true
