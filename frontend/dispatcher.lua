6ce095abfc5cbebe66f688faa2a74eb4ae1de9af
hius07 <62179190+hius07@users.noreply.github.com>
2025-02-09 12:02:18 +0200

    dispatcher: fix remove actions

--- frontend/dispatcher.lua
+++ frontend/dispatcher.lua
@@ -942,17 +942,15 @@ function Dispatcher:_addItem(caller, menu, location, settings, section)
 end
 
 function Dispatcher.removeActions(actions, do_remove)
-    if actions then
-        local count = Dispatcher:_itemsCount(actions)
-        if count == 1 then
-            do_remove()
-        elseif count > 1 then
-            local ConfirmBox = require("ui/widget/confirmbox")
-            UIManager:show(ConfirmBox:new{
-                text = T(NC_("Dispatcher", "1 action will be removed.", "%1 actions will be removed.", count), count),
-                ok_callback = do_remove,
-            })
-        end
+    local count = actions and Dispatcher:_itemsCount(actions) or 0
+    if count > 1 then
+        local ConfirmBox = require("ui/widget/confirmbox")
+        UIManager:show(ConfirmBox:new{
+            text = T(NC_("Dispatcher", "1 action will be removed.", "%1 actions will be removed.", count), count),
+            ok_callback = do_remove,
+        })
+    else
+        do_remove()
     end
 end
 
