2346daab091c8ff5e765388c6d48e86ba1214730
Martín Fernández <paziusss@gmail.com>
2025-02-11 16:06:36 +0100

    tick tack

--- plugins/calibre.koplugin/main.lua
+++ plugins/calibre.koplugin/main.lua
@@ -54,7 +54,7 @@ function Calibre:onStartWirelessConnection()
             text = _("Connecting to calibre")
         })
     end)
-    UIManager:scheduleIn(0.1, function()
+    UIManager:tickAfterNext(function()
         self:startWirelessConnection()
     end)
 end
