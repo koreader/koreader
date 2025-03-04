b8c20abbfc02f71ac1731bac7bfb53e8ff049c39
Commodore64user <97603719+Commodore64user@users.noreply.github.com>
2025-03-04 22:41:36 +0000

    fix half focus

--- frontend/ui/widget/focusmanager.lua
+++ frontend/ui/widget/focusmanager.lua
@@ -156,19 +156,23 @@ function FocusManager:onFocusHalfMove(args)
             dy = #self.layout - y -- last row
         end
     elseif direction == "left" then
-        dx = - math.floor(#row / 2)
+        dx = BD.mirroredUILayout() and math.floor(#row / 2) or -math.floor(#row / 2)
         if dx == 0 then
             dx = -1
         elseif dx + x <= 0 then
             dx = -x + 1 -- first column
         end
     elseif direction == "right" then
-        dx = math.floor(#row / 2)
+        dx = BD.mirroredUILayout() and -math.floor(#row / 2) or math.floor(#row / 2)
         if dx == 0 then
             dx = 1
         elseif dx + x > #row then
             dx = #row - x -- last column
         end
+    end
+    -- Flip horizontal direction in RTL mode
+    if dx ~= 0 and BD.mirroredUILayout() then
+        dx = -dx
     end
     return self:onFocusMove({dx, dy})
 end
