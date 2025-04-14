9a05f5ebc9916550fdf40c7d244938c9c1fd63c8
Commodore64user <97603719+Commodore64user@users.noreply.github.com>
2025-04-14 01:13:19 +0100

    fix function

--- frontend/ui/elements/screensaver_menu.lua
+++ frontend/ui/elements/screensaver_menu.lua
@@ -14,10 +14,12 @@ local function isReaderProgressEnabled()
 end
 
 local function isRandomImageFolder()
+    local ignore_book_cover = G_reader_settings:isTrue("screensaver_exclude_on_hold_books")
+        or G_reader_settings:isTrue("screensaver_exclude_finished_books")
+        or G_reader_settings:isTrue("screensaver_hide_cover_in_filemanager")
     return G_reader_settings:readSetting("screensaver_type") == "random_image"
-            or G_reader_settings:isTrue("screensaver_exclude_on_hold_books")
-            or G_reader_settings:isTrue("screensaver_exclude_finished_books")
-            or G_reader_settings:isTrue("screensaver_hide_cover_in_filemanager")
+            or (G_reader_settings:readSetting("screensaver_type") == "cover"
+            and ignore_book_cover)
 end
 
 local function genMenuItem(text, setting, value, enabled_func, separator)
