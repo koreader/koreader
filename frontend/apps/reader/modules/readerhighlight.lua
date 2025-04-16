c8fe09b623af5009fc356fe9e67dfd144099e999
Commodore64user <97603719+Commodore64user@users.noreply.github.com>
2025-04-16 22:58:44 +0100

    change for-loop value

--- frontend/apps/reader/modules/readerhighlight.lua
+++ frontend/apps/reader/modules/readerhighlight.lua
@@ -894,8 +894,9 @@ Except when in two columns mode, where this is limited to showing only the previ
                 }
             }
         }
+        local post_long_press_action = #menu_items.selection_text.sub_item_table + #long_press_action -- index after long_press_action
         -- Copy remaining items (anything after long_press_action) directly to selection_text's sub_item_table
-        for i = #long_press_action + 2, #menu_items.long_press.sub_item_table do
+        for i = post_long_press_action, #menu_items.long_press.sub_item_table do
             table.insert(menu_items.selection_text.sub_item_table, menu_items.long_press.sub_item_table[i])
         end
         menu_items.long_press = nil
