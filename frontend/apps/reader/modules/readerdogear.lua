466cbfed3d6b8f62a1d2a24a054bc2aa66d23f93
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2025-04-29 14:06:33 +0200

    feat: make dogears clearer when in dual page mode
    When in dual page mode, show dog ears on both pages
    to illustrate which page has been bookmarked.
    
    Change-Id: 2a67387f44acbfe2a9f37995cf40a8a0

--- frontend/apps/reader/modules/readerdogear.lua
+++ frontend/apps/reader/modules/readerdogear.lua
@@ -3,12 +3,21 @@ local Device = require("device")
 local Geom = require("ui/geometry")
 local IconWidget = require("ui/widget/iconwidget")
 local RightContainer = require("ui/widget/container/rightcontainer")
+local LeftContainer= require("ui/widget/container/leftcontainer")
 local VerticalGroup = require("ui/widget/verticalgroup")
 local VerticalSpan = require("ui/widget/verticalspan")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local Screen = Device.screen
+local logger = require("logger")
 
 local ReaderDogear = WidgetContainer:extend{}
+
+-- These constants are used to instruct ReaderDogear on which corner to paint the dogear
+-- This is mainly used in Dual Page mode
+-- Default is right top corner
+SIDE_LEFT = 1
+SIDE_RIGHT = 2
+SIDE_BOTH = 3
 
 function ReaderDogear:init()
     -- This image could be scaled for DPI (with scale_for_dpi=true, scale_factor=0.7),
@@ -20,9 +29,19 @@ function ReaderDogear:init()
     self.dogear_min_size = math.ceil(math.min(Screen:getWidth(), Screen:getHeight()) * (1/40))
     self.dogear_max_size = math.ceil(math.min(Screen:getWidth(), Screen:getHeight()) * (1/32))
     self.dogear_size = nil
-    self.icon = nil
-    self.dogear_y_offset = 0
+
+    self.icon_right = nil
+    self.icon_left = nil
+    self.vgroup_right = nil
+    self.vgroup_left = nil
     self.top_pad = nil
+
+    self.right_ear = nil
+    self.left_ear = nil
+
+    self.dogear_y_offset = 0
+    self.dimen = nil
+    self.sides = SIDE_RIGHT
     self:setupDogear()
     self:resetLayout()
 end
@@ -33,26 +52,68 @@ function ReaderDogear:setupDogear(new_dogear_size)
     end
     if new_dogear_size ~= self.dogear_size then
         self.dogear_size = new_dogear_size
-        if self[1] then
-            self[1]:free()
+
+        if self.right_ear then
+            self.right_ear:free()
         end
-        self.icon = IconWidget:new{
+
+        if self.left_ear then
+            self.left_ear:free()
+        end
+
+        self.top_pad = VerticalSpan:new { width = self.dogear_y_offset }
+
+        self.icon_right = IconWidget:new {
             icon = "dogear.alpha",
             rotation_angle = BD.mirroredUILayout() and 90 or 0,
             width = self.dogear_size,
             height = self.dogear_size,
             alpha = true, -- Keep the alpha layer intact
         }
-        self.top_pad = VerticalSpan:new{width = self.dogear_y_offset}
-        self.vgroup = VerticalGroup:new{
+
+        self.vgroup_right = VerticalGroup:new {
             self.top_pad,
-            self.icon,
+            self.icon_right,
         }
-        self[1] = RightContainer:new{
-            dimen = Geom:new{w = Screen:getWidth(), h = self.dogear_y_offset + self.dogear_size},
-            self.vgroup
+
+        self.right_ear = RightContainer:new {
+            dimen = Geom:new { w = Screen:getWidth(), h = self.dogear_y_offset + self.dogear_size },
+            self.vgroup_right
+        }
+
+        self.icon_left = IconWidget:new {
+            icon = "dogear.alpha",
+            rotation_angle = self.icon_right.rotation_angle + 90,
+            width = self.dogear_size,
+            height = self.dogear_size,
+            alpha = true, -- Keep the alpha layer intact
+        }
+
+        self.vgroup_left = VerticalGroup:new {
+            self.top_pad,
+            self.icon_left,
+        }
+
+        self.left_ear = LeftContainer:new {
+            dimen = Geom:new { w = Screen:getWidth(), h = self.dogear_y_offset + self.dogear_size },
+            self.vgroup_left
         }
     end
+end
+
+function ReaderDogear:paintTo(bb, x, y)
+    logger.dbg("ReaderDogear:paintTo with sides", self.sides)
+
+    if self.sides == SIDE_RIGHT or self.sides == SIDE_BOTH then
+        self.right_ear:paintTo(bb, x, y)
+    end
+
+    -- Exit early if we don't need to paint left side.
+    if self.sides ~= SIDE_LEFT and self.sides ~= SIDE_BOTH then
+        return
+    end
+
+    self.left_ear:paintTo(bb, x, y)
 end
 
 function ReaderDogear:onReadSettings(config)
@@ -88,11 +149,19 @@ function ReaderDogear:updateDogearOffset()
     if self.view.view_mode == "page" then
         self.dogear_y_offset = self.ui.document:getHeaderHeight()
     end
-    -- Update components heights and positionnings
-    if self[1] then
-        self[1].dimen.h = self.dogear_y_offset + self.dogear_size
+
+    if self.right_ear or self.left_ear then
         self.top_pad.width = self.dogear_y_offset
-        self.vgroup:resetLayout()
+    end
+
+    if self.right_ear then
+        self.right_ear.dimen.h = self.dogear_y_offset + self.dogear_size
+        self.vgroup_right:resetLayout()
+    end
+
+    if self.left_ear then
+        self.left_ear.dimen.h = self.dogear_y_offset + self.dogear_size
+        self.vgroup_left:resetLayout()
     end
 end
 
@@ -114,16 +183,27 @@ end
 
 function ReaderDogear:resetLayout()
     -- NOTE: RightContainer aligns to the right of its *own* width...
-    self[1].dimen.w = Screen:getWidth()
+    -- self[1].dimen.w = Screen:getWidth()
+    -- self.dimen = Screen:getSize()
+    self.right_ear.dimen.w = Screen:getWidth()
+    self.left_ear.dimen.w = Screen:getWidth()
 end
 
 function ReaderDogear:getRefreshRegion()
     -- We can't use self.dimen because of the width/height quirks of Left/RightContainer, so use the IconWidget's...
-    return self.icon.dimen
+    -- The dimension of both icons should be the same.
+    return self.icon_right.dimen
 end
 
-function ReaderDogear:onSetDogearVisibility(visible)
+-- @param visible boolean
+-- @param side number 1 only left, 2 only right, if 3 both sides, nil == 2
+-- TODO(ogkevin): if sides change while we're already showing a dogear, gotta trigger a partial refresh.
+-- For some reason, only the left ear has this weird behaviour
+function ReaderDogear:onSetDogearVisibility(visible, sides)
+    logger.dbg("ReaderDogear:onSetDogearVisibility", visible, sides)
+    self.sides = sides or SIDE_RIGHT
     self.view.dogear_visible = visible
+
     return true
 end
 
