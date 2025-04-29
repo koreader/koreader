e1ab015db4e66f48ce4c276e17b7d4f9ac098dcd
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2025-04-29 10:29:02 +0200

    fix: crash when switching from page_scroll to page
    Dual Page and Page Scroll both use page_states to
    list the pages and their states on how to be drawn.
    
    The properties are a bit different for the 2 modes.
    Dual Page expects ReaderPaging to also set the
    dimensions of the page in the table.
    When switching from the 2 modes, these dimensions
    are missing and the data is incorrect for Dual Paging.
    
    So, when scroll mode is changed, correclty calculate
    page_states for Dual Page.
    
    Change-Id: 79de9d9ba67f4338b6a45f8ef442d2a9

--- frontend/apps/reader/modules/readerpaging.lua
+++ frontend/apps/reader/modules/readerpaging.lua
@@ -878,7 +878,16 @@ function ReaderPaging:autoEnableDualPageModeIfLandscape()
         not self.dual_page_mode and
         self.settings.auto_enable_dual_page_mode
 
-    logger.dbg("ReaderPaging:autoEnableDualPageModeIfLandscape", should_enable)
+    logger.dbg("ReaderPaging:autoEnableDualPageModeIfLandscape", should_enable, self.view.page_scroll)
+
+    if should_enable and self.view.page_scroll then
+        UIManager:show(InfoMessage:new {
+            text = _([[Dual Page mode not automatically enabled due to Continues View Mode]]),
+            timeout = 4,
+        })
+
+        return
+    end
 
     -- Auto enable Dual Page Mode if we rotate to landscape
     if should_enable then
@@ -967,13 +976,12 @@ function ReaderPaging:onToggleDualPageMode()
     self:onRedrawCurrentPage()
 end
 
---When page scroll is enalbed, we need to disable Dual Page mode
---@param page_scroll bool if page_scroll is on or not
-function ReaderPaging:onSetScrollMode(page_scroll)
-    if not self:supportsDualPage() then
-        return
-    end
-
+ --When page scroll is enalbed, we need to disable Dual Page mode
+ --@param page_scroll bool if page_scroll is on or not
+ function ReaderPaging:onSetScrollMode(page_scroll)
+     if not self:supportsDualPage() then
+         return
+     end
     if page_scroll then
         self:onSetPageMode(1)
 
@@ -1650,6 +1658,8 @@ function ReaderPaging:onRedrawCurrentPage()
     -- something different?
     if self:isDualPageEnabled() then
         page = self:getDualPageBaseFromPage(self.current_page)
+        -- Make sure page states are up to date
+        self:updatePagePairStatesForBase(page)
     end
 
     self.ui:handleEvent(Event:new("PageUpdate", page))
