460728ca2ab80a754528b8c27b1bae441605f373
hius07 <62179190+hius07@users.noreply.github.com>
2025-05-19 08:03:03 +0300

    dispatcher: wording

--- frontend/dispatcher.lua
+++ frontend/dispatcher.lua
@@ -180,7 +180,7 @@ local settingsList = {
     ----
     fulltext_search = {category="none", event="ShowFulltextSearchInput", title=_("Fulltext search"), reader=true},
     fulltext_search_findall_results = {category="none", event="ShowFindAllResults", title=_("Last fulltext search results"), reader=true},
-    fulltext_search_start_page = {category="none", event="GoToStartPage", title=_("Go to fulltext search start page"), reader=true},
+    fulltext_search_start_page = {category="none", event="GoToStartPage", title=_("Fulltext search: go back to original page"), reader=true},
     toc = {category="none", event="ShowToc", title=_("Table of contents"), reader=true},
     book_map = {category="none", event="ShowBookMap", title=_("Book map"), reader=true, condition=Device:isTouchDevice() or (Device:hasDPad() and Device:useDPadAsActionKeys())},
     book_map_overview = {category="none", event="ShowBookMap", arg=true, title=_("Book map (overview)"), reader=true, condition=Device:isTouchDevice() or (Device:hasDPad() and Device:useDPadAsActionKeys())},
