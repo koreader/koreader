2c6f9fdebc5875db4a31e76e9e895f28cbc1ac14
Jadehawk <333657+jadehawk@users.noreply.github.com>
2025-05-27 22:25:22 -0400

    Added Onyx Boox Go 7 to devices that follow gravity. (https://github.com/koreader/koreader/issues/9223, https://github.com/koreader/koreader/issues/12423)

--- frontend/device/android/device.lua
+++ frontend/device/android/device.lua
@@ -244,7 +244,7 @@ function Device:init()
     }
 
     -- disable translation for specific models, where media keys follow gravity, see https://github.com/koreader/koreader/issues/12423
-    if android.prop.model == "moaanmix7" or android.prop.model == "xiaomi_reader" or android.prop.model == "gocolor7" then
+    if android.prop.model == "moaanmix7" or android.prop.model == "xiaomi_reader" or android.prop.model == "gocolor7" or android.prop.model == "go7" then
         self.input:disableRotationMap()
     end
 
