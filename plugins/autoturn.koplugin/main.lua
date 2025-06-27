6c4a0fe000a8c15a83e0d0fcafd37c8a83f3cc55
hius07 <62179190+hius07@users.noreply.github.com>
2025-06-27 14:04:43 +0300

    autoturn plugin: fix menu refreshing on check

--- plugins/autoturn.koplugin/main.lua
+++ plugins/autoturn.koplugin/main.lua
@@ -131,6 +131,7 @@ function AutoTurn:addToMainMenu(menu_items)
             return self:_enabled() and T(_("Autoturn: %1"), time_string) or _("Autoturn")
         end,
         checked_func = function() return self:_enabled() end,
+        on_check_updated_in_callback = true,
         callback = function(menu)
             local DateTimeWidget = require("ui/widget/datetimewidget")
             local autoturn_seconds = G_reader_settings:readSetting("autoturn_timeout_seconds", 30)
