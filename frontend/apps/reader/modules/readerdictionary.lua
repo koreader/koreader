e3080482c5c2e6756cda07cac4fd1a6dbea821b6
Commodore64user <97603719+Commodore64user@users.noreply.github.com>
2025-07-06 23:23:22 +0100

    clean interpuncts, pipes and up arrows from text selections

--- frontend/apps/reader/modules/readerdictionary.lua
+++ frontend/apps/reader/modules/readerdictionary.lua
@@ -852,6 +852,10 @@ function ReaderDictionary:cleanSelection(text, is_sane)
         text = text:gsub("\u{2019}", "'") -- Right single quotation mark
         -- Strip punctuation characters around selection
         text = util.stripPunctuation(text)
+        -- In some dictionaries, both interpuncts and pipes are used to delimiter syllables.
+        text = text:gsub("·", "")  -- interpunct
+        text = text:gsub("|", "")  -- pipe
+        text = text:gsub("↑", "") -- and up arrow, used in some dictionaries to indicate related words
         -- Strip some common english grammatical construct
         text = text:gsub("'s$", '') -- english possessive
         -- Strip some common french grammatical constructs
