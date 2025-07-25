79e9daaec6b2c27d2c17641896adf08cf3362f9d
lepeli <hello@yvan.dev>
2025-07-26 00:10:04 +0200

    Added restart when xovi is used on remarkable 2

--- frontend/device/remarkable/device.lua
+++ frontend/device/remarkable/device.lua
@@ -344,9 +344,10 @@ end
 function Remarkable:exit()
     if isRmPaperPro then
         os.execute("mv -f ~/.config/remarkable/xochitl.conf.bak ~/.config/remarkable/xochitl.conf")
-        if os.getenv("KO_DONT_GRAB_INPUT") == "1" then
-            os.execute("~/xovi/start")
-        end
+    end
+
+    if os.getenv("KO_DONT_GRAB_INPUT") == "1" then
+        os.execute("~/xovi/start")
     end
     Generic.exit(self)
 end
