8d562f2ce674b41480bf154d115b9a89cc1902fa
Noa Himesaka <himesaka@noa.codes>
2025-08-13 02:55:29 +0900

    fix/remarkable: make makefile shorter

--- make/remarkable-aarch64.mk
+++ make/remarkable-aarch64.mk
@@ -1,20 +1,2 @@
 REMARKABLE_DIR = $(PLATFORM_DIR)/remarkable-aarch64
-REMARKABLE_PACKAGE = koreader-$(DIST)$(KODEDUG_SUFFIX)-$(VERSION).zip
-REMARKABLE_PACKAGE_OTA = koreader-$(DIST)$(KODEDUG_SUFFIX)-$(VERSION).targz
-
-define UPDATE_PATH_EXCLUDES +=
-plugins/SSH.koplugin
-tools
-endef
-
-update: all
-	# ensure that the binaries were built for ARM
-	file --dereference $(INSTALL_DIR)/koreader/luajit | grep ARM
-	# Remarkable scripts
-	$(SYMLINK) $(REMARKABLE_DIR)/* $(INSTALL_DIR)/koreader/
-	$(SYMLINK) $(COMMON_DIR)/spinning_zsync $(INSTALL_DIR)/koreader/
-	# Create packages.
-	$(strip $(call mkupdate,$(REMARKABLE_PACKAGE)))
-	$(strip $(call mkupdate,$(REMARKABLE_PACKAGE_OTA)))
-
-PHONY += update
+include make/remarkable.mk
