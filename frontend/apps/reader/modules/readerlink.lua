bc9d18f5d6bf3bd84edd9ab172569f72a65aec5a
hius07 <62179190+hius07@users.noreply.github.com>
2025-08-27 08:37:12 +0300

    readerlink: open local files on page/position

--- frontend/apps/reader/modules/readerlink.lua
+++ frontend/apps/reader/modules/readerlink.lua
@@ -6,7 +6,6 @@ local BD = require("ui/bidi")
 local ButtonDialog = require("ui/widget/buttondialog")
 local ConfirmBox = require("ui/widget/confirmbox")
 local Device = require("device")
-local DocumentRegistry = require("document/documentregistry")
 local Event = require("ui/event")
 local InfoMessage = require("ui/widget/infomessage")
 local InputContainer = require("ui/widget/container/inputcontainer")
@@ -897,36 +896,7 @@ function ReaderLink:onGotoLink(link, neglect_current_location, allow_footnote_po
     end
 
     -- Check if it is a link to a local file
-    local linked_filename = link_url:gsub("^file:(//)?", "") -- remove local file protocol if any
-    local anchor
-    if linked_filename:find("?") then -- remove any query string (including any following anchor)
-        linked_filename, anchor = linked_filename:match("^(.-)(%?.*)$")
-    elseif linked_filename:find("#") then -- remove any anchor
-        linked_filename, anchor = linked_filename:match("^(.-)(#.*)$")
-    end
-    linked_filename  = ffiutil.joinPath(self.document_dir, linked_filename) -- get full path
-    linked_filename = ffiutil.realpath(linked_filename) -- clean full path from ./ or ../
-    if linked_filename and lfs.attributes(linked_filename, "mode") == "file" then
-        if DocumentRegistry:hasProvider(linked_filename) then
-            -- Display filename with anchor or query string, so the user gets
-            -- this information and can manually go to the appropriate place
-            local display_filename = linked_filename
-            if anchor then
-                display_filename = display_filename .. anchor
-            end
-            UIManager:show(ConfirmBox:new{
-                text = T(_("Would you like to read this local document?\n\n%1\n"), BD.filepath(display_filename)),
-                ok_callback = function()
-                    UIManager:scheduleIn(0.1, function()
-                        self.ui:switchDocument(linked_filename)
-                    end)
-                end
-            })
-        else
-            UIManager:show(InfoMessage:new{
-                text = T(_("Link to unsupported local file:\n%1"), BD.url(link_url)),
-            })
-        end
+    if self:openFileFromLink(link_url) then
         return true
     end
 
@@ -937,6 +907,54 @@ function ReaderLink:onGotoLink(link, neglect_current_location, allow_footnote_po
     })
     -- don't propagate, user will notice and tap elsewhere if he wants to change page
     return true
+end
+
+function ReaderLink:openFileFromLink(link_url)
+    local anchor, pn_xp, caller_callback
+    local linked_filename = link_url:gsub("^file:///", "") -- remove local file protocol if any
+    if linked_filename:find("?") then -- remove any query string (including any following anchor)
+        linked_filename, anchor = linked_filename:match("^(.-)(%?.*)$")
+    elseif linked_filename:find("#") then -- remove any anchor
+        linked_filename, anchor = linked_filename:match("^(.-)(#.*)$")
+    end
+    if anchor then
+        local count
+        pn_xp, count = anchor:gsub("^?pn_xp=", "") -- page number or xpointer
+        pn_xp = count > 0 and pn_xp or nil
+    end
+    if pn_xp then
+        caller_callback = function(ui)
+            ui.link:addCurrentLocationToStack()
+            if ui.rolling then
+                ui.rolling:onGotoXPointer(pn_xp, pn_xp)
+            else
+                pn_xp = tonumber(pn_xp)
+                if pn_xp then
+                    ui.paging:onGotoPage(pn_xp)
+                end
+            end
+        end
+    else
+        linked_filename = ffiutil.joinPath(self.document_dir, linked_filename) -- get full path
+    end
+    linked_filename = ffiutil.realpath(linked_filename) -- clean full path from ./ or ../
+    if linked_filename and lfs.attributes(linked_filename, "mode") == "file" then
+        local display_filename = linked_filename
+        if anchor and pn_xp == nil then
+            -- Display filename with anchor or query string, so the user gets
+            -- this information and can manually go to the appropriate place
+            display_filename = display_filename .. anchor
+        end
+        UIManager:show(ConfirmBox:new{
+            text = T(_("Would you like to read this local document?\n\n%1\n"), BD.filepath(display_filename)),
+            ok_callback = function()
+                UIManager:scheduleIn(0.1, function()
+                    self.ui:switchDocument(linked_filename, nil, caller_callback)
+                end)
+            end,
+        })
+        return true
+    end
 end
 
 function ReaderLink:onGoToExternalLink(link_url)
@@ -1593,7 +1611,7 @@ function ReaderLink:getButtonsForExternalLinkDialog(link_url)
         local button = fn_button(self, link_url)
         local show, button_title
 
-        if button.show_in_dialog_func then
+        if type(button.show_in_dialog_func) == "function" then
             show, button_title = button.show_in_dialog_func(link_url)
         else
             -- If the button doesn't have the show_in_dialog_func, then assume that the button
