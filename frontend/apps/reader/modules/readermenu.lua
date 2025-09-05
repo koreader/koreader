ef8349f0cf43ba8f63f65f565db554446fca0d12
Frans de Jonge <fransdejonge@gmail.com>
2025-09-05 17:20:55 +0200

    [minor, lang] Fix 'invert page turn taps and swipes' string (#14281)
    See https://github.com/koreader/koreader/pull/13382#issuecomment-3237817634

--- frontend/apps/reader/modules/readermenu.lua
+++ frontend/apps/reader/modules/readermenu.lua
@@ -272,7 +272,7 @@ function ReaderMenu:setUpdateItemTable()
             end,
             help_text = _([[
 When enabled the UI direction for the Table of Contents, Book Map, and Page Browser dialogs will mirror the default UI direction.
-Useful when used alongside 'Invert page turns'.]]),
+Useful when used alongside 'Invert page turn taps and swipes'.]]),
         })
     end
 
