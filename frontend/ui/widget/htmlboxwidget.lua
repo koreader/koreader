e6504c72f42a8926d7a5fddf88ecbdb129a3df22
Frans de Jonge <frans@clevercast.com>
2025-10-19 18:05:07 +0200

    Change to setRawContent

--- frontend/ui/widget/htmlboxwidget.lua
+++ frontend/ui/widget/htmlboxwidget.lua
@@ -268,11 +268,12 @@ function HtmlBoxWidget:setContent(body, css, default_font_size, is_xhtml, no_css
     self:clearHighlight()
 end
 
-function HtmlBoxWidget:setSVGContent(svg_text, default_font_size, html_resource_directory)
+function HtmlBoxWidget:setRawContent(svg_text, magic, default_font_size, html_resource_directory)
     local ok
-    ok, self.document = pcall(Mupdf.openDocumentFromText, svg_text, "svg", html_resource_directory)
+    ok, self.document = pcall(Mupdf.openDocumentFromText, svg_text, magic, html_resource_directory)
     if not ok then
         logger.warn("SVG loading error:", self.document)
+        return nil, self.document
     end
 
     self.document:layoutDocument(self.dimen.w, self.dimen.h, default_font_size)
