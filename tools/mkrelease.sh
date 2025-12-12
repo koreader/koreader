761c688f3752d67d4c6843f466a7d59a7bbf29ea
Benoit Pierre <benoit.pierre@gmail.com>
2025-12-13 00:49:50 +0100

    tools/mkrelease: tweak TAR.XZ output for new OTA mechanism
    Create one XZ block per TAR entry.

--- tools/mkrelease.sh
+++ tools/mkrelease.sh
@@ -27,6 +27,24 @@ function reverse_entry() {
 }
 '
 
+# shellcheck disable=SC2016
+declare -r AWK_TAR_BLOCKLIST='
+BEGIN {
+    FS = "[: ]+"
+    ORS = ""
+    TAR_BS = 512
+    prev_offset = 0
+}
+{
+    if ($2)
+        print ($2 - prev_offset) * TAR_BS ","
+    prev_offset = $2
+}
+END {
+    print 0
+}
+'
+
 if [[ "${OSTYPE}" = darwin* ]]; then
     declare -r READLINK=greadlink
     declare -r TAR=gtar
@@ -300,8 +318,23 @@ case "${format}" in
         ;;
     tar.xz)
         echo "Creating archive: ${output}"
-        "${tar_compress_cmd[@]}" |
-            xz -9 ${jobs:+--threads=${jobs}} "${options[@]}" |
+        # Note: we create one XZ block per TAR entry.
+        tarfile="${tmpdir}/release.tar"
+        tarindex="${tarfile}.index"
+        blocklist="${tmpdir}/release.blocklist"
+        # Create the initial uncompressed TAR file.
+        "${tar_compress_cmd[@]}" --file="${tarfile}"
+        # Find out where each entry starts.
+        "${TAR}" --block-number --list --file="${tarfile}" >"${tarindex}"
+        # Generate block list for XZ.
+        awk "${AWK_TAR_BLOCKLIST}" <"${tarindex}" >"${blocklist}"
+        # Create the final TAR.XZ file.
+        # NOTE: we set the block size to a big enough number to
+        # prevent xz from splitting an entry into multiple blocks.
+        xz -9 ${jobs:+--threads=${jobs}} \
+            --block-size=$((32 * 1024 * 1024)) \
+            --block-list="$(cat "${blocklist}")" \
+            "${options[@]}" --stdout "${tarfile}" |
             write_to_file "${output}"
         ;;
     tar.zst)
