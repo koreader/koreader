9a8bc7ed95575d7a9760d2737ab3347ae6cb278b
zwim <36999612+zwim@users.noreply.github.com>
2026-01-06 19:20:48 +0100

    and for android

--- platform/android/llapp_main.lua
+++ platform/android/llapp_main.lua
@@ -1,11 +1,8 @@
 local android = require("android")
 
--- setup Lua paths, and ffi helper / override
-require("setupkoenv")
+require("ffi/posix_h")
 
-local lfs = require("libs/libkoreader-lfs")
 local ffi = require("ffi")
-local dummy = require("ffi/posix_h")
 local C = ffi.C
 
 -- check uri of the intent that starts this application
@@ -15,9 +12,6 @@ if file ~= nil then
     android.LOGI("intent file path " .. file)
 end
 
--- path to primary external storage partition
-local path = android.getExternalStoragePath()
-
 -- create fake command-line arguments
 -- luacheck: ignore 121
 if android.isDebuggable() then
@@ -26,4 +20,51 @@ else
     arg = {file}
 end
 
-dofile(android.dir.."/reader.lua")
+local protected_modules = {
+    ["_G"] = true,
+    ["android"] = true, -- Wichtig auf Android!
+    ["bit"] = true,
+    ["ffi"] = true,
+--    ["ffi/*"] = true, -- will be checked below
+    ["gettext"] = true,
+    ["jit"] = true,
+    ["package"] = true,
+    ["lua-ljsqlite3/init"] = true,
+
+}
+for k, v in pairs(package.loaded) do
+    protected_modules[k] = true
+end
+
+local KO_RC_RESTART=85
+local crash_count = 0
+
+while crash_count < 5 do
+    -- force a clean start of the safemode module
+    for k, _ in pairs(package.loaded) do
+        if not protected_modules[k] and not k:find("^ffi/") then
+            package.loaded[k] = nil
+        end
+    end
+
+    C.setenv("CRASH_COUNT", tostring(crash_count), 1)
+
+    local chunk, loadError = loadfile(android.dir.."/reader.lua") -- check syntax, don't acutally run it
+
+    if not chunk then
+        android.LOGE("Syntax Error: " .. loadError)
+    else
+        local success, value = pcall(chunk) -- value stores error or return value
+
+        if not success then
+            crash_count = crash_count + 1
+            android.LOGE("Runtime Crash: " .. value)
+        elseif value == KO_RC_RESTART then
+            crash_count = 0
+            android.LOGI("Restart KOReader")
+        else
+            android.LOGI("Leave KOReader")
+            break
+        end
+    end
+end
