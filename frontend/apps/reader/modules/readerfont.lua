65669a9edba7dc84aaf501cb362b210f8e134098
Commodore64user <97603719+Commodore64user@users.noreply.github.com>
2026-01-25 01:45:41 +0000

    fix CI

--- frontend/apps/reader/modules/readerfont.lua
+++ frontend/apps/reader/modules/readerfont.lua
@@ -5,7 +5,6 @@ local Event = require("ui/event")
 local Font = require("ui/font")
 local FontList = require("fontlist")
 local InfoMessage = require("ui/widget/infomessage")
-local Input = Device.input
 local InputContainer = require("ui/widget/container/inputcontainer")
 local MultiConfirmBox = require("ui/widget/multiconfirmbox")
 local Notification = require("ui/widget/notification")
