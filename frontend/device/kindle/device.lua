4b9399dc60ecc740d428265fa3c3f331f7f384e8
k4rnaj1k <vladliasota@gmail.com>
2026-02-02 00:11:16 +0100

    Fix comments

--- frontend/device/kindle/device.lua
+++ frontend/device/kindle/device.lua
@@ -541,8 +541,7 @@ function Kindle:openInputDevices()
         FBInkInput = { fbink_input_scan = function() end }
     end
     local dev_count = ffi.new("size_t[1]")
-    -- We care about: the touchscreen, a properly scaled stylus, pagination buttons, a home button, a fiveway; and the fancy "tap on frame" stuff.
-    -- KINDLE SCRIBE FIX: Also include INPUT_TABLET to catch the raw WacomDigitizer device (event2) in addition to the scaled stylus (event4).
+    -- We care about: the touchscreen, the raw WacomDigitizer tablet, a properly scaled stylus, pagination buttons, a home button, a fiveway; and the fancy "tap on frame" stuff.
     local match_mask = bit.bor(C.INPUT_TOUCHSCREEN, C.INPUT_TABLET, C.INPUT_SCALED_TABLET, C.INPUT_PAGINATION_BUTTONS, C.INPUT_HOME_BUTTON, C.INPUT_DPAD, C.INPUT_KINDLE_FRAME_TAP)
     local devices = FBInkInput.fbink_input_scan(match_mask, 0, 0, dev_count)
     if devices ~= nil then
