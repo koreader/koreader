koreader/koreader

commit a7cb81c2298e97a22120cd0904542954fc541a06

[plugin] NewsDownloader: allow downloading supported documents (EPUB etc.) (#15065)

plugins/newsdownloader.koplugin/main.lua
@@ -1,5 +1,6 @@
 local BD = require("ui/bidi")
 local DataStorage = require("datastorage")
+local DocumentRegistry = require("document/documentregistry")
 local DownloadBackend = require("epubdownloadbackend")
 local ReadHistory = require("readhistory")
 local FFIUtil = require("ffi/util")
@@ -411,7 +412,7 @@ function NewsDownloader:processFeedSource(url, credentials, http_auth, limit, un
     -- Check if we have a cached response first
     local cache = DownloadBackend:getCache()
     local cached_response = cache:check(url)
-    local ok, error, response
+    local ok, error, content_type, response
 
     local cookies = nil
     local extra_headers = nil
@@ -494,7 +495,7 @@ function NewsDownloader:processFeedSource(url, credentials, http_auth, limit, un
     end
 
     if not response then
-        ok, response = pcall(function()
+        ok, content_type, response = pcall(function()
             return DownloadBackend:getResponseAsString(url, cookies, true, extra_headers)
         end)
     end
@@ -505,7 +506,7 @@ function NewsDownloader:processFeedSource(url, credentials, http_auth, limit, un
         feeds, err = self:deserializeXMLString(response)
         if not feeds then
             logger.err("NewsDownloader: Error during feed deserialization:", err)
-            logger.dbg("NewsDownloader: Response was:", response)
+            logger.dbg("NewsDownloader: content_type:", content_type, "response:", response)
         end
     end
     -- If the response is not available (for a reason that we don't know),
@@ -771,8 +772,20 @@ function NewsDownloader:downloadFeed(feed, cookies, http_auth, feed_output_dir, 
         if http_auth and http_auth.username and http_auth.password then
             extra_headers = { ["Authorization"] = "Basic " .. mime.b64((http_auth.username or "") .. ":" .. (http_auth.password or "")) }
         end
-        local html = DownloadBackend:loadPage(link, cookies, extra_headers)
-        DownloadBackend:createEpub(news_file_path, html, link, include_images, article_message, enable_filter, filter_element, block_element)
+        local content_type, content = DownloadBackend:loadPage(link, cookies, extra_headers)
+        if not content_type then
+            logger.err("NewsDownloader: No content type, not saving", link)
+            return
+        elseif content_type == "text/html" or content_type == "application/xhtml+xml" then
+            DownloadBackend:createEpub(news_file_path, content, link, include_images, article_message, enable_filter, filter_element, block_element)
+        elseif DocumentRegistry:hasProvider(nil, content_type) then
+            local file = io.open(news_file_path, "w")
+            file:write(content)
+            file:close()
+        else
+            logger.err("NewsDownloader: Unsupported feed Content-Type:", content_type)
+            return
+        end
     end
 end
 

plugins/newsdownloader.koplugin/epubdownloadbackend.lua
@@ -262,21 +262,21 @@ local function getUrlContent(url, cookies, timeout, maxtime, add_to_cache, extra
        code == socketutil.SINK_TIMEOUT_CODE
     then
         logger.warn("request interrupted:", status or code)
-        return false, code
+        return false, nil, code
     end
     if code >= 400 and code < 500 then
         logger.warn("HTTP error:", status or code)
-        return false, status or code
+        return false, nil, status or code
     end
     if headers == nil then
         logger.warn("No HTTP headers:", status or code or "network unreachable")
-        return false, "Network or remote server unavailable"
+        return false, nil, "Network or remote server unavailable"
     end
     if headers and headers["content-length"] then
         -- Check we really got the announced content size
         local content_length = tonumber(headers["content-length"])
         if #content ~= content_length then
-            return false, "Incomplete content received"
+            return false, nil, "Incomplete content received"
         end
     end
 
@@ -288,8 +288,15 @@ local function getUrlContent(url, cookies, timeout, maxtime, add_to_cache, extra
         })
     end
 
+    local content_type = nil
+    if headers and headers["content-type"] then
+        content_type = headers["content-type"]
+    else
+        logger.warn("NewsDownloader: Request didn't return a Content-Type header")
+    end
+
     logger.dbg("Returning content ok")
-    return true, content
+    return true, content_type, content
 end
 
 function EpubDownloadBackend:getCache()
@@ -332,9 +339,9 @@ end
 
 function EpubDownloadBackend:getResponseAsString(url, cookies, add_to_cache, extra_headers)
     logger.dbg("EpubDownloadBackend:getResponseAsString(", url, ")")
-    local success, content = getUrlContent(url, cookies, nil, nil, add_to_cache, extra_headers)
+    local success, content_type, content = getUrlContent(url, cookies, nil, nil, add_to_cache, extra_headers)
     if (success) then
-        return content
+        return content_type, content
     else
         error("Failed to download content for url:", url)
     end
@@ -349,7 +356,7 @@ function EpubDownloadBackend:resetTrapWidget()
 end
 
 function EpubDownloadBackend:loadPage(url, cookies, extra_headers)
-    local completed, success, content
+    local completed, success, content_type, content
     if self.trap_widget then -- if previously set with EpubDownloadBackend:setTrapWidget()
         local Trapper = require("ui/trapper")
         local timeout, maxtime = 30, 60
@@ -362,13 +369,13 @@ function EpubDownloadBackend:loadPage(url, cookies, extra_headers)
         end
     else
         local timeout, maxtime = 10, 60
-        success, content = getUrlContent(url, cookies, timeout, maxtime, nil, extra_headers)
+        success, content_type, content = getUrlContent(url, cookies, timeout, maxtime, nil, extra_headers)
     end
     logger.dbg("success:", success, "type(content):", type(content), "content:", type(content) == "string" and content:sub(1, 500), "...")
     if not success then
         error(content)
     else
-        return content
+        return content_type, content
     end
 end
 
@@ -681,8 +688,8 @@ function EpubDownloadBackend:createEpub(epub_path, html, url, include_images, me
                 src = img.src2x
             end
             logger.dbg("Getting img ", src)
-            local success, content = getUrlContent(src)
-            -- success, content = getUrlContent(src..".unexistant") -- to simulate failure
+            local success, _, content = getUrlContent(src)
+            -- success, _, content = getUrlContent(src..".unexistant") -- to simulate failure
             if success then
                 logger.dbg("success, size:", #content)
             else

commit 70302e8651077b2d63c419837a03211fae090819

[fix] NewsDownloader: reduce content-type to mimetype
Fixes #15174.

Regression in #15065.

plugins/newsdownloader.koplugin/main.lua
@@ -776,7 +776,9 @@ function NewsDownloader:downloadFeed(feed, cookies, http_auth, feed_output_dir, 
         if not content_type then
             logger.err("NewsDownloader: No content type, not saving", link)
             return
-        elseif content_type == "text/html" or content_type == "application/xhtml+xml" then
+        end
+        content_type = util.trim(content_type:match("^[^;]*")) --- @todo Don't ignore encoding but use it for the EPUB.
+        if content_type == "text/html" or content_type == "application/xhtml+xml" then
             DownloadBackend:createEpub(news_file_path, content, link, include_images, article_message, enable_filter, filter_element, block_element)
         elseif DocumentRegistry:hasProvider(nil, content_type) then
             local file = io.open(news_file_path, "w")