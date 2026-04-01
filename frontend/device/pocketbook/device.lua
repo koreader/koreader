542b0da13aad032e9c29fe9f75242b795e950584
Benoit Pierre <benoit.pierre@gmail.com>
2026-04-01 19:50:59 +0200

    pocketbook: drop redundant trace
    We already show the actual and targeted PocketBook software versions
    when initializing our inkview support.

--- frontend/device/pocketbook/device.lua
+++ frontend/device/pocketbook/device.lua
@@ -406,10 +406,6 @@ function PocketBook:initNetworkManager(NetworkMgr)
     end
 end
 
-function PocketBook:getSoftwareVersion()
-    return ffi.string(inkview.GetSoftwareVersion())
-end
-
 function PocketBook:getDeviceModel()
     return ffi.string(inkview.GetDeviceModel())
 end
@@ -853,8 +849,6 @@ local PocketBook1040 = PocketBook:extend{
     usingForcedRotation = landscape_ccw,
     hasNaturalLight = yes,
 }
-
-logger.info('SoftwareVersion: ', PocketBook:getSoftwareVersion())
 
 local full_codename = PocketBook:getDeviceModel()
 
