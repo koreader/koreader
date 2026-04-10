5c1b1cc3db44ec59c28ea33e538d4a967271eae3
hius07 <62179190+hius07@users.noreply.github.com>
2026-04-10 09:48:07 +0300

    filemanager: keep annotations on book deletion

--- frontend/apps/filemanager/filemanager.lua
+++ frontend/apps/filemanager/filemanager.lua
@@ -548,14 +548,17 @@ function FileManager:getPlusDialogButtons()
                     text = _("Delete"),
                     enabled = actions_enabled,
                     callback = function()
-                        UIManager:show(ConfirmBox:new{
-                            text = _("Delete selected files?\nIf you delete a file, it is permanently lost."),
+                        local confirmbox
+                        confirmbox = ConfirmBox:new{
+                            text = _("Delete selected files?\nIf you delete a file, it is permanently lost.") .. "\n",
                             ok_text = _("Delete"),
                             ok_callback = function()
                                 UIManager:close(self.plus_dialog)
                                 self:deleteSelectedFiles()
                             end,
-                        })
+                        }
+                        self.addOrphanCheckButton(confirmbox)
+                        UIManager:show(confirmbox)
                     end,
                 },
                 {
@@ -1117,11 +1120,10 @@ function FileManager:showDeleteFileDialog(filepath, post_delete_callback, pre_de
         return
     end
     local is_file = isFile(file)
-    local text = (is_file and _("Delete file permanently?") or _("Delete folder permanently?")) .. "\n\n" .. BD.filepath(file)
-    if is_file and BookList.hasBookBeenOpened(file) then
-        text = text .. "\n\n" .. _("Book settings, highlights and notes will be deleted.")
-    end
-    UIManager:show(ConfirmBox:new{
+    local text = (is_file and _("Delete file permanently?") or _("Delete folder permanently?"))
+        .. "\n\n" .. BD.filepath(file) .. "\n"
+    local confirmbox
+    confirmbox = ConfirmBox:new{
         text = text,
         ok_text = _("Delete"),
         ok_callback = function()
@@ -1131,6 +1133,24 @@ function FileManager:showDeleteFileDialog(filepath, post_delete_callback, pre_de
             if self:deleteFile(file, is_file) and post_delete_callback then
                 post_delete_callback()
             end
+        end,
+    }
+    if not is_file or (BookList.hasBookBeenOpened(file) and BookList.getBookInfo(file).has_annotations) then
+        self.addOrphanCheckButton(confirmbox)
+    end
+    UIManager:show(confirmbox)
+end
+
+function FileManager.addOrphanCheckButton(confirmbox)
+    local folder = G_reader_settings:readSetting("annotations_orphans_folder")
+    local enabled = folder ~= nil and lfs.attributes(folder, "mode") == "directory"
+    confirmbox:addWidget(CheckButton:new{
+        text = _("keep book annotations"),
+        enabled = enabled,
+        checked = enabled and G_reader_settings:isTrue("annotations_orphans_enabled"),
+        parent = confirmbox,
+        callback = function()
+            G_reader_settings:flipNilOrFalse("annotations_orphans_enabled")
         end,
     })
 end
