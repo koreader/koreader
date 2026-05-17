411c9c60248239d55c9a6e4cb4ab70bbd2b6ef9a
hius07 <62179190+hius07@users.noreply.github.com>
2026-05-17 08:13:56 +0300

    filemanager: getHomeFolder()

--- frontend/apps/filemanager/filemanager.lua
+++ frontend/apps/filemanager/filemanager.lua
@@ -1361,7 +1361,7 @@ function FileManager:onShowFolderMenu()
         }}
     end
 
-    local home_dir = G_reader_settings:readSetting("home_dir") or filemanagerutil.getDefaultDir()
+    local home_dir = filemanagerutil.getHomeFolder()
     local home_dir_shortened = G_reader_settings:nilOrTrue("shorten_home_dir")
     local home_dir_not_locked = G_reader_settings:nilOrFalse("lock_home_folder")
     local home_dir_suffix = "  \u{f015}" -- "home" character
