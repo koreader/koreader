6d3e35f22688b105e4d57a7e5bbddec970bfde10
Benoit Pierre <benoit.pierre@gmail.com>
2026-05-28 17:14:50 +0200

    platform: drop `spinning_zsync` scripts
    Not used anymore.

--- make/remarkable.mk
+++ make/remarkable.mk
@@ -12,7 +12,6 @@ update-prepare: all
 	# ensure that the binaries were built for ARM
 	file --dereference $(INSTALL_DIR)/koreader/luajit | grep ARM
 	# Remarkable scripts
-	$(SYMLINK) $(COMMON_DIR)/spinning_zsync $(INSTALL_DIR)/koreader/
 	$(SYMLINK) $(REMARKABLE_DIR)/koreader.sh $(INSTALL_DIR)/koreader/
 	$(SYMLINK) resources/koreader.png $(INSTALL_DIR)/koreader/icon.png
 	$(SYMLINK) $(REMARKABLE_DIR)/external.manifest.json $(INSTALL_DIR)/koreader/
