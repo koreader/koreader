a865d0a0226ef7e761c45b3b45c070d31eb95edd
mergen3107 <6279855+mergen3107@users.noreply.github.com>
2026-07-15 00:14:56 +0500

    fix callback

--- frontend/apps/reader/readerui.lua
+++ frontend/apps/reader/readerui.lua
@@ -104,6 +104,7 @@ function ReaderUI:registerModule(name, ui_module, always_active)
 end
 
 function ReaderUI:registerPostInitCallback(callback)
+    self.postInitCallback = self.postInitCallback or {}
     table.insert(self.postInitCallback, callback)
 end
 
