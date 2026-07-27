e992f6b30154e5fcbc48f1db0ddb44316860ed6e
hius07 <62179190+hius07@users.noreply.github.com>
2026-07-27 20:35:49 +0300

    Update plugins/opds.koplugin/opdsbrowser.lua
    Co-authored-by: Frans de Jonge <fransdejonge@gmail.com>

--- plugins/opds.koplugin/opdsbrowser.lua
+++ plugins/opds.koplugin/opdsbrowser.lua
@@ -37,7 +37,7 @@ local CatalogCache = Cache:new{
 }
 
 local OPDSBrowser = Menu:extend{
-    opds20_feed          = "application/opds+json,application/atom+xml",
+    opds20_feed          = "application/opds+json, application/atom+xml;profile=opds-catalog, */*",
     catalog_type         = "application/atom%+xml",
     search_type          = "application/opensearchdescription%+xml",
     search_template_type = "application/atom%+xml",
