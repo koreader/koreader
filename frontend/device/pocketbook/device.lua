062f6f3a68bbd8f4737a8d3c1315dc0324a16bd5
Benoit Pierre <benoit.pierre@gmail.com>
2026-08-07 20:40:51 +0200

    pocketbook: fix InkPad One OTA model (#15833)

--- frontend/device/pocketbook/device.lua
+++ frontend/device/pocketbook/device.lua
@@ -907,6 +907,7 @@ local PocketBook1030 = PocketBook:extend{
     model = "PB1030",
     display_dpi = 226,
     hasNaturalLight = yes,
+    ota_model = "pocketbookhf",
 }
 
 -- PocketBook InkPad X (1040)
