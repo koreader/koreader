3cdaea76c3bbedbc748f493689671dd4b055f5e8
Frans de Jonge <fransdejonge@gmail.com>
2026-09-03 09:49:30 +0200

    Apply suggestion from @Commodore64user
    Co-authored-by: David <97603719+Commodore64user@users.noreply.github.com>

--- frontend/ui/size.lua
+++ frontend/ui/size.lua
@@ -68,7 +68,7 @@ local Size = {
         focus_indicator = Screen:scaleBySize(Screen:getDPI() < 200 and 5 or 3),
         -- Underline under a focused row, thicker below 200 DPI so that it stands apart from
         -- the separators around it.
-        focus_row = Screen:scaleBySize(Screen:getDPI() < 200 and 3 or 1.5),
+        focus_row = Screen:scaleBySize(Screen:getDPI() < 200 and 5 or 1.5),
         progress = Screen:scaleBySize(7),
     },
     item = {
