6bfb2cc0ce515fd127154a9d3329835613330b1a
Kekoa Riggin <141699665+okaykoa@users.noreply.github.com>
2026-09-05 21:21:30 -0600

    Features for writers: New text file in the file manager plus menu

--- frontend/apps/filemanager/filemanager.lua
+++ frontend/apps/filemanager/filemanager.lua
@@ -680,6 +680,20 @@ function FileManager:getPlusDialogButtons()
             },
             {
                 {
+                    text = _("New text file"),
+                    enabled = self.texteditor and true or false,
+                    callback = function()
+                        UIManager:close(self.plus_dialog)
+                        if self.texteditor then
+                            local new_file_folder = folder
+                            if new_file_folder:sub(-1) ~= "/" then new_file_folder = new_file_folder .. "/" end
+                            self.texteditor:newFile(new_file_folder)
+                        end
+                    end,
+                },
+            },
+            {
+                {
                     text = _("Paste"),
                     enabled = self.clipboard and true or false,
                     callback = function()
