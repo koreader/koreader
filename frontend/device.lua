5bc505863d7fdb770854636543872f90ea49de60
Wesley Guedes <guedes48@gmail.com>
2026-09-24 14:15:10 -0300

    Fixing the sorting

--- frontend/device.lua
+++ frontend/device.lua
@@ -6,14 +6,14 @@ local function probeDevice()
     if platform then
         if platform:sub(1, #"android") == "android" then
             return require("device/android/device")
+        elseif platform:sub(1, #"bookeen") == "bookeen" then
+            return require("device/bookeen/device")
         elseif platform:sub(1, #"cervantes") == "cervantes" then
             return require("device/cervantes/device")
         elseif platform:sub(1, #"kindle") == "kindle" then
             return require("device/kindle/device")
         elseif platform:sub(1, #"kobo") == "kobo" then
             return require("device/kobo/device")
-        elseif platform:sub(1, #"bookeen") == "bookeen" then
-            return require("device/bookeen/device")
         elseif platform:sub(1, #"pocketbook") == "pocketbook" then
             return require("device/pocketbook/device")
         elseif platform:sub(1, #"remarkable") == "remarkable" then
